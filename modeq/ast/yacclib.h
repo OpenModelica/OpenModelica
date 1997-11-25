2cf23062c031eafd3bbf7ffbaa442dc77351c88b
x97davka <x97davka@ida.liu.se>
1997-11-25 10:21:46 +0000

    Taken from rml 1.3.6.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@32 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ modeq/ast/yacclib.h
@@ -0,0 +1,22 @@
+/* yacclib.h */
+
+extern void *alloc_bytes(unsigned nbytes);
+extern void *alloc_words(unsigned nwords);
+
+extern void print_icon(FILE*, void*);
+extern void print_rcon(FILE*, void*);
+extern void print_scon(FILE*, void*);
+
+extern void *mk_icon(int);
+extern void *mk_rcon(double);
+extern void *mk_scon(char*);
+extern void *mk_nil(void);
+extern void *mk_cons(void*, void*);
+extern void *mk_none(void);
+extern void *mk_some(void*);
+extern void *mk_box0(unsigned ctor);
+extern void *mk_box1(unsigned ctor, void*);
+extern void *mk_box2(unsigned ctor, void*, void*);
+extern void *mk_box3(unsigned ctor, void*, void*, void*);
+extern void *mk_box4(unsigned ctor, void*, void*, void*, void*);
+extern void *mk_box5(unsigned ctor, void*, void*, void*, void*, void*);
