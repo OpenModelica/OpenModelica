7d40084c00ed783b3410ec48adb4881c350bf3d3
ankar <ankar@ida.liu.se>
2002-01-25 09:41:51 +0000

    Added main header.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@691 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ c_runtime/modelica.h
@@ -0,0 +1,12 @@
+#ifndef MODELICA_H_
+#define MODELICA_H_
+
+#include "memory_pool.h"
+#include "index_spec.h"
+
+#include "real_array.h"
+#include "integer_array.h"
+#include "string_array.h"
+#include "boolean_array.h"
+
+#endif
