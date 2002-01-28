afb8b5892ea18f02a775789c04547cfa840efc98
henjo <henjo@ida.liu.se>
2002-01-28 13:06:10 +0000

    Now generates stub fn
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@698 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- modeq/test_codegen/test.main.c
+++ modeq/test_codegen/test.main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 
 #include "test.c"
+
 
 int main(int argc, char** argv)
 {
 
-  t_rettype r;
+  if (argc != 3)
+    {
+      fprintf(stderr,"# Incorrrect number of arguments\n");
+      return 1;
+    }
 
-  r = t(5.0);
-
-  printf("t(5.0) == %e\n",r.y);
+  
+  t_read_call_write(argv[1],argv[2]);
 
   return 0;
 
