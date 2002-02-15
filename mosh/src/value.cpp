7368c3fae67a49e0afb7e5354488f1b4cb6f5416
henjo <henjo@ida.liu.se>
2002-02-15 14:49:10 +0000

    Added more copyright notices
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@742 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- mosh/src/value.cpp
+++ mosh/src/value.cpp
@@ -1,6 +1,24 @@
-//
-// Copyright PELAB, Linkoping University
-//
+/*
+    Copyright PELAB, Linkoping University
+
+    This file is part of Open Source Modelica (OSM).
+
+    OSM is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    OSM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
 
 #include "value.hpp"
 
@@ -719,7 +737,7 @@ const value& value::operator-= (const value& val)
 	    }
 	  else if (is_integer_array() && val.is_integer_array())
 	    {
-	      m_integer_array = val.m_integer_array;
+	      m_integer_array -= val.m_integer_array;
 	    }
 	  else
 	    {
