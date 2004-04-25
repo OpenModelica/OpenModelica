769acc7f4e3ed644e5a621bae3878c1b80a67010
Levon Saldamli <levon.sa@gmail.com>
2004-04-25 06:16:19 +0000

    Added corba stuff to configure and made it an optional package
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@1161 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ modeq/runtime/corbaimpl_stub.cpp
@@ -0,0 +1,56 @@
+
+extern "C" {
+#include "rml.h"
+#include "../values.h"
+#include <stdio.h>
+#include "../absyn_builder/yacclib.h"
+#include <pthread.h>
+}
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+
+extern "C" {
+  
+void Corba_5finit(void)
+{
+}
+
+void errmsg() {
+  cerr << "CORBA disabled. Configure with --with-CORBA and recompile for enabling." << endl;
+}
+
+RML_BEGIN_LABEL(Corba__initialize)
+{
+  errmsg();
+  RML_TAILCALLK(rmlFC);
+}
+RML_END_LABEL
+
+
+RML_BEGIN_LABEL(Corba__wait_5ffor_5fcommand)
+{
+  errmsg();
+  RML_TAILCALLK(rmlFC);
+}
+RML_END_LABEL
+
+RML_BEGIN_LABEL(Corba__sendreply)
+{
+  errmsg();
+  RML_TAILCALLK(rmlFC);
+}
+RML_END_LABEL
+
+RML_BEGIN_LABEL(Corba__close)
+{
+  errmsg();
+  RML_TAILCALLK(rmlFC);
+}
+RML_END_LABEL
+}
