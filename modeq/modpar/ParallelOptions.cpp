a880f9a2ef53a008670ff446edbb047314005a6f
Peter Aronsson <peter.aronsson@mathcore.com>
2004-07-01 07:30:51 +0000

    Forgot paralleloptions..
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@1204 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ modeq/modpar/ParallelOptions.cpp
@@ -0,0 +1,2 @@
+#include "ParallelOptions.hpp"
+
---
+++ modeq/modpar/ParallelOptions.hpp
@@ -0,0 +1,34 @@
+#ifndef _PARALLELOPTIONS_H
+#define _PARALLELOPTIONS_H
+#include <iostream>
+using namespace std;
+
+/* Class for handling of parallel options, like number of processors,
+   latency and bandwith characteristics, etc.
+*/
+
+class ParallelOptions 
+{
+public:
+  ParallelOptions(int nproc,double latency=10.0,double bandwidth=1.0) 
+    : m_nproc(nproc), m_latency(latency), m_bandwidth(bandwidth) { };
+  
+  ParallelOptions() : m_nproc(0), m_latency(0), m_bandwidth(0) { };
+  
+  friend ostream& operator << (ostream&os,ParallelOptions &opt) {
+    os << "ParallelOptions: "<< endl << "  number_of_processors: " << opt.get_nproc() << endl;
+    os << "  latency: " << opt.get_latency() << endl;
+    os << "  bandwidth: " << opt.get_bandwidth() << endl;
+    return os;
+  };
+    
+  int get_nproc() { return m_nproc;};
+  double get_latency() { return m_latency; };
+  double get_bandwidth() { return m_bandwidth; };
+private:
+  int m_nproc;
+  double m_latency; // latency in microseconds
+  double m_bandwidth; // Bandwidth in Mb/s
+};
+
+#endif
