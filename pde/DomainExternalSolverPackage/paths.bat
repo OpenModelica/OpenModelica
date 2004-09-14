342e4d1e0ffa6498319a7981c84bb9872144539b
Levon Saldamli <levon.sa@gmail.com>
2004-09-14 08:59:56 +0000

    *** empty log message ***
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@1234 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ pde/DomainExternalSolverPackage/paths.bat
@@ -0,0 +1,3 @@
+set MODELICAPATH=D:\home\levsa\cvs\modelica\pde\DomainExternalSolverPackage\ModelicaLibraries
+set PATH=%PATH%;D:\home\levsa\cvs\modelica\pde\DomainExternalSolverPackage\ModelicaLibraries\ExternalC
+set C_INCLUDE_PATH=D:\home\levsa\cvs\modelica\pde\DomainExternalSolverPackage\ModelicaLibraries\ExternalC
