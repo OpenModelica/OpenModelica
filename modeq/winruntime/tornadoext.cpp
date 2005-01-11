d352a24af4fc7ebb6d848c912225bb8e0411f63e
x02lucpo <x02lucpo@ida.liu.se>
2005-01-11 15:02:34 +0000

    added get_input_var_number and get_output_var_number
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@1387 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- modeq/winruntime/tornadoext.cpp
+++ modeq/winruntime/tornadoext.cpp
@@ -15,7 +15,9 @@ extern "C"
 #include "rml.h"
 #include "../absyn_builder/yacclib.h"
 
-  map<string,int> alg_state_var_Number;
+  map<string,int> output_var_number;
+  map<string,int> input_var_number;
+
 
   
   void TORNADOEXT_5finit(void)
@@ -33,16 +35,16 @@ extern "C"
   }
   RML_END_LABEL
 
-  RML_BEGIN_LABEL(TORNADOEXT__get_5fstate_5falg_5fnumber)
+  RML_BEGIN_LABEL(TORNADOEXT__get_5foutput_5fvar_5fnumber)
   {
     int nvars = RML_UNTAGFIXNUM(rmlA0);
     char* str = RML_STRINGDATA(rmlA0);
     string str_key = string(str);
     
     map<string, int>::const_iterator search;
-    search = alg_state_var_Number.find(str_key);
+    search = output_var_number.find(str_key);
     int ret_val = 0;
-    if(search != alg_state_var_Number.end()){
+    if(search != output_var_number.end()){
       ret_val = static_cast<int>(search->second);
       ret_val += 1;
 //       cout << "\nSTATE_ALG:" << str_key << " " << ret_val << nvars << endl;
@@ -51,12 +53,35 @@ extern "C"
 //        cout << "\nNO STATE_ALG:" << str_key << " " << ret_val << nvars << endl;
 //     }
 
-    alg_state_var_Number[str_key] = ret_val;
+    output_var_number[str_key] = ret_val;
     rmlA0 = (void*) mk_icon(ret_val);
     RML_TAILCALLK(rmlSC);
   }
   RML_END_LABEL
 
+  RML_BEGIN_LABEL(TORNADOEXT__get_5finput_5fvar_5fnumber)
+  {
+    int nvars = RML_UNTAGFIXNUM(rmlA0);
+    char* str = RML_STRINGDATA(rmlA0);
+    string str_key = string(str);
+    
+    map<string, int>::const_iterator search;
+    search = input_var_number.find(str_key);
+    int ret_val = 0;
+    if(search != input_var_number.end()){
+      ret_val = static_cast<int>(search->second);
+      ret_val += 1;
+//       cout << "\nSTATE_ALG:" << str_key << " " << ret_val << nvars << endl;
+
+    } // else {
+//        cout << "\nNO STATE_ALG:" << str_key << " " << ret_val << nvars << endl;
+//     }
+
+    input_var_number[str_key] = ret_val;
+    rmlA0 = (void*) mk_icon(ret_val);
+    RML_TAILCALLK(rmlSC);
+  }
+  RML_END_LABEL
 
 
 } // extern "C"
