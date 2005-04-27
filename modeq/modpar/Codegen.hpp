dc1ea922e81f589841d4cefd888e0676baf6bd7e
Peter Aronsson <peter.aronsson@mathcore.com>
2005-04-27 06:38:01 +0000

    Fixed the stuff removed in luc's merge.... :(
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@1732 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- modeq/modpar/Codegen.hpp
+++ modeq/modpar/Codegen.hpp
@@ -62,6 +62,9 @@ private:
   void generateTemporaries();
   void generateParallelFunctionLocals(TaskList *tasks);
 
+  // Generates the residual function for solving nonlinear equation systems.
+  void generateNonLinearResidualFunc(VertexID task);
+
   void generateKillCommand();
   void generateTmpDeclarations();
 
