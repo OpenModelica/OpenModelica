b5f6c2a700519272c6d7f7242566026bd7d1a09f
x02lucpo <x02lucpo@ida.liu.se>
2005-11-28 07:05:22 +0000

    redirected the stderr and stdout to <fileprefix>.log
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@1989 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/scripts/Compile.bat
+++ Compiler/scripts/Compile.bat
@@ -4,7 +4,7 @@ set OLDPATH=%PATH%
 pushd "%OPENMODELICAHOME%\MinGW\bin"
 set PATH=%CD%
 popd
-mingw32-make -f %1.makefile >nul
+mingw32-make -f %1.makefile 2>&1 >%1.log
 set RESULT=%ERRORLEVEL%
 set PATH=%OLDPATH%
 rem exit %RESULT%
