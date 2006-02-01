9412a570130bbb749fcd6a4dc36bf5a33cd830fd
Adrian Pop <adrian.pop@liu.se>
2006-02-01 13:56:20 +0000

    Adrian Pop, adrpo@ida.liu.se, 2006-02-01 - rmldep.sh changed so it is based on rml - rmldep-old.sh is present also there if you need it.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@2058 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/rml2sig/rmldep.sh
+++ Compiler/rml2sig/rmldep.sh
@@ -3,24 +3,29 @@
 # rmldep: Generates a new signature if the old is different.
 # BEDNARSKI Andrzej
 # Last update: 11 June, 2003
+# Changed by Adrian Pop, adrpo@ida.liu.se, 2006-02-01
 #
 
 mydir="`dirname $0`"
 
 if [ ! "$#" -eq 1 ]; then
-  echo "Usage: $0 <file>.rml";
+  echo "Usage: $0 <file>.mo";
   exit;
 fi
 
 tmp_file="$1.$$"
-sig_file="`basename "$1" ".rml"`.sig"
+if [ "mo" = $(OMC_BUILD_FROM) ]; then
+ sig_file="`basename "$1" ".mo"`.rsig"
+else
+ sig_file="`basename "$1" ".rml"`.rsig"
+fi
 
 if [ ! -f $sig_file ]; then
    # echo "Sig file does not exist...";
-  $mydir/rml2sig $1 > $sig_file;
+  rml -fdump-interface $1 > $sig_file;
 else 
   # echo "Generates tmp sig."
-  $mydir/rml2sig $1 > $tmp_file
+  rml -fdump-interface $1 > $tmp_file
   # echo "Diffing"
   diff $tmp_file $sig_file > /dev/null
   if [ $? -eq 0 ]; then
