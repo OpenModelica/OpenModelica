e996e5ca3fad8aca108e88369f34a3b387bb23be
Peter Aronsson <peter.aronsson@mathcore.com>
2006-02-02 09:12:34 +0000

    Removed adrians changes!
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@2067 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/rml2sig/rmldep.sh
+++ Compiler/rml2sig/rmldep.sh
@@ -3,29 +3,24 @@
 # rmldep: Generates a new signature if the old is different.
 # BEDNARSKI Andrzej
 # Last update: 11 June, 2003
-# Changed by Adrian Pop, adrpo@ida.liu.se, 2006-02-01
 #
 
 mydir="`dirname $0`"
 
 if [ ! "$#" -eq 1 ]; then
-  echo "Usage: $0 <file>.(mo|rml)";
+  echo "Usage: $0 <file>.rml";
   exit;
 fi
 
 tmp_file="$1.$$"
-if [ "mo" = "${OMC_BUILD_FROM}" ]; then
- sig_file="`basename "$1" ".mo"`.rsig"
-else
- sig_file="`basename "$1" ".rml"`.rsig"
-fi
+sig_file="`basename "$1" ".rml"`.sig"
 
 if [ ! -f $sig_file ]; then
    # echo "Sig file does not exist...";
-  rml -fdump-interface $1 > $sig_file;
+  $mydir/rml2sig $1 > $sig_file;
 else 
   # echo "Generates tmp sig."
-  rml -fdump-interface $1 > $tmp_file
+  $mydir/rml2sig $1 > $tmp_file
   # echo "Diffing"
   diff $tmp_file $sig_file > /dev/null
   if [ $? -eq 0 ]; then
