abc7c1586e1f7fb938e97c8b53581d529fdda5aa
Adrian Pop <adrian.pop@liu.se>
2006-02-02 12:12:07 +0000

    Adrian Pop, adrpo@ida.liu.se, 2006-02-02 - fixed so that Makefile.omdev.mingw use rml2sig/rmldep-new.sh instead of rmldep.sh - fixed some text messages in the Makefile.omdev.mingw Makefiles that appear when   variables are not set
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@2070 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ Compiler/rml2sig/rmldep-new.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# rmldep: Generates a new signature if the old is different.
+# BEDNARSKI Andrzej
+# Last update: 11 June, 2003
+# Changed by Adrian Pop, adrpo@ida.liu.se, 2006-02-02
+#
+
+mydir="`dirname $0`"
+
+if [ ! "$#" -eq 1 ]; then
+  echo "Usage: $0 <file>.(mo|rml)";
+  exit;
+fi
+
+tmp_file="$1.$$"
+if [ "mo" = "${OMC_BUILD_FROM}" ]; then
+ sig_file="`basename "$1" ".mo"`.rsig"
+else
+ sig_file="`basename "$1" ".rml"`.rsig"
+fi
+
+if [ ! -f $sig_file ]; then
+   # echo "Sig file does not exist...";
+  rml -fdump-interface $1 > $sig_file;
+else 
+  # echo "Generates tmp sig."
+  rml -fdump-interface $1 > $tmp_file
+  # echo "Diffing"
+  diff $tmp_file $sig_file > /dev/null
+  if [ $? -eq 0 ]; then
+    # echo "Same files"
+    rm $tmp_file
+  else
+    # echo "Interface changed"
+    \mv $tmp_file $sig_file
+  fi
+fi
+
