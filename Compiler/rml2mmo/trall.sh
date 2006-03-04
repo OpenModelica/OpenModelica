7da42ef6b72472603d5b4631010ace0ebf207d91
Adrian Pop <adrian.pop@liu.se>
2006-03-04 21:39:06 +0000

    Just text changes
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@2183 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/rml2mmo/trall.sh
+++ Compiler/rml2mmo/trall.sh
@@ -9,7 +9,7 @@ RMLFILES=`ls *.rml`
 echo Calling the translator with flags: $@ -napAll
 echo This can really take a while!!!
 echo On my computer it takes:
-echo [BENCH:	116.00 seconds, 497 minor collections, 20 major collections]
+echo [BENCH: 431.35 seconds, 2716 minor collections, 112 major collections]
 ${OMDEV}/tools/rml/bin/rml2mod -bench -young-size=1024000 $RMLFILES $@ -napAll
 echo Deleting inetrmediate files "*.rmod *.rsig *.rdb"
 rm -f *.rmod *.rsig *.rdb
