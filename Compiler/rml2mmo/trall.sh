94805582080253562ec14e944bf44451cb83ad63
Adrian Pop <adrian.pop@liu.se>
2006-04-03 13:30:02 +0000

    - added more stack to the translator
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@2300 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/rml2mmo/trall.sh
+++ Compiler/rml2mmo/trall.sh
@@ -10,7 +10,7 @@ echo Calling the translator with flags: $@ -napAll
 echo This can really take a while!!!
 echo On my computer it takes:
 echo [BENCH: 431.35 seconds, 2716 minor collections, 112 major collections]
-${OMDEV}/tools/rml/bin/rml2mod -bench -young-size=1024000 $RMLFILES $@ -napAll
+${OMDEV}/tools/rml/bin/rml2mod -bench -stack-size=1024000 -young-size=1024000 $RMLFILES $@ -napAll
 echo Deleting inetrmediate files "*.rmod *.rsig *.rdb"
 rm -f *.rmod *.rsig *.rdb
 fi
