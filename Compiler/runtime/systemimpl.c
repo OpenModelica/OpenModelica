34de655c993006fcabccac6ffe885ddde2281786
Adrian Pop <adrian.pop@liu.se>
2006-10-06 22:53:30 +0000

    replaced // with /* */ as some compilers crap on this
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@2570 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/runtime/systemimpl.c
+++ Compiler/runtime/systemimpl.c
@@ -528,8 +528,9 @@ RML_BEGIN_LABEL(System__writeFile)
 		  1);
     RML_TAILCALLK(rmlFC);
   } 
-  // adrpo changed 2006-10-06 
-  // fprintf(file,"%s",data);
+  /* adrpo changed 2006-10-06 
+   * fprintf(file,"%s",data);
+   */
   fwrite(RML_STRINGDATA(rmlA1), RML_HDRSTRLEN(RML_GETHDR(rmlA1)), 1, file);
   fflush(file);
   fclose(file);
@@ -1730,8 +1731,9 @@ RML_BEGIN_LABEL(System__writeFile)
 		  1);
     RML_TAILCALLK(rmlFC);
   }
-  // adrpo changed 2006-10-06 
-  // fprintf(file,"%s",data);
+  /* adrpo changed 2006-10-06 
+   * fprintf(file,"%s",data);
+   */
   fwrite(RML_STRINGDATA(rmlA1), RML_HDRSTRLEN(RML_GETHDR(rmlA1)), 1, file);
   fflush(file);
   fclose(file);
