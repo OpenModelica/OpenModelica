caec13eb55c65a69c7384315ca57c04a72d59a0f
Peter Aronsson <peter.aronsson@mathcore.com>
2007-02-12 08:59:26 +0000

    A small error in the compiler.bat file.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@2702 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/scripts/Compile.bat
+++ Compiler/scripts/Compile.bat
@@ -2,7 +2,7 @@
 REM Clear all environment variables that may interfere during compile and link phases.
 set GCC_EXEC_PREFIX=
 set CPLUS_INCLUDE_PATH=
-set C_INCLUDE_PATH=D
+set C_INCLUDE_PATH=
 set LIBRARY_PATH=
 set OLD_PATH=%PATH%
 pushd "%OPENMODELICAHOME%\MinGW\bin" >%1.log 2<&1
