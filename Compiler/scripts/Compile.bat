21b715c6bbdde0f7002c982fe970d48c26d9237c
Peter Aronsson <peter.aronsson@mathcore.com>
2007-02-12 07:44:21 +0000

    Fixed bug with compiling when e.g. LIBRARY_PATH is defined.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@2699 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/scripts/Compile.bat
+++ Compiler/scripts/Compile.bat
@@ -1,10 +1,15 @@
 @echo off
+REM Clear all environment variables that may interfere during compile and link phases.
 set GCC_EXEC_PREFIX=
-set OLDPATH=%PATH%
+set CPLUS_INCLUDE_PATH=
+set C_INCLUDE_PATH=D
+set LIBRARY_PATH=
+set OLD_PATH=%PATH%
 pushd "%OPENMODELICAHOME%\MinGW\bin" >%1.log 2<&1
 set PATH=%CD%;%PATH%
 popd
 mingw32-make -f %1.makefile >%1.log 2<&1
 set RESULT=%ERRORLEVEL%
-set PATH=%OLDPATH%
+set PATH=%OLD_PATH%
+set OLD_PATH=
 exit /B %RESULT%
