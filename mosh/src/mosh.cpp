6039cea13edf21aeb0674a0a21cd6e804564b933
Adrian Pop <adrian.pop@liu.se>
2007-06-02 02:04:25 +0000

    - mosh.cpp changes   + wait for some time when we wait for omc to start   + wait for some time when we wait for omc to quit
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@2814 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- mosh/src/mosh.cpp
+++ mosh/src/mosh.cpp
@@ -1,22 +1,40 @@
 /*
-    Copyright PELAB, Linkoping University
+Copyright (c) 1998-2007, Linköpings universitet, Department of
+Computer and Information Science, PELAB
 
-    This file is part of Open Source Modelica (OSM).
+All rights reserved.
 
-    OSM is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+(The new BSD license, see also
+http://www.opensource.org/licenses/bsd-license.php)
 
-    OSM is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Foobar; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
 
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in
+  the documentation and/or other materials provided with the
+  distribution.
+
+* Neither the name of Linköpings universitet nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+\"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <cstdlib>
@@ -100,7 +118,7 @@ int main(int argc, char* argv[])
     cout << "Using corba communication" << endl;
   }  
   if(!scriptname) {
-    cout << "Open Source Modelica Shell" << endl
+    cout << "Open Source Modelica Terminal Shell" << endl
 	 << "Copyright 1997-2007, PELAB, Linkoping University" << endl << endl
 	 << "To get help on using Mosh and OpenModelica, type \"help()\" and press enter" << endl;
   }
@@ -113,6 +131,7 @@ int main(int argc, char* argv[])
       if (!scriptname)
 	cout << "Started server using:"<< systemstr << "\n res = " << res << endl;
     }
+    sleep(1); // wait a second for the server to start
     doCorbaCommunication(argc,argv,scriptname);
   } else {
     if (!noserv) {
@@ -124,7 +143,7 @@ int main(int argc, char* argv[])
       if (!scriptname)
 	cout << "Started server using:"<< systemstr << "\n res = " << res << endl;
     }
-    sleep(1);  
+    sleep(1); // wait a second for the server to start
     doSocketCommunication(scriptname);
   }
 
@@ -180,6 +199,9 @@ void doCorbaCommunication(int argc, char **argv, const string *scriptname)
     if (strcmp(line,"\n")!=0 && strcmp(line,"") != 0) { 
       add_history(line);
       char *res =client->sendExpression(line);
+      if (strcmp(line,"quit()") == 0) {
+	  sleep(1);
+      }
       cout << res;
       CORBA::string_free(res);
     }
