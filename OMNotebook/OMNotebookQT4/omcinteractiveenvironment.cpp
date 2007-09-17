e203175285beee327ab3aae6a2267ffcbe56bbd1
Adrian Pop <adrian.pop@liu.se>
2007-09-17 20:34:19 +0000

    - fixes a crash when OPENMODELICAHOME is set to the empty string.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@2921 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMNotebook/OMNotebookQT4/omcinteractiveenvironment.cpp
+++ OMNotebook/OMNotebookQT4/omcinteractiveenvironment.cpp
@@ -212,32 +212,34 @@ namespace IAEX
 		try
 		{
 			// 2006-02-28 AF, use environment varable to find omc.exe
-			string OMCPath( getenv( "OPENMODELICAHOME" ) );
-			if( OMCPath.empty() )
+			// string OMCPath( getenv( "OPENMODELICAHOME" ) );
+			QString OMCPath( getenv( "OPENMODELICAHOME" ) );
+
+			if( OMCPath.isEmpty() )
 				throw std::runtime_error( "Could not find environment variable OPENMODELICAHOME" );
 
 			// location of omc in openmodelica folder
 			QDir dir;
 #ifdef WIN32
-			if( dir.exists( QString(OMCPath.c_str()) + "\\bin\\omc.exe" ) )
+			if( dir.exists( OMCPath + "\\bin\\omc.exe" ) )
 				OMCPath += "\\bin\\";
-			else if( dir.exists( QString(OMCPath.c_str()) + "\\omc.exe" ) )
+			else if( dir.exists( OMCPath + "\\omc.exe" ) )
 				OMCPath;
 			else if( dir.exists( "omc.exe" ))
 				OMCPath = "";
 			else
 			{
-				string msg = "Unable to find OMC, searched in:\n" +
+				QString msg = "Unable to find OMC, searched in:\n" +
 					OMCPath + "\\bin\\\n" +
 					OMCPath + "\n" +
-					dir.absolutePath().toStdString();
+					dir.absolutePath();
 
-				throw std::runtime_error( msg.c_str() );
+				throw std::runtime_error( msg.toStdString().c_str() );
 			}
 #else /* unix */
-			if( dir.exists( QString(OMCPath.c_str()) + "/bin/omc" ) )
+			if( dir.exists( OMCPath + "/bin/omc" ) )
 				OMCPath += "/bin/";
-			else if( dir.exists( QString(OMCPath.c_str()) + "/omc" ) )
+			else if( dir.exists( OMCPath + "/omc" ) )
 				OMCPath;
 			else if( dir.exists( "omc.exe" ))
 				OMCPath = "";
@@ -256,9 +258,9 @@ namespace IAEX
 			// 2006-03-14 AF, set omc loaction and parameters
 			QString omc;
 			#ifdef WIN32
-				omc = QString( OMCPath.c_str() ) + "omc.exe";
+				omc = OMCPath + "omc.exe";
 			#else
-				omc = QString( OMCPath.c_str() ) + "omc";
+				omc = OMCPath + "omc";
 			#endif
 			
 			QStringList parameters;
