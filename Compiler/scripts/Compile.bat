c8fb92d0c94ccbe344e7a1edc055dc7dea7f121e
Adrian Pop <adrian.pop@liu.se>
2008-10-02 19:14:14 +0000

    - updated the PATH setting in Compile.bat to the new GCC 3.4.5 as is needed by Windows Vista
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@3662 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/scripts/Compile.bat
+++ Compiler/scripts/Compile.bat
@@ -6,7 +6,7 @@ set C_INCLUDE_PATH=
 set LIBRARY_PATH=
 set OLD_PATH=%PATH%
 pushd "%OPENMODELICAHOME%\MinGW\bin" >%1.log 2<&1
-set PATH=%CD%;%OPENMODELICAHOME%\MinGW\libexec\gcc\mingw32\3.4.4\;%PATH%
+set PATH=%CD%;%OPENMODELICAHOME%\MinGW\libexec\gcc\mingw32\3.4.5\;%PATH%
 popd
 %OPENMODELICAHOME%\MinGW\bin\mingw32-make -f %1.makefile >%1.log 2<&1
 set RESULT=%ERRORLEVEL%
