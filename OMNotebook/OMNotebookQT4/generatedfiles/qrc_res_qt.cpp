b374450cc0dc3a797f5507a9a0fce1aec19d3136
Adrian Pop <adrian.pop@liu.se>
2008-10-31 08:45:23 +0000

    - had to rename build/lib/libQt[NAME].a to build/lib/libQt[NAME]-mingw.a   so that they don't interfere with Linux Qt libs. - changed ext.vcproj and trunk.vcproj to copy the executables and *.xml to build/bin - new OMNotebook.exe and ext.exe
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@3724 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMNotebook/OMNotebookQT4/generatedfiles/qrc_res_qt.cpp
+++ OMNotebook/OMNotebookQT4/generatedfiles/qrc_res_qt.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Resource object code
 **
-** Created: Thu 2. Oct 08:58:23 2008
+** Created: Sun 26. Oct 12:56:40 2008
 **      by: The Resource Compiler for Qt version 4.4.2
 **
 ** WARNING! All changes made in this file will be lost!
