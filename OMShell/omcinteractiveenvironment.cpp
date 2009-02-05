68974769c5a7a2ea319435af53de20e090e3d9cc
Adrian Pop <adrian.pop@liu.se>
2009-02-05 10:35:25 +0000

    - fixed an OMC crash on Vista (sendData.cpp) - added icons for Plot Window (ext.exe) and OMC (omc.exe) - display the Plot Window (ext.exe) on the taskbar - updated copyright in OMShell - added .pdb files for releases: ext.exe, OMShell.exe, omc.exe   to make it easier to debug crashes - fixed the last date in OpenModelica System Guide - fixed 3D_Bouncinball3D.onb - fixed OMShell runtime error at start
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@3844 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMShell/omcinteractiveenvironment.cpp
+++ OMShell/omcinteractiveenvironment.cpp
@@ -1,48 +1,33 @@
 /*
-------------------------------------------------------------------------------------
-This file is part of OpenModelica.
-
-Copyright (c) 1998-2006, Linköpings universitet,
-Department of Computer and Information Science, PELAB
-See also: www.ida.liu.se/projects/OpenModelica
-
-All rights reserved.
-
-(The new BSD license, see also
-http://www.opensource.org/licenses/bsd-license.php)
-
-
-Redistribution and use in source and binary forms, with or without
-modification,
-are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice,
-      this list of conditions and the following disclaimer.
-
-	* Redistributions in binary form must reproduce the above copyright notice,
-      this list of conditions and the following disclaimer in the documentation
-      and/or other materials provided with the distribution.
-
-    * Neither the name of Linköpings universitet nor the names of its contributors
-      may be used to endorse or promote products derived from this software without
-      specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-For more information about the Qt-library visit TrollTech:s webpage regarding
-licence: http://www.trolltech.com/products/qt/licensing.html
-
-------------------------------------------------------------------------------------
+* This file is part of OpenModelica.
+*
+* Copyright (c) 1998-2008, Linköpings University,
+* Department of Computer and Information Science,
+* SE-58183 Linköping, Sweden.
+*
+* All rights reserved.
+*
+* THIS PROGRAM IS PROVIDED UNDER THE TERMS OF THIS OSMC PUBLIC
+* LICENSE (OSMC-PL). ANY USE, REPRODUCTION OR DISTRIBUTION OF
+* THIS PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE OF THE OSMC
+* PUBLIC LICENSE.
+*
+* The OpenModelica software and the Open Source Modelica
+* Consortium (OSMC) Public License (OSMC-PL) are obtained
+* from Linköpings University, either from the above address,
+* from the URL: http://www.ida.liu.se/projects/OpenModelica
+* and in the OpenModelica distribution.
+*
+* This program is distributed  WITHOUT ANY WARRANTY; without
+* even the implied warranty of  MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE, EXCEPT AS EXPRESSLY SET FORTH
+* IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE CONDITIONS
+* OF OSMC-PL.
+*
+* See the full OSMC Public License conditions for more details.
+*
+* For more information about the Qt-library visit TrollTech's webpage 
+* regarding the Qt licence: http://www.trolltech.com/products/qt/licensing.html
 */
 
 #include <exception>
@@ -58,149 +43,149 @@ using namespace std;
 
 namespace IAEX
 {
-	/*! \class OmcInteractiveEnvironment
-	 *
-	 * \brief Implements evaluation for modelica code.
-	 */
-	OmcInteractiveEnvironment::OmcInteractiveEnvironment()
-		: comm_(OmcCommunicator::getInstance()),result_("")
-	{
-		//Communicate with Omc.
-		if(!comm_.isConnected())
-		{
-			if(!comm_.establishConnection())
-			{
-				throw runtime_error("OmcInteractiveEnvironment(): No connection to Omc established");
-			}
-		}
-	}
+  /*! \class OmcInteractiveEnvironment
+  *
+  * \brief Implements evaluation for modelica code.
+  */
+  OmcInteractiveEnvironment::OmcInteractiveEnvironment()
+    : comm_(OmcCommunicator::getInstance()),result_("")
+  {
+    //Communicate with Omc.
+    if(!comm_.isConnected())
+    {
+      if(!comm_.establishConnection())
+      {
+         throw runtime_error("OmcInteractiveEnvironment(): No connection to Omc established");
+      }
+    }
+  }
 
-   OmcInteractiveEnvironment::~OmcInteractiveEnvironment(){}
+  OmcInteractiveEnvironment::~OmcInteractiveEnvironment(){}
 
-   QString OmcInteractiveEnvironment::getResult()
-   {
-      return result_;
-   }
+  QString OmcInteractiveEnvironment::getResult()
+  {
+    return result_;
+  }
 
-   void OmcInteractiveEnvironment::evalExpression(QString& expr)
-   {
-		// 2006-02-02 AF, Added try-catch
-		try
-		{
-			result_ = comm_.callOmc(expr);
-		}
-		catch( exception &e )
-		{
-			throw e;
-		}
-	}
+  void OmcInteractiveEnvironment::evalExpression(QString& expr)
+  {
+    // 2006-02-02 AF, Added try-catch
+    try
+    {
+      result_ = comm_.callOmc(expr);
+    }
+    catch( exception &e )
+    {
+      throw e;
+    }
+  }
 
-   void OmcInteractiveEnvironment::closeConnection()
-   {
-	   comm_.closeConnection();
-   }
+  void OmcInteractiveEnvironment::closeConnection()
+  {
+    comm_.closeConnection();
+  }
 
-   	void OmcInteractiveEnvironment::reconnect()
-	{
-		//Communicate with Omc.
-		if(!comm_.isConnected())
-		{
-			if(!comm_.establishConnection())
-			{
-				throw runtime_error("OmcInteractiveEnvironment(): No connection to Omc established");
-			}
-		}
-	}
+  void OmcInteractiveEnvironment::reconnect()
+  {
+    //Communicate with Omc.
+    if(!comm_.isConnected())
+    {      
+      if(!comm_.establishConnection())
+      {
+        throw runtime_error("OmcInteractiveEnvironment(): No connection to Omc established");
+      }
+    }
+  }
 
-	bool OmcInteractiveEnvironment::startDelegate()
-	{
-		// if not connected and can not establish connection,
-		// try to start OMC
-		if( !comm_.isConnected() && !comm_.establishConnection() )
-		{
-			return  OmcInteractiveEnvironment::startOMC();
-		}
-		else
-			return false;
-	}
+  bool OmcInteractiveEnvironment::startDelegate()
+  {
+    // if not connected and can not establish connection,
+    // try to start OMC
+    if( !comm_.isConnected() && !comm_.establishConnection() )
+    {
+      return  OmcInteractiveEnvironment::startOMC();
+    }
+    else
+      return false;
+  }
 
-	bool OmcInteractiveEnvironment::startOMC()
-	{
-		bool flag = false;
+  bool OmcInteractiveEnvironment::startOMC()
+  {
+    bool flag = false;
 
-		try
-		{
-			// 2006-02-28 AF, use environment varable to find omc.exe
-			string OMCPath( getenv( "OPENMODELICAHOME" ) );
-			if( OMCPath.empty() )
-				throw std::runtime_error( "Could not find environment variable OPENMODELICAHOME" );
+    try
+    {
+      // 2006-02-28 AF, use environment varable to find omc.exe
+      string OMCPath( getenv( "OPENMODELICAHOME" ) );
+      if( OMCPath.empty() )
+        throw std::runtime_error( "Could not find environment variable OPENMODELICAHOME" );
 
-			// location of omc in openmodelica folder
-			QDir dir;
+      // location of omc in openmodelica folder
+      QDir dir;
 #ifdef WIN32
-			if( dir.exists( QString(OMCPath.c_str()) + "\\bin\\omc.exe" ) )
-				OMCPath += "\\bin\\";
-			else if( dir.exists( QString(OMCPath.c_str()) + "\\omc.exe" ) )
-				OMCPath;
-			else if( dir.exists( "omc.exe" ))
-				OMCPath = "";
-			else
-			{
-				string msg = "Unable to find OMC, searched in:\n" +
-					OMCPath + "\\bin\\\n" +
-					OMCPath + "\n" +
-					dir.absolutePath().toStdString();
+      if( dir.exists( QString(OMCPath.c_str()) + "\\bin\\omc.exe" ) )
+        OMCPath += "\\bin\\";
+      else if( dir.exists( QString(OMCPath.c_str()) + "\\omc.exe" ) )
+        OMCPath;
+      else if( dir.exists( "omc.exe" ))
+        OMCPath = "";
+      else
+      {
+        string msg = "Unable to find OMC, searched in:\n" +
+          OMCPath + "\\bin\\\n" +
+          OMCPath + "\n" +
+          dir.absolutePath().toStdString();
 
-				throw std::runtime_error( msg.c_str() );
-			}
+        throw std::runtime_error( msg.c_str() );
+      }
 #else /* unix */
-			if( dir.exists( QString(OMCPath.c_str()) + "/bin/omc" ) )
-				OMCPath += "/bin/";
-			else if( dir.exists( QString(OMCPath.c_str()) + "/omc" ) )
-				OMCPath;
-			else if( dir.exists( "omc.exe" ))
-				OMCPath = "";
-			else
-			{
-				string msg = "Unable to find OMC, searched in:\n" +
-					OMCPath + "/bin/\n" +
-					OMCPath + "\n" +
-					dir.absolutePath().toStdString();
+      if( dir.exists( QString(OMCPath.c_str()) + "/bin/omc" ) )
+        OMCPath += "/bin/";
+      else if( dir.exists( QString(OMCPath.c_str()) + "/omc" ) )
+        OMCPath;
+      else if( dir.exists( "omc.exe" ))
+        OMCPath = "";
+      else
+      {
+        string msg = "Unable to find OMC, searched in:\n" +
+          OMCPath + "/bin/\n" +
+          OMCPath + "\n" +
+          dir.absolutePath().toStdString();
 
-				throw std::runtime_error( msg.c_str() );
-			}
+        throw std::runtime_error( msg.c_str() );
+      }
 #endif
 
-			// 2006-03-14 AF, set omc loaction and parameters
-			QString omc;
+      // 2006-03-14 AF, set omc loaction and parameters
+      QString omc;
 #ifdef WIN32
-				omc = QString( OMCPath.c_str() ) + "omc.exe";
+      omc = QString( OMCPath.c_str() ) + "omc.exe";
 #else /* unix */
-				omc = QString( OMCPath.c_str() ) + "omc";
+      omc = QString( OMCPath.c_str() ) + "omc";
 #endif
 
-			QStringList parameters;
-			parameters << "+d=interactiveCorba";
+      QStringList parameters;
+      parameters << "+d=interactiveCorba";
 
-			// 2006-03-14 AF, create qt process
-			QProcess *omcProcess = new QProcess();
+      // 2006-03-14 AF, create qt process
+      QProcess *omcProcess = new QProcess();
 
-			// 2006-03-14 AF, start omc
-			omcProcess->start( omc, parameters );
+      // 2006-03-14 AF, start omc
+      omcProcess->start( omc, parameters );
 
-			if( omcProcess->waitForStarted(2000) )
-				flag = true;
-			else
-				flag = false;
+      if( omcProcess->waitForStarted(7000) )
+        flag = true;
+      else
+        flag = false;
 
 
-		}
-		catch( exception &e )
-		{
-			QString msg = e.what();
-			QMessageBox::warning( 0, "Error", msg, "OK" );
-		}
+    }
+    catch( exception &e )
+    {
+      QString msg = e.what();
+      QMessageBox::warning( 0, "Error", msg, "OK" );
+    }
 
-		return flag;
-	}
+    return flag;
+  }
 }
