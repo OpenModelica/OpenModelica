99f4a4df3e16c577b6d2c4b9d78ac5fd71abd50d
Jens Frenkel <jens.frenkel@tu-dresden.de>
2010-03-24 16:59:11 +0000

    - missing file for division zero problem implementation
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@5199 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ c_runtime/division.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of OpenModelica.
+ *
+ * Copyright (c) 1998-CurrentYear, Linköping University,
+ * Department of Computer and Information Science,
+ * SE-58183 Linköping, Sweden.
+ *
+ * All rights reserved.
+ *
+ * THIS PROGRAM IS PROVIDED UNDER THE TERMS OF GPL VERSION 3 
+ * AND THIS OSMC PUBLIC LICENSE (OSMC-PL). 
+ * ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES RECIPIENT'S  
+ * ACCEPTANCE OF THE OSMC PUBLIC LICENSE.
+ *
+ * The OpenModelica software and the Open Source Modelica
+ * Consortium (OSMC) Public License (OSMC-PL) are obtained
+ * from Linköping University, either from the above address,
+ * from the URLs: http://www.ida.liu.se/projects/OpenModelica or  
+ * http://www.openmodelica.org, and in the OpenModelica distribution. 
+ * GNU version 3 is obtained from: http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without
+ * even the implied warranty of  MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE, EXCEPT AS EXPRESSLY SET FORTH
+ * IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE CONDITIONS
+ * OF OSMC-PL.
+ *
+ * See the full OSMC Public License conditions for more details.
+ *
+ */
+
+
+#ifndef DIVISION_H
+#define DIVISION_H
+
+#include "modelica.h"
+
+#define DIVISION(a,b,c) ((b != 0) ? a / b : a / division_error(b,c))
+
+
+modelica_real division_error(modelica_real b, const char* division_str);
+
+#endif
