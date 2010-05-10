45cbdbf66f93b46d86832e8c8b13aef14a5e50c1
Daniel Hedberg <daniel@mathcore.com>
2010-05-10 13:10:47 +0000

    INFINITY and NAN are not defined in math.h on Windows (MSVC). Removed the code involving those definitions for MSVC.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@5496 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/runtime/ptolemyio.cpp
+++ Compiler/runtime/ptolemyio.cpp
@@ -52,7 +52,6 @@ extern "C"
 #include <math.h>
 void print_error_buf_impl(const char* str);
 
-
 /* Given a file name and an array of variables, return the RML datastructure
    in Values for Real[size(vars,1],:] i.e. a matrix of variable values, one column for each variable. */
 void * read_ptolemy_dataset(char*filename, int size,char**vars,int datasize)
@@ -124,6 +123,8 @@ void * read_ptolemy_dataset(char*filename, int size,char**vars,int datasize)
       
       buf1 = values.substr(commapos+1).c_str();
       val = strtod(buf1,&buf2); // Second value after comma
+// INFINITY and NAN is not defined in the MSVC version of math.h., hence the exclusion of the code snippet below on MSVC.
+#if !defined(_MSC_VER)
       if (buf1 == buf2) {
         // We may be trying to parse Infinity on a Windows platform.
         // Don't we feel stupid expecting this to work?
@@ -134,7 +135,7 @@ void * read_ptolemy_dataset(char*filename, int size,char**vars,int datasize)
         else if (0 == strncmp(buf1,"-inf",4)) val = -INFINITY;
         else val = NAN;
       }
-   
+#endif   
       lst = (void*)mk_cons(Values__REAL(mk_rcon(val)),lst);
       j++;
     }
