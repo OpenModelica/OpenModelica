OpenModelica/OpenModelica

commit 1426014fcdb636bdba0e7f1e8340521173a94878

Print messages in the right order in getMessageStr().
git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@6450 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

Compiler/runtime/errorext.cpp
@@ -472,7 +472,7 @@ extern "C"
   {
     std::string res("{");
     while(!errorMessageQueue.empty()) {
-      res = res + errorMessageQueue.top()->getFullMessage();
+      res = errorMessageQueue.top()->getFullMessage() + res;
       delete errorMessageQueue.top();
       errorMessageQueue.pop();
       if (!errorMessageQueue.empty()) { res = res + string(","); }

commit 1021c80db9e435c2f81246b04836c42dad9c1c47

Fixed a bug introduced by r6450: Better get the commas and braces in the right place as well.
git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@6473 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

Compiler/runtime/errorext.cpp
@@ -470,14 +470,14 @@ extern "C"
 
   RML_BEGIN_LABEL(ErrorExt__getMessagesStr)
   {
-    std::string res("{");
+    std::string res("}");
     while(!errorMessageQueue.empty()) {
       res = errorMessageQueue.top()->getFullMessage() + res;
       delete errorMessageQueue.top();
       errorMessageQueue.pop();
-      if (!errorMessageQueue.empty()) { res = res + string(","); }
+      if (!errorMessageQueue.empty()) { res = string(",") + res; }
     }
-    res = res + string("}");
+    res = string("{") + res;
     rmlA0 = mk_scon((char*)res.c_str());
     RML_TAILCALLK(rmlSC);
   }