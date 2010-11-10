OpenModelica/OpenModelica

commit 80a8c899959e095f0db031ec55ca2ac1510b70bf

- Added work-around for checkModel when BackendDAECreate.lowerArrEqn gets an   array equation of unknown size. - This fixes the mosfiles/ArrayParameterSize test case.
git-svn-id: https://openmodelica.org/svn/OpenModelica/branches/sjoelund-functiontree@6938 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

Compiler/Expression.mo
@@ -60,20 +60,21 @@ public type Subscript = DAE.Subscript;
 public type Var = DAE.ExpVar;
 
 // protected imports
+protected import Algorithm;
 protected import ClassInf;
 protected import ComponentReference;
-protected import ExpressionSimplify;
-protected import Error;
-protected import RTOpts;
-protected import Util;
-protected import ModUtil;
-protected import Debug;
-protected import Static;
-protected import Env;
-protected import ExpressionDump;
 protected import DAEUtil;
-protected import Algorithm;
+protected import Debug;
+protected import Env;
+protected import Error;
+protected import ExpressionDump;
+protected import ExpressionSimplify;
+protected import ModUtil;
+protected import OptManager;
 protected import Prefix;
+protected import RTOpts;
+protected import Static;
+protected import Util;
 
 /***************************************************/
 /* transform to other types */
@@ -1390,6 +1391,11 @@ algorithm
       Integer i;
     case DAE.DIM_INTEGER(integer = i) then i;
     case DAE.DIM_ENUM(size = i) then i; 
+    case DAE.DIM_UNKNOWN()
+      equation
+        true = OptManager.getOption("checkModel");
+      then
+        0;
   end matchcontinue;
 end dimensionSize;
 
@@ -1398,22 +1404,7 @@ public function dimensionsSizes
   input list<DAE.Dimension> inDims;
   output list<Integer> outValues;
 algorithm
-  outValues := matchcontinue(inDims)
-    local
-      DAE.Dimension dim;
-      list<DAE.Dimension> dims;
-      Integer i;
-      list<Integer> ii;
-    
-    case {} then {};
-    
-    case (dim::dims)
-      equation
-        i = dimensionSize(dim);
-        ii = dimensionsSizes(dims);
-      then
-        i::ii;   
-  end matchcontinue;
+  outValues := Util.listMap(inDims, dimensionSize);
 end dimensionsSizes;
 
 public function typeof "

commit 00accde8f868d6ace7e95891afe13b063638260f

- Fixed issues introduced in r6938. - Updated parallel test script.
git-svn-id: https://openmodelica.org/svn/OpenModelica/branches/sjoelund-functiontree@6942 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

Compiler/Expression.mo
@@ -1391,20 +1391,41 @@ algorithm
       Integer i;
     case DAE.DIM_INTEGER(integer = i) then i;
     case DAE.DIM_ENUM(size = i) then i; 
+  end matchcontinue;
+end dimensionSize;
+
+public function dimensionSizeAll
+  "Extracts an integer from an array dimension. Also handles DIM_EXP and
+  DIM_UNKNOWN if checkModel is used."
+  input DAE.Dimension dim;
+  output Integer value;
+algorithm
+  value := matchcontinue(dim)
+    local
+      Integer i;
+      DAE.Exp e;
+    case DAE.DIM_INTEGER(integer = i) then i;
+    case DAE.DIM_ENUM(size = i) then i;
+    case DAE.DIM_EXP(exp = e) then expInt(e);
+    case DAE.DIM_EXP(exp = _)
+      equation
+        true = OptManager.getOption("checkModel");
+      then
+        0;
     case DAE.DIM_UNKNOWN()
       equation
         true = OptManager.getOption("checkModel");
       then
         0;
   end matchcontinue;
-end dimensionSize;
+end dimensionSizeAll;
 
 public function dimensionsSizes
   "Extracts a list of integers from a list of array dimensions"
   input list<DAE.Dimension> inDims;
   output list<Integer> outValues;
 algorithm
-  outValues := Util.listMap(inDims, dimensionSize);
+  outValues := Util.listMap(inDims, dimensionSizeAll);
 end dimensionsSizes;
 
 public function typeof "