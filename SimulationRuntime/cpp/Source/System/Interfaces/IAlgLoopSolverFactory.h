523e3a8db97dad2477a4cfd605968ce0955f605c
Niklas Worschech <niklas.worschech@boschrexroth.de>
2011-11-04 13:13:20 +0000

    Added forgotten AlgloopsolverFactory Interface
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@10296 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ SimulationRuntime/cpp/Source/System/Interfaces/IAlgLoopSolverFactory.h
@@ -0,0 +1,31 @@
+#pragma once
+
+class IAlgLoopSolver;
+class IAlgLoop;
+
+/*****************************************************************************/
+/**
+
+Abstract interface class for numerical methods for the (possibly iterative)
+solution of algebraic loops in open modelica.
+
+\date     October, 1st, 2008
+\author   
+
+*/
+/*****************************************************************************
+Copyright (c) 2008, OSMC
+*****************************************************************************/
+class IAlgLoopSolverFactory
+{
+
+public:
+	
+
+	virtual ~IAlgLoopSolverFactory()	{};
+
+	virtual  boost::shared_ptr<IAlgLoopSolver> createAlgLoopSolver(IAlgLoop* algLoop) = 0;
+
+	
+
+};
