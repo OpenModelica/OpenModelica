bca75f907d17bad3f931356b5f12f3e4ed077792
Adrian Pop <adrian.pop@liu.se>
2011-11-06 09:18:52 +0000

    - pushd doesn't understand any string with forward slashes, use cd instead. - add more info to the log files so we can debug a bit if needed. - force just the paths we need and NOTHING else.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@10300 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/scripts/Compile.bat
+++ Compiler/scripts/Compile.bat
@@ -9,10 +9,14 @@ set MINGW=%OPENMODELICAHOME%\MinGW
 REM If OMDEV is set, use MinGW from there instead of OPENMODELICAHOME
 REM It is not certain that release OMC is installed
 if not %OMDEV%a==a set MINGW=%OMDEV%\tools\MinGW
-pushd "%MINGW%\bin" >%1.log 2<&1
-set PATH=%CD%;%MinGW%\libexec\gcc\mingw32\4.4.0\;%PATH%
+echo OPENMODELICAHOME = %OPENMODELICAHOME% > %1.log 2>&1
+pushd . >>%1.log 2>&1
+cd "%MINGW%\bin"
+set PATH=%CD%;%CD%\..\libexec\gcc\mingw32\4.4.0\;
 popd
-%MinGW%\bin\mingw32-make -f %1.makefile >>%1.log 2<&1
+echo PATH = %PATH% >>%1.log 2>&1
+echo CD = %CD% >>%1.log 2>&1
+%MinGW%\bin\mingw32-make -f %1.makefile >>%1.log 2>&1
 set RESULT=%ERRORLEVEL%
 set PATH=%OLD_PATH%
 set OLD_PATH=
