4e96a46acb91380bad05a0f4a0f1a53e9745df11
Willi Braun <wbraun@fh-bielefeld.de>
2011-11-16 03:06:05 +0000

    - updated FMI import and moved fmu to test suite
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@10528 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- FMI/import/source/moGenerator.h
+++ FMI/import/source/moGenerator.h
@@ -1,3 +1,34 @@
+/*
+ * This file is part of OpenModelica.
+ *
+ * Copyright (c) 1998-CurrentYear, Linköping University,
+ * Department of Computer and Information Science,
+ * SE-58183 Linköping, Sweden.
+ *
+ * All rights reserved.
+ *
+ * THIS PROGRAM IS PROVIDED UNDER THE TERMS OF GPL VERSION 3
+ * AND THIS OSMC PUBLIC LICENSE (OSMC-PL).
+ * ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES RECIPIENT'S
+ * ACCEPTANCE OF THE OSMC PUBLIC LICENSE.
+ *
+ * The OpenModelica software and the Open Source Modelica
+ * Consortium (OSMC) Public License (OSMC-PL) are obtained
+ * from Linköping University, either from the above address,
+ * from the URLs: http://www.ida.liu.se/projects/OpenModelica or
+ * http://www.openmodelica.org, and in the OpenModelica distribution.
+ * GNU version 3 is obtained from: http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without
+ * even the implied warranty of  MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE, EXCEPT AS EXPRESSLY SET FORTH
+ * IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE CONDITIONS
+ * OF OSMC-PL.
+ *
+ * See the full OSMC Public License conditions for more details.
+ *
+ */
+
 #include "xmlparser.h"
 #include "fmuWrapper.h"
 
@@ -11,6 +42,7 @@ typedef enum{sv_real, sv_integer, sv_boolean, sv_string,sv_enum} fmiScalarVariab
 typedef struct {
 	const char* name;
 	fmiValueReference vr;
+	int aliasInd;
 	void* next;
 } fmuOutputVar;
 
@@ -18,12 +50,14 @@ typedef struct {
 typedef struct{
 	const char* declType;
 	const char* start;
+	int defStart;
 	fmiBooleanXML fixed;
 } fmiSTRING;
 
 typedef struct{
 	fmiBooleanXML start;
 	const char* declType;
+	int defStart;
 	fmiBooleanXML fixed;
 } fmiBOOLEAN;
 
@@ -31,10 +65,25 @@ typedef struct{
 	const char* declType;
 	const char* quantity;
 	double min;
+	int defMin;
 	double max;
+	int defMax;
 	int start;
+	int defStart;
 	fmiBooleanXML fixed;
 } fmiINTEGER;
+
+typedef struct{
+  const char *declType;
+  const char *quantity;
+  int min;
+  int defMin;
+  int max;
+  int defMax;
+  int start;
+  int defStart;
+  fmiBooleanXML fixed;
+} fmiENUM;
 
 typedef struct{
 	const char* declType;
@@ -42,10 +91,14 @@ typedef struct{
 	const char* unit;
 	const char* displayUnit;
 	fmiBooleanXML relQuantity;
-	double min;
-	double max;
-	double nominal;
-	double start;
+  double min;
+  int defMin;
+  double max;
+  int defMax;
+  double nominal;
+  int defNorminal;
+  double start;
+  int defStart;
 	fmiBooleanXML fixed;
 } fmiREAL;
 
@@ -54,15 +107,15 @@ typedef struct{
 	char* flatName;
 	fmiValueReference vr; // value reference;
 	const char* description;
-	Enu var;
-	Enu causality;
-	Enu alias;
+	fmiVariability var;
+	fmiCausality causality;
+	fmiAlias alias;
 	fmiScalarVariableType type;
 	void* variable;	
 } fmiScalarVariable;
 
 typedef struct{
-	
+  void *vp;
 } fmiArrayVariable;
 
 typedef struct{
@@ -71,6 +124,12 @@ typedef struct{
 	int nav; // number of array variables
 	fmiArrayVariable* list_av;	
 } fmiModelVariable;
+
+typedef struct{
+  double startTime;
+  double stopTime;
+  double tolerance;
+} fmiDefaultExperiment;
 
 typedef struct{
 	const char* fmiver; // fmi version number;
@@ -85,8 +144,10 @@ typedef struct{
 	fmiNamingConvention nconv; // variable naming convention;
 	unsigned int ncs; // number of continuous states;
 	unsigned int nei; // number of event indicators;
+	fmiDefaultExperiment *defaultExperiment;
 	fmiModelVariable* modelVariable;
 } fmuModelDescription;
+
 
 fmiScalarVariableType getElementType(ScalarVariable* sv);
 void* allocateElmSV(fmiScalarVariable sv);
