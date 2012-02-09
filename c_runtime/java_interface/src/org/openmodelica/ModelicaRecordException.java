297cb69f52b030b7e770c09e7f9d378077817d59
Martin Sjölund <martin.sjolund@liu.se>
2012-02-09 13:57:55 +0000

    - Die with fire
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@11055 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- c_runtime/java_interface/src/org/openmodelica/ModelicaRecordException.java
+++
@@ -1,9 +0,0 @@
-package org.openmodelica;
-
-public class ModelicaRecordException extends Exception {
-  private static final long serialVersionUID = -6370341451295238391L;
-
-  public ModelicaRecordException(String ex) {
-    super(ex);
-  }
-}
