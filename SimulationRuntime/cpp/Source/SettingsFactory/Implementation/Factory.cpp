c35b460c57690dbe84b2a4832f1073cc5e81d989
Jens Frenkel <jens.frenkel@tu-dresden.de>
2012-06-20 16:04:51 +0000

    -fix loading of settings file in cpp runtime
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@12152 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- SimulationRuntime/cpp/Source/SettingsFactory/Implementation/Factory.cpp
+++ SimulationRuntime/cpp/Source/SettingsFactory/Implementation/Factory.cpp
@@ -21,17 +21,18 @@ tuple<boost::shared_ptr<IGlobalSettings>,boost::shared_ptr<ISolverSettings> > Se
 {
   
  
-
-  cout<<"Read Settings..."<<std::endl;
-  
-  //load global settings or use default settings
+  fs::path settingsfile_name("GlobalSettings.xml");
+  fs::path settingsfile_path = libraries_path;
+  fs::path settingsfolder_name("config");
+  settingsfile_path/=settingsfolder_name;
+  settingsfile_path/=settingsfile_name;
+  cout<<"Read Settings from "<< settingsfile_path << std::endl;
+   //load global settings or use default settings
   _global_settings =  boost::shared_ptr<IGlobalSettings>(new GlobalSettings());
-  _global_settings->load("config//GlobalSettings.xml");
+  _global_settings->load( settingsfile_path.c_str());
   std::string solver_dll;
   //Load solver dll
-  
-
-  if(_global_settings->getSelectedSolver().compare("Euler")==0)
+   if(_global_settings->getSelectedSolver().compare("Euler")==0)
     solver_dll.assign(EULER_LIB);
   else if(_global_settings->getSelectedSolver().compare("Idas")==0)
     solver_dll.assign(IDAS_LIB);
@@ -41,10 +42,21 @@ tuple<boost::shared_ptr<IGlobalSettings>,boost::shared_ptr<ISolverSettings> > Se
     solver_dll.assign(CVODE_LIB);
   else
     throw std::invalid_argument("Selected Solver is not available");
+  
+  
+  
+
+
   string settings = _global_settings->getSelectedSolver().append("Settings");
-  string settings_file ="config//";
+  string settings_file;
   settings_file.append(
       _global_settings->getSelectedSolver().append("Settings.xml"));
+  
+  fs::path solversettingsfile_name(settings_file);
+  fs::path solversettingsfile_path = libraries_path;
+  solversettingsfile_path/=settingsfolder_name;
+  solversettingsfile_path/=solversettingsfile_name;
+
   type_map types;
   
   fs::path solver_name(solver_dll);
@@ -53,8 +65,7 @@ tuple<boost::shared_ptr<IGlobalSettings>,boost::shared_ptr<ISolverSettings> > Se
    fs::path solver_default_name(SOLVER_LIB);
   fs::path solver_default_path = libraries_path;
   solver_default_path/=solver_default_name;
-  cout << "loading solver" << std::endl;
-  if(!load_single_library(types,solver_default_path.c_str()))
+   if(!load_single_library(types,solver_default_path.c_str()))
     throw std::invalid_argument(solver_default_path.native()  + " library could not be loaded");
 
   if(!load_single_library(types,solver_path.c_str()))
@@ -69,7 +80,9 @@ tuple<boost::shared_ptr<IGlobalSettings>,boost::shared_ptr<ISolverSettings> > Se
   }
   //create with solver factory selected solver settings
   _solver_settings = boost::shared_ptr<ISolverSettings>(iter->second.create(_global_settings.get())); 
-  _solver_settings->load(settings_file);
+  cout<<"Read Settings from "<< solversettingsfile_path << std::endl;
+ 
+ _solver_settings->load( solversettingsfile_path.c_str());
   
   //return global and solver settings 
   tuple<boost::shared_ptr<IGlobalSettings>,boost::shared_ptr<ISolverSettings> > settings_pair(_global_settings,_solver_settings);
