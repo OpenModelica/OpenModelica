186ab3f01298dded12d822a1606697cfa6a5de11
Niklas Worschech <niklas.worschech@boschrexroth.de>
2012-09-13 07:54:32 +0000

    removed tabs
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@12911 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- SimulationRuntime/cpp/Source/Math/Interfaces/OMAPI.h
+++ SimulationRuntime/cpp/Source/Math/Interfaces/OMAPI.h
@@ -6,12 +6,12 @@
 #endif
 
 #if defined( _WIN32 ) && defined( _MSC_VER )
-#	if defined OMC_EXPORTS
-#		define OMC_API __declspec( dllexport )
-#	else
-#		define OMC_API __declspec( dllimport )
-#		pragma warning( disable : 4251 )	// 'identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'
-#	endif
+# if defined OMC_EXPORTS
+#  define OMC_API __declspec( dllexport )
+#   else
+#  define OMC_API __declspec( dllimport )
+# pragma warning( disable : 4251 )	// 'identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'
+# endif
 #endif
 
 #if !defined(OMC_API)
