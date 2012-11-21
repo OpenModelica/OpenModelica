4fd76865dbffc8f24cb9303bc3323086bfef3450
Martin Sjölund <martin.sjolund@liu.se>
2012-11-21 12:04:46 +0000

    - Adding the mathmod paper
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk/doc@14006 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- bibliography/upload.sh
+++ bibliography/upload.sh
@@ -3,4 +3,8 @@ TMPNAME=/tmp/openmodelica.org-bibgen
 rm -rf $TMPNAME
 mkdir -p $TMPNAME
 sh generate.sh $TMPNAME
+for f in $TMPNAME/*; do
+  echo "" >> $f
+  echo "<!-- This code was automatically generated from the OpenModelica trunk --!>" >> $f
+done
 scp $TMPNAME/* openmodelica.org:/var/www/joomla/bibliography
