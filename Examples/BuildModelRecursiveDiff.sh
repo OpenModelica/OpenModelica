8cb7d67eab374feebca7852cebfe7a903faa7cf3
Martin Sjölund <martin.sjolund@liu.se>
2012-12-14 08:20:54 +0000

    - Also handle tests that crash omc
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@14372 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Examples/BuildModelRecursiveDiff.sh
+++ Examples/BuildModelRecursiveDiff.sh
@@ -4,17 +4,18 @@ fi
 if ! test -f "$2"; then
   echo "File \'$2\' does not exist"
 fi
-BUILD="tr.*/td.*/td.*td bgcolor=..FF0000.*/td.*/td.*/td.*/td.*/td"
-SIM="tr.*/td.*td bgcolor=..FF0000.*/td.*/td.*/td.*/td"
+BUILD="(<tr><td bgcolor=.#ff0000)|(tr.*/td.*/td.*td bgcolor=..FF0000.*/td.*/td.*/td.*/td.*/td)"
+SIM="(<tr><td bgcolor=.#ff0000)|(tr.*/td.*td bgcolor=..FF0000.*/td.*/td.*/td.*/td)"
 SEARCH=">Modelica[A-Za-z0-9._]*(<| [(])"
 REV1=`grep -o "[(]r[0-9]*" "$1" | tr -d "("`
 REV2=`grep -o "[(]r[0-9]*" "$2" | tr -d "("`
-grep "$BUILD" $1 | egrep -o "$SEARCH" | tr -d "<> (" > "$1.build"
-grep "$BUILD" $2 | egrep -o "$SEARCH" | tr -d "<> (" > "$2.build"
+egrep "$BUILD" $1 | egrep -o "$SEARCH" | tr -d "<> (" > "$1.build"
+egrep "$BUILD" $2 | egrep -o "$SEARCH" | tr -d "<> (" > "$2.build"
 echo "Build diff (failures between $REV1 and $REV2)"
 diff -u "$1.build" "$2.build" | grep ^[+-]
 
-grep "$SIM" "$1" | egrep -o "$SEARCH" | tr -d "<> (" > "$1.sim"
-grep "$SIM" "$2" | egrep -o "$SEARCH" | tr -d "<> (" > "$2.sim"
+egrep "$SIM" "$1" | egrep -o "$SEARCH" | tr -d "<> (" > "$1.sim"
+egrep "$SIM" "$2" | egrep -o "$SEARCH" | tr -d "<> (" > "$2.sim"
 echo "Sim diff (failures between $REV1 and $REV2)"
 diff -u "$1.sim" "$2.sim" | grep "^[+-][A-Za-z0-9._]"
+rm -f "$1.build" "$2.build" "$1.sim" "$2.sim"
