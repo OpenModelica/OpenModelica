9e352e5d97fe4466aa6d75761ff891e3bf5a9b5a
Martin Sjölund <martin.sjolund@liu.se>
2012-12-27 23:10:29 +0000

    #2000 - Make linspace simplify better
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk/doc@14551 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- bibliography/generate.sh
+++ bibliography/generate.sh
@@ -6,6 +6,6 @@ for type in papers msc journals phd; do
   ./bib2html.sh $type openmodelica utf8 bibliography\\/ || exit 1
 done
 
-bibtex2html -a -charset utf8 openmodelica.bib
+bibtex2html -a -charset utf8 openmodelica.bib || exit 1
 
 mv *.bibinc openmodelica_bib.html openmodelica.html $1
--- bibliography/upload.sh
+++ bibliography/upload.sh
@@ -2,7 +2,7 @@
 TMPNAME=/tmp/openmodelica.org-bibgen
 rm -rf $TMPNAME
 mkdir -p $TMPNAME
-sh generate.sh $TMPNAME
+sh generate.sh $TMPNAME || exit 1
 for f in $TMPNAME/*; do
   echo "" >> $f
   echo "<!-- This code was automatically generated from the OpenModelica trunk --!>" >> $f
