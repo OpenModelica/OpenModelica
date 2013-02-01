01bfc57e3720bd0a50c52846bf09c10704bf49d7
Adrian Pop <adrian.pop@liu.se>
2013-02-01 02:17:59 +0000

    - bump version number to beta4+dev.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@15031 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/runtime/config.h
+++ Compiler/runtime/config.h
@@ -98,9 +98,9 @@
 #endif
 
 #ifdef CONFIG_REVISION
-#define CONFIG_VERSION "1.9.0 beta4 (r" CONFIG_REVISION ")"
+#define CONFIG_VERSION "1.9.0 beta4+dev (r" CONFIG_REVISION ")"
 #else
-#define CONFIG_VERSION "1.9.0 beta4"
+#define CONFIG_VERSION "1.9.0 beta4+dev"
 #endif
 
 
