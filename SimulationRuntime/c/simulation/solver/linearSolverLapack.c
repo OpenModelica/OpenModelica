2c85ce1d494215f9c0530a53594b608e897f4901
Willi Braun <wbraun@fh-bielefeld.de>
2013-02-12 19:35:27 +0000

    - added fogotten files
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@15156 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ SimulationRuntime/c/simulation/solver/linearSolverLapack.c
@@ -0,0 +1,166 @@
+/*
+ * This file is part of OpenModelica.
+ *
+ * Copyright (c) 1998-CurrentYear, Linköping University,
+ * Department of Computer and Information Science,
+ * SE-58183 Linköping, Sweden.
+ *
+ * All rights reserved.
+ *
+ * THIS PROGRAM IS PROVIDED UNDER THE TERMS OF GPL VERSION 3
+ * AND THIS OSMC PUBLIC LICENSE (OSMC-PL).
+ * ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES RECIPIENT'S
+ * ACCEPTANCE OF THE OSMC PUBLIC LICENSE.
+ *
+ * The OpenModelica software and the Open Source Modelica
+ * Consortium (OSMC) Public License (OSMC-PL) are obtained
+ * from Linköping University, either from the above address,
+ * from the URLs: http://www.ida.liu.se/projects/OpenModelica or
+ * http://www.openmodelica.org, and in the OpenModelica distribution.
+ * GNU version 3 is obtained from: http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without
+ * even the implied warranty of  MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE, EXCEPT AS EXPRESSLY SET FORTH
+ * IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE CONDITIONS
+ * OF OSMC-PL.
+ *
+ * See the full OSMC Public License conditions for more details.
+ *
+ */
+
+/*! \file nonlinear_solver.c
+ */
+
+#include <math.h>
+#include <stdlib.h>
+#include <string.h> /* memcpy */
+
+#include "simulation_data.h"
+#include "omc_error.h"
+#include "varinfo.h"
+#include "model_help.h"
+
+#include "linearSystem.h"
+#include "linearSolverLapack.h"
+
+typedef struct DATA_LAPACK
+{
+  integer *ipiv;  /* vector pivot values */
+  integer nrhs;   /* number of righthand sides*/
+  integer info;   /* output */
+} DATA_LAPACK;
+
+/*! \fn allocate memory for linear system solver lapack
+ *
+ */
+int allocateLapackData(int size, void** voiddata)
+{
+  DATA_LAPACK* data = (DATA_LAPACK*) malloc(sizeof(DATA_LAPACK));
+
+  data->ipiv = (integer*) malloc(size*sizeof(modelica_integer));
+  ASSERT(data->ipiv, "Could not allocate datae for linear solver lapack.");
+  data->nrhs = 1;
+  data->info = 0;
+
+  *voiddata = (void*)data;
+  return 0;
+}
+
+/*! \fn free memory for nonlinear solver hybrd
+ *
+ */
+int freeLapackData(void **voiddata)
+{
+  DATA_LAPACK* data = (DATA_LAPACK*) *voiddata;
+
+  free(data->ipiv);
+
+  return 0;
+}
+
+
+/*! \fn solve linear system with lapack method
+ *
+ *  \param  [in]  [data]
+ *                [sysNumber] index of the corresponing non-linear system
+ *
+ *  \author wbraun
+ */
+int solveLapack(DATA *data, int sysNumber)
+{
+  LINEAR_SYSTEM_DATA* systemData = &(data->simulationInfo.linearSystemData[sysNumber]);
+  DATA_LAPACK* solverData = (DATA_LAPACK*)systemData->solverData;
+
+  /* We are given the number of the linear system.
+   * We want to look it up among all equations. */
+  int eqSystemNumber = systemData->equationIndex;
+
+  int success = 1;
+
+  /* reset matrix A */
+  memset(systemData->A, 0, (systemData->size)*(systemData->size)*sizeof(double));
+
+  /* update matrix A */
+  systemData->setA(data, systemData);
+  /* update vector b (rhs) */
+  systemData->setb(data, systemData);
+
+  _omc_dgesv_((integer*) &systemData->size,
+              (integer*) &solverData->nrhs,
+              systemData->A,
+              (integer*) &systemData->size,
+              solverData->ipiv,
+              systemData->b,
+              (integer*) &systemData->size,
+              &solverData->info);
+
+  if(solverData->info < 0) {
+    data->simulationInfo.found_solution = -3;
+    WARNING3(LOG_STDOUT, "Error solving linear system of equations (no. %d) at time %f. Argument %d illegal.", (int)systemData->equationIndex, data->localData[0]->timeValue, solverData->info);
+    success = 0;
+  }else if(solverData->info > 0) {
+    WARNING4(LOG_STDOUT,
+        "Failed to solve linear system of equations (no. %d) at time %f, system is singular for U[%d,%d].",
+        (int)systemData->equationIndex, data->localData[0]->timeValue, solverData->info+1, solverData->info+1);
+
+    /* debug output */
+    if (ACTIVE_STREAM(LOG_LS))
+    {
+      long int l = 0;
+      long int k = 0;
+      char buffer[4096];
+      INDENT(LOG_LS);
+      DEBUG(LOG_LS,"Matrix U:");
+      for (l = 0; l < systemData->size; l++)
+      {
+        buffer[0] = 0;
+        for (k = 0; k < systemData->size; k++)
+          sprintf(buffer, "%s%10g ", buffer, systemData->A[l + k*systemData->size]);
+        DEBUG(LOG_LS, buffer);
+      }
+      DEBUG(LOG_LS,"Solution x:");
+      buffer[0] = 0;
+      for (k = 0; k < systemData->size; k++)
+        sprintf(buffer, "%s%10g ", buffer, systemData->b[k]);
+      DEBUG(LOG_LS, buffer);
+      DEBUG(LOG_LS,"Solution x:");
+      buffer[0] = 0;
+      for (k = 0; k < systemData->size; k++)
+        sprintf(buffer, "%s%10g ", buffer, systemData->b[k]);
+      DEBUG(LOG_LS, buffer);
+      RELEASE(LOG_LS);
+    }
+    /* used for hybrid systems */
+    data->simulationInfo.found_solution = -3;
+
+    success = 0;
+  }
+
+  /* take the solution */
+  memcpy(systemData->x, systemData->b, systemData->size*(sizeof(modelica_real)));
+
+
+
+  return success;
+}
---
+++ SimulationRuntime/c/simulation/solver/linearSystem.c
@@ -0,0 +1,160 @@
+/*
+ * This file is part of OpenModelica.
+ *
+ * Copyright (c) 1998-CurrentYear, Linköping University,
+ * Department of Computer and Information Science,
+ * SE-58183 Linköping, Sweden.
+ *
+ * All rights reserved.
+ *
+ * THIS PROGRAM IS PROVIDED UNDER THE TERMS OF GPL VERSION 3
+ * AND THIS OSMC PUBLIC LICENSE (OSMC-PL).
+ * ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES RECIPIENT'S
+ * ACCEPTANCE OF THE OSMC PUBLIC LICENSE.
+ *
+ * The OpenModelica software and the Open Source Modelica
+ * Consortium (OSMC) Public License (OSMC-PL) are obtained
+ * from Linköping University, either from the above address,
+ * from the URLs: http://www.ida.liu.se/projects/OpenModelica or
+ * http://www.openmodelica.org, and in the OpenModelica distribution.
+ * GNU version 3 is obtained from: http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without
+ * even the implied warranty of  MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE, EXCEPT AS EXPRESSLY SET FORTH
+ * IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE CONDITIONS
+ * OF OSMC-PL.
+ *
+ * See the full OSMC Public License conditions for more details.
+ *
+ */
+
+/*! \file linearSystem.c
+ */
+
+#include <math.h>
+
+#include "omc_error.h"
+#include "linearSystem.h"
+#include "linearSolverLapack.h"
+
+/*! \fn int allocatelinearSystem(DATA *data)
+ *
+ *  This function allocates memory for all linear systems.
+ *
+ *  \param [ref] [data]
+ */
+int allocatelinearSystem(DATA *data)
+{
+  int i;
+  int size;
+  LINEAR_SYSTEM_DATA *linsys = data->simulationInfo.linearSystemData;
+
+  for(i=0; i<data->modelData.nLinearSystems; ++i)
+  {
+    size = linsys[i].size;
+
+    /* allocate system data */
+    linsys[i].x = (double*) malloc(size*sizeof(double));
+    linsys[i].b = (double*) malloc(size*sizeof(double));
+    linsys[i].A = (double*) malloc(size*size*sizeof(double));
+
+    /* allocate solver data */
+    switch(data->simulationInfo.lsMethod)
+    {
+    case LS_LAPACK:
+      allocateLapackData(size, &linsys[i].solverData);
+      break;
+    default:
+      THROW("unrecognized linear solver");
+    }
+  }
+  return 0;
+}
+
+/*! \fn freelinearSystem
+ *
+ *  Thi function frees memory of linear systems.
+ *
+ *  \param [ref] [data]
+ */
+int freelinearSystem(DATA *data)
+{
+  int i;
+  LINEAR_SYSTEM_DATA* linsys = data->simulationInfo.linearSystemData;
+
+  for(i=0;i<data->modelData.nLinearSystems;++i)
+  {
+    free(linsys[i].x);
+    free(linsys[i].b);
+    free(linsys[i].A);
+
+    /* allocate solver data */
+    switch(data->simulationInfo.lsMethod)
+    {
+    case LS_LAPACK:
+      freeLapackData(&linsys[i].solverData);
+      break;
+    default:
+      THROW("unrecognized linear solver");
+    }
+
+    free(linsys[i].solverData);
+  }
+
+  return 0;
+}
+
+/*! \fn solve non-linear systems
+ *
+ *  \param  [in]  [data]
+ *                [sysNumber] index of corresponding non-linear System
+ *
+ *  \author wbraun
+ */
+int solve_linear_system(DATA *data, int sysNumber)
+{
+  /* NONLINEAR_SYSTEM_DATA* system = &(data->simulationInfo.nonlinearSystemData[sysNumber]); */
+  int success;
+  LINEAR_SYSTEM_DATA* linsys = data->simulationInfo.linearSystemData;
+
+  data->simulationInfo.currentLinearSystemIndex = sysNumber;
+
+  /* for now just use lapack solver as before */
+  switch(data->simulationInfo.lsMethod)
+  {
+  case LS_LAPACK:
+    success = solveLapack(data, sysNumber);
+    break;
+  default:
+    THROW("unrecognized linear solver");
+  }
+  linsys[sysNumber].solved = success;
+
+  return 0;
+}
+
+/*! \fn check_linear_solutions
+ *   This function check whether some of linear systems
+ *   are failed to solve. if one is failed it return true, otherwise false.
+ *
+ *  \param  [in]  [data]
+ *
+ *  \author wbraun
+ */
+int check_linear_solutions(DATA *data)
+{
+  LINEAR_SYSTEM_DATA* linsys = data->simulationInfo.linearSystemData;
+  int i,returnValue = 0;
+
+  for(i=0;i<data->modelData.nLinearSystems;++i)
+  {
+    if(linsys[i].solved == 0)
+    {
+      returnValue = 1;
+      break;
+    }
+  }
+
+  return returnValue;
+}
