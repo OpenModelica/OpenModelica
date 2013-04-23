fc8553e8d4bc6c6ecdf65b93daee4daba80e3546
Martin Sjölund <martin.sjolund@liu.se>
2013-04-23 08:45:07 +0000

    Pretty code
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@15890 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- SimulationRuntime/c/util/memory_pool.c
+++ SimulationRuntime/c/util/memory_pool.c
@@ -110,9 +110,8 @@ void clear_current_state(void)
 void* alloc_elements(int n, int sz)
 {
   _index_t start,nelem;
-  int ix = 0;
+  int ix = get_thread_index();
   assert(n>=0);
-  ix = get_thread_index();
   start = current_states[ix].current_state.offset;
   nelem = (((n * sz)+(sizeof(int)-1))/sizeof(int));
   assert(nelem <= NR_ELEMENTS);
