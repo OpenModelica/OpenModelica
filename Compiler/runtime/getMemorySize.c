adde5c1aeef76ccb26516f01d982a7b2b2a52215
OpenModelica Hudson <openmodelica@ida.liu.se>
2013-08-26 06:35:50 +0000

    [Janitor mode] Fix tabs
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@16912 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/runtime/getMemorySize.c
+++ Compiler/runtime/getMemorySize.c
@@ -30,71 +30,71 @@
 static size_t getMemorySizeBytes( )
 {
 #if defined(_WIN32) && (defined(__CYGWIN__) || defined(__CYGWIN32__))
-	/* Cygwin under Windows. ------------------------------------ */
-	/* New 64-bit MEMORYSTATUSEX isn't available.  Use old 32.bit */
-	MEMORYSTATUS status;
-	status.dwLength = sizeof(status);
-	GlobalMemoryStatus( &status );
-	return (size_t)status.dwTotalPhys;
+  /* Cygwin under Windows. ------------------------------------ */
+  /* New 64-bit MEMORYSTATUSEX isn't available.  Use old 32.bit */
+  MEMORYSTATUS status;
+  status.dwLength = sizeof(status);
+  GlobalMemoryStatus( &status );
+  return (size_t)status.dwTotalPhys;
 
 #elif defined(_WIN32)
-	/* Windows. ------------------------------------------------- */
-	/* Use new 64-bit MEMORYSTATUSEX, not old 32-bit MEMORYSTATUS */
-	MEMORYSTATUSEX status;
-	status.dwLength = sizeof(status);
-	GlobalMemoryStatusEx( &status );
-	return (size_t)status.ullTotalPhys;
+  /* Windows. ------------------------------------------------- */
+  /* Use new 64-bit MEMORYSTATUSEX, not old 32-bit MEMORYSTATUS */
+  MEMORYSTATUSEX status;
+  status.dwLength = sizeof(status);
+  GlobalMemoryStatusEx( &status );
+  return (size_t)status.ullTotalPhys;
 
 #elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
-	/* UNIX variants. ------------------------------------------- */
-	/* Prefer sysctl() over sysconf() except sysctl() HW_REALMEM and HW_PHYSMEM */
+  /* UNIX variants. ------------------------------------------- */
+  /* Prefer sysctl() over sysconf() except sysctl() HW_REALMEM and HW_PHYSMEM */
 
 #if defined(CTL_HW) && (defined(HW_MEMSIZE) || defined(HW_PHYSMEM64))
-	int mib[2];
-	mib[0] = CTL_HW;
+  int mib[2];
+  mib[0] = CTL_HW;
 #if defined(HW_MEMSIZE)
-	mib[1] = HW_MEMSIZE;            /* OSX. --------------------- */
+  mib[1] = HW_MEMSIZE;            /* OSX. --------------------- */
 #elif defined(HW_PHYSMEM64)
-	mib[1] = HW_PHYSMEM64;          /* NetBSD, OpenBSD. --------- */
+  mib[1] = HW_PHYSMEM64;          /* NetBSD, OpenBSD. --------- */
 #endif
-	int64_t size = 0;               /* 64-bit */
-	size_t len = sizeof( size );
-	if ( sysctl( mib, 2, &size, &len, NULL, 0 ) == 0 )
-		return (size_t)size;
-	return 0L;			/* Failed? */
+  int64_t size = 0;               /* 64-bit */
+  size_t len = sizeof( size );
+  if ( sysctl( mib, 2, &size, &len, NULL, 0 ) == 0 )
+    return (size_t)size;
+  return 0L;      /* Failed? */
 
 #elif defined(_SC_AIX_REALMEM)
-	/* AIX. ----------------------------------------------------- */
-	return (size_t)sysconf( _SC_AIX_REALMEM ) * (size_t)1024L;
+  /* AIX. ----------------------------------------------------- */
+  return (size_t)sysconf( _SC_AIX_REALMEM ) * (size_t)1024L;
 
 #elif defined(_SC_PHYS_PAGES) && defined(_SC_PAGESIZE)
-	/* FreeBSD, Linux, OpenBSD, and Solaris. -------------------- */
-	return (size_t)sysconf( _SC_PHYS_PAGES ) *
-		(size_t)sysconf( _SC_PAGESIZE );
+  /* FreeBSD, Linux, OpenBSD, and Solaris. -------------------- */
+  return (size_t)sysconf( _SC_PHYS_PAGES ) *
+    (size_t)sysconf( _SC_PAGESIZE );
 
 #elif defined(_SC_PHYS_PAGES) && defined(_SC_PAGE_SIZE)
-	/* Legacy. -------------------------------------------------- */
-	return (size_t)sysconf( _SC_PHYS_PAGES ) *
-		(size_t)sysconf( _SC_PAGE_SIZE );
+  /* Legacy. -------------------------------------------------- */
+  return (size_t)sysconf( _SC_PHYS_PAGES ) *
+    (size_t)sysconf( _SC_PAGE_SIZE );
 
 #elif defined(CTL_HW) && (defined(HW_PHYSMEM) || defined(HW_REALMEM))
-	/* DragonFly BSD, FreeBSD, NetBSD, OpenBSD, and OSX. -------- */
-	int mib[2];
-	mib[0] = CTL_HW;
+  /* DragonFly BSD, FreeBSD, NetBSD, OpenBSD, and OSX. -------- */
+  int mib[2];
+  mib[0] = CTL_HW;
 #if defined(HW_REALMEM)
-	mib[1] = HW_REALMEM;		/* FreeBSD. ----------------- */
+  mib[1] = HW_REALMEM;    /* FreeBSD. ----------------- */
 #elif defined(HW_PYSMEM)
-	mib[1] = HW_PHYSMEM;		/* Others. ------------------ */
+  mib[1] = HW_PHYSMEM;    /* Others. ------------------ */
 #endif
-	unsigned int size = 0;		/* 32-bit */
-	size_t len = sizeof( size );
-	if ( sysctl( mib, 2, &size, &len, NULL, 0 ) == 0 )
-		return (size_t)size;
-	return 0L;			/* Failed? */
+  unsigned int size = 0;    /* 32-bit */
+  size_t len = sizeof( size );
+  if ( sysctl( mib, 2, &size, &len, NULL, 0 ) == 0 )
+    return (size_t)size;
+  return 0L;      /* Failed? */
 #endif /* sysctl and sysconf variants */
 
 #else
-	return 0L;			/* Unknown OS. */
+  return 0L;      /* Unknown OS. */
 #endif
 }
 
