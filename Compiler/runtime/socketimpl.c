024e3ea026e3bbadc5f19b6394cce2bf00db5cd7
Adrian Pop <adrian.pop@liu.se>
2013-09-03 07:26:41 +0000

    - there is no fsync for MinGW/Windows, use FlushFileBuffers
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@17039 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/runtime/socketimpl.c
+++ Compiler/runtime/socketimpl.c
@@ -34,6 +34,46 @@
 #if defined(__MINGW32__) || defined(_MSC_VER)
 #include <winsock2.h>
 #include <ws2tcpip.h>
+
+#include <unistd.h>
+#include <errno.h>
+#include <io.h>
+
+int
+fsync (int fd)
+{
+  HANDLE h = (HANDLE) _get_osfhandle (fd);
+  DWORD err;
+
+  if (h == INVALID_HANDLE_VALUE)
+    {
+      errno = EBADF;
+      return -1;
+    }
+
+  if (!FlushFileBuffers (h))
+    {
+      /* Translate some Windows errors into rough approximations of Unix
+       * errors.  MSDN is useless as usual - in this case it doesn't
+       * document the full range of errors.
+       */
+      err = GetLastError ();
+      switch (err)
+       {
+         /* eg. Trying to fsync a tty. */
+       case ERROR_INVALID_HANDLE:
+         errno = EINVAL;
+         break;
+
+       default:
+         errno = EIO;
+       }
+      return -1;
+    }
+
+  return 0;
+}
+
 #else
 #include <sys/socket.h>
 #include <netinet/in.h>
