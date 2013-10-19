059cb5a798dac31ad8ce1fe1b482df0b350e4dff
Martin Sjölund <martin.sjolund@liu.se>
2013-10-19 12:28:46 +0000

    Support checking csv format in the library test
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@17766 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Examples/LibraryTest.sh
+++ Examples/LibraryTest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 # Script to generate detailed test reports in a given directory, with history in another
 
-if test ! "$#" = 7 -a ! "$#" = 6; then
-  echo "Usage: $0 omhome workdir libraries_dir libdirname library_name library_version [path/to/referenceFiles]"
+if ! test "$#" = 6 -o "$#" = 7 -o "$#" = 8 -o "$#" = 9; then
+  echo "Usage: $0 omhome workdir libraries_dir libdirname library_name library_version [path/to/referenceFiles] [mat] [.]"
   echo "Example: $0 /path/to/build/ OpenModelica/BuildModelTest/MSL_3.2.1 /var/www/libraries/ MSL_3.2.1 Modelica 3.2.1 /path/to/trunk/testsuite/simulation/libraries/msl32/ReferenceFiles"
   exit 1
 fi
@@ -14,7 +14,16 @@ LIB_DIR="$4"
 LIB_NAME="$5"
 LIB_VERSION="$6"
 REF_FILES="$7"
-
+if test -z "$8"; then
+  REF_EXT="mat"
+else
+  REF_EXT="$8"
+fi
+if test -z "$9"; then
+  REF_NAME_DELIMITER="."
+else
+  REF_NAME_DELIMITER="$9"
+fi
 TESTMODELS="$OMHOME/share/doc/omc/testmodels/"
 HISTORY="$WWW/history"
 
@@ -24,7 +33,12 @@ rm -rf "$WORKDIR"
 mkdir -p "$WORKDIR" "$WWW/$LIB_DIR" "$HISTORY"
 cd "$WORKDIR"
 
-sed "s/^libraryVersion:=\"default\";/libraryVersion:=\"$LIB_VERSION\";/" "$TESTMODELS/BuildModelRecursive.mos" | sed "s/library:=.*/library:=\$TypeName($LIB_NAME);/" | sed "s,referenceFiles:=.*,referenceFiles:=\"$REF_FILES\";," > BuildModelRecursive.mos
+sed "s/^libraryVersion:=\"default\";/libraryVersion:=\"$LIB_VERSION\";/" "$TESTMODELS/BuildModelRecursive.mos" | \
+sed "s/library:=.*/library:=\$TypeName($LIB_NAME);/" | \
+sed "s,referenceFiles:=.*,referenceFiles:=\"$REF_FILES\";," \
+sed "s,referenceFileExtension:=.*,referenceFileExtension:=\"$REF_EXT\";," \
+sed "s,referenceFileNameDelimiter:=.*,referenceFileNameDelimiter:=\"$REF_NAME_DELIMITER\";," \
+> BuildModelRecursive.mos
 "$OMHOME/bin/omc" +g=MetaModelica BuildModelRecursive.mos
 
 shopt -s nullglob
