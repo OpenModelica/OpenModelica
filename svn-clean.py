67a40b3dd460956eb7600db98418d6224f2a614f
Martin Sjölund <martin.sjolund@liu.se>
2013-11-19 15:09:39 +0000

    Adding script to clean trunk (like svn-clean, but more platform-independent)
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@18195 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ svn-clean.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# http://stackoverflow.com/questions/239340/automatically-remove-subversion-unversioned-files
+
+import os
+import re
+
+def removeall(path):
+    if not os.path.isdir(path):
+        os.remove(path)
+        return
+    files=os.listdir(path)
+    for x in files:
+        fullpath=os.path.join(path, x)
+        if os.path.isdir(fullpath):
+            removeall(fullpath)
+        else:
+            os.remove(fullpath)
+    os.rmdir(path)
+
+unversionedRex = re.compile('^ ?[\?ID] *[1-9 ]*[a-zA-Z]* +(.*)')
+for l in  os.popen('svn status --no-ignore -v').readlines():
+    match = unversionedRex.match(l)
+    if match: removeall(match.group(1))
