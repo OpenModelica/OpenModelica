b16c2dc8a8aeb07675d1bf671846f2cfc6acd958
Martin Sjölund <martin.sjolund@liu.se>
2013-11-27 19:56:07 +0000

    - Double fixes for f2c.h problems on OSX; always use our f2c.h
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@18327 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- SimulationRuntime/c/simulation_data.h
+++ SimulationRuntime/c/simulation_data.h
@@ -40,7 +40,13 @@
 #include "openmodelica.h"
 #include "ringbuffer.h"
 #include "omc_error.h"
+#if defined(__cplusplus)
+typedef void* f2c_integer_ptr;
+#else
+/* f2c.h adds stuff C++ does not like at all */
 #include "f2c.h"
+typedef integer* f2c_integer_ptr;
+#endif
 
 #define omc_dummyFileInfo {"",-1,-1,-1,-1,1}
 #define omc_dummyVarInfo {-1,"","",omc_dummyFileInfo}
@@ -258,7 +264,7 @@ typedef struct NONLINEAR_SYSTEM_DATA
   int (*initialAnalyticalJacobian)(void*);
   modelica_integer jacobianIndex;
 
-  void (*residualFunc)(void*, double*, double*, integer*);
+  void (*residualFunc)(void*, double*, double*, f2c_integer_ptr);
   void (*initializeStaticNLSData)(void*, void*);
 
   void *solverData;
