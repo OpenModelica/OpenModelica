6cb7e305017275fe7322f273159d74a74bb2839d
Vitalij Ruge <vitalij.ruge@fh-bielefeld.de>
2014-01-28 09:35:26 +0000

    - free memory for analyticJacobians structure
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@18817 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- SimulationRuntime/c/optimization/initialOptimizer/allocate_ipopt.c
+++ SimulationRuntime/c/optimization/initialOptimizer/allocate_ipopt.c
@@ -258,6 +258,18 @@ int freeIpoptData(IPOPT_DATA_ *iData)
   free(iData->rhs);
   free(iData->sv);
   free(iData->sh);
+
+  /*for(i = 0; i<3;++i);{
+	if(iData-data->simulationInfo.analyticJacobians[i].seedVars){
+      free(iData-data->simulationInfo.analyticJacobians[i].seedVars);
+      free(iData-data->simulationInfo.analyticJacobians[i].resultVars);
+      free(iData-data->simulationInfo.analyticJacobians[i].tmpVars);
+      free()
+	}
+  }
+  */
+
+
   free(iData);
   iData = NULL;
   return 0;
