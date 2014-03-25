e53b7a0394c4bb84cd45c826c7ebfbfad4a6b18e
Martin Sjölund <martin.sjolund@liu.se>
2014-03-25 21:23:43 +0000

    Adding attribute noreturn to relevant ModelicaUtilities functions
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@19758 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- SimulationRuntime/c/ModelicaUtilities.h
+++ SimulationRuntime/c/ModelicaUtilities.h
@@ -3,18 +3,21 @@
 
 #include <stddef.h>
 #include <stdarg.h>
+#include "util/omc_msvc.h" /* for __attribute__() */
 /*
  * Utility functions for external functions.
  * The functionality is defined in the Modelica 2.x and 3.x specifications.
+ *
+ * The noreturn attribute has been added for clarity
  */
 
 extern void ModelicaMessage(const char* string);
 extern void ModelicaFormatMessage(const char* string, ...);
-extern void ModelicaError(const char* string);
-extern void ModelicaFormatError(const char* string, ...);
+extern void ModelicaError(const char* string) __attribute__ ((noreturn));
+extern void ModelicaFormatError(const char* string, ...) __attribute__ ((noreturn));
 extern char* ModelicaAllocateString(size_t len);
 extern char* ModelicaAllocateStringWithErrorReturn(size_t len);
 extern void ModelicaVFormatMessage(const char*string, va_list);
-extern void ModelicaVFormatError(const char*string, va_list);
+extern void ModelicaVFormatError(const char*string, va_list) __attribute__ ((noreturn));
 
 #endif /* MODELICA_UTILITIES_H */
