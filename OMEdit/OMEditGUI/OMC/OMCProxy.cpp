c26df13aa45ed95450bf0da4128a0131ce0526df
Adeel Asghar <adeel.asghar@liu.se>
2014-06-03 19:40:18 +0000

    #2509 - increase argc as well.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@20948 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMEdit/OMEditGUI/OMC/OMCProxy.cpp
+++ OMEdit/OMEditGUI/OMC/OMCProxy.cpp
@@ -353,8 +353,8 @@ bool OMCProxy::startServer()
       }
     }
     // ORB initialization.
-    int argc = 2;
-    static const char *argv[] = {"-NoResolve", "-IIOPAddr", "inet:127.0.0.1:0", "-ORBgiopMaxMsgSize", "2147483647"};
+    int argc = 6;
+    static const char *argv[] = {"OMEdit", "-NoResolve", "-IIOPAddr", "inet:127.0.0.1:0", "-ORBgiopMaxMsgSize", "2147483647"};
     CORBA::ORB_var orb = CORBA::ORB_init(argc, (char **)argv);
     objectRefFile.open(QIODevice::ReadOnly);
     char buf[1024];
