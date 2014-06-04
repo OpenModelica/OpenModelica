a0c0fa57125bf318eaa5f91c75f080fd5cb32559
Adeel Asghar <adeel.asghar@liu.se>
2014-06-04 00:28:07 +0000

    #2509 - Use the omniORB specific parameters. They are different from MICO parameters.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@20949 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMShell/OMShellGUI/omc_communicator.cpp
+++ OMShell/OMShellGUI/omc_communicator.cpp
@@ -97,8 +97,8 @@ bool OmcCommunicator::establishConnection()
   }
 
   // ORB initialization.
-  int argc = 6;
-  static const char *argv[] = { "OMShell", "-NoResolve", "-IIOPAddr", "inet:127.0.0.1:0", "-ORBgiopMaxMsgSize", "10485760" };
+  int argc = 2;
+  static const char *argv[] = {"-ORBgiopMaxMsgSize", "2147483647"};
   CORBA::ORB_var orb = CORBA::ORB_init(argc, (char **)argv);
 
   QFile objectRefFile;
