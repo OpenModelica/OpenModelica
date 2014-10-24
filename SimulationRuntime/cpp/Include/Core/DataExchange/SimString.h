fb4672a5968c1aa3ce20d57b6f61ea825e49b05b
Ruediger Kampfmann <fixed-term.Ruediger.Kampfmann@boschrexroth.de>
2014-10-24 14:08:09 +0000

    -added for vxworks
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@22916 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ SimulationRuntime/cpp/Include/Core/DataExchange/SimString.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "ISimVar.h"
+#include <string>
+using std::string;
+/**
+SimVar Klasse zum verwalten einer String Variable 
+*/
+class SimString : public ISimVar
+{
+
+public:
+	
+	virtual ~SimString()	{};
+	SimString(string value) {_value = value;}
+	virtual string getName() {return _name;}
+	virtual void setName(string name) {_name = name;}
+	string& getValue() { return _value;}
+private:
+	string _name;
+	string _value;
+};
