47134f35ec415ca06e163a58b09575f7d9d1efa8
Willi Braun <wbraun@fh-bielefeld.de>
2014-11-25 14:54:24 +0000

    - update help text
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@23555 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- SimulationRuntime/c/util/simulation_options.c
+++ SimulationRuntime/c/util/simulation_options.c
@@ -91,7 +91,7 @@ const char *FLAG_DESC[FLAG_MAX+1] = {
   /* FLAG_ALARM */                 "aborts after the given number of seconds (0 disables)",
   /* FLAG_CLOCK */                 "selects the type of clock to use -clock=RT, -clock=CYC or -clock=CPU",
   /* FLAG_CPU */                   "dumps the cpu-time into the results-file",
-  /* FLAG_DASSL_JACOBIAN */        "selects the type of the jacobians that is used for the dassl solver.\n  dasslJacobain=[coloredNumericalJacobian|numericalJacobian|internalNumericalJacobian|coloredSymbolicalJacobian|symbolicalJacobian].",
+  /* FLAG_DASSL_JACOBIAN */        "selects the type of the jacobians that is used for the dassl solver.\n  dasslJacobain=[coloredNumerical (default) |numerical|internalNumerical|coloredSymbolical|symbolical].",
   /* FLAG_DASSL_NO_ROOTFINDING */  "flag deactivates the internal root finding procedure of dassl.",
   /* FLAG_DASSL_NO_RESTART */      "flag deactivates the restart of dassl after an event is performed.",
   /* FLAG_EMIT_PROTECTED */        "emits protected variables to the result-file",
@@ -146,7 +146,7 @@ const char *FLAG_DETAILED_DESC[FLAG_MAX+1] = {
   /* FLAG_CLOCK */                 "selects the type of clock to use -clock=RT, -clock=CYC or -clock=CPU\n  RT=monotonic real-time clock, CPU=process-based CPU-time, CYC=cpu cycles measured with RDTSC",
   /* FLAG_CPU */                   "  - dumps the cpu-time into the result-file\n  - $cpuTime is the variable name inside the result-file",
   /* FLAG_DASSL_JACOBIAN */        "selects the type of the jacobians that is used for the dassl solver:"
-      "\n\t -dasslJacobian=coloredNumerical  - colored numerical jacobian."
+      "\n\t -dasslJacobian=coloredNumerical  - colored numerical jacobian, also default setup."
       "\n\t -dasslJacobian=internalNumerical  - internal dassl numerical jacobian."
       "\n\t -dasslJacobian=coloredSymbolical - colored symbolical jacobian. Only usable if the simulation is compiled with +generateSymbolicJacobian or +generateSymbolicLinearization."
       "\n\t -dasslJacobian=numerical - numerical jacobian"
