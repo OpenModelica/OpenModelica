2fb5d6b15167a745dde3ba94277f0529293d2e71
Adrian Pop <adrian.pop@liu.se>
2015-03-17 11:28:26 +0000

    - increase version to 1.9.3 nightly
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@25121 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/runtime/omc_config.h
+++ Compiler/runtime/omc_config.h
@@ -111,9 +111,9 @@
 #endif
 
 #ifdef CONFIG_REVISION
-#define CONFIG_VERSION "1.9.2 (r" CONFIG_REVISION ")"
+#define CONFIG_VERSION "1.9.3+dev (r" CONFIG_REVISION ")"
 #else
-#define CONFIG_VERSION "1.9.2"
+#define CONFIG_VERSION "1.9.3+dev"
 #endif
 
 
