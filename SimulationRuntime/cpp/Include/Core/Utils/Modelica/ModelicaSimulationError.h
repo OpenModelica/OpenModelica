OpenModelica/OpenModelica

commit db98b830e287799ced93026559e482ffcc6e3d23

-adapted cpp runtime files for vxworks
git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@25335 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

SimulationRuntime/cpp/Include/Core/Utils/Modelica/ModelicaSimulationError.h
@@ -46,6 +46,11 @@ class ModelicaSimulationError : public std::runtime_error
     SIMULATION_ERROR _error_id;
 };
 
+ //Helper functions to convert the error id to a readable format
+#if defined (__vxworks) || defined (__TRICORE__)
+#else
+#define BOOST_EXTENSION_EXPORT_DECL 
+#endif
 //Helper functions to convert the error id to a readable format
 BOOST_EXTENSION_EXPORT_DECL string error_id_string(SIMULATION_ERROR id);
 //Helper functions to extend an error information for new additional string and time stamp

commit ca0f0dc615524b2a60aed48387c7c95de61230a4

- fix Windows builds broken since r25335. - make sure that BOOST_EXTENSION_EXPORT_DECL is not undefined for Windows! - force cmake show what is doing (verbose makefile).
git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@25344 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

SimulationRuntime/cpp/Include/Core/Utils/Modelica/ModelicaSimulationError.h
@@ -48,7 +48,7 @@ class ModelicaSimulationError : public std::runtime_error
 
  //Helper functions to convert the error id to a readable format
 #if defined (__vxworks) || defined (__TRICORE__)
-#else
+/* adrpo: undefine BOOST_EXTENSION_EXPORT_DECL for these targets */
 #define BOOST_EXTENSION_EXPORT_DECL
 #endif
 //Helper functions to convert the error id to a readable format