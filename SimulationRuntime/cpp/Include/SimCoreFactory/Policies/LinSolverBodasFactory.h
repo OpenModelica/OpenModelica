ff32e7685579aed21c38ac8ab6de41985863a08e
Nils Menager <Nils.Menager@boschrexroth.de>
2015-04-07 12:20:57 +0000

    added Policies in cpp runtime for BODAS
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@25414 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ SimulationRuntime/cpp/Include/SimCoreFactory/Policies/LinSolverBodasFactory.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <ObjectFactory.h>
+
+/*
+ Policy class to create lin solver object
+ */
+template<class CreationPolicy>
+struct LinSolverBodasFactory : public ObjectFactory<CreationPolicy> 
+{
+public:
+  LinSolverBodasFactory(PATH library_path, PATH modelicasystem_path, PATH config_path)
+      : ObjectFactory<CreationPolicy>(library_path, modelicasystem_path, config_path)
+      , _last_selected_solver("empty")
+  {
+  }
+  
+  virtual ~LinSolverBodasFactory()
+  {
+  }
+
+  virtual boost::shared_ptr<ILinSolverSettings> createLinSolverSettings(string lin_solver)
+  {
+    boost::shared_ptr<ILinSolverSettings> linsolversetting;
+    return linsolversetting;
+  }
+
+  virtual boost::shared_ptr<IAlgLoopSolver> createLinSolver(IAlgLoop* algLoop, string solver_name, boost::shared_ptr<ILinSolverSettings> solver_settings)
+  {
+    boost::shared_ptr<IAlgLoopSolver> solver;
+    return solver;
+  }
+   
+protected:
+  string _last_selected_solver;
+};
---
+++ SimulationRuntime/cpp/Include/SimCoreFactory/Policies/SystemBodasFactory.h
@@ -0,0 +1,44 @@
+#pragma once
+/*
+Policy class to create a Simster-,  Modelica- system or AlgLoopSolver
+*/
+template <class CreationPolicy>
+struct SystemBodasFactory : public ObjectFactory<CreationPolicy>
+{
+public:
+  SystemBodasFactory(PATH library_path, PATH modelicasystem_path, PATH config_path)
+      : ObjectFactory<CreationPolicy>(library_path, modelicasystem_path, config_path)
+  {
+    _use_modelica_compiler = false;
+  }
+
+  ~SystemBodasFactory()
+  {
+  }
+
+  boost::shared_ptr<ISimData> createSimData()
+  {
+    boost::shared_ptr<ISimData> simData = ObjectFactory<CreationPolicy>::_factory->LoadSimData();
+    return simData;
+  }
+
+  boost::shared_ptr<IAlgLoopSolverFactory> createAlgLoopSolverFactory(IGlobalSettings* globalSettings)
+  {
+    boost::shared_ptr<IAlgLoopSolverFactory> algloopsolverfactory = ObjectFactory<CreationPolicy>::_factory->LoadAlgLoopSolverFactory(globalSettings);
+    return algloopsolverfactory;
+  }
+
+  boost::shared_ptr<IMixedSystem> createSystem(string modelLib, string modelKey, IGlobalSettings* globalSettings, boost::shared_ptr<IAlgLoopSolverFactory> algloopsolverfactory, boost::shared_ptr<ISimData> simData)
+  {
+    boost::shared_ptr<IMixedSystem> system = ObjectFactory<CreationPolicy>::_factory->LoadSystem(globalSettings, algloopsolverfactory, simData);
+    return system;
+  }
+
+  boost::shared_ptr<IMixedSystem> createModelicaSystem(string modelLib, string modelKey, IGlobalSettings* globalSettings, boost::shared_ptr<IAlgLoopSolverFactory> algloopsolverfactory, boost::shared_ptr<ISimData> simData)
+  {
+    boost::shared_ptr<IMixedSystem> system = ObjectFactory<CreationPolicy>::_factory->LoadSystem(globalSettings, algloopsolverfactory, simData);
+    return system;
+  }
+
+  bool _use_modelica_compiler;
+};
