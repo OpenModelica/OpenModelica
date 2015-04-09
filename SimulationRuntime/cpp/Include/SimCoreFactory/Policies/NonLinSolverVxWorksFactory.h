e296a44cadbc50928c0b45cc41d25e8d3733d0e1
Ruediger Kampfmann <fixed-term.Ruediger.Kampfmann@boschrexroth.de>
2015-04-09 09:24:48 +0000

    -added files for vxWorks
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@25458 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ SimulationRuntime/cpp/Include/SimCoreFactory/Policies/NonLinSolverVxWorksFactory.h
@@ -0,0 +1,61 @@
+#pragma once
+
+/*
+Policy class to create nonlin solver object
+*/
+template <class CreationPolicy> 
+struct NonLinSolverVxWorksFactory : public  ObjectFactory<CreationPolicy>
+{
+
+public:
+    NonLinSolverVxWorksFactory(PATH library_path,PATH modelicasystem_path,PATH config_path)
+        :ObjectFactory<CreationPolicy>(library_path,modelicasystem_path,config_path)
+        ,_last_selected_solver("empty")
+    {
+
+
+    }
+    ~NonLinSolverVxWorksFactory()
+    {
+    }
+
+   boost::shared_ptr<INonLinSolverSettings> createNonLinSolverSettings(string solver_name)
+   {
+       string nonlin_solver_key;
+        string nonlin_solver;
+        if(solver_name.compare("Newton")==0)
+            nonlin_solver_key.assign("createNewtonSettings");
+        else if(solver_name.compare("Kinsol")==0)
+            nonlin_solver_key.assign("createKinsolSettings");
+        else if(solver_name.compare("Hybrj")==0)
+            nonlin_solver_key.assign("extension_export_hybrj");
+        else
+            throw std::invalid_argument("Selected nonlinear solver is not available");
+        _last_selected_solver =  solver_name;
+        boost::shared_ptr<INonLinSolverSettings> nonlinsolversetting  = ObjectFactory<CreationPolicy>::_factory->LoadAlgLoopSolverSettings(nonlin_solver_key);
+         return nonlinsolversetting;
+
+   }
+
+   boost::shared_ptr<IAlgLoopSolver> createNonLinSolver(IAlgLoop* algLoop, string solver_name, boost::shared_ptr<INonLinSolverSettings>  solver_settings)
+   {
+       if(_last_selected_solver.compare(solver_name)==0)
+       {
+           string nonlin_solver_key;
+           if(solver_name.compare("Newton")==0)
+               nonlin_solver_key.assign("createNewton");
+           else if(solver_name.compare("Kinsol")==0)
+               nonlin_solver_key.assign("createKinsol");
+           else if(solver_name.compare("Hybrj")==0)
+               nonlin_solver_key.assign("extension_export_hybrj");
+           else
+               throw std::invalid_argument("Selected nonlinear solver is not available");
+           boost::shared_ptr<IAlgLoopSolver> nonlinsolver = ObjectFactory<CreationPolicy>::_factory->LoadAlgLoopSolver(algLoop, nonlin_solver_key, solver_settings);
+           return nonlinsolver;
+       }
+       else
+           throw std::invalid_argument("Selected nonlinear solver is not available");
+   }
+
+     string _last_selected_solver;
+};
---
+++ SimulationRuntime/cpp/Include/SimCoreFactory/Policies/SystemVxWorksFactory.h
@@ -0,0 +1,48 @@
+#pragma once
+/*
+Policy class to create a Simster-,  Modelica- system or AlgLoopSolver
+*/
+template <class CreationPolicy>
+struct SystemVxWorksFactory : public  ObjectFactory<CreationPolicy>
+{
+
+public:
+    SystemVxWorksFactory(PATH library_path,PATH modelicasystem_path,PATH config_path)
+        :ObjectFactory<CreationPolicy>(library_path,modelicasystem_path,config_path)
+    {
+        _use_modelica_compiler = false;      
+    }
+
+    ~SystemVxWorksFactory()
+    {
+    }
+
+    boost::shared_ptr<IAlgLoopSolverFactory> createAlgLoopSolverFactory(IGlobalSettings* globalSettings)
+    {
+
+        boost::shared_ptr<IAlgLoopSolverFactory>  algloopsolverfactory = ObjectFactory<CreationPolicy>::_factory->LoadAlgLoopSolverFactory(globalSettings);
+        return algloopsolverfactory;
+    }
+
+    boost::shared_ptr<ISimData> createSimData()
+    {
+
+      boost::shared_ptr<ISimData> simData = ObjectFactory<CreationPolicy>::_factory->LoadSimData();
+      return simData;
+
+    }
+    
+    boost::shared_ptr<IMixedSystem> createSystem(string modelLib,string modelKey,IGlobalSettings* globalSettings,boost::shared_ptr<IAlgLoopSolverFactory> algloopsolverfactory,boost::shared_ptr<ISimData> simData)
+    {
+        boost::shared_ptr<IMixedSystem> system = ObjectFactory<CreationPolicy>::_factory->LoadSystem(globalSettings,algloopsolverfactory, simData);
+         
+        return system;
+    }
+
+    boost::shared_ptr<IMixedSystem>  createModelicaSystem(PATH modelica_path, string modelKey, IGlobalSettings* globalSettings, boost::shared_ptr<IAlgLoopSolverFactory> algloopsolverfactory,boost::shared_ptr<ISimData> simData)
+     {
+       throw ModelicaSimulationError(MODEL_FACTORY,"Modelica is not supported");
+     }
+
+    bool _use_modelica_compiler;
+};
