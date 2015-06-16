eb988c469e3a075685ce0a3bd669bf0a1f74af83
Martin Sjölund <martin.sjolund@liu.se>
2015-06-16 09:36:01 +0200

    Update OMHOME paths for the new location

--- UsersGuide/source/conf.py
+++ UsersGuide/source/conf.py
@@ -16,6 +16,9 @@ import sys
 import os
 import shlex
 
+if not 'OPENMODELICAHOME' in os.environ:
+  os.environ['OPENMODELICAHOME'] = os.path.abspath('../../../build')
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
