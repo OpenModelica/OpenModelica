cbe21ee20aa98470c810559c0a5c6b18b8c304d2
Niklas Worschech <niklas.worschech@boschrexroth.de>
2015-07-30 13:00:29 +0200

    adapted umfpack solver of cpp runtime on new Matrix interfaces

--- SimulationRuntime/cpp/Core/System/AlgLoopSolverFactory.cpp
+++ SimulationRuntime/cpp/Core/System/AlgLoopSolverFactory.cpp
@@ -26,7 +26,7 @@ boost::shared_ptr<IAlgLoopSolver> AlgLoopSolverFactory::createAlgLoopSolver(IAlg
   if(algLoop->getDimReal() > 0)
   {
 
-	/*
+
     if(algLoop->isLinear())
     {
       try
@@ -44,7 +44,7 @@ boost::shared_ptr<IAlgLoopSolver> AlgLoopSolverFactory::createAlgLoopSolver(IAlg
       {
         //the linear solver was not found -> take the nonlinear solver
       }
-    }*/
+    }
 
     string nonlinsolver_name = _global_settings->getSelectedNonLinSolver();
     boost::shared_ptr<INonLinSolverSettings> algsolversetting= createNonLinSolverSettings(nonlinsolver_name);
