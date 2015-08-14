OpenModelica/OpenModelica

commit 88a5a6610abcceba22365b99807f72adb8cc36f1

adapt SimCode and Codegeneration

Compiler/Template/CodegenCpp.tpl
@@ -10715,7 +10715,7 @@ template equationWhen(SimEqSystem eq, Context context, Text &varDecls, SimCode s
 ::=
   let &varDeclsCref = buffer "" /*BUFD*/
   match eq
-     case SES_WHEN(left=left, right=right, conditions=conditions, elseWhen=NONE()) then
+     case SES_WHEN(whenStmtLst = whenStmtLst, conditions=conditions, elseWhen=NONE()) then
       let helpIf = (conditions |> e => ' || (<%cref1(e, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%> && !_discrete_events->pre(<%cref1(e, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%>))')
 
         /*let initial_assign =
@@ -10723,8 +10723,8 @@ template equationWhen(SimEqSystem eq, Context context, Text &varDecls, SimCode s
           whenAssign(left, typeof(right), right, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
         else
            '<%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%> = _discrete_events->pre(<%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%>);'*/
-      let assign = whenAssign(left,typeof(right),right,context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
-      let pre_call = preCall(left, typeof(right), right, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+      let body = whenOperators(whenStmtLst, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+      let pre_call = preCall(whenStmtLst, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
       <<
       if(_initial)
       {
@@ -10732,23 +10732,23 @@ template equationWhen(SimEqSystem eq, Context context, Text &varDecls, SimCode s
       }
       else if (0<%helpIf%>)
       {
-        <%assign%>;
+        <%body%>;
       }
       else
       {
             <%pre_call%>
        }
       >>
-    case SES_WHEN(left=left, right=right, conditions=conditions, elseWhen=SOME(elseWhenEq)) then
+    case SES_WHEN(whenStmtLst = whenStmtLst, conditions=conditions, elseWhen=SOME(elseWhenEq)) then
        let helpIf = (conditions |> e => ' || (<%cref1(e, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%> && !_discrete_events->pre(<%cref1(e, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%>))')
       let initial_assign =
         if initialCall then
-          whenAssign(left,typeof(right),right,context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+          whenOperators(whenStmtLst, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
         else
-         '<%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%> = _discrete_events->pre(<%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%>);'
-      let assign = whenAssign(left, typeof(right), right, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+          preCall(whenStmtLst, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+      let body = whenOperators(whenStmtLst, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
       let elseWhen = equationElseWhen(elseWhenEq, context, varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
-
+      let pre_call = preCall(whenStmtLst, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
       <<
       if(_initial)
       {
@@ -10756,23 +10756,25 @@ template equationWhen(SimEqSystem eq, Context context, Text &varDecls, SimCode s
       }
       else if(0<%helpIf%>)
       {
-        <%assign%>
+        <%body%>
       }
       <%elseWhen%>
       else
       {
-
-         <%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%> = _discrete_events->pre(<%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%>);
+       <%pre_call%>
       }
       >>
 end equationWhen;
 
 
-template preCall(ComponentRef left, Type ty, Exp right, Context context, Text &varDecls, SimCode simCode, Text& extraFuncs, Text& extraFuncsDecl,
+template preCall(list<WhenOperator> whenOps, Context context, Text &varDecls, SimCode simCode, Text& extraFuncs, Text& extraFuncsDecl,
                     Text extraFuncsNamespace, Text stateDerVectorName /*=__zDot*/, Boolean useFlatArrayNotation)
  "Generates assignment for when."
 ::=
-match ty
+  let body = (whenOps |> whenOp =>
+    match whenOp
+      case e as ASSIGN(__) then
+match typeof(e.right)
   case T_ARRAY(dims=dims) then
    let dimensions = checkDimension(dims)
    let i_tmp_var= System.tmpTick()
@@ -10780,12 +10782,12 @@ match ty
    let forloop = match listLength(dims) case 1 then
    <<
     <%forLoopIteration%>
-     <%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%>(i0_<%i_tmp_var%>) = _discrete_events->pre(<%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%>(i0_<%i_tmp_var%>));
+     <%cref1(e.left, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%>(i0_<%i_tmp_var%>) = _discrete_events->pre(<%cref1(e.left, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%>(i0_<%i_tmp_var%>));
    >>
    case 2 then
    <<
      <%forLoopIteration%>
-        <%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%>(i0_<%i_tmp_var%>,i1_<%i_tmp_var%>) = _discrete_events->pre(<%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%>(i0_<%i_tmp_var%>,i1_<%i_tmp_var%>));
+        <%cref1(e.left, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%>(i0_<%i_tmp_var%>,i1_<%i_tmp_var%>) = _discrete_events->pre(<%cref1(e.left, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%>(i0_<%i_tmp_var%>,i1_<%i_tmp_var%>));
    >>
    else
     error(sourceInfo(), 'No support for this sort of pre call')
@@ -10793,8 +10795,15 @@ match ty
    forloop
    else
    <<
-    <%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%> = _discrete_events->pre(<%cref1(left,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%>);
+    <%cref1(e.left, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%> = _discrete_events->pre(<%cref1(e.left, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDecls, stateDerVectorName, useFlatArrayNotation)%>);
    >>
+end match
+   else
+    error(sourceInfo(), 'No support for this sort of pre call')
+;separator="\n")
+<<
+  <%body%>
+>>
 end preCall;
 
 template preCallForArray(Dimensions dims,String tmp)
@@ -10827,27 +10836,64 @@ template equationElseWhen(SimEqSystem eq, Context context, Text &varDecls, SimCo
 ::=
 let &varDeclsCref = buffer "" /*BUFD*/
 match eq
-case SES_WHEN(left=left, right=right, conditions=conditions, elseWhen=NONE()) then
+case SES_WHEN(whenStmtLst = whenStmtLst, conditions=conditions, elseWhen=NONE()) then
   let helpIf =  (conditions |> e => ' || (<%cref1(e, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%> && !_discrete_events->pre(<%cref1(e, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%>))')
-  let assign = whenAssign(left, typeof(right), right, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+  let body = whenOperators(whenStmtLst, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
   <<
   else if(0<%helpIf%>)
   {
-    <%assign%>
+    <%body%>
   }
   >>
-case SES_WHEN(left=left, right=right, conditions=conditions, elseWhen=SOME(elseWhenEq)) then
+case SES_WHEN(whenStmtLst = whenStmtLst, conditions=conditions, elseWhen=SOME(elseWhenEq)) then
   let helpIf = (conditions |> e => ' || (<%cref1(e, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%> && !_discrete_events->pre(<%cref1(e, simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, context, varDeclsCref, stateDerVectorName, useFlatArrayNotation)%>))')
-  let assign = whenAssign(left, typeof(right), right, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+  let body = whenOperators(whenStmtLst, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
   let elseWhen = equationElseWhen(elseWhenEq, context, varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
   <<
   else if(0<%helpIf%>)
   {
-    <%assign%>
+    <%body%>
   }
   <%elseWhen%>
   >>
 end equationElseWhen;
+
+template whenOperators(list<WhenOperator> whenOps, Context context, Text &varDecls /*BUFP*/, SimCode simCode ,Text& extraFuncs,Text& extraFuncsDecl,Text extraFuncsNamespace, Text stateDerVectorName /*=__zDot*/, Boolean useFlatArrayNotation)
+ "Generates re-init statement for when equation."
+::=
+  let body = (whenOps |> whenOp =>
+    match whenOp
+      case ASSIGN(__) then whenAssign(left, typeof(right), right, context, &varDecls, simCode, &extraFuncs, &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+      case REINIT(__) then
+        let &preExp = buffer "" /*BUFD*/
+        let &varDeclsCref = buffer "" /*BUFD*/
+        let val = daeExp(value, contextSimulationDiscrete, &preExp /*BUFC*/, &varDecls /*BUFD*/,simCode , &extraFuncs , &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+        <<
+        state_var_reinitialized = true;
+        <%preExp%>
+        <%cref1(stateVar,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace,contextOther,varDeclsCref,stateDerVectorName,useFlatArrayNotation)%> = <%val%>;
+        >>
+      case TERMINATE(__) then
+        let &preExp = buffer "" /*BUFD*/
+        let msgVar = daeExp(message, contextSimulationDiscrete, &preExp /*BUFC*/, &varDecls /*BUFD*/,simCode , &extraFuncs , &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+        <<
+        <%preExp%>
+        MODELICA_TERMINATE(<%msgVar%>);
+        >>
+      case ASSERT(source=SOURCE(info=info)) then
+        assertCommon(condition, message, contextSimulationDiscrete, &varDecls, info,simCode , &extraFuncs , &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+      case NORETCALL(__) then
+      let &preExp = buffer ""
+      let expPart = daeExp(exp, contextSimulationDiscrete, &preExp, &varDecls,simCode , &extraFuncs , &extraFuncsDecl,  extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
+      <<
+      <%preExp%>
+      <% if isCIdentifier(expPart) then "" else '<%expPart%>;' %>
+      >>
+    ;separator="\n")
+  <<
+  <%body%>
+  >>
+end whenOperators;
 
 template helpvarvector(list<SimWhenClause> whenClauses,SimCode simCode ,Text& extraFuncs,Text& extraFuncsDecl,
                        Text extraFuncsNamespace, Text stateDerVectorName /*=__zDot*/, Boolean useFlatArrayNotation)

Compiler/BackEnd/BackendDAE.mo
@@ -398,6 +398,40 @@ uniontype WhenEquation
 end WhenEquation;
 
 public
+uniontype WhenOperator
+  record ASSIGN " left_cr = right_exp"
+    .DAE.ComponentRef left     "left hand side of equation";
+    .DAE.Exp right             "right hand side of equation";
+    .DAE.ElementSource source  "origin of equation";
+  end ASSIGN;
+
+  record REINIT "Reinit Statement"
+    .DAE.ComponentRef stateVar "State variable to reinit";
+    .DAE.Exp value             "Value after reinit";
+    .DAE.ElementSource source  "origin of equation";
+  end REINIT;
+
+  record ASSERT
+    .DAE.Exp condition;
+    .DAE.Exp message;
+    .DAE.Exp level;
+    .DAE.ElementSource source "the origin of the component/equation/algorithm";
+  end ASSERT;
+
+  record TERMINATE "The Modelica built-in terminate(msg)"
+    .DAE.Exp message;
+    .DAE.ElementSource source "the origin of the component/equation/algorithm";
+  end TERMINATE;
+
+  record NORETCALL "call with no return value, i.e. no equation.
+    Typically side effect call of external function but also
+    Connections.* i.e. Connections.root(...) functions."
+    .DAE.Exp exp;
+    .DAE.ElementSource source "the origin of the component/equation/algorithm";
+  end NORETCALL;
+end WhenOperator;
+
+public
 type ExternalObjectClasses = list<ExternalObjectClass>
 "classes of external objects stored in list";
 
@@ -553,34 +587,6 @@ uniontype EventInfo
     Integer numberMathEvents           "stores the number of math function that trigger events e.g. floor, ceil, integer, ...";
   end EVENT_INFO;
 end EventInfo;
-
-public
-uniontype WhenOperator
-  record REINIT "Reinit Statement"
-    .DAE.ComponentRef stateVar "State variable to reinit";
-    .DAE.Exp value             "Value after reinit";
-    .DAE.ElementSource source  "origin of equation";
-  end REINIT;
-
-  record ASSERT
-    .DAE.Exp condition;
-    .DAE.Exp message;
-    .DAE.Exp level;
-    .DAE.ElementSource source "the origin of the component/equation/algorithm";
-  end ASSERT;
-
-  record TERMINATE "The Modelica built-in terminate(msg)"
-    .DAE.Exp message;
-    .DAE.ElementSource source "the origin of the component/equation/algorithm";
-  end TERMINATE;
-
-  record NORETCALL "call with no return value, i.e. no equation.
-    Typically side effect call of external function but also
-    Connections.* i.e. Connections.root(...) functions."
-    .DAE.Exp exp;
-    .DAE.ElementSource source "the origin of the component/equation/algorithm";
-  end NORETCALL;
-end WhenOperator;
 
 public
 uniontype WhenClause

Compiler/BackEnd/HpcOmScheduler.mo
@@ -4131,6 +4131,7 @@ algorithm
       Option<SimCode.JacobianMatrix> jac;
       Boolean homotopySupport;
       Boolean mixedSystem;
+      list<BackendDAE.WhenOperator> whenStmtLst;
     case(SimCode.SES_RESIDUAL(index=idx,exp=exp,source=source),_)
       equation
         (exp,changed) = BackendVarTransform.replaceExp(exp,replIn,NONE());
@@ -4194,7 +4195,7 @@ algorithm
         changed = List.fold(bLst,boolOr,changed);
         simEqSys = SimCode.SES_MIXED(idx,simEqSys,simVars,simEqSysLst,idxMS);
     then (simEqSys,changed);
-    case(SimCode.SES_WHEN(index=idx,conditions=crefs,initialCall=ic,left=cref,right=exp,elseWhen=NONE(),source=source),_)
+    case(SimCode.SES_WHEN(index=idx,conditions=crefs,initialCall=ic,whenStmtLst={BackendDAE.ASSIGN(left=cref,right=exp)},elseWhen=NONE(),source=source),_)
       equation
         (crefExps,bLst) = List.map1_2(crefs,BackendVarTransform.replaceCref,replIn);
         crefs = List.map(crefExps,Expression.expCref);
@@ -4202,9 +4203,9 @@ algorithm
         changed = List.fold(bLst,boolOr,changed);
         (exp,changed1) = BackendVarTransform.replaceExp(exp,replIn,NONE());
         changed = boolOr(changed,changed1);
-        simEqSys = SimCode.SES_WHEN(idx,crefs,ic,cref,exp,NONE(),source);
+        simEqSys = SimCode.SES_WHEN(idx,crefs,ic,{BackendDAE.ASSIGN(cref, exp, source)},NONE(),source);
     then (simEqSys,changed);
-    case(SimCode.SES_WHEN(index=idx,conditions=crefs,initialCall=ic,left=cref,right=exp,elseWhen=SOME(simEqSys),source=source),_)
+    case(SimCode.SES_WHEN(index=idx,conditions=crefs,initialCall=ic,whenStmtLst={BackendDAE.ASSIGN(left=cref,right=exp)},elseWhen=SOME(simEqSys),source=source),_)
       equation
         (crefExps,bLst) = List.map1_2(crefs,BackendVarTransform.replaceCref,replIn);
         crefs = List.map(crefExps,Expression.expCref);
@@ -4214,7 +4215,7 @@ algorithm
         changed = boolOr(changed,changed1);
         (simEqSys,changed1) = replaceExpsInSimEqSystem(simEqSys,replIn);
         changed = boolOr(changed,changed1);
-        simEqSys = SimCode.SES_WHEN(idx,crefs,ic,cref,exp,SOME(simEqSys),source);
+        simEqSys = SimCode.SES_WHEN(idx,crefs,ic,{BackendDAE.ASSIGN(cref, exp, source)},SOME(simEqSys),source);
     then (simEqSys,changed);
   else
     equation

Compiler/SimCode/SerializeModelInfo.mo
@@ -429,6 +429,7 @@ algorithm
       list<SimCode.SimEqSystem> eqs,jeqs;
       SimCode.LinearSystem lSystem, atL;
       SimCode.NonlinearSystem nlSystem, atNL;
+      BackendDAE.WhenOperator whenOp;
 
     case SimCode.SES_RESIDUAL() equation
       File.write(file, "\n{\"eqIndex\":");
@@ -786,7 +787,7 @@ algorithm
       File.write(file, "]}");
     then true;
 
-    case SimCode.SES_WHEN() equation
+    case SimCode.SES_WHEN() algorithm
       File.write(file, "\n{\"eqIndex\":");
       File.write(file, intString(eq.index));
       if parent <> 0 then
@@ -795,14 +796,38 @@ algorithm
       end if;
       File.write(file, ",\"section\":\"");
       File.write(file, section);
-      File.write(file, "\",\"tag\":\"when\",\"defines\":[");
-      serializeCref(file,eq.left);
-      File.write(file, "],\"uses\":[");
-      serializeUses(file,List.union(eq.conditions,Expression.extractUniqueCrefsFromExp(eq.right)));
-      File.write(file, "],\"equation\":[");
-      serializeExp(file,eq.right);
-      File.write(file, "]}");
-      _ = match eq.elseWhen
+      for whenOps in eq.whenStmtLst loop
+        _ := match whenOps
+          case whenOp as BackendDAE.ASSIGN() equation
+            File.write(file, "\",\"tag\":\"when\",\"defines\":[");
+            serializeCref(file,whenOp.left);
+            File.write(file, "],\"uses\":[");
+            serializeUses(file,List.union(eq.conditions,Expression.extractUniqueCrefsFromExp(whenOp.right)));
+            File.write(file, "],\"equation\":[");
+            serializeExp(file,whenOp.right);
+            File.write(file, "]}");
+          then ();
+          case whenOp as BackendDAE.REINIT() equation
+            File.write(file, "\",\"tag\":\"when\",\"defines\":[");
+            serializeCref(file,whenOp.stateVar);
+            File.write(file, "],\"uses\":[");
+            serializeUses(file,List.union(eq.conditions,Expression.extractUniqueCrefsFromExp(whenOp.value)));
+            File.write(file, "],\"equation\":[");
+            serializeExp(file,whenOp.value);
+            File.write(file, "]}");
+          then ();
+          case whenOp as BackendDAE.ASSERT()
+            /* TODO: fix me */
+          then ();
+          case whenOp as BackendDAE.TERMINATE()
+            /* TODO: fix me */
+          then ();
+          case whenOp as BackendDAE.NORETCALL()
+            /* TODO: fix me */
+          then ();
+        end match;
+      end for;
+      _ := match eq.elseWhen
         local
           SimCode.SimEqSystem e;
         case SOME(e) equation if SimCodeUtil.simEqSystemIndex(e) <>0 then serializeEquation(file,e,section,withOperations); end if; then ();

Compiler/SimCode/SimCode.mo
@@ -436,8 +436,7 @@ uniontype SimEqSystem
     Integer index;
     list<DAE.ComponentRef> conditions "list of boolean variables as conditions";
     Boolean initialCall "true, if top-level branch with initial()";
-    DAE.ComponentRef left;
-    DAE.Exp right;
+    list<BackendDAE.WhenOperator> whenStmtLst;
     Option<SimEqSystem> elseWhen;
     DAE.ElementSource source;
   end SES_WHEN;

commit 2d797bbfe2aec40ce0c8004d2d788069decc0029

adapt Cpp terminate() to other runtimes
The previous implementation was broken after
commit 88a5a6610abcceba22365b99807f72adb8cc36f1.

Compiler/Template/CodegenCpp.tpl
@@ -12128,7 +12128,7 @@ template functionWhenReinitStatementThen(list<WhenOperator> reinits, Text &varDe
         let msgVar = daeExp(message, contextSimulationDiscrete, &preExp /*BUFC*/, &varDecls /*BUFD*/,simCode , &extraFuncs , &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)
         <<
         <%preExp%>
-        Terminate(<%msgVar%>);
+        MODELICA_TERMINATE(<%msgVar%>);
         >>
       case ASSERT(source=SOURCE(info=info)) then
         assertCommon(condition, message, contextSimulationDiscrete, &varDecls, info,simCode , &extraFuncs , &extraFuncsDecl, extraFuncsNamespace, stateDerVectorName, useFlatArrayNotation)

SimulationRuntime/cpp/Include/Core/System/SystemDefaultImplementation.h
@@ -36,6 +36,8 @@ for Open Modelica", September, 10 th, 2008
 Copyright (c) 2008, OSMC
 *****************************************************************************/
 
+#define MODELICA_TERMINATE(msg) Terminate(msg)
+
 //typedef boost::unordered_map<std::string, boost::any> SValuesMap;
 
 template <class T>