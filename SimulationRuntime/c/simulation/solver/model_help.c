4b5a985594adc21a845e99b6cd2ce635880bc2b8
Lennart Ochel <lennart.ochel@fh-bielefeld.de>
2015-10-09 11:03:22 +0200

    Remove useless line of code

--- SimulationRuntime/c/simulation/solver/model_help.c
+++ SimulationRuntime/c/simulation/solver/model_help.c
@@ -374,7 +374,6 @@ void printSparseStructure(DATA *data, int stream)
   for(row=0; row < data->simulationInfo.analyticJacobians[index].sizeRows; row++)
   {
     j=0;
-    buffer[0] = '\0';
     for(col=0; i < data->simulationInfo.analyticJacobians[index].sparsePattern.leadindex[row]; col++)
     {
       if(data->simulationInfo.analyticJacobians[index].sparsePattern.index[i] == col)
