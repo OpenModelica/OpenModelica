OpenModelica/OpenModelica

commit 2ed993a62a3d6d089a36e7247e7c1f38517bea8b

Allow generation of source-code FMUs
All generated (modelica-exchange) FMUs now include the source code of
the runtime, so that it is possible to compile an (almost) static
FMU.

By default, the runtime is still linked to the generated FMU, but
this can be changed by using -d=buildStaticSourceFMU.

The makefiles for the FMUs have been changed, such that we first
generate a source-code FMU, copying in all the files. Then a second
Makefile is used to compile the binary. Both make commands are
executed by default for translateModelFMU, but this API function will
only generate pure source-code FMUs in the future (buildModelFMU will
also compile the FMU, and there should be an API to compile
additional targets for an existing FMU).

FMUs no longer use garbage collection by default since some
simulators used libgc, and it just caused all sorts of headaches.

SimulationRuntime/c/util/omc_init.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of OpenModelica.
  *
- * Copyright (c) 1998-CurrentYear, Open Source Modelica Consortium (OSMC),
+ * Copyright (c) 1998-2014, Open Source Modelica Consortium (OSMC),
  * c/o Linköpings universitet, Department of Computer and Information Science,
  * SE-58183 Linköping, Sweden.
  *
@@ -28,29 +28,32 @@
  *
  */
 
-/*! \file events.h
- */
+#include <stdio.h>
+#include "omc_init.h"
 
-#ifndef _EVENTS_H_
-#define _EVENTS_H_
+pthread_key_t mmc_thread_data_key = 0;
 
-#include "simulation_data.h"
-#include "simulation/solver/solver_main.h"
-#include "util/list.h"
+pthread_once_t mmc_init_once = PTHREAD_ONCE_INIT;
 
-#ifdef __cplusplus
-extern "C" {
+void mmc_init_nogc()
+{
+  pthread_key_create(&mmc_thread_data_key,NULL);
+#if !defined(OMC_MINIMAL_RUNTIME)
+  /* Stack overflow detection is too expensive and fun for small targets
+   * C-code is usually not generated for stack overflow detection anyway... */
+  init_metamodelica_segv_handler();
 #endif
-
-void checkForSampleEvent(DATA *data, SOLVER_INFO* solverInfo);
-int checkEvents(DATA* data, threadData_t *threadData, LIST* eventLst, double *eventTime, SOLVER_INFO* solverInfo);
-
-void handleEvents(DATA* data, threadData_t *threadData, LIST* eventLst, double *eventTime, SOLVER_INFO* solverInfo);
-
-void findRoot(DATA *data, threadData_t *threadData, LIST *eventList, double*);
-
-#ifdef __cplusplus
 }
-#endif
 
+#if defined(OMC_MINIMAL_RUNTIME)
+void mmc_init(int withgc)
+{
+  fprintf(stderr, "Error: called mmc_init (requesting garbage collection) when OMC was compiled with a minimal runtime system.");
+  exit(1);
+}
+#else
+void mmc_init(int withgc)
+{
+  mmc_init_nogc();
+}
 #endif

Compiler/Script/CevalScript.mo
@@ -218,16 +218,20 @@ end isCompleteFunction;
 public function compileModel "Compiles a model given a file-prefix, helper function to buildModel."
   input String fileprefix;
   input list<String> libs;
+  input String workingDir = "";
+  input list<String> makeVars = {};
 protected
   String omhome = Settings.getInstallationDirectoryPath(),omhome_1 = System.stringReplace(omhome, "\"", "");
   String pd = System.pathDelimiter();
-  String libsfilename,libs_str,s_call,filename,winCompileMode;
+  String cdWorkingDir,setMakeVars,libsfilename,libs_str,s_call,filename,winCompileMode;
   String fileDLL = fileprefix + System.getDllExt(),fileEXE = fileprefix + System.getExeExt(),fileLOG = fileprefix + ".log";
   Integer numParallel,res;
   Boolean isWindows = System.os() == "Windows_NT";
+  list<String> makeVarsNoBinding;
 algorithm
   libsfilename := fileprefix + ".libs";
   libs_str := stringDelimitList(libs, " ");
+  makeVarsNoBinding := makeVars; // OMC is stupid and wants to constant evaluate inputs with bindings for iterator variables...
 
   System.writeFile(libsfilename, libs_str);
   if isWindows then
@@ -238,11 +242,15 @@ algorithm
     //        set OPENMODELICAHOME=DIR && actually adds the space between the DIR and &&
     //        to the environment variable! Don't ask me why, ask Microsoft.
     omhome := "set OPENMODELICAHOME=\"" + System.stringReplace(omhome_1, "/", "\\") + "\"&& ";
+    setMakeVars := sum("set "+var+"&& " for var in makeVarsNoBinding);
+    cdWorkingDir := if stringLength(workingDir) == 0 then "" else ("cd \"" + workingDir + "\"");
     winCompileMode := if Config.getRunningTestsuite() then "serial" else "parallel";
-    s_call := stringAppendList({omhome,"\"",omhome_1,pd,"share",pd,"omc",pd,"scripts",pd,"Compile","\""," ",fileprefix," ",Config.simulationCodeTarget()," ", winCompileMode});
+    s_call := stringAppendList({omhome,cdWorkingDir,setMakeVars,"\"",omhome_1,pd,"share",pd,"omc",pd,"scripts",pd,"Compile","\""," ",fileprefix," ",Config.simulationCodeTarget()," ", winCompileMode});
   else
     numParallel := if Config.getRunningTestsuite() then 1 else Config.noProc();
-    s_call := stringAppendList({System.getMakeCommand()," -j",intString(numParallel)," -f ",fileprefix,".makefile"});
+    cdWorkingDir := if stringLength(workingDir) == 0 then "" else (" -C \"" + workingDir + "\"");
+    setMakeVars := sum(" "+var for var in makeVarsNoBinding);
+    s_call := stringAppendList({System.getMakeCommand()," -j",intString(numParallel),cdWorkingDir," -f ",fileprefix,".makefile",setMakeVars});
   end if;
   if Flags.isSet(Flags.DYN_LOAD) then
     Debug.traceln("compileModel: running " + s_call);

Compiler/Script/CevalScriptBackend.mo
@@ -2608,7 +2608,10 @@ protected function translateModelFMU " author: Frenkel TUD
   output FCore.Cache outCache;
   output Values.Value outValue;
   output GlobalScript.SymbolTable outInteractiveSymbolTable;
+protected
+  Boolean staticSourceCodeFMU;
 algorithm
+  staticSourceCodeFMU := Flags.isSet(Flags.BUILD_STATIC_SOURCE_FMU);
   (outCache,outValue,outInteractiveSymbolTable):=
   match (inCache,inEnv,className,inInteractiveSymbolTable,inFMUVersion,inFMUType,inFileNamePrefix,addDummy,inSimSettingsOpt)
     local
@@ -2618,18 +2621,37 @@ algorithm
       GlobalScript.SymbolTable st;
       list<String> libs;
       Values.Value outValMsg;
-      String file_dir, FMUVersion, FMUType, fileNamePrefix, str;
+      String file_dir, FMUVersion, FMUType, fileNamePrefix, str, fmutmp;
     case (cache,env,_,st,FMUVersion,FMUType,fileNamePrefix,_,_) /* mo file directory */
       equation
         (cache, outValMsg, st,_, libs,_, _) =
           SimCodeMain.translateModelFMU(cache,env,className,st,FMUVersion,FMUType,fileNamePrefix,addDummy,inSimSettingsOpt);
 
         // compile
-        fileNamePrefix = stringAppend(fileNamePrefix,"_FMU");
-        CevalScript.compileModel(fileNamePrefix , libs);
-
-      then
-        (cache,outValMsg,st);
+        CevalScript.compileModel(fileNamePrefix+"_FMU" , libs);
+        if Config.simCodeTarget() <> "Cpp" then
+          fmutmp = fileNamePrefix + ".fmutmp";
+          /* Let's just assume we have a pristine source directory in fmutmp
+          if System.directoryExists(fmutmp) then
+            System.removeDirectory(fmutmp);
+          end if;
+          unzip(...);
+          */
+          // CevalScript.compileModel(fileNamePrefix , libs, workingDir=fmutmp+"/sources", makeVars={"CC=arm-linux-gnueabi-gcc","FMIPLATFORM=arm-linux-gnueabi","DLLEXT=.so"});
+          CevalScript.compileModel(fileNamePrefix , libs, workingDir=fmutmp+"/sources", makeVars={
+              "CC="+System.getCCompiler(),
+              "'CFLAGS="+System.getCFlags()+"'",
+              "CPPFLAGS=",
+              "'LDFLAGS="+System.getLDFlags()+" "+(if staticSourceCodeFMU then System.getRTLibsFMU() /*TODO: Should not be needed, once we remove the need for lapack/expat*/ else System.getRTLibsSim())+"'",
+              "FMIPLATFORM="+System.modelicaPlatform(),
+              "DLLEXT="+System.getDllExt(),
+              "'LD="+System.getLinker()+"'",
+              if staticSourceCodeFMU then "OPENMODELICA_DYNAMIC=" else "OPENMODELICA_DYNAMIC=1"
+          });
+          // CevalScript.compileModel(fileNamePrefix , libs, workingDir=fmutmp+"/sources", makeVars={});
+          System.removeDirectory(fmutmp);
+        end if;
+      then (cache,outValMsg,st);
     else /* mo file directory */
       equation
          str = Error.printMessagesStr(false);

Compiler/SimCode/SimCodeFunctionUtil.mo
@@ -2755,6 +2755,7 @@ public function createMakefileParams
   input list<String> libs;
   input list<String> libPaths;
   input Boolean isFunction;
+  input Boolean isFMU=false;
   output SimCode.MakefileParams makefileParams;
 protected
   String omhome, ccompiler, cxxcompiler, linker, exeext, dllext, cflags, ldflags, rtlibs, platform, fopenmp,compileDir;
@@ -2771,7 +2772,7 @@ algorithm
             (if Flags.isSet(Flags.HPCOM) then "-fopenmp" else "");
   cflags := if stringEq(Config.simCodeTarget(),"JavaScript") then "-Os -Wno-warn-absolute-paths" else cflags;
   ldflags := System.getLDFlags();
-  rtlibs := if isFunction then System.getRTLibs() else System.getRTLibsSim();
+  rtlibs := if isFunction then System.getRTLibs() else (if isFMU then System.getRTLibsFMU() else System.getRTLibsSim());
   platform := System.modelicaPlatform();
   compileDir :=  System.pwd() + System.pathDelimiter();
   makefileParams := SimCode.MAKEFILE_PARAMS(ccompiler, cxxcompiler, linker, exeext, dllext,

Compiler/SimCode/SimCodeMain.mo
@@ -147,7 +147,7 @@ algorithm
   (libs,libPaths,includes, includeDirs, recordDecls, functions, literals) :=
     SimCodeUtil.createFunctions(p, inBackendDAE);
   simCode := createSimCode(inBackendDAE, inInitDAE, inUseHomotopy, inRemovedInitialEquationLst, inPrimaryParameters, inAllPrimaryParameters,
-    className, filenamePrefix, fileDir, functions, includes, includeDirs, libs, libPaths, simSettingsOpt, recordDecls, literals, Absyn.FUNCTIONARGS({},{}));
+    className, filenamePrefix, fileDir, functions, includes, includeDirs, libs, libPaths, simSettingsOpt, recordDecls, literals, Absyn.FUNCTIONARGS({},{}), isFMU=true);
   timeSimCode := System.realtimeTock(ClockIndexes.RT_CLOCK_SIMCODE);
   SimCodeFunctionUtil.execStat("SimCode");
 
@@ -439,6 +439,7 @@ protected function createSimCode "
   input list<SimCode.RecordDeclaration> recordDecls;
   input tuple<Integer, HashTableExpToIndex.HashTable, list<DAE.Exp>> literals;
   input Absyn.FunctionArgs args;
+  input Boolean isFMU=false;
   output SimCode.SimCode simCode;
 algorithm
   simCode := matchcontinue(inBackendDAE, inClassName, filenamePrefix, inString11, functions, externalFunctionIncludes, includeDirs, libs, libPaths,simSettingsOpt, recordDecls, literals, args)
@@ -472,7 +473,7 @@ algorithm
     then HpcOmSimCodeMain.createSimCode(inBackendDAE, inInitDAE, inUseHomotopy, inRemovedInitialEquationLst, inPrimaryParameters, inAllPrimaryParameters, inClassName, filenamePrefix, inString11, functions, externalFunctionIncludes, includeDirs, libs, libPaths,simSettingsOpt, recordDecls, literals, args);
 
     else equation
-      (tmpSimCode, _) = SimCodeUtil.createSimCode(inBackendDAE, inInitDAE, inUseHomotopy, inRemovedInitialEquationLst, inPrimaryParameters, inAllPrimaryParameters, inClassName, filenamePrefix, inString11, functions, externalFunctionIncludes, includeDirs, libs,libPaths, simSettingsOpt, recordDecls, literals, args);
+      (tmpSimCode, _) = SimCodeUtil.createSimCode(inBackendDAE, inInitDAE, inUseHomotopy, inRemovedInitialEquationLst, inPrimaryParameters, inAllPrimaryParameters, inClassName, filenamePrefix, inString11, functions, externalFunctionIncludes, includeDirs, libs,libPaths, simSettingsOpt, recordDecls, literals, args, isFMU=isFMU);
     then tmpSimCode;
   end matchcontinue;
 end createSimCode;
@@ -591,12 +592,22 @@ algorithm
   _ := match (simCode,target)
     local
       String str;
+      String fmutmp;
 
     case (SimCode.SIMCODE(),"C")
       algorithm
+        fmutmp := simCode.fileNamePrefix + ".fmutmp";
+        if System.directoryExists(fmutmp) then
+          if not System.removeDirectory(fmutmp) then
+            print("Failed to remove directory: " + fmutmp + "\n");
+            fail();
+          end if;
+        end if;
+        Util.createDirectoryTree(fmutmp + "/sources/include/");
         if Flags.isSet(Flags.MODEL_INFO_JSON) then
           SerializeModelInfo.serialize(simCode, Flags.isSet(Flags.INFO_XML_OPERATIONS));
-          true := System.covertTextFileToCLiteral(simCode.fileNamePrefix+"_info.json", simCode.fileNamePrefix+"_info.c");
+          str := fmutmp + "/sources/" + simCode.fileNamePrefix;
+          true := System.covertTextFileToCLiteral(simCode.fileNamePrefix+"_info.json", str+"_info.c");
         else
           Tpl.tplNoret2(SimCodeDump.dumpSimCodeToC, simCode, false);
         end if;

Compiler/SimCode/SimCodeUtil.mo
@@ -165,6 +165,7 @@ public function createSimCode "entry point to create SimCode from BackendDAE."
   input list<SimCode.RecordDeclaration> recordDecls;
   input tuple<Integer, HashTableExpToIndex.HashTable, list<DAE.Exp>> literals;
   input Absyn.FunctionArgs args;
+  input Boolean isFMU=false;
   output SimCode.SimCode simCode;
   output tuple<Integer, list<tuple<Integer, Integer>>> outMapping "the highest simEqIndex in the mapping and the mapping simEq-Index -> scc-Index itself";
 protected
@@ -280,7 +281,7 @@ algorithm
 
     ((uniqueEqIndex, algorithmAndEquationAsserts)) := BackendDAEUtil.foldEqSystem(dlow, createAlgorithmAndEquationAsserts, (uniqueEqIndex, {}));
     discreteModelVars := BackendDAEUtil.foldEqSystem(dlow, extractDiscreteModelVars, {});
-    makefileParams := SimCodeFunctionUtil.createMakefileParams(includeDirs, libs, libPaths, false);
+    makefileParams := SimCodeFunctionUtil.createMakefileParams(includeDirs, libs, libPaths, false, isFMU);
     (delayedExps, maxDelayedExpIndex) := extractDelayedExpressions(dlow);
 
     // append removed equation to all equations, since these are actually
@@ -4134,11 +4135,11 @@ protected
   list<BackendDAE.Equation> eqs;
 algorithm
   try
-	  (varsWithBind,varsWithoutBind) := List.separateOnTrue(varLstIn,BackendVariable.varHasBindExp);
-	  (comps,ass1,ass2) := BackendDAEUtil.causalizeVarBindSystem(varsWithBind);
-	  order := List.map1(List.flatten(comps),Array.getIndexFirst,ass1);
-	  varsWithBind := List.map1(order,List.getIndexFirst,varsWithBind);
-	  varLstOut := listAppend(varsWithoutBind,varsWithBind);
+    (varsWithBind,varsWithoutBind) := List.separateOnTrue(varLstIn,BackendVariable.varHasBindExp);
+    (comps,ass1,ass2) := BackendDAEUtil.causalizeVarBindSystem(varsWithBind);
+    order := List.map1(List.flatten(comps),Array.getIndexFirst,ass1);
+    varsWithBind := List.map1(order,List.getIndexFirst,varsWithBind);
+    varLstOut := listAppend(varsWithoutBind,varsWithBind);
   else
     varLstOut := varLstIn;
   end try;

Compiler/Template/CodegenC.tpl
@@ -161,41 +161,41 @@ end simulationHeaderFile;
   match simCode
     case simCode as SIMCODE(__) then
      // external objects
-     let()= textFileConvertLines(simulationFile_exo(simCode,guid), '<%fileNamePrefix%>_01exo.c')
+     let()= textFileConvertLines(simulationFile_exo(simCode,guid), '<%modelNamePrefix%>_01exo.c')
      // non-linear systems
-     let()= textFileConvertLines(simulationFile_nls(simCode,guid), '<%fileNamePrefix%>_02nls.c')
+     let()= textFileConvertLines(simulationFile_nls(simCode,guid), '<%modelNamePrefix%>_02nls.c')
      // linear systems
-     let()= textFileConvertLines(simulationFile_lsy(simCode,guid), '<%fileNamePrefix%>_03lsy.c')
+     let()= textFileConvertLines(simulationFile_lsy(simCode,guid), '<%modelNamePrefix%>_03lsy.c')
      // state set
-     let()= textFileConvertLines(simulationFile_set(simCode,guid), '<%fileNamePrefix%>_04set.c')
+     let()= textFileConvertLines(simulationFile_set(simCode,guid), '<%modelNamePrefix%>_04set.c')
      // events: sample, zero crossings, relations
-     let()= textFileConvertLines(simulationFile_evt(simCode,guid), '<%fileNamePrefix%>_05evt.c')
+     let()= textFileConvertLines(simulationFile_evt(simCode,guid), '<%modelNamePrefix%>_05evt.c')
      // initialization
-     let()= textFileConvertLines(simulationFile_inz(simCode,guid), '<%fileNamePrefix%>_06inz.c')
+     let()= textFileConvertLines(simulationFile_inz(simCode,guid), '<%modelNamePrefix%>_06inz.c')
      // delay
-     let()= textFileConvertLines(simulationFile_dly(simCode,guid), '<%fileNamePrefix%>_07dly.c')
+     let()= textFileConvertLines(simulationFile_dly(simCode,guid), '<%modelNamePrefix%>_07dly.c')
      // update bound start values, update bound parameters
-     let()= textFileConvertLines(simulationFile_bnd(simCode,guid), '<%fileNamePrefix%>_08bnd.c')
+     let()= textFileConvertLines(simulationFile_bnd(simCode,guid), '<%modelNamePrefix%>_08bnd.c')
      // algebraic
-     let()= textFileConvertLines(simulationFile_alg(simCode,guid), '<%fileNamePrefix%>_09alg.c')
+     let()= textFileConvertLines(simulationFile_alg(simCode,guid), '<%modelNamePrefix%>_09alg.c')
      // asserts
-     let()= textFileConvertLines(simulationFile_asr(simCode,guid), '<%fileNamePrefix%>_10asr.c')
+     let()= textFileConvertLines(simulationFile_asr(simCode,guid), '<%modelNamePrefix%>_10asr.c')
      // mixed systems
      let &mixheader = buffer ""
-     let()= textFileConvertLines(simulationFile_mix(simCode,guid,&mixheader), '<%fileNamePrefix%>_11mix.c')
-     let()= textFile(&mixheader, '<%fileNamePrefix%>_11mix.h')
+     let()= textFileConvertLines(simulationFile_mix(simCode,guid,&mixheader), '<%modelNamePrefix%>_11mix.c')
+     let()= textFile(&mixheader, '<%modelNamePrefix%>_11mix.h')
      // jacobians
-     let()= textFileConvertLines(simulationFile_jac(simCode,guid), '<%fileNamePrefix%>_12jac.c')
-     let()= textFile(simulationFile_jac_header(simCode,guid), '<%fileNamePrefix%>_12jac.h')
+     let()= textFileConvertLines(simulationFile_jac(simCode,guid), '<%modelNamePrefix%>_12jac.c')
+     let()= textFile(simulationFile_jac_header(simCode,guid), '<%modelNamePrefix%>_12jac.h')
      // optimization
-     let()= textFileConvertLines(simulationFile_opt(simCode,guid), '<%fileNamePrefix%>_13opt.c')
-     let()= textFile(simulationFile_opt_header(simCode,guid), '<%fileNamePrefix%>_13opt.h')
+     let()= textFileConvertLines(simulationFile_opt(simCode,guid), '<%modelNamePrefix%>_13opt.c')
+     let()= textFile(simulationFile_opt_header(simCode,guid), '<%modelNamePrefix%>_13opt.h')
      // linearization
-     let()= textFileConvertLines(simulationFile_lnz(simCode,guid), '<%fileNamePrefix%>_14lnz.c')
+     let()= textFileConvertLines(simulationFile_lnz(simCode,guid), '<%modelNamePrefix%>_14lnz.c')
      // synchronous
-     let()= textFileConvertLines(simulationFile_syn(simCode,guid), '<%fileNamePrefix%>_15syn.c')
+     let()= textFileConvertLines(simulationFile_syn(simCode,guid), '<%modelNamePrefix%>_15syn.c')
      // main file
-     let()= textFileConvertLines(simulationFile(simCode,guid,isModelExchangeFMU), '<%fileNamePrefix%>.c')
+     let()= textFileConvertLines(simulationFile(simCode,guid,isModelExchangeFMU), '<%modelNamePrefix%>.c')
      ""
   end match
 end generateSimulationFiles;
@@ -816,7 +816,7 @@ template simulationFile(SimCode simCode, String guid, Boolean isModelExchangeFMU
   match simCode
     case simCode as SIMCODE(hpcomData=HPCOMDATA(__)) then
     let modelNamePrefixStr = modelNamePrefix(simCode)
-    let mainInit = if boolOr(Flags.isSet(Flags.PARMODAUTO), Flags.isSet(HPCOM)) then
+    let mainInit = if boolOr(isModelExchangeFMU, boolOr(Flags.isSet(Flags.PARMODAUTO), Flags.isSet(HPCOM))) then
                      <<
                      mmc_init_nogc();
                      omc_alloc_interface = omc_alloc_interface_pooled;
@@ -947,6 +947,8 @@ template simulationFile(SimCode simCode, String guid, Boolean isModelExchangeFMU
       return 1;
     }
 
+    <% if boolNot(isModelExchangeFMU) then
+    <<
     #if defined(threadData)
     #undef threadData
     #endif
@@ -965,6 +967,8 @@ template simulationFile(SimCode simCode, String guid, Boolean isModelExchangeFMU
       return res;
     }
     <%\n%>
+    >>
+    %>
     >>
     /* adrpo: leave a newline at the end of file to get ridsymbolName(String fileNamePrefix of the warning */
   end match

commit fe751df68fe0ff0a07635dc34b1d4dead5a4ece9

Init Boehm GC (necessary for OSX)
Bug was introduced in 2ed993a, copy-paste error.

SimulationRuntime/c/util/omc_init.c
@@ -30,6 +30,7 @@
 
 #include <stdio.h>
 #include "omc_init.h"
+#include "meta/meta_modelica_segv.h"
 
 pthread_key_t mmc_thread_data_key = 0;
 
@@ -46,14 +47,16 @@ void mmc_init_nogc()
 }
 
 #if defined(OMC_MINIMAL_RUNTIME)
-void mmc_init(int withgc)
+void mmc_init()
 {
   fprintf(stderr, "Error: called mmc_init (requesting garbage collection) when OMC was compiled with a minimal runtime system.");
   exit(1);
 }
 #else
-void mmc_init(int withgc)
+#include "meta/gc/mmc_gc.h"
+void mmc_init()
 {
   mmc_init_nogc();
+  mmc_GC_init();
 }
 #endif