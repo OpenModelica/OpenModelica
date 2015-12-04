cef0c57b4b9fa0224223e825cc1822f2155d7576
Martin Sjölund <martin.sjolund@liu.se>
2015-12-04 17:01:28 +0100

    Update URL of the repository

--- bibliography/upload.sh
+++ bibliography/upload.sh
@@ -4,7 +4,11 @@ rm -rf $TMPNAME
 mkdir -p $TMPNAME
 sh generate.sh $TMPNAME || exit 1
 for f in $TMPNAME/*; do
-  echo "" >> $f
-  echo "<!-- This code was automatically generated from the OpenModelica trunk --!>" >> $f
+  echo "<!-- This code was automatically generated from the repository at https://github.com/OpenModelica/OpenModelica-doc/ --!>" > $f.tmp
+  echo "" >> $f.tmp
+  cat $f >> $f.tmp
+  echo "" >> $f.tmp
+  echo "<!-- This code was automatically generated from the repository at https://github.com/OpenModelica/OpenModelica-doc/ --!>" >> $f.tmp
+  mv $f.tmp $f
 done
 scp $TMPNAME/* openmodelica.org:/var/www/joomla/bibliography
