49412430ccb2d077bb99fdb3dd7cb52578de6603
mflehmig <martin.flehmig@tu-dresden.de>
2015-12-11 12:24:19 +0100

    - Added dgesv to makefile, compile it into static library

--- SimulationRuntime/cpp/CMake/FindScoreP.cmake
+++ SimulationRuntime/cpp/CMake/FindScoreP.cmake
@@ -3,20 +3,24 @@
 # Sets the usual variables expected for find_package scripts:
 #
 # SCOREP_INCLUDE_DIR - header location
-# SCOREP_FOUND - true if pugixml was found.
+# SCOREP_FOUND - true if scorep was found.
 #
 # To influence the behaviour, you can use the following variable:
 #   SCOREP_HOME - the folder that contains the scorep include folder
 
-find_path (SCOREP_INCLUDE_DIR NAMES scorep/SCOREP_User.h PATHS ${SCOREP_HOME}/include)
+IF(NOT SCOREP_HOME)
+  SET(SCOREP_HOME $ENV{SCOREP_HOME})
+ENDIF()
 
-include (FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS (SCOREP DEFAULT_MSG SCOREP_INCLUDE_DIR)
+FIND_PATH(SCOREP_INCLUDE_DIR NAMES scorep/SCOREP_User.h PATHS ${SCOREP_HOME}/include)
 
-if (NOT SCOREP_FOUND)
-  message (FATAL_ERROR "ScoreP includes not found")
-endif(NOT SCOREP_FOUND)
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SCOREP DEFAULT_MSG SCOREP_INCLUDE_DIR)
 
-message(STATUS "ScoreP includes ${SCOREP_INCLUDE_DIR}")
+IF(NOT SCOREP_FOUND)
+  MESSAGE(FATAL_ERROR "ScoreP includes not found")
+ENDIF(NOT SCOREP_FOUND)
 
-mark_as_advanced (SCOREP_INCLUDE_DIR)
+MESSAGE(STATUS "ScoreP includes ${SCOREP_INCLUDE_DIR}")
+
+MARK_AS_ADVANCED(SCOREP_INCLUDE_DIR)
