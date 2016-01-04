a646bc72fb2c9295b897464c9ba499b374525865
mflehmig <martin.flehmig@tu-dresden.de>
2016-01-04 10:16:05 +0100

    Added support for dgesv to cpp runtime
    - Depending on the size of linear and nonlinear systems of a model, the use of dgesv (OMCompiler/3rdParty) or the use of Lapack is recommended.
    - Added support for dgesv to makefile generation and ModelicaConfig file.

---
+++ SimulationRuntime/cpp/CMake/FindDgesv.cmake
@@ -0,0 +1,36 @@
+# Find the header and source files of the dgesv library.
+#
+# Sets the usual variables expected for find_package scripts:
+#
+# DGESV_INCLUDE_DIR - header location
+# DGESV_SRCS - list of all source files (cause they are splited above several directories).
+# DGESV_HEADERS - list all header files.
+# DGESV_FOUND - true if dgesv was found.
+#
+# To influence the behaviour, you can use the following variable:
+#   DGESV_HOME - the folder that contains the dgesv include and source folders.
+
+IF(NOT DGESV_HOME)
+  SET(DGESV_HOME $ENV{DGESV_HOME})
+ENDIF()
+
+FIND_PATH(DGESV_INCLUDE_DIR NAMES blaswrap.h PATHS "${CMAKE_SOURCE_DIR}/../../3rdParty/dgesv/include/" "${DGESV_HOME}/include/" NO_DEFAULT_PATH)
+
+IF(DGESV_INCLUDE_DIR)
+  FILE(GLOB DGESV_HEADERS "${DGESV_INCLUDE_DIR}/*.h")
+  FIND_PATH(DGESV_SRC_DIR_BLAS NAMES dgemm.c PATHS "${CMAKE_SOURCE_DIR}/../../3rdParty/dgesv/blas/" "${DGESV_HOME}/blas/" NO_DEFAULT_PATH)
+  FIND_PATH(DGESV_SRC_DIR_LAPACK NAMES dgesv.c PATHS "${CMAKE_SOURCE_DIR}/../../3rdParty/dgesv/lapack/" "${DGESV_HOME}/lapack/" NO_DEFAULT_PATH)
+  FIND_PATH(DGESV_SRC_DIR_F2C NAMES d_cnjg.c PATHS "${CMAKE_SOURCE_DIR}/../../3rdParty/dgesv/libf2c/" "${DGESV_HOME}/libf2c/" NO_DEFAULT_PATH)
+
+  IF(DGESV_SRC_DIR_BLAS AND DGESV_SRC_DIR_LAPACK AND DGESV_SRC_DIR_F2C)
+    FILE(GLOB DGESV_SRCS "${DGESV_SRC_DIR_BLAS}/*.c" "${DGESV_SRC_DIR_LAPACK}/*.c" "${DGESV_SRC_DIR_F2C}/*.c")
+  ELSE()
+    MESSAGE(STATUS "Dgesv sources not found.")
+  ENDIF()
+
+ELSE(DGESV_INCLUDE_DIR)
+  MESSAGE(STATUS "Dgesv includes not found.")
+ENDIF(DGESV_INCLUDE_DIR)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(DGESV DEFAULT_MSG DGESV_INCLUDE_DIR DGESV_SRCS)
