f1fe098c42f985e99f40b6331d750ae6e4b8b21d
Adeel Asghar <adeel.asghar@liu.se>
2016-01-28 10:55:18 +0100

    Fixes #3648. Fixed spelling mistake.

--- OMEdit/OMEditGUI/Options/OptionsDialog.cpp
+++ OMEdit/OMEditGUI/Options/OptionsDialog.cpp
@@ -1263,10 +1263,10 @@ GeneralSettingsPage::GeneralSettingsPage(OptionsDialog *pOptionsDialog)
   pDefaultViewRadioButtonsGridLayout->addWidget(mpTextViewRadioButton, 1, 0);
   pDefaultViewRadioButtonsGridLayout->addWidget(mpDocumentationViewRadioButton, 1, 1);
   // set the layout of default view group
-  QGridLayout *pDefautlViewLayout = new QGridLayout;
-  pDefautlViewLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
-  pDefautlViewLayout->addLayout(pDefaultViewRadioButtonsGridLayout, 0, 0);
-  mpDefaultViewGroupBox->setLayout(pDefautlViewLayout);
+  QGridLayout *pDefaultViewLayout = new QGridLayout;
+  pDefaultViewLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+  pDefaultViewLayout->addLayout(pDefaultViewRadioButtonsGridLayout, 0, 0);
+  mpDefaultViewGroupBox->setLayout(pDefaultViewLayout);
   // Auto Save
   mpEnableAutoSaveGroupBox = new QGroupBox(tr("Enable Auto Save"));
   mpEnableAutoSaveGroupBox->setToolTip("Auto save feature is experimental. If you encounter unexpected crashes then disable it.");
