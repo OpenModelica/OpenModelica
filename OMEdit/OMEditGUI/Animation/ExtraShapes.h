997caa118db835973600ef1064cdc7c3d0b1d3b1
vwaurich <volker.waurich@gmx.de>
2016-09-07 10:52:55 +0200

    - added a spring, yeah

--- OMEdit/OMEditGUI/Animation/ExtraShapes.h
+++ OMEdit/OMEditGUI/Animation/ExtraShapes.h
@@ -49,5 +49,24 @@ public:
 };
 
 
+class Spring : public osg::Geometry
+{
+public:
+	Spring(float r, float rCoil, float nWindings,  float l);
+	~Spring() {};
+private:
+   osg::Vec3f getNormal(osg::Vec3f vec, float length = 1);
+   osg::Vec3f normalize(osg::Vec3f vec);
+   osg::Vec3f rotateX(osg::Vec3f vec, float phi);
+   osg::Vec3f rotateY(osg::Vec3f vec, float phi);
+   osg::Vec3f rotateZ(osg::Vec3f vec, float phi);
+   osg::Vec3f rotateArbitraryAxes_expensive(osg::Vec3f vec, osg::Vec3f axes, float phi);
+   osg::Vec3f rotateArbitraryAxes(osg::Vec3f vec, osg::Vec3f axes, float phi);
+   float absoluteVector(osg::Vec3f vec);
+   float angleBetweenVectors(osg::Vec3f vec1, osg::Vec3f vec2);
+
+   osg::Vec3Array* outerVertices;
+   osg::Vec3Array* splineVertices;
+};
 
 #endif //end EXTRASHAPES_H
