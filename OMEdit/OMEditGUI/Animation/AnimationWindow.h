f1516019cd3b9983a69eaa1e6578651585a82be6
Adeel Asghar <adeel.asghar@liu.se>
2016-09-29 12:10:51 +0200

    Fixed internal osgQt for Windows.

--- OMEdit/OMEditGUI/Animation/AnimationWindow.h
+++ OMEdit/OMEditGUI/Animation/AnimationWindow.h
@@ -50,19 +50,11 @@
 #include <osg/Vec3>
 #include <osgDB/ReadFile>
 #include <osgGA/MultiTouchTrackballManipulator>
+#include <../../osgQt/GraphicsWindowQt>
 #include <osg/Version>
 #include <osgViewer/CompositeViewer>
 #include <osgViewer/View>
 #include <osgViewer/ViewerEventHandlers>
-
-#if OPENSCENEGRAPH_MAJOR_VERSION>=3 && OPENSCENEGRAPH_MINOR_VERSION>=4
-  #include <osgQt/GraphicsWindowQt>
-#else
-  #include <../../osgQt/GraphicsWindowQt>
-#endif
-
-
-
 
 class MainWindow;
 class PlotWindowContainer;
