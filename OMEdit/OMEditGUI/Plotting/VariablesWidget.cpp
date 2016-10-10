9a9d061d456521f685afe992fed1cd2d3e860672
Rüdiger Franke <rdgfranke@gmail.com>
2016-10-10 21:38:56 +0200

    Restrict unit selector of variables browser and use displayUnit
    - the values are shown with displayUnit
    - the unit selector is restricted to displayUnit and actual unit
      to avoid possibly wrong choices resulting from getDerivedUnits

--- OMEdit/OMEditGUI/Plotting/VariablesWidget.cpp
+++ OMEdit/OMEditGUI/Plotting/VariablesWidget.cpp
@@ -570,8 +570,18 @@ void VariablesTreeModel::insertVariablesItems(QString fileName, QString filePath
       if (!variableData[5].toString().isEmpty()) {
         QStringList displayUnits;
         displayUnits << variableData[5].toString();
-        displayUnits << mpVariablesTreeView->getVariablesWidget()->getMainWindow()->getOMCProxy()->getDerivedUnits(variableData[5].toString());
+        displayUnits << (QList<QString>() << variableData[6].toString());
         variableData << displayUnits;
+        // convert value to displayUnit
+        OMCInterface::convertUnits_res convertUnit = mpVariablesTreeView->getVariablesWidget()->getMainWindow()->getOMCProxy()->convertUnits(variableData[5].toString(), variableData[6].toString());
+        if (convertUnit.unitsCompatible) {
+          bool ok = true;
+          qreal realValue = variableData[4].toDouble(&ok);
+          if (ok) {
+            realValue = Utilities::convertUnit(realValue, convertUnit.offset, convertUnit.scaleFactor);
+            variableData[4] = QString::number(realValue);
+          }
+        }
       } else {
         variableData << QStringList();
       }
@@ -1332,6 +1342,15 @@ void VariablesWidget::unitChanged(const QModelIndex &index)
     OMCInterface::convertUnits_res convertUnit = mpMainWindow->getOMCProxy()->convertUnits(pVariablesTreeItem->getPreviousUnit(),
                                                                                            pVariablesTreeItem->getDisplayUnit());
     if (convertUnit.unitsCompatible) {
+      // update value
+      QVariant stringValue = pVariablesTreeItem->data(1, Qt::EditRole);
+      bool ok = true;
+      qreal realValue = stringValue.toDouble(&ok);
+      if (ok) {
+        realValue = Utilities::convertUnit(realValue, convertUnit.offset, convertUnit.scaleFactor);
+        pVariablesTreeItem->setData(1, QString::number(realValue), Qt::EditRole);
+      }
+      // update plots
       foreach (PlotCurve *pPlotCurve, pPlotWindow->getPlot()->getPlotCurvesList()) {
         QString curveTitle = pPlotCurve->getNameStructure();
         if (curveTitle.compare(pVariablesTreeItem->getVariableName()) == 0) {
