4a3d23830de6952f0c46ed2172f306183e273d6e
Martin Sjölund <martin.sjolund@liu.se>
2017-01-12 13:57:20 +0100

    Fix bug when author names have digits in them

--- bibliography/bib2html.sh
+++ bibliography/bib2html.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Inputs:
-# $1: basename of the cite-file 
+# $1: basename of the cite-file
 # $2: basename of the bib-file
 # $3: target encoding
 # $4: path to where the files are stored (relative on the server)
 rm -f $1.bibinc
-for year in `grep -o [0-9]* $1.cite | sort -ru`; do
+for year in `grep -o '[0-9][0-9][0-9][0-9]' $1.cite | sort -ru`; do
   grep $year $1.cite > $1.cite.$year
   bibtex2html -r -d --note supervisor --note examiner -charset utf8 -citefile $1.cite.$year -nf pdf pdf -nodoc -noheader -nofooter $2.bib || exit 1
   iconv -f utf8 -t $3 $2.html > $1.bibinc.tmp || exit 1
