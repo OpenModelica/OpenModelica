82dba2189cf31c2c522bef82231fab08f77cbac6
Adeel Asghar <adeel.asghar@liu.se>
2017-01-16 17:50:57 +0100

    Read the position & angle321 attributes of the sub-models.

--- OMEdit/OMEditGUI/Modeling/ModelWidgetContainer.cpp
+++ OMEdit/OMEditGUI/Modeling/ModelWidgetContainer.cpp
@@ -4198,6 +4198,8 @@ void ModelWidget::getMetaModelSubModels()
                                                               Helper::errorLevel));
       }
     }
+    pComponentInfo->setPosition(subModel.attribute("Position"));
+    pComponentInfo->setAngle321(subModel.attribute("Angle321"));
     // add submodel as component to view.
     mpDiagramGraphicsView->addComponentToView(subModel.attribute("Name"), pLibraryTreeItem, transformation, QPointF(0.0, 0.0), dialogAnnotation,
                                               pComponentInfo, false, true);
