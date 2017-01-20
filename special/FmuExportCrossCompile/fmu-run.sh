a0ae22ecdd86dd634e1ef30b5029efda09d7b976
Martin Sjölund <martin.sjolund@liu.se>
2017-01-20 12:57:35 +0100

    Check the FMU XML for source/ARM

--- special/FmuExportCrossCompile/fmu-run.sh
+++ special/FmuExportCrossCompile/fmu-run.sh
@@ -73,6 +73,7 @@ for t in me cs; do
   DIR="./Test_FMUs/FMI_2.0/$d/$a/OpenModelica/$VERSION/$1"
   mkdir -p "$DIR"
   CMD=`echo fmuCheck.$a -c , -f -n 2000 -d -k $t -e "$DIR/$1_cc.log" -o "$DIR/$1_cc.csv" "$DIR/$1.fmu"`
+  fmuCheck.linux64 -c , -f -n 2000 -d -k xml -e "$DIR/$1_cc.log" "$DIR/$1.fmu"
   cp "$1.fmu" "$DIR/" || exit 1
   cp "$1_ref.opt" "$DIR/" || exit 1
   cp "$1_res.csv" "$DIR/$1_ref.csv" || exit 1
