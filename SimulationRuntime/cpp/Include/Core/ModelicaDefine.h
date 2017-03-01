afb17e2554939f4455433cd9b0444188dc1d1fbf
Niklas Worschech <niklas.worschech@boschrexroth.de>
2017-03-01 15:46:57 +0100

    fix for visual studio 2015

--- SimulationRuntime/cpp/Include/Core/ModelicaDefine.h
+++ SimulationRuntime/cpp/Include/Core/ModelicaDefine.h
@@ -68,4 +68,28 @@ typedef double coshRetType;
     #define BOOST_ALL_DYN_LINK
   #endif
 #endif
+
+// Visual C++ 2015 by default does not link the CRT if the entry point is overriden. Force linking. Macros according to
+    //  "MSDN — Predefined Macros"; library names according to "Visual C++ Team Blog — Introducing the Universal CRT".
+    #if _MSC_VER >= 1900
+    #       if _DEBUG
+    #               if _DLL
+    #                       pragma comment(lib, "vcruntimed")
+    #                       pragma comment(lib, "ucrtd")
+    #               else
+    #                       pragma comment(lib, "libvcruntimed")
+    #                       pragma comment(lib, "libucrtd")
+    #               endif
+    #       else
+    #               if _DLL
+    #                       pragma comment(lib, "vcruntime")
+    #                       pragma comment(lib, "ucrt")
+    #               else
+    #                       pragma comment(lib, "libvcruntime")
+    #                       pragma comment(lib, "libucrt")
+    #               endif
+    #       endif
+    #endif
+
+
 /** @} */ // end of core
