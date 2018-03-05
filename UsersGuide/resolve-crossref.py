09bfe049b8b7ebfa2773980f63a248c23daf7b5b
Martin Sjölund <martin.sjolund@liu.se>
2018-03-05 11:03:13 +0100

    Fixes for new (broken) bibtexparser

--- UsersGuide/resolve-crossref.py
+++ UsersGuide/resolve-crossref.py
@@ -1,22 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import bibtexparser
-import sys
+import re, sys
 
 with open(sys.argv[1], 'r') as bibtex_file:
     bibtex_str = bibtex_file.read()
-
+# Work-around for new (broken?) bibtexparser
+for (m,month) in [("jan","January"),("feb","February"),("mar","March"),("apr","April"),("may","May"),("jun","June"),("jul","July"),("aug","August"),("sep","September"),("oct","October"),("nov","November"),("dec","December")]:
+  bibtex_str = re.sub(r"\n *month *= *%s *" % m, "\nmonth={%s}\n" % month, bibtex_str, flags=re.IGNORECASE)
 bib_database = bibtexparser.loads(bibtex_str)
 
 for e in bib_database.entries:
-  if e.has_key('crossref'):
+  if 'crossref' in e:
     e2 = bib_database.entries_dict[e['crossref']]
     for k in e2.keys():
-      if not e.has_key(k):
+      if not k in e:
         e[k] = e2[k]
     del(e['crossref'])
     if 'pdf' in e:
       del(e['pdf']) # Not used by the template and contains %20 sometimes...
 
-open(sys.argv[2], "w").write(bibtexparser.dumps(bib_database).encode("utf8"))
+open(sys.argv[2], "w").write(bibtexparser.dumps(bib_database))
