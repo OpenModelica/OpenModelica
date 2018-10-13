1619f730c8b4acefc17565d45c2879575037ecb1
hkiel <henning.kiel@w-hs.de>
2018-10-13 10:30:17 +0200

    fix race condition on concurrent call
    Belonging to [master]:
      - OpenModelica/OMCompiler#2720

--- Examples/generate_icons.py
+++ Examples/generate_icons.py
@@ -1239,7 +1239,10 @@ def main():
     for f in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']:
         output_dirx = os.path.join(output_dir, f)
         if not os.path.exists(output_dirx):
-            os.makedirs(output_dirx)
+            try:
+                os.makedirs(output_dirx)
+            except:
+                pass
 
     success = True
 
