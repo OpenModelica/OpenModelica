327dd8ece0efd0cd0d1603d1ee8f84e4f807f15c
johti <johti17@liu.se>
2019-07-03 17:06:48 +0200

    All syntax tests now run

--- OMCompiler/toJulia/MMToJuliaTestSuite/runSyntaxCheck.jl
+++ OMCompiler/toJulia/MMToJuliaTestSuite/runSyntaxCheck.jl
@@ -33,6 +33,5 @@
 =#
 include("./syntaxCheck.jl")
 using OMJulia: OMCSession, sendExpression
-@time OMCSession(ARGS[1])
 omc = size(ARGS, 1) == 1 ? OMCSession(ARGS[1]) : OMCSession()
 SyntaxTest.syntaxCheck(omc)
