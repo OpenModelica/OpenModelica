67bcb30666d5e4ee6f8446d003fc8d9a9cf6c9c6
johti <johti17@liu.se>
2019-07-30 13:42:11 +0200

    improved testAlgorithms.jl

--- OMCompiler/toJulia/MMToJuliaTestSuite/SemanticCheckPrimitives/testAlgorithms.jl
+++ OMCompiler/toJulia/MMToJuliaTestSuite/SemanticCheckPrimitives/testAlgorithms.jl
@@ -3,7 +3,6 @@ module TestAlgorithms
 #= We might fail here already =#
 include("../OutputAlgorithms/Algorithms.jl")
 include("../testsuiteUtil.jl")
-#include("./tableu.jl") For simplex
 using Test
 using .Algorithms
 using .MMToJuliaTestSuiteUtil
