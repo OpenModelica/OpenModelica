db5872ac0c3162e484b715d2c8f8f50de293fc4d
Martin Sjölund <martin.sjolund@liu.se>
2019-08-02 18:27:09 +0200

    Add the missing Parser scripts

---
+++ OMCompiler/toJulia/Parser/OpenModelicaParser.jl
@@ -0,0 +1,23 @@
+module OpenModelicaParser
+  import Absyn
+  using MetaModelica
+
+  struct ParseError
+  end
+
+  function isDerCref(exp::Absyn.Exp)::Bool
+    @match exp begin
+      Absyn.CALL(Absyn.CREF_IDENT("der",  nil()), Absyn.FUNCTIONARGS(Absyn.CREF(__) <|  nil(),  nil()))  => true
+      _ => false
+    end
+  end
+
+  const _libpath = joinpath(dirname(dirname(@__DIR__)),"Parser","libomparse-julia.so")
+  function parseFile(fileName::String)::Absyn.Program
+    res = ccall((:parseFile,_libpath),Any,(String,),fileName)
+    if res == nothing
+      throw(ParseError())
+    end
+    res
+  end
+end
