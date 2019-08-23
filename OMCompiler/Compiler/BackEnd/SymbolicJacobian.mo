OpenModelica/OpenModelica

commit 85d96360c18d5239b3805f8faa3e814e23c49e7e

[BE] update iteration var warning and dump  - ticket #5602  - fixup testsuite

OMCompiler/Compiler/BackEnd/SymbolicJacobian.mo
@@ -2394,6 +2394,29 @@ algorithm
   end match;
 end getJacobianMatrixbyName;
 
+public function updateJacobianDependencies
+  input output BackendDAE.Jacobian jacobian;
+algorithm
+  jacobian := match jacobian
+    local
+      BackendDAE.Jacobian jac;
+      BackendDAE.SymbolicJacobian symJac;
+      BackendDAE.EqSystem syst;
+      BackendDAE.Shared shared;
+      String name;
+      list<BackendDAE.Var> diffVars;
+      list<BackendDAE.Var> diffedVars;
+      list<BackendDAE.Var> allDiffedVars;
+      list<DAE.ComponentRef> dependencies;
+    case jac as BackendDAE.GENERIC_JACOBIAN()
+      algorithm
+        SOME(symJac as (BackendDAE.DAE({syst}, shared),name,diffVars,diffedVars,allDiffedVars,dependencies)) := jac.jacobian;
+        dependencies := calcJacobianDependencies(symJac);
+        jac.jacobian := SOME((BackendDAE.DAE({syst}, shared),name,diffVars,diffedVars,allDiffedVars,dependencies));
+    then jac;
+    else jacobian;
+  end match;
+end updateJacobianDependencies;
 
 public function calcJacobianDependencies
   input BackendDAE.SymbolicJacobian jacobian;
@@ -2621,6 +2644,7 @@ algorithm
 end calculateTearingSetJacobian;
 
 protected function calculateJacobianComponent
+  "Calculates jacobian matrix for strong components of torn systems and non-linear systems."
   input BackendDAE.StrongComponent inComp;
   input BackendDAE.Variables inVars;
   input BackendDAE.EquationArray inEqns;
@@ -2719,12 +2743,218 @@ algorithm
 
           // generate generic jacobian backend dae
           (jacobian, shared) = getSymbolicJacobian(diffVars, eqns, resVars, oeqns, ovars, inShared, inVars, name, onlySparsePattern);
-
       then (BackendDAE.EQUATIONSYSTEM(residualequations, iterationvarsInts, jacobian, BackendDAE.JAC_GENERIC(), mixedSystem), shared);
 
       case (comp, _, _, _) then (comp, inShared);
   end matchcontinue;
+
+  // Check if all nonlinear iteration variables have start values
+  if BackendDAEUtil.isInitializationDAE(inShared) then
+      checkNonLinDependecies(outComp,inEqns);
+  end if;
 end calculateJacobianComponent;
+
+protected function checkNonLinDependecies
+  "Check if all non-linear iteartion variables of given non-linear equation
+   system have a start value and throw warning if not. Only start values for
+   those have an influence on solver iteration."
+  input BackendDAE.StrongComponent inComp;
+  input BackendDAE.EquationArray inEqns;
+protected
+  String name, msg;
+  Boolean existNonLin;
+algorithm
+  if Flags.isSet(Flags.INITIALIZATION) then
+    // Dump full information.
+    _ := match (inComp)
+      local
+        BackendDAE.Jacobian jac;
+        list<Integer> resIndices, eqnIndices = {};
+        BackendDAE.InnerEquations innerEquations;
+        Boolean linear;
+        String str;
+      // Case non-linear teared equation system
+      case (BackendDAE.TORNSYSTEM(strictTearingSet=BackendDAE.TEARINGSET(jac=jac, residualequations=resIndices, innerEquations=innerEquations), linear=false))
+        algorithm
+          for eq in innerEquations loop
+            eqnIndices := match eq
+              local
+                Integer idx;
+              case BackendDAE.INNEREQUATION(eqn = idx) then idx::eqnIndices;
+              case BackendDAE.INNEREQUATIONCONSTRAINTS(eqn = idx) then idx::eqnIndices;
+              else eqnIndices;
+            end match;
+          end for;
+          eqnIndices := listAppend(resIndices,eqnIndices);
+          printNonLinIterVarsAndEqs(jac,eqnIndices,inEqns);
+        then "";
+
+      // Case non-linear non-teared equation system
+      case (BackendDAE.EQUATIONSYSTEM(eqns=eqnIndices, jac=jac, jacType=BackendDAE.JAC_NONLINEAR()))
+        algorithm
+          printNonLinIterVarsAndEqs(jac,eqnIndices,inEqns);
+        then "";
+
+      // ToDo: Check if jacType=BackendDAE.JAC_GENERIC is needed
+      //case BackendDAE.EQUATIONSYSTEM(jac=jac, jacType=BackendDAE.JAC_GENERIC())
+      else "";
+    end match;
+  else
+    // Only error message.
+    (existNonLin, name) := match (inComp)
+      local
+        BackendDAE.Jacobian jac;
+        Boolean linear;
+        String str;
+      // Case non-linear teared equation system
+      case (BackendDAE.TORNSYSTEM(strictTearingSet=BackendDAE.TEARINGSET(jac=jac), linear=false))
+        then existNonLinIterVars(jac);
+
+      // Case non-linear non-teared equation system
+      case (BackendDAE.EQUATIONSYSTEM(jac=jac, jacType=BackendDAE.JAC_NONLINEAR()))
+        then existNonLinIterVars(jac);
+
+      // ToDo: Check if jacType=BackendDAE.JAC_GENERIC is needed
+      //case BackendDAE.EQUATIONSYSTEM(jac=jac, jacType=BackendDAE.JAC_GENERIC())
+      else (false,"");
+    end match;
+    if existNonLin then
+      msg := System.gettext("For more information set -d=initialization. In OMEdit Tools->Options->Simulation->OMCFlags, in OMNotebook call setCommandLineOptions(\"-d=initialization\")");
+      Error.addMessage(Error.INITIALIZATION_ITERATION_VARIABLES, {name, msg});
+    end if;
+  end if;
+end checkNonLinDependecies;
+
+protected function existNonLinIterVars
+  "Helper function for checkNonLinDependecies. Returns true if any non-linear
+   iteration variables without start value are contained in given jacobian."
+  input BackendDAE.Jacobian jacobian_in;
+  output Boolean existNonLin;
+  output String jacName;
+algorithm
+  (existNonLin, jacName) := match (jacobian_in)
+    local
+      list<BackendDAE.Var> diffVars, residualVars, allDiffedVars;
+      list<DAE.ComponentRef> dependentVarsCref;
+      DAE.ComponentRef varCref;
+      BackendDAE.Var var;
+      String name;
+      Boolean exist=false;
+    case BackendDAE.GENERIC_JACOBIAN(SOME((_,name,diffVars,residualVars,allDiffedVars,dependentVarsCref))) algorithm
+      // Search for non-linear variables without start value
+      for varCref in dependentVarsCref loop
+        for var in diffVars loop
+          if ComponentReference.crefEqual(varCref, var.varName) then
+            if (not BackendVariable.varHasStartValue(var)) then
+              exist:= true;
+              break;
+            end if;
+          end if;
+        end for;
+        if exist then
+          break;
+        end if;
+      end for;
+    then (exist, name);
+
+  // ToDo
+  // case BackendDAE.FULL_JACOBIAN() algorithm
+    else (false, "");
+  end match;
+end existNonLinIterVars;
+
+protected function printNonLinIterVarsAndEqs
+  "Helper function for checkNonLinDependecies. Prints relevant information regarding
+  start attributes of non linear iteration variables."
+  input BackendDAE.Jacobian jacobian;
+  input list<Integer> eqnIndices;
+  input BackendDAE.EquationArray inEqns;
+protected
+  BackendDAE.EqSystem syst;
+  BackendDAE.Shared shared;
+  list<BackendDAE.Equation> nonLinEqns = {};
+  Integer idx = 1;
+  list<BackendDAE.Var> diffVars, residualVars, allDiffedVars, nonLin = {}, nonLinStart = {}, lin = {};
+  list<DAE.ComponentRef> dependentVarsCref;
+  DAE.ComponentRef varCref;
+  BackendDAE.Var var;
+  String name;
+algorithm
+    BackendDAE.GENERIC_JACOBIAN(SOME((BackendDAE.DAE({syst}, shared),name,diffVars,residualVars,allDiffedVars,dependentVarsCref))) := jacobian;
+
+    // Get non-linear variables without start value
+    for varCref in dependentVarsCref loop
+      for var in diffVars loop
+        if ComponentReference.crefEqual(varCref, var.varName) then
+          if (not BackendVariable.varHasStartValue(var)) then
+            nonLin := var::nonLin;
+          else
+            nonLinStart := var::nonLinStart;
+          end if;
+        end if;
+      end for;
+    end for;
+    if not listEmpty(nonLin) then
+      BackendDump.dumpVarList(nonLin, "Nonlinear iteration variables with default zero start attribute in " + name + ".");
+    end if;
+    if not listEmpty(nonLinStart) then
+      BackendDump.dumpVarList(nonLinStart, "Nonlinear iteration variables with predefined start attribute in " + name + ".");
+    end if;
+
+    // Get linear variables with start value, but ignore discrete vars
+    for var in allDiffedVars loop
+      if (BackendVariable.varHasStartValue(var) and not BackendVariable.isVarDiscrete(var) ) then
+        lin := var::lin;
+      end if;
+    end for;
+    if not listEmpty(lin) then
+      BackendDump.dumpVarList(lin, "Linear iteration variables with predefined start attributes that are unrelevant in " + name + ".");
+    end if;
+
+    // Get nonlinear equations
+    for i in eqnIndices loop
+      nonLinEqns := addNonlinearEquation(ExpandableArray.get(i,inEqns),ExpandableArray.get(idx,syst.orderedEqs),syst.orderedVars, shared.globalKnownVars,nonLinEqns);
+      //BackendDump.dumpEquationList({ExpandableArray.get(i,inEqns)}, "check eq");
+      //BackendDump.dumpEquationList({ExpandableArray.get(idx,syst.orderedEqs)}, "diffed eq");
+      idx := idx + 1;
+    end for;
+    if not listEmpty(nonLinEqns) then
+      BackendDump.dumpEquationList(nonLinEqns, "Relevant nonlinear equations in " + name + ".");
+    end if;
+
+    if not (listEmpty(nonLin) and listEmpty(nonLinStart) and listEmpty(lin)) then
+      print("Info: Only non-linear iteration variables in non-linear eqation systems require start values. " +
+           "All other start values have no influence on convergence and are ignored. " +
+           "Use \"-d=dumpLoops\" to show all loops. In OMEdit Tools->Options->Simulation->OMCFlags, in "+
+           "OMNotebook call setCommandLineOptions(\"-d=dumpLoops\")\n\n");
+     end if;
+  // ToDo
+  // BackendDAE.FULL_JACOBIAN()
+end printNonLinIterVarsAndEqs;
+
+protected function addNonlinearEquation
+  "Checks an equation for nonlinearity regarding input vars and adds
+  it to a list if the check succeeds. "
+  input BackendDAE.Equation originalEqn;
+  input BackendDAE.Equation diffedEqn;
+  input BackendDAE.Variables vars;
+  input BackendDAE.Variables knownVars;
+  input output list<BackendDAE.Equation> nonlinearEquationLst;
+protected
+  Integer eqnIndex;
+  HashTable.HashTable ht;
+algorithm
+  try
+    ht := HashTable.emptyHashTable();
+    (_, (_, _, ht)) := BackendEquation.findUnknownCrefs(diffedEqn,(vars, knownVars, ht));
+
+    if not listEmpty(BaseHashTable.hashTableKeyList(ht)) then
+      nonlinearEquationLst := originalEqn::nonlinearEquationLst;
+    end if;
+  else
+    Error.addMessage(Error.INTERNAL_ERROR,{"SymbolicJacobian.addNonlinearEquation failed"});
+  end try;
+end addNonlinearEquation;
 
 protected function traverserhasEqnNonDiffParts
 "function breaks differentiation for

testsuite/simulation/modelica/initialization/OverdeterminedInitialization.Fluid.TwoVolumesFullInitial.mos
@@ -20,7 +20,5 @@ buildModel(OverdeterminedInitialization.Fluid.TwoVolumesFullInitial); getErrorSt
 // Warning: It was not possible to determine if the initialization problem is consistent, because of not evaluable parameters/start values during compile time: V1.medium.p = V1.p_start (V2.p_start = V1.p_start)
 // Warning: The initial conditions are over specified. The following 1 initial equations are redundant, so they are removed from the initialization sytem:
 //          V1.medium.p = V1.p_start.
-// Warning: Iteration variables with default zero start attribute in torn nonlinear equation system:
-//          V1.ports[2].m_flow:VARIABLE(flow=true min = -100000.0 max = 100000.0 unit = "kg/s" )  "Mass flow rate from the connection point into the component" type: Real  [2]
 // "
 // endResult

testsuite/simulation/modelica/initialization/OverdeterminedInitialization.Fluid.TwoVolumesFullInitialInconsistent.mos
@@ -28,8 +28,6 @@ simulate(OverdeterminedInitialization.Fluid.TwoVolumesFullInitialInconsistent); 
 // Warning: It was not possible to determine if the initialization problem is consistent, because of not evaluable parameters/start values during compile time: V1.medium.p = V1.p_start (V2.p_start = V1.p_start)
 // Warning: The initial conditions are over specified. The following 1 initial equations are redundant, so they are removed from the initialization sytem:
 //          V1.medium.p = V1.p_start.
-// Warning: Iteration variables with default zero start attribute in torn nonlinear equation system:
-//          V1.ports[2].m_flow:VARIABLE(flow=true min = -100000.0 max = 100000.0 unit = "kg/s" )  "Mass flow rate from the connection point into the component" type: Real  [2]
 // Warning: The model contains alias variables with conflicting start and/or nominal values. It is recommended to resolve the conflicts, because otherwise the system could be hard to solve. To print the conflicting alias sets and the chosen candidates please use -d=aliasConflicts.
 // "
 // endResult

commit 3934b526b7fc930791e9d92f9999dc6a2ebe18d1

Hotfix for failing libraries from commit 85d9636
  - Suppresses error but doesn't solve the underlying problem with
    `checkNonLinDependecies` function

OMCompiler/Compiler/BackEnd/SymbolicJacobian.mo
@@ -2750,7 +2750,12 @@ algorithm
 
   // Check if all nonlinear iteration variables have start values
   if BackendDAEUtil.isInitializationDAE(inShared) then
-      checkNonLinDependecies(outComp,inEqns);
+      try
+          checkNonLinDependecies(outComp,inEqns);
+      else
+          // ToDo Fix me! Like seriously!
+          Error.addInternalError("function calculateJacobianComponent failed to check all non-linear iteration variables for start values.", sourceInfo());
+      end try;
   end if;
 end calculateJacobianComponent;
 

testsuite/simulation/modelica/initialization/OverdeterminedInitialization.Fluid.DynamicPipeInitialValues.mos
@@ -23,6 +23,7 @@ simulate(OverdeterminedInitialization.Fluid.DynamicPipeInitialValues); getErrorS
 // "
 // end SimulationResult;
 // "Warning: The model contains alias variables with conflicting start and/or nominal values. It is recommended to resolve the conflicts, because otherwise the system could be hard to solve. To print the conflicting alias sets and the chosen candidates please use -d=aliasConflicts.
+// [BackEnd/SymbolicJacobian.mo:0:0-0:0:writable] Error: Internal error function calculateJacobianComponent failed to check all non-linear iteration variables for start values.
 // Warning: Assuming fixed start value for the following 1 variables:
 //          m_flow_initial:DISCRETE(unit = "kg/s" fixed = false )  type: Real
 // Warning: The initial conditions are over specified. The following 1 initial equations are redundant, so they are removed from the initialization sytem:

testsuite/simulation/modelica/initialization/OverdeterminedInitialization.Fluid.DynamicPipeLumpedPressureInitialization.mos
@@ -17,6 +17,7 @@ buildModel(OverdeterminedInitialization.Fluid.DynamicPipeLumpedPressureInitializ
 // ""
 // {"OverdeterminedInitialization.Fluid.DynamicPipeLumpedPressureInitialization","OverdeterminedInitialization.Fluid.DynamicPipeLumpedPressureInitialization_init.xml"}
 // "Warning: The model contains alias variables with conflicting start and/or nominal values. It is recommended to resolve the conflicts, because otherwise the system could be hard to solve. To print the conflicting alias sets and the chosen candidates please use -d=aliasConflicts.
+// [BackEnd/SymbolicJacobian.mo:0:0-0:0:writable] Error: Internal error function calculateJacobianComponent failed to check all non-linear iteration variables for start values.
 // Warning: Assuming fixed start value for the following 1 variables:
 //          m_flow_initial:DISCRETE(unit = "kg/s" fixed = false )  type: Real
 // Warning: The initial conditions are over specified. The following 4 initial equations are redundant, so they are removed from the initialization sytem:

testsuite/simulation/modelica/initialization/OverdeterminedInitialization.Fluid.DynamicPipesSeriesLargeNSteadyStateInitial.mos
@@ -18,6 +18,7 @@ buildModel(OverdeterminedInitialization.Fluid.DynamicPipesSeriesLargeNSteadyStat
 // {"OverdeterminedInitialization.Fluid.DynamicPipesSeriesLargeNSteadyStateInitial","OverdeterminedInitialization.Fluid.DynamicPipesSeriesLargeNSteadyStateInitial_init.xml"}
 // "Warning: The model contains alias variables with conflicting start and/or nominal values. It is recommended to resolve the conflicts, because otherwise the system could be hard to solve. To print the conflicting alias sets and the chosen candidates please use -d=aliasConflicts.
 // Notification: The following initial equation is redundant and consistent due to simplifications in RemoveSimpleEquations and therefore removed from the initialization problem: der(pipe1.mediums[1].p) = 0.0 -> 0.0 = 0.0
+// [BackEnd/SymbolicJacobian.mo:0:0-0:0:writable] Error: Internal error function calculateJacobianComponent failed to check all non-linear iteration variables for start values.
 // Warning: The initial conditions are over specified. The following 1 initial equations are redundant, so they are removed from the initialization sytem:
 //          $DER.pipe1.mediums[50].p = 0.0.
 // "

testsuite/simulation/modelica/initialization/OverdeterminedInitialization.Fluid.DynamicPipesSeriesSteadyStateInitial.mos
@@ -18,6 +18,7 @@ buildModel(OverdeterminedInitialization.Fluid.DynamicPipesSeriesSteadyStateIniti
 // {"OverdeterminedInitialization.Fluid.DynamicPipesSeriesSteadyStateInitial","OverdeterminedInitialization.Fluid.DynamicPipesSeriesSteadyStateInitial_init.xml"}
 // "Warning: The model contains alias variables with conflicting start and/or nominal values. It is recommended to resolve the conflicts, because otherwise the system could be hard to solve. To print the conflicting alias sets and the chosen candidates please use -d=aliasConflicts.
 // Notification: The following initial equation is redundant and consistent due to simplifications in RemoveSimpleEquations and therefore removed from the initialization problem: der(pipe1.mediums[1].p) = 0.0 -> 0.0 = 0.0
+// [BackEnd/SymbolicJacobian.mo:0:0-0:0:writable] Error: Internal error function calculateJacobianComponent failed to check all non-linear iteration variables for start values.
 // Warning: The initial conditions are over specified. The following 1 initial equations are redundant, so they are removed from the initialization sytem:
 //          $DER.pipe1.mediums[5].p = 0.0.
 // "

testsuite/simulation/modelica/initialization/OverdeterminedInitialization.Fluid.TwoVolumesFullInitial.mos
@@ -18,6 +18,7 @@ buildModel(OverdeterminedInitialization.Fluid.TwoVolumesFullInitial); getErrorSt
 // {"OverdeterminedInitialization.Fluid.TwoVolumesFullInitial","OverdeterminedInitialization.Fluid.TwoVolumesFullInitial_init.xml"}
 // "Warning: The model contains alias variables with conflicting start and/or nominal values. It is recommended to resolve the conflicts, because otherwise the system could be hard to solve. To print the conflicting alias sets and the chosen candidates please use -d=aliasConflicts.
 // Warning: It was not possible to determine if the initialization problem is consistent, because of not evaluable parameters/start values during compile time: V1.medium.p = V1.p_start (V2.p_start = V1.p_start)
+// [BackEnd/SymbolicJacobian.mo:0:0-0:0:writable] Error: Internal error function calculateJacobianComponent failed to check all non-linear iteration variables for start values.
 // Warning: The initial conditions are over specified. The following 1 initial equations are redundant, so they are removed from the initialization sytem:
 //          V1.medium.p = V1.p_start.
 // "

testsuite/simulation/modelica/initialization/OverdeterminedInitialization.Fluid.TwoVolumesFullInitialInconsistent.mos
@@ -26,6 +26,7 @@ simulate(OverdeterminedInitialization.Fluid.TwoVolumesFullInitialInconsistent); 
 // end SimulationResult;
 // "Warning: The model contains alias variables with conflicting start and/or nominal values. It is recommended to resolve the conflicts, because otherwise the system could be hard to solve. To print the conflicting alias sets and the chosen candidates please use -d=aliasConflicts.
 // Warning: It was not possible to determine if the initialization problem is consistent, because of not evaluable parameters/start values during compile time: V1.medium.p = V1.p_start (V2.p_start = V1.p_start)
+// [BackEnd/SymbolicJacobian.mo:0:0-0:0:writable] Error: Internal error function calculateJacobianComponent failed to check all non-linear iteration variables for start values.
 // Warning: The initial conditions are over specified. The following 1 initial equations are redundant, so they are removed from the initialization sytem:
 //          V1.medium.p = V1.p_start.
 // Warning: The model contains alias variables with conflicting start and/or nominal values. It is recommended to resolve the conflicts, because otherwise the system could be hard to solve. To print the conflicting alias sets and the chosen candidates please use -d=aliasConflicts.

testsuite/simulation/modelica/initialization/OverdeterminedInitialization.Fluid.TwoVolumesFullSteadyStatePressureAndTemperature.mos
@@ -17,6 +17,7 @@ buildModel(OverdeterminedInitialization.Fluid.TwoVolumesFullSteadyStatePressureA
 // ""
 // {"OverdeterminedInitialization.Fluid.TwoVolumesFullSteadyStatePressureAndTemperature","OverdeterminedInitialization.Fluid.TwoVolumesFullSteadyStatePressureAndTemperature_init.xml"}
 // "Warning: The model contains alias variables with conflicting start and/or nominal values. It is recommended to resolve the conflicts, because otherwise the system could be hard to solve. To print the conflicting alias sets and the chosen candidates please use -d=aliasConflicts.
+// [BackEnd/SymbolicJacobian.mo:0:0-0:0:writable] Error: Internal error function calculateJacobianComponent failed to check all non-linear iteration variables for start values.
 // Warning: The initial conditions are over specified. The following 1 initial equations are redundant, so they are removed from the initialization sytem:
 //          $DER.V1.medium.p = 0.0.
 // "