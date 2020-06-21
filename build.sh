a2011f4f0977341fe0334cdaad9565b88f828891
ppenguin <hieronymusv@gmail.com>
2020-06-21 20:02:29 +0200

    Update build.sh to configure for all om libraries

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # for configure include the system's ipopt/coin include paths
 # just in case (is pkgconfig used?)
 autoconf
-./configure --prefix=/opt/OpenModelica --without-ipopt \
+./configure --prefix=/opt/OpenModelica --without-ipopt --with-omlibrary=all \
     CFLAGS="-I/usr/include/qwt -I/usr/include/coin-or -I/usr/include/coin -L/usr/lib"
