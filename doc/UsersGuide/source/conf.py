6e4764fe73beb183105557a56babe37ab1e8d8e3
Martin Sjölund <martin.sjolund@liu.se>
2020-10-01 16:00:51 +0200

    Also include API/etc in OMSimulator documentation

--- doc/UsersGuide/source/conf.py
+++ doc/UsersGuide/source/conf.py
@@ -136,7 +136,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['omsimulator/*.rst']
+exclude_patterns = ['omsimulator/*.rst','omsimulator/api/*.rst','api/*.rst','tmp.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
