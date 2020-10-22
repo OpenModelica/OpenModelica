e33ed3d1d9b2b7611af1306323a98646b3765367
Mahder Gebremedhin <mahge@users.noreply.github.com>
2020-10-22 14:31:02 +0200

    CMake improvments and refactoring (#6855)
    @mahge
    Refactor and format Compiler/runtime/CMakeLists
    5d30aa0
    
    @mahge
    Rename generated interface base file to .stamp.
    6db7ddd
    
      - It is used as a baseline for checking if interface of MM file is
        changed. Its timestamp is also used to signify that the interface has
        been checked since the MM file was modified. So stamp is a more
        appropriate extension.
    
    @mahge
    Add the generated directory as include directory.
    a6743d1
    
      - The antrl generated files go into a folder in the build tree.
        This was not added to include before. It was working only because the
        normal (autotools, non-cmake) build generated those files in the
        source directory and the header was picked.
    
      - We add them before the current source directory to make sure that they
        are included even if there are files generated by the normal autotools
        build.
    
    @mahge
    Refactor and format Parser/CMakeLists
    0f08730
    
      - The file containing macros is now moved to .cmake directory.
      - macros have been prefixed with omc_

--- OMCompiler/Compiler/.cmake/dep_toucher.cmake
+++ OMCompiler/Compiler/.cmake/dep_toucher.cmake
@@ -3,6 +3,6 @@
 file(READ ${REV_DEP_FILE} DEPENDEE_LIST)
 
 foreach(OMC_MM_SOURCE ${DEPENDEE_LIST})
-    # message("Touching ${INTERFACE_FILES_DIR}/${OMC_MM_SOURCE}.interface.mo.tmp")
-    file(TOUCH_NOCREATE ${INTERFACE_FILES_DIR}/${OMC_MM_SOURCE}.interface.mo.tmp)
+    # message("Touching ${INTERFACE_FILES_DIR}/${OMC_MM_SOURCE}.interface.mo.stamp")
+    file(TOUCH_NOCREATE ${INTERFACE_FILES_DIR}/${OMC_MM_SOURCE}.interface.mo.stamp)
 endforeach()
