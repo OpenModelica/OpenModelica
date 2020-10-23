aa1734ebdb0321e7034e10e606f77fa8f89d1a97
Lennart Ochel <lennart.ochel@liu.se>
2020-10-23 14:26:10 +0200

    some simplifications :D

--- OMEdit/OMEditLIB/OMS/ElementPropertiesDialog.cpp
+++ OMEdit/OMEditLIB/OMS/ElementPropertiesDialog.cpp
@@ -415,23 +415,32 @@ bool ElementPropertiesDialog::eventFilter(QObject *pObject, QEvent *pEvent)
 {
   QLineEdit *pLineEdit = qobject_cast<QLineEdit*>(pObject);
 
-  if (pLineEdit && pEvent->type() == QEvent::FocusOut) {
-    if (pLineEdit->text().isEmpty()) {
-      // search the lineEdit index in parameters or inputs
-      int parameterIndex = mParameterLineEdits.indexOf(pLineEdit);
-      int inputIndex = mInputLineEdits.indexOf(pLineEdit);
-
-      if (parameterIndex != -1) {
-        QString parameterLabelText = mParameterLabels.at(parameterIndex)->text();
-        deleteStartValueAndRestoreDefault(parameterLabelText, pLineEdit);
-      }
-
-      if (inputIndex != -1) {
-        QString inputLabelText = mInputLabels.at(inputIndex)->text();
-        deleteStartValueAndRestoreDefault(inputLabelText, pLineEdit);
-      }
-    }
+  if (pLineEdit && pEvent->type() != QEvent::FocusOut) {
+    return QWidget::eventFilter(pObject, pEvent);
   }
+
+  if (!pLineEdit->text().isEmpty()) {
+    return QWidget::eventFilter(pObject, pEvent);
+  }
+
+  if (!mpComponent->getLibraryTreeItem()->getOMSElement() || !mpComponent->getLibraryTreeItem()->getOMSElement()->connectors) {
+    return QWidget::eventFilter(pObject, pEvent);
+  }
+
+  // search the lineEdit index in parameters
+  int parameterIndex = mParameterLineEdits.indexOf(pLineEdit);
+  if (parameterIndex != -1) {
+    QString parameterLabelText = mParameterLabels.at(parameterIndex)->text();
+    deleteStartValueAndRestoreDefault(parameterLabelText, pLineEdit);
+  }
+
+  // search the lineEdit index in inputs
+  int inputIndex = mInputLineEdits.indexOf(pLineEdit);
+  if (inputIndex != -1) {
+    QString inputLabelText = mInputLabels.at(inputIndex)->text();
+    deleteStartValueAndRestoreDefault(inputLabelText, pLineEdit);
+  }
+
   return QWidget::eventFilter(pObject, pEvent);
 }
 
@@ -441,45 +450,44 @@ bool ElementPropertiesDialog::eventFilter(QObject *pObject, QEvent *pEvent)
  */
 void ElementPropertiesDialog::deleteStartValueAndRestoreDefault(const QString name, QLineEdit * pLineEdit)
 {
-  if (mpComponent->getLibraryTreeItem()->getOMSElement() && mpComponent->getLibraryTreeItem()->getOMSElement()->connectors) {
-    oms_connector_t** pInterfaces = mpComponent->getLibraryTreeItem()->getOMSElement()->connectors;
-    for (int i = 0 ; pInterfaces[i] ; i++) {
-      QString nameStructure = QString("%1.%2").arg(mpComponent->getLibraryTreeItem()->getNameStructure(), QString(pInterfaces[i]->name));
-      if (QString(pInterfaces[i]->name).compare(name) == 0) {
-        if (oms_causality_parameter == pInterfaces[i]->causality || oms_causality_input == pInterfaces[i]->causality) {
-          OMSProxy::instance()->omsDelete(nameStructure + ":start");
-          //restoreDefaultStartValueHelper(pInterfaces[i], nameStructure, pLineEdit);
-          bool status = false;
-          if (pInterfaces[i]->type == oms_signal_type_real) {
-            double value;
-            if ((status = OMSProxy::instance()->getReal(nameStructure, &value))) {
-              pLineEdit->setText(QString::number(value));
-            }
-          } else if (pInterfaces[i]->type == oms_signal_type_integer) {
-            int value;
-            if ((status = OMSProxy::instance()->getInteger(nameStructure, &value))) {
-              pLineEdit->setText(QString::number(value));
-            }
-          } else if (pInterfaces[i]->type == oms_signal_type_boolean) {
-            bool value;
-            if ((status = OMSProxy::instance()->getBoolean(nameStructure, &value))) {
-              pLineEdit->setText(QString::number(value));
-            }
-          } else if (pInterfaces[i]->type == oms_signal_type_string) {
-            qDebug() << "ElementPropertiesDialog::ElementPropertiesDialog() oms_signal_type_string not implemented yet.";
-          } else if (pInterfaces[i]->type == oms_signal_type_enum) {
-            qDebug() << "ElementPropertiesDialog::ElementPropertiesDialog() oms_signal_type_enum not implemented yet.";
-          } else if (pInterfaces[i]->type == oms_signal_type_bus) {
-            qDebug() << "ElementPropertiesDialog::ElementPropertiesDialog() oms_signal_type_bus not implemented yet.";
-          } else {
-            qDebug() << "ElementPropertiesDialog::ElementPropertiesDialog() unknown oms_signal_type_enu_t.";
-          }
-          if (!status) {
-            pLineEdit->setPlaceholderText("unknown");
-          }
-          return;
-        }
-      }
-    }
+  oms_connector_t** pInterfaces = mpComponent->getLibraryTreeItem()->getOMSElement()->connectors;
+  int i=0;
+  while (pInterfaces[i] && QString(pInterfaces[i]->name).compare(name) != 0) {
+    i++;
   }
+
+  if (!pInterfaces[i]) {
+    return;
+  }
+
+  if (oms_causality_parameter != pInterfaces[i]->causality && oms_causality_input != pInterfaces[i]->causality) {
+    return;
+  }
+
+  QString nameStructure = QString("%1.%2").arg(mpComponent->getLibraryTreeItem()->getNameStructure(), QString(pInterfaces[i]->name));
+  OMSProxy::instance()->omsDelete(nameStructure + ":start");
+
+  bool status = false;
+  if (pInterfaces[i]->type == oms_signal_type_real) {
+    double value;
+    if ((status = OMSProxy::instance()->getReal(nameStructure, &value))) {
+      pLineEdit->setText(QString::number(value));
+    }
+  } else if (pInterfaces[i]->type == oms_signal_type_integer) {
+    int value;
+    if ((status = OMSProxy::instance()->getInteger(nameStructure, &value))) {
+      pLineEdit->setText(QString::number(value));
+    }
+  } else if (pInterfaces[i]->type == oms_signal_type_boolean) {
+    bool value;
+    if ((status = OMSProxy::instance()->getBoolean(nameStructure, &value))) {
+      pLineEdit->setText(QString::number(value));
+    }
+  } else {
+    qDebug() << "ElementPropertiesDialog::deleteStartValueAndRestoreDefault() unknown signal type";
+  }
+  if (!status) {
+    pLineEdit->setPlaceholderText("unknown");
+  }
+  return;
 }
