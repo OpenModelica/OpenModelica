583fa2da264f21e8745273af331674f0857b61f3
Adeel Asghar <adeel.asghar@liu.se>
2021-03-04 12:33:00 +0100

    Use monospaced font for structured simulation output
    Fixes #7241

--- OMEdit/OMEditLIB/Simulation/SimulationOutputWidget.cpp
+++ OMEdit/OMEditLIB/Simulation/SimulationOutputWidget.cpp
@@ -73,6 +73,7 @@ SimulationOutputTree::SimulationOutputTree(SimulationOutputWidget *pSimulationOu
   setMouseTracking(true); /* important for Debug more links. */
   setSelectionMode(QAbstractItemView::ExtendedSelection);
   setContextMenuPolicy(Qt::CustomContextMenu);
+  setFont(QFont(Helper::monospacedFontInfo.family()));
   connect(this, SIGNAL(customContextMenuRequested(QPoint)), SLOT(showContextMenu(QPoint)));
   connect(header(), SIGNAL(sectionResized(int,int,int)), SLOT(callLayoutChanged(int,int,int)));
   // create actions
