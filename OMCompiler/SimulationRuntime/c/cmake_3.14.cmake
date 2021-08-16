6aa92faca83cc5e1ddb3faaecec2741dde072bb0
Mahder Gebremedhin <mahder.gebremedhin@liu.se>
2021-08-16 18:10:30 +0200

    [cmake] Add a separate 'memory' library.
      - This used to be part of OpenModelicaRuntimeC library. the code provides
        memory related (garbage collection and memory_pool) functionality. Due
        to this OpenModelicaRuntimeC was being linked from other libraries
        just to get this functionality.
    
        the memory related functionality is now a separate lib. This lib can
        be used in places where we need the memory management related interface
        and functionality of omc.
    
       - Misc
         Remove unused library omcgraphstream-boot

--- OMCompiler/SimulationRuntime/c/cmake_3.14.cmake
+++ OMCompiler/SimulationRuntime/c/cmake_3.14.cmake
@@ -1,7 +1,5 @@
 cmake_minimum_required(VERSION 3.14)
 
-project(SimulationRuntimeC)
-
 file(GLOB OMC_SIMRT_UTIL_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/util/*.c)
 file(GLOB OMC_SIMRT_UTIL_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/util/*.h)
 
@@ -14,32 +12,46 @@ file(GLOB OMC_SIMRT_GC_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/gc/*.h)
 file(GLOB OMC_SIMRT_FMI_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/fmi/*.c)
 file(GLOB OMC_SIMRT_FMI_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/fmi/*.h)
 
+
+
+# ######################################################################################################################
+# Library: omcmemory
+## This tiny library provides the memory related functionality of OM (garbage collection and memory_pool).
+## The reason it is separated is because its functionality is clearly defined and should not be part of
+## a bunch of other libraries. For example there is no need to link to OpenModelicaRuntimeC just to get GC
+## functionality in Compiler/runtime.
+add_library(omcmemory STATIC)
+add_library(omc::simrt::memory ALIAS omcmemory)
+
+target_sources(omcmemory PRIVATE ${OMC_SIMRT_GC_SOURCES})
+target_link_libraries(omcmemory PUBLIC omc::3rd::omcgc)
+target_include_directories(omcmemory PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
+
+
 # ######################################################################################################################
 # Library: OpenModelicaRuntimeC
 add_library(OpenModelicaRuntimeC STATIC)
 add_library(omc::simrt::runtime ALIAS OpenModelicaRuntimeC)
 
-target_sources(OpenModelicaRuntimeC PRIVATE ${OMC_SIMRT_UTIL_SOURCES} ${OMC_SIMRT_META_SOURCES} ${OMC_SIMRT_GC_SOURCES})
-target_link_libraries(OpenModelicaRuntimeC PUBLIC omc::3rd::omcgc)
+target_sources(OpenModelicaRuntimeC PRIVATE ${OMC_SIMRT_UTIL_SOURCES} ${OMC_SIMRT_META_SOURCES})
+target_link_libraries(OpenModelicaRuntimeC PUBLIC omc::simrt::memory)
 
 if(WIN32)
   target_link_libraries(OpenModelicaRuntimeC PUBLIC dbghelp)
   target_link_libraries(OpenModelicaRuntimeC PUBLIC regex)
 endif(WIN32)
 
-target_include_directories(OpenModelicaRuntimeC PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
 
 install(TARGETS OpenModelicaRuntimeC)
 
 # ######################################################################################################################
 # Library: OpenModelicaFMIRuntimeC
-add_library(OpenModelicaFMIRuntimeC STATIC ${OMC_SIMRT_FMI_SOURCES})
+add_library(OpenModelicaFMIRuntimeC STATIC)
 add_library(omc::simrt::fmiruntime ALIAS OpenModelicaFMIRuntimeC)
 
 target_sources(OpenModelicaFMIRuntimeC PRIVATE ${OMC_SIMRT_FMI_SOURCES})
 
 target_link_libraries(OpenModelicaFMIRuntimeC PUBLIC omc::3rd::fmilib::static)
-target_link_libraries(OpenModelicaFMIRuntimeC PUBLIC omc::simrt::runtime)
 
 install(TARGETS OpenModelicaFMIRuntimeC)
 
