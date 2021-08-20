81537a472c9f54ea14ecf335343005f18f4e83af
Mahder Gebremedhin <mahge@users.noreply.github.com>
2021-08-20 16:09:40 +0200

    Add omc_fread() to wrap fread() (#7780)
    * Add omc_fread() to wrap fread()
    
      - We tend to ignore the return value from fread() which leads to warnings.
    
      - It is also good to make sure errors are always checked and reported.
    
      - It is also good to wrap the common functions to reduce the amount of
        error check code scattered around.
    
    * Allow early EOF if needed.
    
      - Sometimes we do not know how big the file is. We want to read chunks
        from it while it has some. In this case it is okay if we reach EOF
        before we read the whole chunk on the last read call.
    
      - It is, of course, possible to determine how big the file is and then
        read just that much. Do which seems convenient for your case.

--- OMCompiler/SimulationRuntime/c/util/omc_file.h
+++ OMCompiler/SimulationRuntime/c/util/omc_file.h
@@ -88,11 +88,19 @@ extern "C" {
 #endif /* mingw and msvc */
 
 FILE* omc_fopen(const char *filename, const char *mode);
+
+/// The last argument allow_early_eof specifies wheather the call is okay or not with reaching
+/// EOF before reading the specified amount. Set it to 1 if you do not exactly know how much to read
+/// and would not mind if the file ends before 'count' elements are read from it.
+/// If you are not sure what to do start by passing 0.
+size_t omc_fread(void *buffer, size_t size, size_t count, FILE *stream, int allow_early_eof);
+
 #if defined(__MINGW32__) || defined(_MSC_VER)
 int omc_stat(const char *filename, struct _stat *statbuf);
 #else
 int omc_stat(const char *filename, struct stat *statbuf);
 #endif
+
 int omc_unlink(const char *filename);
 
 #ifdef __cplusplus
