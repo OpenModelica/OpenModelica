f621802d89b3a8d6da954dd12e80605c4834019f
Mahder Gebremedhin <mahder.gebremedhin@liu.se>
2021-08-24 12:32:23 +0200

    [cmake] Build the needed runtime libs as shared.
      - The shared libs that exist right now are:
    
      - `libomcmemory`: A lib for garbage collection and memory_pool related
        functionality. This is used by both OMC itself and simulation
        executables.
    
        There are two reasons why this is a separate shared lib.
        It is SEPARATE lib because it is has a clear purpose and its functionality
        is used all over the OpenModelica code. So it is nice to have a small
        library that can be linked everywhere.
    
        It is made a SHARED lib because we really do not want to link to static
        GC lib  functionality from two different DLLs as this will create two
        instances of the garbage collector. This will not work in anything
        multi-threaded if you are even a little unlucky since Boehm GC depends
        a lot on global variables.
    
        We really want to have only one instance of Boehm GC at all times
        if we can achieve that.
    
        The best debugging strategy is just initial paranoia and refusal to give
        any room for complications :)
    
      - `libOpenModelicaRuntime`: A lib for common runtime functionality
        (e.g the functionality in c/util or c/meta goes here). This (like
        `libomcmemory`) is used by both OMC itself and simulation executables.
    
      - `libSimulationRuntimeC`: Contains everything solver related including
        linearization and data reconciliation support. This the library needed
        for normal simulation executables generated by OMC.
        **Depends on**: `libOpenModelicaRuntimeC` and `libomcmemory`.
    
      - `libOpenModelicaOptimization`: A lib for optimization enabled simulation.
        will incorporate our optimization code as well as `ipopt` and `coinmumps.`
        **Depends on**: libSimulationRuntimeC.

--- OMCompiler/SimulationRuntime/c/cmake_3.14.cmake
+++ OMCompiler/SimulationRuntime/c/cmake_3.14.cmake
@@ -29,7 +29,10 @@ file(GLOB_RECURSE OMC_SIMRT_OPTIMIZATION_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/opt
 file(GLOB OMC_SIMRT_FMI_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/fmi/*.c)
 file(GLOB OMC_SIMRT_FMI_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/fmi/*.h)
 
-
+## This should be set. The reason it is not now is because there is a cyclic dependency between
+## gc/ and meta/ sources which are part of two different libraries at the moment. Either fix the
+## code to remove the cyclic dependency or move meta/ sources out of libOpenModelicaRuntimeC and into libomcmemory
+# set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-undefined")
 
 # ######################################################################################################################
 # Library: omcmemory
@@ -37,7 +40,7 @@ file(GLOB OMC_SIMRT_FMI_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/fmi/*.h)
 ## The reason it is separated is because its functionality is clearly defined and should not be part of
 ## a bunch of other libraries. For example there is no need to link to OpenModelicaRuntimeC just to get GC
 ## functionality in Compiler/runtime.
-add_library(omcmemory STATIC)
+add_library(omcmemory SHARED)
 add_library(omc::simrt::memory ALIAS omcmemory)
 
 target_sources(omcmemory PRIVATE ${OMC_SIMRT_GC_SOURCES})
@@ -48,7 +51,7 @@ install(TARGETS omcmemory)
 
 # ######################################################################################################################
 # Library: OpenModelicaRuntimeC
-add_library(OpenModelicaRuntimeC STATIC)
+add_library(OpenModelicaRuntimeC SHARED)
 add_library(omc::simrt::runtime ALIAS OpenModelicaRuntimeC)
 
 target_sources(OpenModelicaRuntimeC PRIVATE ${OMC_SIMRT_UTIL_SOURCES} ${OMC_SIMRT_META_SOURCES})
@@ -64,10 +67,15 @@ install(TARGETS OpenModelicaRuntimeC)
 
 # ######################################################################################################################
 # Library: OpenModelicaFMIRuntimeC
+## This library is built as a static library and contains everything needed to run an OpenModelica FMU.
+## Therefore it has to include the functionality from the other libraries.
+## It is not complete yet. I have to see what needs to go in here.
 add_library(OpenModelicaFMIRuntimeC STATIC)
 add_library(omc::simrt::fmiruntime ALIAS OpenModelicaFMIRuntimeC)
 
-target_sources(OpenModelicaFMIRuntimeC PRIVATE ${OMC_SIMRT_FMI_SOURCES})
+target_sources(OpenModelicaFMIRuntimeC PRIVATE ${OMC_SIMRT_FMI_SOURCES}
+                                               $<TARGET_OBJECTS:OpenModelicaRuntimeC>
+                                               $<TARGET_OBJECTS:omcmemory>)
 
 target_link_libraries(OpenModelicaFMIRuntimeC PUBLIC omc::3rd::fmilib)
 
@@ -75,25 +83,8 @@ install(TARGETS OpenModelicaFMIRuntimeC)
 
 
 # ######################################################################################################################
-# Library: OptimizationRuntime
-## This is now separated from SimulationRuntimeC. Just for clarity. It can be put back in there if needed.
-## However having it as a separate lib will allow us to remove it based on an option. This means we can
-## also remove the need for ipopt and mumps if this is disabled.
-add_library(OptimizationRuntime STATIC)
-add_library(omc::simrt::optimize ALIAS OptimizationRuntime)
-
-target_sources(OptimizationRuntime PRIVATE ${OMC_SIMRT_OPTIMIZATION_SOURCES})
-
-target_link_libraries(OptimizationRuntime PUBLIC omc::config)
-target_link_libraries(OptimizationRuntime PUBLIC omc::simrt::memory)
-target_link_libraries(OptimizationRuntime PUBLIC omc::3rd::ipopt)
-
-install(TARGETS OptimizationRuntime)
-
-
-# ######################################################################################################################
 # Library: SimulationRuntimeC
-add_library(SimulationRuntimeC STATIC)
+add_library(SimulationRuntimeC SHARED)
 add_library(omc::simrt::simruntime ALIAS SimulationRuntimeC)
 
 target_sources(SimulationRuntimeC PRIVATE ${OMC_SIMRT_SIMULATION_SOURCES}
@@ -103,6 +94,7 @@ target_sources(SimulationRuntimeC PRIVATE ${OMC_SIMRT_SIMULATION_SOURCES}
 
 target_link_libraries(SimulationRuntimeC PUBLIC omc::config)
 target_link_libraries(SimulationRuntimeC PUBLIC omc::simrt::memory)
+target_link_libraries(SimulationRuntimeC PUBLIC omc::simrt::runtime)
 target_link_libraries(SimulationRuntimeC PUBLIC omc::3rd::FMIL::expat)
 target_link_libraries(SimulationRuntimeC PUBLIC omc::3rd::sundials::cvode)
 target_link_libraries(SimulationRuntimeC PUBLIC omc::3rd::sundials::idas)
@@ -123,6 +115,39 @@ target_link_libraries(SimulationRuntimeC PUBLIC omc::3rd::lis)
 target_include_directories(SimulationRuntimeC PRIVATE ${OMCompiler_SOURCE_DIR}/3rdParty/dgesv/include/)
 
 install(TARGETS SimulationRuntimeC)
+
+
+# ######################################################################################################################
+# Library: OptimizationRuntime
+## This is now separated from SimulationRuntimeC. Just for clarity. It can be put back in there if needed.
+## However having it as a separate lib will allow us to remove it based on an option. This means we can
+## also remove the need for ipopt and mumps if this is disabled.
+add_library(OptimizationRuntime SHARED)
+add_library(omc::simrt::optimize ALIAS OptimizationRuntime)
+
+target_sources(OptimizationRuntime PRIVATE ${OMC_SIMRT_OPTIMIZATION_SOURCES})
+
+target_link_libraries(OptimizationRuntime PUBLIC omc::config)
+target_link_libraries(OptimizationRuntime PUBLIC omc::simrt::memory)
+target_link_libraries(OptimizationRuntime PUBLIC omc::simrt::simruntime)
+target_link_libraries(OptimizationRuntime PUBLIC omc::3rd::ipopt)
+
+
+install(TARGETS OptimizationRuntime)
+
+
+# ######################################################################################################################
+# Library: OpenModelicaSimulation
+## This is a shared library containing everything needed for simulation. This is not intended to be used by the
+## simulation executables. If something is needed for simulation executables add it here.
+# add_library(OpenModelicaSimulation SHARED $<TARGET_OBJECTS:SimulationRuntimeC>
+#                                           $<TARGET_OBJECTS:OpenModelicaRuntimeC>)
+# add_library(omc::simrt::simulation ALIAS OpenModelicaSimulation)
+
+# target_link_libraries(OpenModelicaSimulation PUBLIC
+#                       $<TARGET_PROPERTY:SimulationRuntimeC,INTERFACE_LINK_LIBRARIES>)
+
+# install(TARGETS OpenModelicaSimulation)
 
 
 # ######################################################################################################################
