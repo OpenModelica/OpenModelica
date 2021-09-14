OpenModelica/OpenModelica

commit 853df898f6eccfeab28d61914fc6db83ff43106c

Turn error into warning if singular Jacobian for dynamic state selection
- SystemStateSelection.cpp: additionally don't reset changed flag
  (see Modelica.Mechanics.MultiBody.Examples.Constraints.UniversalConstraint)
- CodegenCpp/Old.tpl: avoid compiler warning for unreached code
- update msl32_cpp tests

OMCompiler/SimulationRuntime/cpp/Core/Solver/FactoryExport.h
@@ -9,6 +9,7 @@
 #define BOOST_EXTENSION_SOLVERSETTINGS_DECL
 #define BOOST_EXTENSION_STATESELECT_DECL
 #define BOOST_EXTENSION_MONITOR_DECL
+#define BOOST_EXTENSION_LOGGER_DECL
 
 #elif defined(OMC_BUILD) || defined(SIMSTER_BUILD)
 
@@ -16,6 +17,7 @@
 #define BOOST_EXTENSION_STATESELECT_DECL BOOST_EXTENSION_EXPORT_DECL
 #define BOOST_EXTENSION_SOLVERSETTINGS_DECL BOOST_EXTENSION_EXPORT_DECL
 #define BOOST_EXTENSION_MONITOR_DECL BOOST_EXTENSION_EXPORT_DECL
+#define BOOST_EXTENSION_LOGGER_DECL BOOST_EXTENSION_EXPORT_DECL
 #else
     error "operating system not supported"
 #endif

OMCompiler/Compiler/Template/CodegenCpp.tpl
@@ -1040,7 +1040,11 @@ case SIMCODE(modelInfo = MODELINFO(__)) then
          break;
        >>
        case ("sparse") then
-       'throw ModelicaSimulationError(MATH_FUNCTION,"Dense matrix is not activated");'
+       <<
+       case <%i1%>:
+         throw ModelicaSimulationError(MATH_FUNCTION, "Dense matrix is not activated");'
+         break;
+       >>
        else "A matrix type is not supported"
        )
        ;separator="\n")
@@ -1052,14 +1056,17 @@ case SIMCODE(modelInfo = MODELINFO(__)) then
        match jacobianMatrix case JAC_MATRIX(matrixName=name) then
        match type
        case ("dense") then
-       'throw ModelicaSimulationError(MATH_FUNCTION,"Sparse matrix is not activated");'
+       <<
+       case <%i1%>:
+         throw ModelicaSimulationError(MATH_FUNCTION, "Sparse matrix is not activated");
+         break;
+       >>
        case ("sparse") then
        <<
        case <%i1%>:
          return get<%name%>Jacobian();
          break;
        >>
-
        else "A matrix type is not supported"
        )
        ;separator="\n")

OMCompiler/Compiler/Template/CodegenCppOld.tpl
@@ -1048,7 +1048,11 @@ case SIMCODE(modelInfo = MODELINFO(__)) then
          break;
        >>
        case ("sparse") then
-       'throw ModelicaSimulationError(MATH_FUNCTION,"Dense matrix is not activated");'
+       <<
+       case <%i1%>:
+         throw ModelicaSimulationError(MATH_FUNCTION, "Dense matrix is not activated");'
+         break;
+       >>
        else "A matrix type is not supported"
        )
        ;separator="\n")
@@ -1060,14 +1064,17 @@ case SIMCODE(modelInfo = MODELINFO(__)) then
        match jacobianMatrix case JAC_MATRIX(matrixName=name) then
        match type
        case ("dense") then
-       'throw ModelicaSimulationError(MATH_FUNCTION,"Sparse matrix is not activated");'
+       <<
+       case <%i1%>:
+         throw ModelicaSimulationError(MATH_FUNCTION, "Sparse matrix is not activated");
+         break;
+       >>
        case ("sparse") then
        <<
        case <%i1%>:
          return get<%name%>Jacobian();
          break;
        >>
-
        else "A matrix type is not supported"
        )
        ;separator="\n")

OMCompiler/SimulationRuntime/cpp/Core/Solver/SystemStateSelection.cpp
@@ -8,6 +8,7 @@
 #include <Core/Solver/SystemStateSelection.h>
 #include <Core/Math/ArrayOperations.h>
 #include <Core/Math/Functions.h>
+#include <Core/Utils/extension/logger.hpp>
 
 
 SystemStateSelection::SystemStateSelection(IMixedSystem* system)
@@ -67,8 +68,7 @@ SystemStateSelection::~SystemStateSelection()
 bool SystemStateSelection::stateSelection(int switchStates)
 {
 #if defined(__vxworks)
-return true;
-
+  return true;
 #else
   if(!_initialized)
     initialize();
@@ -81,23 +81,19 @@ return true;
     const matrix_t& stateset_matrix =  _system->getStateSetJacobian(i);
 
     /* call pivoting function to select the states */
-
-
-
     memcpy(oldColPivot.get(), _colPivot[i].get(), _dimStateCanditates[i]*sizeof(int));
     memcpy(oldRowPivot.get(), _rowPivot[i].get(), _dimDummyStates[i]*sizeof(int));
 
-    const double* jac =    stateset_matrix.data().begin();
+    const double* jac = stateset_matrix.data().begin();
     int* piv=_colPivot[i].get();
 
-   double* jac_ = new double[_dimDummyStates[i]*_dimStateCanditates[i]];
-   memcpy(jac_, jac, _dimDummyStates[i]*_dimStateCanditates[i]*sizeof(double));
-
-
+    double* jac_ = new double[_dimDummyStates[i]*_dimStateCanditates[i]];
+    memcpy(jac_, jac, _dimDummyStates[i]*_dimStateCanditates[i]*sizeof(double));
 
     if((pivot(jac_, _dimDummyStates[i], _dimStateCanditates[i], _rowPivot[i].get(), _colPivot[i].get()) != 0))
     {
-      throw ModelicaSimulationError(MATH_FUNCTION,"Error, singular Jacobian for dynamic state selection at time");
+      LOGGER_WRITE("Singular Jacobian for dynamic state selection in set " + to_string(i + 1), LC_SOLVER, LL_WARNING);
+      continue;
     }
     /* if we have a new set throw event for reinitialization
     and set the A matrix for set.x=A*(states) */
@@ -106,14 +102,13 @@ return true;
     {
       memcpy(_colPivot[i].get(), oldColPivot.get(), _dimStateCanditates[i]*sizeof(int));
       memcpy(_rowPivot[i].get(), oldRowPivot.get(), _dimDummyStates[i]*sizeof(int));
-
-
     }
     delete [] jac_;
     if(res)
+    {
+      LOGGER_WRITE("Dynamic state selection changed set " + to_string(i + 1), LC_SOLVER, LL_DEBUG);
       changed = true;
-    else
-      changed = false;
+    }
   }
   return changed;
 #endif

testsuite/simulation/libraries/msl32_cpp/Modelica.Mechanics.MultiBody.Examples.Constraints.RevoluteConstraint.mos
@@ -10,7 +10,7 @@
 runScript("../common/ModelTestingDefaults.mos"); getErrorString();
 
 setMatchingAlgorithm("MC21A"); getErrorString();
-modelTestingType := OpenModelicaModelTesting.Kind.Compilation;
+modelTestingType := OpenModelicaModelTesting.Kind.SimpleSimulation;
 modelName := $TypeName(Modelica.Mechanics.MultiBody.Examples.Constraints.RevoluteConstraint);
 compareVars :=
 {
@@ -37,13 +37,15 @@ runScript(modelTesting);getErrorString();
 // ""
 // true
 // ""
-// OpenModelicaModelTesting.Kind.Compilation
+// OpenModelicaModelTesting.Kind.SimpleSimulation
 // Modelica.Mechanics.MultiBody.Examples.Constraints.RevoluteConstraint
 // {"bodyOfJoint.body.w_a[2]","joint.phi","freeMotionScalarInit.initAngle.angle[1]","freeMotionScalarInit.initAngle.angle[2]","freeMotionScalarInit.initAngle.angle[3]","bodyOfConstraint.body.w_a[1]","bodyOfConstraint.body.w_a[2]","bodyOfConstraint.body.w_a[3]","freeMotionScalarInit.angle_d_1","freeMotionScalarInit.angle_d_2","freeMotionScalarInit.angle_d_3"}
 // OpenModelicaModelTesting.SimulationRuntime.Cpp
-// Compilation succeeded
+// Simulation options: startTime = 0.0, stopTime = 10.0, numberOfIntervals = 500, tolerance = 1e-06, method = 'dassl', fileNamePrefix = 'Modelica.Mechanics.MultiBody.Examples.Constraints.RevoluteConstraint', options = '', outputFormat = 'mat', variableFilter = 'time|bodyOfJoint.body.w_a.2.|joint.phi|freeMotionScalarInit.initAngle.angle.1.|freeMotionScalarInit.initAngle.angle.2.|freeMotionScalarInit.initAngle.angle.3.|bodyOfConstraint.body.w_a.1.|bodyOfConstraint.body.w_a.2.|bodyOfConstraint.body.w_a.3.|freeMotionScalarInit.angle_d_1|freeMotionScalarInit.angle_d_2|freeMotionScalarInit.angle_d_3', cflags = '', simflags = ' -emit_protected'
+// Result file: Modelica.Mechanics.MultiBody.Examples.Constraints.RevoluteConstraint_res.mat
 // Warning: The model contains alias variables with redundant start and/or conflicting nominal values. It is recommended to resolve the conflicts, because otherwise the system could be hard to solve. To print the conflicting alias sets and the chosen candidates please use -d=aliasConflicts.
 // [Modelica 3.2.1+maint.om/Mechanics/MultiBody/Joints.mo:260:5-270:3:writable] Warning: Variable joint.phi has attribute stateSelect=StateSelect.never, but was selected as a state
+//
 // "true
 // "
 // ""

testsuite/simulation/libraries/msl32_cpp/Modelica.Mechanics.MultiBody.Examples.Constraints.UniversalConstraint.mos
@@ -9,7 +9,7 @@
 
 runScript("../common/ModelTestingDefaults.mos"); getErrorString();
 
-modelTestingType := OpenModelicaModelTesting.Kind.Compilation;
+modelTestingType := OpenModelicaModelTesting.Kind.VerifiedSimulation;
 modelName := $TypeName(Modelica.Mechanics.MultiBody.Examples.Constraints.UniversalConstraint);
 compareVars :=
 {
@@ -37,12 +37,15 @@ runScript(modelTesting);getErrorString();
 // "true
 // "
 // ""
-// OpenModelicaModelTesting.Kind.Compilation
+// OpenModelicaModelTesting.Kind.VerifiedSimulation
 // Modelica.Mechanics.MultiBody.Examples.Constraints.UniversalConstraint
 // {"joint.phi_a","joint.phi_b","joint.w_a","joint.w_b","freeMotionScalarInit.angle_3","freeMotionScalarInit.initAngle.angle[1]","freeMotionScalarInit.initAngle.angle[2]","freeMotionScalarInit.initAngle.angle[3]","bodyOfConstraint.body.w_a[1]","bodyOfConstraint.body.w_a[2]","bodyOfConstraint.body.w_a[3]","freeMotionScalarInit.angle_d_1","freeMotionScalarInit.angle_d_2","freeMotionScalarInit.angle_d_3"}
 // OpenModelicaModelTesting.SimulationRuntime.Cpp
-// Compilation succeeded
+// Simulation options: startTime = 0.0, stopTime = 10.0, numberOfIntervals = 500, tolerance = 1e-06, method = 'dassl', fileNamePrefix = 'Modelica.Mechanics.MultiBody.Examples.Constraints.UniversalConstraint', options = '', outputFormat = 'mat', variableFilter = 'time|joint.phi_a|joint.phi_b|joint.w_a|joint.w_b|freeMotionScalarInit.angle_3|freeMotionScalarInit.initAngle.angle.1.|freeMotionScalarInit.initAngle.angle.2.|freeMotionScalarInit.initAngle.angle.3.|bodyOfConstraint.body.w_a.1.|bodyOfConstraint.body.w_a.2.|bodyOfConstraint.body.w_a.3.|freeMotionScalarInit.angle_d_1|freeMotionScalarInit.angle_d_2|freeMotionScalarInit.angle_d_3', cflags = '', simflags = ' -emit_protected'
+// Result file: Modelica.Mechanics.MultiBody.Examples.Constraints.UniversalConstraint_res.mat
+// Files Equal!
 // Warning: The model contains alias variables with redundant start and/or conflicting nominal values. It is recommended to resolve the conflicts, because otherwise the system could be hard to solve. To print the conflicting alias sets and the chosen candidates please use -d=aliasConflicts.
+//
 // "true
 // "
 // ""

testsuite/simulation/libraries/msl32_cpp/Modelica.Mechanics.Rotational.Examples.LossyGearDemo2.mos
@@ -40,7 +40,7 @@ runScript(modelTesting);getErrorString();
 // Result file: Modelica.Mechanics.Rotational.Examples.LossyGearDemo2_res.mat
 // Messages: ERROR  : init  : SimManager: Could not initialize system
 // ERROR  : init  : SimManager: Nonlinear solver 62 stopped at time 0 with error in algloop solver: 
-// error solving nonlinear system (iteration limit: 50)
+// Can't get sufficient decrease of solution
 // ERROR  : solver: SimController: Simulation failed using nonlinear solver newton
 // ERROR  : solver: SimController: Recovering with nonlinear solver kinsol
 //

commit aa9f3aa7802d7532701616ed5567004b8c2cfdd5

Fix Windows build with logs for dynamic state selection (issue #7893)
This was wrongly introduced with commit 853df898f6eccfeab28d61914fc6db83ff43106c.

OMCompiler/SimulationRuntime/cpp/Core/Solver/FactoryExport.h
@@ -5,19 +5,19 @@
  */
 #if defined(__vxworks) || defined(__TRICORE__) || defined(RUNTIME_STATIC_LINKING)
 
+#define BOOST_EXTENSION_LOGGER_DECL
 #define BOOST_EXTENSION_SOLVER_DECL
 #define BOOST_EXTENSION_SOLVERSETTINGS_DECL
 #define BOOST_EXTENSION_STATESELECT_DECL
 #define BOOST_EXTENSION_MONITOR_DECL
-#define BOOST_EXTENSION_LOGGER_DECL
 
 #elif defined(OMC_BUILD) || defined(SIMSTER_BUILD)
 
+#define BOOST_EXTENSION_LOGGER_DECL BOOST_EXTENSION_IMPORT_DECL
 #define BOOST_EXTENSION_SOLVER_DECL BOOST_EXTENSION_EXPORT_DECL
 #define BOOST_EXTENSION_STATESELECT_DECL BOOST_EXTENSION_EXPORT_DECL
 #define BOOST_EXTENSION_SOLVERSETTINGS_DECL BOOST_EXTENSION_EXPORT_DECL
 #define BOOST_EXTENSION_MONITOR_DECL BOOST_EXTENSION_EXPORT_DECL
-#define BOOST_EXTENSION_LOGGER_DECL BOOST_EXTENSION_EXPORT_DECL
 #else
     error "operating system not supported"
 #endif