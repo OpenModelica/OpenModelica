a07c0d7198160da907c2ec7037d0a0be7515b41a
Mahder Gebremedhin <mahder.geb@gmail.com>
2021-10-05 12:06:42 +0300

    [cmake] Improve CMake config for CPP runtime.
      - Add libraries
        - libOMCppDASSL
        - libOMCppDgesv
        - libOMCppNewton
    
      - Improve generation of config file LibrariesConfig.h
    
      - All tests that use the shared build of CPP runtime are passing now.

--- OMCompiler/SimulationRuntime/cpp/cmake_3.14.cmake
+++ OMCompiler/SimulationRuntime/cpp/cmake_3.14.cmake
@@ -9,11 +9,6 @@ set(CMAKE_INSTALL_LIBDIR ${CMAKE_INSTALL_LIBDIR}/cpp)
 set(CMAKE_INSTALL_INCLUDEDIR ${CMAKE_INSTALL_INCLUDEDIR}/omc/cpp)
 
 
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/LibrariesConfig.h.in ${CMAKE_CURRENT_BINARY_DIR}/LibrariesConfig.h)
-
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/LibrariesConfig.h
-        TYPE INCLUDE)
-
 
 # An interface library for providing common include directories for all the CPP libs.
 add_library(OMCppConfig INTERFACE)
@@ -28,7 +23,40 @@ target_include_directories(OMCppConfig INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/Inc
 add_subdirectory(SimCoreFactory)
 add_subdirectory(Core)
 add_subdirectory(Solver)
+add_subdirectory(FMU)
 
+
+## This little function will give us the filename of the CPP runtime
+## library given its alias.
+function (omc_get_library_filename target_alias LIB_FILENAME)
+  message(STATUS ${target_alias})
+  get_target_property(LIB_FILENAME_LOCAL ${target_alias} ALIASED_TARGET)
+  message(STATUS ${LIB_FILENAME_LOCAL})
+  set(${LIB_FILENAME} ${CMAKE_SHARED_LIBRARY_PREFIX}${LIB_FILENAME_LOCAL}${CMAKE_SHARED_LIBRARY_SUFFIX} PARENT_SCOPE)
+endfunction(omc_get_library_filename)
+
+## Get the actual output filenames of the CPP runtime shared libs
+## This are to be used in LibrariesConfig for the purpose of loading the
+## libs at simulation time using their file name.
+omc_get_library_filename(omc::simrt::cpp::core::system SYSTEM_LIB)
+omc_get_library_filename(omc::simrt::cpp::core::dataexchange DATAEXCHANGE_LIB)
+omc_get_library_filename(omc::simrt::cpp::core::math MATH_LIB)
+omc_get_library_filename(omc::simrt::cpp::core::simsettings SETTINGSFACTORY_LIB)
+omc_get_library_filename(omc::simrt::cpp::core::solver SOLVER_LIB)
+
+omc_get_library_filename(omc::simrt::cpp::solver::cvode CVODE_LIB)
+omc_get_library_filename(omc::simrt::cpp::solver::dassl DASSL_LIB)
+omc_get_library_filename(omc::simrt::cpp::solver::dgesvsolver DGESVSOLVER_LIB)
+omc_get_library_filename(omc::simrt::cpp::solver::kinsol KINSOL_LIB)
+omc_get_library_filename(omc::simrt::cpp::solver::newton NEWTON_LIB)
+# omc_get_library_filename(omc::simrt::cpp::core::utils::extension EXTENSIONUTILITIES_LIB)
+omc_get_library_filename(omc::simrt::cpp::core::simcontroller SIMCONTROLLER_LIB)
+
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/LibrariesConfig.h.in ${CMAKE_CURRENT_BINARY_DIR}/LibrariesConfig.h)
+
+
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/LibrariesConfig.h
+        TYPE INCLUDE)
 
 # This folder contains only one file right now. Something should be done about it.
 install (DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/Include/
