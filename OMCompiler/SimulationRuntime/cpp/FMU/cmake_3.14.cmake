65dd679947bc136b8ae923b762033bbe3338af9a
Mahder Gebremedhin <mahder.geb@gmail.com>
2021-10-06 12:44:38 +0300

    [cmake] Add OMCppFMU library.

---
+++ OMCompiler/SimulationRuntime/cpp/FMU/cmake_3.14.cmake
@@ -0,0 +1,35 @@
+#####################################################################################################
+# OMCppFMU
+set(OMC_SIMRT_CPP_FMU_SOURCES FMULogger.cpp)
+
+add_library(OMCppFMU SHARED)
+add_library(omc::simrt::cpp::fmu ALIAS OMCppFMU)
+
+target_sources(OMCppFMU PRIVATE ${OMC_SIMRT_CPP_FMU_SOURCES})
+
+target_link_libraries(OMCppFMU PUBLIC omc::simrt::cpp::config)
+target_link_libraries(OMCppFMU PUBLIC omc::simrt::cpp::core::utils::extension)
+
+target_link_options(OMCppFMU PRIVATE  -Wl,--no-undefined)
+
+install(TARGETS OMCppFMU)
+
+# OMCppFMU_static
+add_library(OMCppFMU_static STATIC)
+add_library(omc::simrt::cpp::fmu::static ALIAS OMCppFMU_static)
+
+target_sources(OMCppFMU_static PRIVATE ${OMC_SIMRT_CPP_FMU_SOURCES})
+
+target_compile_definitions(OMCppFMU_static PRIVATE -DRUNTIME_STATIC_LINKING)
+
+target_link_libraries(OMCppFMU_static PUBLIC omc::simrt::cpp::config)
+
+install(TARGETS OMCppFMU_static)
+##
+
+
+install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        TYPE INCLUDE
+        FILES_MATCHING
+        PATTERN *.h
+)
