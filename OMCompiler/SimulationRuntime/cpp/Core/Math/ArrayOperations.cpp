432352fc7d511f7fdfbc1a485414872d453b58b5
Rüdiger Franke <rdgfranke@gmail.com>
2021-10-20 07:04:16 +0200

    Fix Cpp reduction for record and add test
    Remove special treatment to treat array of record as regular array.
    
    Also remove idx_type, spec_type and getNextIndex from ArrayOperations
    that are not used anymore since removal of create/fill_array_from_shape.

--- OMCompiler/SimulationRuntime/cpp/Core/Math/ArrayOperations.cpp
+++ OMCompiler/SimulationRuntime/cpp/Core/Math/ArrayOperations.cpp
@@ -11,20 +11,6 @@
 
 using namespace std;
 
-//void boost::assertion_failed(char const * expr, char const * function,
-//                             char const * file, long line)
-//{
-//  fprintf(stdout, "Range check failed for Array please check indices \n" );
-//}
-
-size_t getNextIndex(const vector<size_t> idx, size_t k)
-{
-  if((idx.size()-1)<k)
-    return idx.back();
-  else
-    return idx[k];
-}
-
 /**
 Concatenates n real arrays along the k:th dimension.
 */
