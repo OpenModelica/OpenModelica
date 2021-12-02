fb970a4a986c72065ca50bbfb392d69b13fc0857
Mahder Gebremedhin <mahder.geb@gmail.com>
2021-12-02 13:45:57 +0100

    [cmake] Improve handling of OMSimulator.
      - Add it as an ExternalProject.
    
      - Do not allow it to output things to the install directory while building
        - Insted give it a directory in the build dir as an install dir.
          Then once it installs things in there, we handle the actual installation
          manually.
        - Essentially, this prevents from populating anything in the
          install directory while building.
    
      - Add an imported library target for it.
        This can be used to link to it from our other targets.
    
      - Add install logic for it.
    
      - OMSimulator wants to start with a Makefiles to invoke the actual CMake
        operations. As a result it is designed with that in mind.
        While the current support from this side is not ideal, it should suffice
        for now.

--- omsimulator.cmake
+++ omsimulator.cmake
@@ -1,22 +1,53 @@
 
-# message(FATAL_ERROR "${CMAKE_LIBRARY_ARCHITECTURE}")
 
-add_custom_target(omsimulator
-                  COMMAND ${CMAKE_MAKE_PROGRAM} config-3rdParty
+include(ExternalProject)
+include(ProcessorCount)
+ProcessorCount(NUM_PROCESSPRS)
+
+
+ExternalProject_Add(OMSimulator_external
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/OMSimulator
+    PREFIX OMSimulator
+    #--Configure step-------------
+    CONFIGURE_COMMAND COMMAND ${CMAKE_MAKE_PROGRAM} -C ${CMAKE_CURRENT_SOURCE_DIR}/OMSimulator
+                                                -j${NUM_PROCESSPRS}
+                                                config-3rdParty
                                                 CERES=OFF
                                                 host_short=${CMAKE_LIBRARY_ARCHITECTURE}
                                                 CMAKE=${CMAKE_COMMAND}
-
-                  COMMAND ${CMAKE_MAKE_PROGRAM} config-OMSimulator
+                      COMMAND ${CMAKE_MAKE_PROGRAM} -C ${CMAKE_CURRENT_SOURCE_DIR}/OMSimulator
+                                                -j${NUM_PROCESSPRS}
+                                                config-OMSimulator
                                                 OMSYSIDENT=OFF
-                                                OMBUILDDIR=${CMAKE_INSTALL_PREFIX}
+                                                OMBUILDDIR=${CMAKE_CURRENT_BINARY_DIR}/OMSimulator
                                                 host_short=${CMAKE_LIBRARY_ARCHITECTURE}
                                                 CMAKE=${CMAKE_COMMAND}
-
-                  COMMAND ${CMAKE_MAKE_PROGRAM} OMSimulator
-                                                OMBUILDDIR=${CMAKE_INSTALL_PREFIX}
+    #--Build step-----------------
+    BUILD_COMMAND COMMAND ${CMAKE_MAKE_PROGRAM} -C ${CMAKE_CURRENT_SOURCE_DIR}/OMSimulator
+                                                -j${NUM_PROCESSPRS}
+                                                OMSimulator
+                                                OMBUILDDIR=${CMAKE_CURRENT_BINARY_DIR}/OMSimulator
                                                 host_short=${CMAKE_LIBRARY_ARCHITECTURE}
                                                 CMAKE=${CMAKE_COMMAND}
+    #--Install step---------------
+    INSTALL_COMMAND ""
+)
 
-                  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/OMSimulator)
+set_target_properties(OMSimulator_external PROPERTIES EXCLUDE_FROM_ALL TRUE)
 
+
+add_library(libOMSimulator SHARED IMPORTED)
+set_target_properties(libOMSimulator PROPERTIES
+  IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/OMSimulator/lib/x86_64-linux-gnu/omc/libOMSimulator.so
+  INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_CURRENT_SOURCE_DIR}/OMSimulator/src/OMSimulatorLib
+)
+
+add_dependencies(libOMSimulator OMSimulator_external)
+
+
+install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/OMSimulator/
+        DESTINATION ${CMAKE_INSTALL_PREFIX}
+        USE_SOURCE_PERMISSIONS
+        # Exclude the directories created by CMake's ExternalProject
+        PATTERN src EXCLUDE
+        PATTERN tmp EXCLUDE)
