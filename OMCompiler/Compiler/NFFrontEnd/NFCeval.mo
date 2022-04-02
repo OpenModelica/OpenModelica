OpenModelica/OpenModelica

commit 65ed5abb254497e52bc5cc08954ee15775e4edcc

Performance improvements (#8788)
- Don't traverse literal arrays in Ceval.subscriptBinding and
  EvalConstants.traverseExp.
- Set the initial number of buckets when creating hash maps in
  Ceval.subscriptBinding, since we know how many elements there are.
- Mark arrays as literal already during instantiation.
- Preserve the literalness of arrays during typing.

OMCompiler/Compiler/NFFrontEnd/NFCeval.mo
@@ -501,8 +501,7 @@ algorithm
   end if;
 
   exp := Expression.applySubscripts(subs, exp);
-  exp := Expression.mapFold(exp,
-    function subscriptBinding2(cref = cref, evalSubscripts = evalSubscripts), NONE());
+  exp := subscriptBinding2(exp, cref, evalSubscripts, NONE());
 end subscriptBinding;
 
 function subscriptBinding2
@@ -514,20 +513,25 @@ protected
   type SubscriptList = list<Subscript>;
   UnorderedMap<InstNode, list<Subscript>> sub_map;
   list<Subscript> subs;
+  list<ComponentRef> cref_parts;
+  Expression e;
 algorithm
-  exp := match exp
+  (exp, subMap) := match exp
     case Expression.SUBSCRIPTED_EXP(subscripts = subs)
       algorithm
         if isSome(subMap) then
           SOME(sub_map) := subMap;
         else
-          // Create a map that maps each part of the cref to the subscripts on that part.
-          sub_map := UnorderedMap.new<SubscriptList>(InstNode.hash, InstNode.refEqual);
-
           // If the cref hasn't been flattened then subscripts that reference
           // the scope parts of the cref should be kept as they are, so the
           // scope isn't added to the map in that case.
-          for cr in ComponentRef.toListReverse(cref, includeScope = isFlatCref(cref)) loop
+          cref_parts := ComponentRef.toListReverse(cref, includeScope = isFlatCref(cref));
+
+          // Create a map that maps each part of the cref to the subscripts on that part.
+          sub_map := UnorderedMap.new<SubscriptList>(InstNode.hash,
+            InstNode.refEqual, Util.nextPrime(listLength(cref_parts)));
+
+          for cr in cref_parts loop
             UnorderedMap.addUnique(ComponentRef.node(cr), ComponentRef.getSubscripts(cr), sub_map);
           end for;
 
@@ -541,10 +545,17 @@ algorithm
         if evalSubscripts then
           subs := list(Subscript.eval(s) for s in subs);
         end if;
-      then
-        Expression.applySubscripts(subs, exp.exp);
 
-    else exp;
+        e := Expression.applySubscripts(subs, exp.exp);
+      then
+        Expression.mapFoldShallow(e,
+          function subscriptBinding2(cref = cref, evalSubscripts = evalSubscripts), subMap);
+
+    case Expression.ARRAY(literal = true) then (exp, subMap);
+
+    else Expression.mapFoldShallow(exp,
+      function subscriptBinding2(cref = cref, evalSubscripts = evalSubscripts), subMap);
+
   end match;
 end subscriptBinding2;
 

OMCompiler/Compiler/NFFrontEnd/NFEvalConstants.mo
@@ -187,6 +187,9 @@ algorithm
       then
         outExp;
 
+    case Expression.ARRAY(literal = true)
+      then exp;
+
     case Expression.IF()
       algorithm
         (outExp, outChanged) := evaluateIfExp(exp, info);

OMCompiler/Compiler/NFFrontEnd/NFInst.mo
@@ -2673,7 +2673,7 @@ algorithm
         arr := Array.mapList(absynExp.arrayExp,
           function instExp(scope = scope, context = context, info = info));
       then
-        Expression.makeArray(Type.UNKNOWN(), arr);
+        Expression.makeArray(Type.UNKNOWN(), arr, Array.all(arr, Expression.isLiteral));
 
     case Absyn.Exp.MATRIX()
       algorithm

OMCompiler/Compiler/NFFrontEnd/NFTyping.mo
@@ -1173,7 +1173,7 @@ algorithm
       then
         (exp, exp.ty, Variability.CONSTANT, Purity.PURE);
 
-    case Expression.ARRAY()  then typeArray(exp.elements, context, info);
+    case Expression.ARRAY()  then typeArray(exp.elements, exp.literal, context, info);
     case Expression.MATRIX() then typeMatrix(exp.elements, context, info);
     case Expression.RANGE()  then typeRange(exp, context, info);
     case Expression.TUPLE()  then typeTuple(exp.elements, context, info);
@@ -1485,7 +1485,7 @@ algorithm
 
         expl := listReverseInPlace(expl);
         ty := Type.liftArrayLeft(ty, Dimension.fromInteger(listLength(expl)));
-        outExp := Expression.makeArray(ty, listArray(expl));
+        outExp := Expression.makeArray(ty, listArray(expl), exp.literal);
       then
         (outExp, ty, variability, purity);
 
@@ -1985,6 +1985,7 @@ end typeSubscript;
 
 function typeArray
   input array<Expression> elements;
+  input Boolean isLiteral;
   input InstContext.Type context;
   input SourceInfo info;
   output Expression arrayExp;
@@ -2038,7 +2039,7 @@ algorithm
   end for;
 
   arrayType := Type.liftArrayLeft(ty1, Dimension.fromExpList(expl2));
-  arrayExp := Expression.makeArray(arrayType, listArray(expl2));
+  arrayExp := Expression.makeArray(arrayType, listArray(expl2), isLiteral);
 end typeArray;
 
 function typeMatrix "The array concatenation operator"

commit 22e1010920bfbb73137eee3c87b47acfdb958765

Fix more regressions from #8788
- Process the subscripted expression before subscripting it in
  Ceval.subscriptBinding2.
- Don't automatically mark created arrays as literal in
  Expression.mapSplitExpressions2, they can be anything.

OMCompiler/Compiler/NFFrontEnd/NFCeval.mo
@@ -545,10 +545,10 @@ algorithm
           subs := list(Subscript.eval(s) for s in subs);
         end if;
 
-        e := Expression.applySubscripts(subs, exp.exp);
+        (e, subMap) := subscriptBinding2(exp.exp, cref, evalSubscripts, subMap);
+        e := Expression.applySubscripts(subs, e);
       then
-        Expression.mapFoldShallow(e,
-          function subscriptBinding2(cref = cref, evalSubscripts = evalSubscripts), subMap);
+        (e, subMap);
 
     case Expression.ARRAY(literal = true) then (exp, subMap);
 

OMCompiler/Compiler/NFFrontEnd/NFExpression.mo
@@ -825,7 +825,7 @@ public
           arr := Array.map(arr, function typeCast(ty = ety));
           t := Type.setArrayElementType(t, ety);
         then
-          ARRAY(t, arr, exp.literal);
+          makeArray(t, arr, exp.literal);
 
       case RANGE(ty = t)
         algorithm
@@ -2478,7 +2478,7 @@ public
 
       case CLKCONST() then CLKCONST(ClockKind.mapExp(exp.clk, func));
       case CREF() then CREF(exp.ty, ComponentRef.mapExp(exp.cref, func));
-      case ARRAY() then ARRAY(exp.ty, Array.map(exp.elements, function map(func = func)), exp.literal);
+      case ARRAY() then makeArray(exp.ty, Array.map(exp.elements, function map(func = func)), exp.literal);
       case MATRIX() then MATRIX(list(list(map(e, func) for e in row) for row in exp.elements));
 
       case RANGE(step = SOME(e2))
@@ -2654,7 +2654,7 @@ public
 
       case CLKCONST() then CLKCONST(ClockKind.mapExp(exp.clk, func));
       case CREF() then CREF(exp.ty, ComponentRef.mapExp(exp.cref, func));
-      case ARRAY() then ARRAY(exp.ty, Array.map(exp.elements, function mapReverse(func = func)), exp.literal);
+      case ARRAY() then makeArray(exp.ty, Array.map(exp.elements, function mapReverse(func = func)), exp.literal);
       case MATRIX() then MATRIX(list(list(mapReverse(e, func) for e in row) for row in exp.elements));
 
       case RANGE(step = SOME(e2))
@@ -2811,7 +2811,7 @@ public
 
       case CLKCONST() then CLKCONST(ClockKind.mapExpShallow(exp.clk, func));
       case CREF() then CREF(exp.ty, ComponentRef.mapExpShallow(exp.cref, func));
-      case ARRAY() then ARRAY(exp.ty, Array.map(exp.elements, func), exp.literal);
+      case ARRAY() then makeArray(exp.ty, Array.map(exp.elements, func), exp.literal);
       case MATRIX() then MATRIX(list(list(func(e) for e in row) for row in exp.elements));
 
       case RANGE(step = SOME(e2))
@@ -3492,7 +3492,7 @@ public
         algorithm
           (arr, arg) := Array.mapFold(exp.elements, function mapFold(func = func), arg);
         then
-          ARRAY(exp.ty, arr, exp.literal);
+          makeArray(exp.ty, arr, exp.literal);
 
       case MATRIX()
         algorithm
@@ -3731,7 +3731,7 @@ public
         algorithm
           (arr, arg) := Array.mapFold(exp.elements, func, arg);
         then
-          ARRAY(exp.ty, arr, exp.literal);
+          makeArray(exp.ty, arr, exp.literal);
 
       case MATRIX()
         algorithm
@@ -5658,7 +5658,7 @@ public
       end for;
 
       ty := typeOf(if arrayEmpty(expl) then exp else arrayGet(expl, 1));
-      outExp := makeExpArray(expl, ty, isLiteral = true);
+      outExp := makeExpArray(expl, ty, Array.all(expl, isLiteral));
     end if;
   end mapSplitExpressions2;
 