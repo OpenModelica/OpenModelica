3ae4c6e593cd9204275d1c1a9dc3870043d2436a
Adeel Asghar <adeel.asghar@liu.se>
2022-05-20 19:57:24 +0200

    Fix encryption build (#8994)

--- OMCompiler/Parser/parse.c
+++ OMCompiler/Parser/parse.c
@@ -429,7 +429,7 @@ static void* parseFile(const char* fileName, const char* infoName, int flags, co
 
 #ifdef OMENCRYPTION
   if (len > 3 && 0==strcmp(fileName+len-4,".moc")) {
-    return parseEncryptedFile(fileName, langStd, runningTestsuite, libraryPath, lveInstance);
+    return parseEncryptedFile(fileName, langStd, strict, runningTestsuite, libraryPath, lveInstance);
   }
 #else
   if (len > 3 && 0==strcmp(fileName+len-4,".moc")) {
