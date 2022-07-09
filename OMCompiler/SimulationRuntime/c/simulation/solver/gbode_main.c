2341cd1d8f17612dc6ebfd8adc527b9c0a5bd4e3
bernhardbachmann <bernhard.bachmann@fh-bielefeld.de>
2022-07-09 19:21:11 +0200

    Suggested change for IController

--- OMCompiler/SimulationRuntime/c/simulation/solver/gbode_main.c
+++ OMCompiler/SimulationRuntime/c/simulation/solver/gbode_main.c
@@ -1329,7 +1329,7 @@ int gbode_birate(DATA *data, threadData_t *threadData, SOLVER_INFO *solverInfo)
       }
       if (gbData->ctrl_method != GB_CTRL_CNST && ((gbData->interpolation == GB_INTERPOL_HERMITE_ERRCTRL)  || (gbData->interpolation == GB_DENSE_OUTPUT_ERRCTRL))) {
         gbData->errValues[0] = fmax(err, gbData->err_int);
-        gbData->stepSize = gbData->lastStepSize * IController(gbData->errValues, gbData->stepSizeValues, gbData->tableau->error_order);
+        gbData->stepSize = gbData->lastStepSize * gbData->stepSize_control(gbData->errValues, gbData->stepSizeValues, gbData->tableau->error_order);
       }
       // reject step, if interpolaton error is too large
       if (( gbData->nFastStates>0) && (gbData->err_int > 1 ) && gbData->ctrl_method != GB_CTRL_CNST &&
