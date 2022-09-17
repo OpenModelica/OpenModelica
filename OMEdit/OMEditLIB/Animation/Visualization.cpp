534f8627f39030fb999ccdd4f5116318c7d2272c
anotheruserofgithub <96748782+anotheruserofgithub@users.noreply.github.com>
2022-09-17 15:36:15 +0200

    Fix setting name and state set of geodes/nodes

--- OMEdit/OMEditLIB/Animation/Visualization.cpp
+++ OMEdit/OMEditLIB/Animation/Visualization.cpp
@@ -552,13 +552,13 @@ void OSGScene::setUpScene(std::vector<ShapeObject>& shapes)
       osg::ref_ptr<osg::Node> node = osgDB::readNodeFile(shape._fileName);
       if (node.valid())
       {
+        node->setName(shape._id);
+
         osg::ref_ptr<osg::Material> material = new osg::Material();
         material->setDiffuse(osg::Material::FRONT, osg::Vec4f(0.0, 0.0, 0.0, 0.0));
 
         osg::ref_ptr<osg::StateSet> ss = node->getOrCreateStateSet();
         ss->setAttribute(material.get());
-
-        node->setStateSet(ss.get());
 
         transf->addChild(node.get());
       }
@@ -569,6 +569,7 @@ void OSGScene::setUpScene(std::vector<ShapeObject>& shapes)
       osg::ref_ptr<DXFile> dxfDraw = new DXFile(shape._fileName);
 
       osg::ref_ptr<osg::Geode> geode = new osg::Geode();
+      geode->setName(shape._id);
       geode->addDrawable(dxfDraw.get());
 
       transf->addChild(geode.get());
@@ -579,6 +580,7 @@ void OSGScene::setUpScene(std::vector<ShapeObject>& shapes)
       shapeDraw->setColor(osg::Vec4(1.0, 1.0, 1.0, 1.0));
 
       osg::ref_ptr<osg::Geode> geode = new osg::Geode();
+      geode->setName(shape._id);
       geode->addDrawable(shapeDraw.get());
 
       osg::ref_ptr<osg::Material> material = new osg::Material();
@@ -586,8 +588,6 @@ void OSGScene::setUpScene(std::vector<ShapeObject>& shapes)
 
       osg::ref_ptr<osg::StateSet> ss = geode->getOrCreateStateSet();
       ss->setAttribute(material.get());
-
-      geode->setStateSet(ss.get());
 
       transf->addChild(geode.get());
     }
@@ -616,6 +616,7 @@ void OSGScene::setUpScene(std::vector<VectorObject>& vectors)
     shapeDraw2->setColor(osg::Vec4(1.0, 1.0, 1.0, 1.0));
 
     osg::ref_ptr<osg::Geode> geode = new osg::Geode();
+    geode->setName(vector._id);
     geode->addDrawable(shapeDraw0.get());
     geode->addDrawable(shapeDraw1.get());
     geode->addDrawable(shapeDraw2.get());
@@ -625,8 +626,6 @@ void OSGScene::setUpScene(std::vector<VectorObject>& vectors)
 
     osg::ref_ptr<osg::StateSet> ss = geode->getOrCreateStateSet();
     ss->setAttribute(material.get());
-
-    geode->setStateSet(ss.get());
 
     transf->addChild(geode.get());
 
@@ -675,8 +674,6 @@ void UpdateVisitor::apply(osg::MatrixTransform& node)
 void UpdateVisitor::apply(osg::Geode& node)
 {
   //std::cout<<"GEODE "<< _visualizer->_id<<" "<<_visualizer->getTransparency()<<std::endl;
-  osg::ref_ptr<osg::StateSet> ss = node.getOrCreateStateSet();
-  node.setName(_visualizer->_id);
   switch (_visualizer->getStateSetAction())
   {
   case StateSetAction::update:
@@ -791,7 +788,7 @@ void UpdateVisitor::apply(osg::Geode& node)
   case StateSetAction::modify:
    {
      //apply texture
-     applyTexture(ss.get(), _visualizer->getTextureImagePath());
+     applyTexture(node.getOrCreateStateSet(), _visualizer->getTextureImagePath());
      break;
    }//end case action modify
 
@@ -803,13 +800,12 @@ void UpdateVisitor::apply(osg::Geode& node)
   if (_changeMaterialProperties) {
     //set color
     if (!_visualizer->isShape() or _visualizer->asShape()->_type.compare("dxf") != 0)
-      changeColor(ss.get(), _visualizer->_color[0].exp, _visualizer->_color[1].exp, _visualizer->_color[2].exp);
+      changeColor(node.getOrCreateStateSet(), _visualizer->_color[0].exp, _visualizer->_color[1].exp, _visualizer->_color[2].exp);
 
     //set transparency
-    changeTransparency(ss.get(), _visualizer->getTransparency());
+    changeTransparency(node.getOrCreateStateSet(), _visualizer->getTransparency());
   }
 
-  node.setStateSet(ss.get());
   traverse(node);
 }
 
