b859ff6f4d40ee12f305d4d2086ff8cc42438a7a
Adeel Asghar <adeel.asghar@liu.se>
2022-11-17 13:50:27 +0100

    Keep the original text annotation
    Fixes #9715

--- OMEdit/OMEditLIB/Annotations/ShapeAnnotation.cpp
+++ OMEdit/OMEditLIB/Annotations/ShapeAnnotation.cpp
@@ -365,7 +365,6 @@ void ShapeAnnotation::setDefaults()
   mStartAngle = 0;
   mEndAngle = 360;
   mClosure = StringHandler::ClosureChord;
-  mTextString = "";
   mFontSize = 0;
   mFontName = Helper::systemFontInfo.family();
   mTextStyles.clear();
@@ -400,6 +399,7 @@ void ShapeAnnotation::setDefaults(ShapeAnnotation *pShapeAnnotation)
   mStartAngle = pShapeAnnotation->mStartAngle;
   mEndAngle = pShapeAnnotation->mEndAngle;
   mClosure = pShapeAnnotation->mClosure;
+  mOriginalTextString = pShapeAnnotation->mOriginalTextString;
   mTextString = pShapeAnnotation->mTextString;
   mFontSize = pShapeAnnotation->mFontSize;
   mFontName = pShapeAnnotation->mFontName;
@@ -823,6 +823,7 @@ void ShapeAnnotation::setOriginItemPos(const QPointF point)
   */
 void ShapeAnnotation::setTextString(QString textString)
 {
+  mOriginalTextString = textString;
   mTextString = textString;
 }
 
