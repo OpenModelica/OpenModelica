02a867953c4f67272d07fd7d9eefe2eff368d670
arun3688 <rain100falls@gmail.com>
2022-12-04 20:47:05 +0100

    improve debug headers

--- OMCompiler/SimulationRuntime/c/dataReconciliation/dataReconciliation.cpp
+++ OMCompiler/SimulationRuntime/c/dataReconciliation/dataReconciliation.cpp
@@ -2872,7 +2872,7 @@ int stateEstimation(DATA *data, threadData_t *threadData, inputData x, matrixDat
   matrixData reconciled_Sx = {datareconciliationdata.reconciled_SX.rows, datareconciliationdata.reconciled_SX.column, datareconciliationdata.reconciled_SX.data};
   reconcileBoundaryConditions(data, threadData, reconciled_x, reconciled_Sx, boundaryconditiondata, logfile);
 
-  printBoundaryConditionsResults(boundaryconditiondata.boundaryConditionVarsResults, boundaryconditiondata.reconSt_diag,  boundaryconditiondata.boundaryConditionVars.size(), 1, boundaryconditiondata.boundaryConditionVars, "Final Results Arun", logfile);
+  //printBoundaryConditionsResults(boundaryconditiondata.boundaryConditionVarsResults, boundaryconditiondata.reconSt_diag,  boundaryconditiondata.boundaryConditionVars.size(), 1, boundaryconditiondata.boundaryConditionVars, "Final Results Copied", logfile);
 
   createHtmlReportFordataReconciliation(data, datareconciliationdata.csvinputs, datareconciliationdata.xdiag, datareconciliationdata.reconciled_X, datareconciliationdata.copyreconSx_diag, datareconciliationdata.newX, eps, datareconciliationdata.iterationcount, datareconciliationdata.value, datareconciliationdata.J, warningCorrelationData, boundaryconditiondata);
 
