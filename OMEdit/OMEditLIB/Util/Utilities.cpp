5e9eb77d54c8c324a4322bd750d83367fe50eba3
Adeel Asghar <adeel.asghar@liu.se>
2022-12-07 11:51:31 +0100

    Use temp directory if we fail to create our own path

--- OMEdit/OMEditLIB/Util/Utilities.cpp
+++ OMEdit/OMEditLIB/Util/Utilities.cpp
@@ -556,8 +556,14 @@ QString& Utilities::tempDirectory()
     tmpPath = QDir::tempPath() + "/OpenModelica_" + QString(user ? user : "nobody") + "/OMEdit/";
 #endif
     tmpPath.remove("\"");
-    if (!QDir().exists(tmpPath))
-      QDir().mkpath(tmpPath);
+    if (!QDir().exists(tmpPath)) {
+      if (!QDir().mkpath(tmpPath)) {
+        qDebug() << "Failed to create the tempDirectory" << tmpPath
+                 << "will use" << QDir::tempPath() << "instead.";
+        tmpPath = QDir::tempPath();
+        tmpPath.remove("\"");
+      }
+    }
   }
   return tmpPath;
 }
