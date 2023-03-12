c9e00a765e95db1d6c73a8ea4b6a16f2bf2f4299
bernhardbachmann <bernhard.bachmann@fh-bielefeld.de>
2023-03-12 09:53:54 +0100

    Utilize sinulation flag -noRootFinding in GBODE

--- OMCompiler/SimulationRuntime/c/simulation/solver/gbode_events.c
+++ OMCompiler/SimulationRuntime/c/simulation/solver/gbode_events.c
@@ -275,8 +275,13 @@ double checkForEvents(DATA* data, threadData_t* threadData, SOLVER_INFO* solverI
   *foundEvent = checkForStateEvent(data, solverInfo->eventLst);
 
   if (*foundEvent) {
-    eventTime = findRoot_gb(data, threadData, solverInfo, solverInfo->eventLst, timeLeft, leftValues, timeRight, rightValues, isInnerIntegration);
-    infoStreamPrint(LOG_SOLVER, 0, "gbode detected an event at time: %20.16g", eventTime);
+     if(omc_flag[FLAG_NO_ROOTFINDING]) {
+       eventTime = timeRight;
+       infoStreamPrint(LOG_SOLVER, 0, "gbode detected an event at time: %20.16g (rootfinding is disabled)", eventTime);
+     } else {
+       eventTime = findRoot_gb(data, threadData, solverInfo, solverInfo->eventLst, timeLeft, leftValues, timeRight, rightValues, isInnerIntegration);
+       infoStreamPrint(LOG_SOLVER, 0, "gbode detected an event at time: %20.16g", eventTime);
+    }
   }
 
   // re-store the pre values of the zeroCrossings for comparison
