OpenModelica/OpenModelica

commit 80a64ee97768ebd4cfe8e15b46572f6074ec070f

Improve handling of record bindings (#10820)
- Don't evaluate non-structural parameters.
- Use `tryEvalExp` instead of `evalExp` when trying to evaluate record
  bindings, to hide any eventual error messages since failure is ok.
- Improve the function inlining by merging the inlining of record
  constructors with the inlining of normal function calls, and allowing
  forced inlining regardless of annotation.

Fixes #10812

OMCompiler/Compiler/NFFrontEnd/NFCeval.mo
@@ -368,7 +368,7 @@ algorithm
   exp := match cref
     case ComponentRef.CREF(node = c as InstNode.COMPONENT_NODE())
       guard not ComponentRef.isIterator(cref) and
-            ComponentRef.nodeVariability(cref) <= Variability.NON_STRUCTURAL_PARAMETER
+            ComponentRef.nodeVariability(cref) < Variability.NON_STRUCTURAL_PARAMETER
       then evalComponentBinding(c, cref, defaultExp, target, evalSubscripts, liftExp);
 
     else defaultExp;

OMCompiler/Compiler/NFFrontEnd/NFInline.mo
@@ -50,7 +50,10 @@ import Type = NFType;
 
 public
 function inlineCallExp
+  "Inlines a call if it has an EarlyInline=true annotation, or always if
+   forceInline is set to true."
   input Expression callExp;
+  input Boolean forceInline = false;
   output Expression result;
 algorithm
   result := match callExp
@@ -64,11 +67,11 @@ algorithm
           case DAE.InlineType.BUILTIN_EARLY_INLINE() then true;
           case DAE.InlineType.EARLY_INLINE()
             guard Flags.isSet(Flags.INLINE_FUNCTIONS) then true;
-          case DAE.InlineType.NORM_INLINE() then Flags.getConfigBool(Flags.FRONTEND_INLINE);
-          else false;
+          case DAE.InlineType.NORM_INLINE() then forceInline or Flags.getConfigBool(Flags.FRONTEND_INLINE);
+          else forceInline;
         end match;
       then
-        if shouldInline then inlineCall(call) else callExp;
+        if shouldInline then inlineCall(call, forceInline) else callExp;
 
     else callExp;
   end match;
@@ -76,6 +79,7 @@ end inlineCallExp;
 
 function inlineCall
   input Call call;
+  input Boolean forceInline = false;
   output Expression exp;
 algorithm
   exp := match call
@@ -86,7 +90,34 @@ algorithm
       list<InstNode> inputs, outputs, locals;
       list<Statement> body;
       Statement stmt;
+      Binding binding;
 
+    // Record constructor
+    case Call.TYPED_CALL(fn = fn, arguments = args)
+        guard InstNode.name(InstNode.parentScope(fn.node)) == "'constructor'"
+      algorithm
+        body := Function.getBody(fn);
+        true := listEmpty(body);
+        true := listEmpty(fn.locals);
+
+        binding := Component.getBinding(InstNode.component(listHead(fn.outputs)));
+
+        if Binding.hasExp(binding) then
+          exp := Binding.getExp(binding);
+          true := Expression.isRecord(exp);
+        else
+          exp := Class.makeRecordExp(listHead(fn.outputs));
+        end if;
+
+        for i in fn.inputs loop
+          arg :: args := args;
+          arg := inlineCallExp(arg, forceInline);
+          exp := Expression.map(exp, func = function replaceCrefNode(node = i, value = arg));
+        end for;
+      then
+        exp;
+
+    // Normal function
     case Call.TYPED_CALL(fn = fn as Function.FUNCTION(inputs = inputs, outputs = outputs, locals = locals),
                          arguments = args)
       algorithm
@@ -120,12 +151,13 @@ algorithm
           //       statement once.
           for i in inputs loop
             arg :: args := args;
+            arg := inlineCallExp(arg, forceInline);
             stmt := Statement.mapExp(stmt,
               function Expression.map(func = function replaceCrefNode(node = i, value = arg)));
           end for;
 
           exp := getOutputExp(stmt, listHead(outputs), call);
-          exp := inlineCallExp(exp);
+          exp := inlineCallExp(exp, forceInline);
         else
           exp := Expression.CALL(call);
         end try;
@@ -135,42 +167,6 @@ algorithm
     else Expression.CALL(call);
   end match;
 end inlineCall;
-
-function inlineRecordConstructorCall
-  input Expression exp;
-  output Expression outExp;
-protected
-  Function fn;
-  Expression arg;
-  list<Expression> args;
-  list<Statement> body;
-  Binding binding;
-algorithm
-  outExp := match exp
-    case Expression.CALL(call = Call.TYPED_CALL(fn = fn, arguments = args))
-        guard InstNode.name(InstNode.parentScope(fn.node)) == "'constructor'"
-      algorithm
-        body := Function.getBody(fn);
-        true := listEmpty(body);
-        true := listEmpty(fn.locals);
-
-        binding := Component.getBinding(InstNode.component(listHead(fn.outputs)));
-
-        if Binding.hasExp(binding) then
-          outExp := Binding.getExp(binding);
-          true := Expression.isRecord(outExp);
-        else
-          outExp := Class.makeRecordExp(listHead(fn.outputs));
-        end if;
-
-        for i in fn.inputs loop
-          arg :: args := args;
-          outExp := Expression.map(outExp, func = function replaceCrefNode(node = i, value = arg));
-        end for;
-      then
-        outExp;
-  end match;
-end inlineRecordConstructorCall;
 
 protected
 function replaceCrefNode

testsuite/flattening/modelica/scodeinst/Makefile
@@ -753,6 +753,7 @@ ImpureCall1.mo \
 Inline1.mo \
 Inline2.mo \
 Inline3.mo \
+Inline4.mo \
 InnerOuter1.mo \
 InnerOuter2.mo \
 InnerOuter3.mo \

testsuite/openmodelica/visualization/Surfaces.mos
@@ -649,6 +649,36 @@ getErrorString();
 //       <transparency><exp>0.0</exp></transparency>
 //   </surface>
 //   <surface>
+//       <ident>world.surface</ident>
+//       <T>
+//           <cref>world.surface.R.T[1,1]</cref>
+//           <cref>world.surface.R.T[1,2]</cref>
+//           <cref>world.surface.R.T[1,3]</cref>
+//           <cref>world.surface.R.T[2,1]</cref>
+//           <cref>world.surface.R.T[2,2]</cref>
+//           <cref>world.surface.R.T[2,3]</cref>
+//           <cref>world.surface.R.T[3,1]</cref>
+//           <cref>world.surface.R.T[3,2]</cref>
+//           <cref>world.surface.R.T[3,3]</cref>
+//       </T>
+//       <r>
+//           <exp>0.0</exp>
+//           <exp>0.0</exp>
+//           <exp>0.0</exp>
+//       </r>
+//       <nu><exp>2</exp></nu>
+//       <nv><exp>2</exp></nv>
+//       <wireframe><bconst>false</bconst></wireframe>
+//       <multiColored><bconst>false</bconst></multiColored>
+//       <color>
+//           <exp>215.0</exp>
+//           <exp>215.0</exp>
+//           <exp>215.0</exp>
+//       </color>
+//       <specCoeff><exp>0.0</exp></specCoeff>
+//       <transparency><exp>0.0</exp></transparency>
+//   </surface>
+//   <surface>
 //       <ident>surface</ident>
 //       <T>
 //           <exp>1.0</exp>
@@ -858,36 +888,6 @@ getErrorString();
 //       </color>
 //       <specCoeff><exp>0.0</exp></specCoeff>
 //   </shape>
-//   <surface>
-//       <ident>world.surface</ident>
-//       <T>
-//           <exp>1.0</exp>
-//           <exp>6.123233995736766e-17</exp>
-//           <exp>-6.123233995736766e-17</exp>
-//           <exp>6.123233995736766e-17</exp>
-//           <exp>-1.0</exp>
-//           <exp>0.0</exp>
-//           <exp>-6.123233995736766e-17</exp>
-//           <exp>-3.749399456654644e-33</exp>
-//           <exp>-1.0</exp>
-//       </T>
-//       <r>
-//           <exp>0.0</exp>
-//           <exp>0.0</exp>
-//           <exp>0.0</exp>
-//       </r>
-//       <nu><exp>2</exp></nu>
-//       <nv><exp>2</exp></nv>
-//       <wireframe><bconst>false</bconst></wireframe>
-//       <multiColored><bconst>false</bconst></multiColored>
-//       <color>
-//           <exp>215.0</exp>
-//           <exp>215.0</exp>
-//           <exp>215.0</exp>
-//       </color>
-//       <specCoeff><exp>0.0</exp></specCoeff>
-//       <transparency><exp>0.0</exp></transparency>
-//   </surface>
 // </visualization>"
 // ""
 // endResult

OMCompiler/Compiler/NFFrontEnd/NFFlatten.mo
@@ -773,14 +773,14 @@ algorithm
     elseif binding_var == Variability.PARAMETER and Component.isFinal(comp) then
       // Try to use inlining first.
       try
-        binding_exp := Inline.inlineRecordConstructorCall(binding_exp);
+        binding_exp := Inline.inlineCallExp(binding_exp, forceInline = true);
       else
       end try;
 
       // If inlining fails, try to evaluate the binding instead.
       if not (Expression.isRecord(binding_exp) or Expression.isCref(binding_exp)) then
         try
-          binding_exp_eval := Ceval.evalExp(binding_exp);
+          binding_exp_eval := Ceval.tryEvalExp(binding_exp);
           binding_exp_eval := flattenExp(binding_exp_eval, Prefix.toNonIndexedPrefix(prefix));
 
           // Throw away the evaluated binding if the number of dimensions no

testsuite/flattening/modelica/scodeinst/Inline4.mo
@@ -1,0 +1,51 @@
+// name: Inline4
+// keywords:
+// status: correct
+// cflags: -d=newInst
+//
+
+operator record Complex
+  replaceable Real re;
+  replaceable Real im;
+
+  encapsulated operator 'constructor'
+    function fromReal
+      import Complex;
+      input Real re;
+      input Real im = 0;
+      output Complex result(re = re, im = im);
+    algorithm
+      annotation(Inline = true);
+    end fromReal;
+  end 'constructor';
+end Complex;
+
+function fromPolar
+  input Real len;
+  input Real phi;
+  output Complex c;
+algorithm
+  c := Complex(len*cos(phi), len*sin(phi));
+  annotation(Inline = true);
+end fromPolar;
+
+model PortAC
+  parameter Real UNom(start = 400e3);
+  parameter Real UStart = UNom;
+  final parameter Complex vStart = fromPolar(UStart/sqrt(3), UStart);
+end PortAC;
+
+model Inline4
+  parameter Real UStart(fixed = false);
+  PortAC port(final UStart = UStart);
+end Inline4;
+
+// Result:
+// class Inline4
+//   parameter Real UStart(fixed = false);
+//   parameter Real port.UNom(start = 400000.0);
+//   final parameter Real port.UStart = UStart;
+//   final parameter Real port.vStart.re = port.UStart / 1.732050807568877 * cos(port.UStart);
+//   final parameter Real port.vStart.im = port.UStart / 1.732050807568877 * sin(port.UStart);
+// end Inline4;
+// endResult

commit 225cad132545757d58dd3373a566fdfd559ac97c

Reactivate forced inlining of normal functions
- Fix constant evaluation of `size` by evaluating the dimension
  size(s) instead of just returning whatever `Dimension.sizeExp`
  returns, which was causing the regressions from #10820.
- Reactivate inlining of normal functions when trying to split record
  bindings.

OMCompiler/Compiler/NFFrontEnd/NFCeval.mo
@@ -3197,9 +3197,11 @@ algorithm
 
     // Return the size expression for the found dimension.
     outExp := Dimension.sizeExp(dim);
+    outExp := evalExp(outExp, target);
   else
     (outExp, ty) := Typing.typeExp(exp, NFInstContext.CLASS, info);
     arr := Array.mapList(Type.arrayDims(ty), Dimension.sizeExp);
+    Array.mapNoCopy(arr, function evalExp(target = target));
     dim := Dimension.fromInteger(arrayLength(arr), Variability.PARAMETER);
     outExp := Expression.makeArray(Type.ARRAY(Type.INTEGER(), {dim}), arr);
   end if;

OMCompiler/Compiler/NFFrontEnd/NFInline.mo
@@ -120,7 +120,6 @@ algorithm
     // Normal function
     case Call.TYPED_CALL(fn = fn as Function.FUNCTION(inputs = inputs, outputs = outputs, locals = locals),
                          arguments = args)
-      guard not forceInline
       algorithm
         body := Function.getBody(fn);
 

testsuite/flattening/modelica/scodeinst/FuncUnknownDim3.mo
@@ -19,7 +19,7 @@ end FuncUnknownDim3;
 // function f
 //   input Real[n] v;
 //   output Real[n] result = fill(1.0, n);
-//   protected parameter Integer n = n;
+//   protected parameter Integer n = size(v, 1);
 // end f;
 //
 // class FuncUnknownDim3

testsuite/flattening/modelica/scodeinst/Inline5.mo
@@ -1,0 +1,44 @@
+// name: Inline5
+// keywords:
+// status: correct
+// cflags: -d=newInst
+//
+
+package PartialMedium
+  constant Real[nX] reference_X = {0.01, 0.99};
+  constant Integer nX = 2;
+  constant Integer nXi = 1;
+
+  replaceable record ThermodynamicState
+    Real p;
+    Real T;
+    Real[nX] X;
+  end ThermodynamicState;
+
+  replaceable function setState_pTX
+    input Real p;
+    input Real T;
+    input Real[:] X = reference_X;
+    output ThermodynamicState state;
+  algorithm
+    state := if size(X, 1) == nX then ThermodynamicState(p = p, T = T, X = X) else ThermodynamicState(p = p, T = T, X = cat(1, X, {1 - sum(X)}));
+    annotation(Inline = true);
+  end setState_pTX;
+end PartialMedium;
+
+model Inline5
+  replaceable package Medium = PartialMedium;
+  final parameter Medium.ThermodynamicState state_start = Medium.setState_pTX(T = 20, p = 20, X = X_start[1:Medium.nXi]);
+  parameter Real[Medium.nX] X_start = Medium.reference_X;
+end Inline5;
+
+// Result:
+// class Inline5
+//   final parameter Real state_start.p = 20.0;
+//   final parameter Real state_start.T = 20.0;
+//   final parameter Real state_start.X[1] = 0.01;
+//   final parameter Real state_start.X[2] = 0.99;
+//   parameter Real X_start[1] = 0.01;
+//   parameter Real X_start[2] = 0.99;
+// end Inline5;
+// endResult

testsuite/flattening/modelica/scodeinst/Makefile
@@ -753,6 +753,8 @@ ImpureCall1.mo \
 Inline1.mo \
 Inline2.mo \
 Inline3.mo \
+Inline4.mo \
+Inline5.mo \
 InnerOuter1.mo \
 InnerOuter2.mo \
 InnerOuter3.mo \
@@ -1159,7 +1161,6 @@ sts.mos \
 # test that currently fail. Move up when fixed.
 # Run make testfailing
 FAILINGTESTFILES=\
-Inline4.mo \
 ProtectedMod4.mo \
 CevalFuncRecord3.mo \
 CevalFuncRecord4.mo \

testsuite/openmodelica/visualization/Surfaces.mos
@@ -649,6 +649,36 @@ getErrorString();
 //       <transparency><exp>0.0</exp></transparency>
 //   </surface>
 //   <surface>
+//       <ident>world.surface</ident>
+//       <T>
+//           <cref>world.surface.R.T[1,1]</cref>
+//           <cref>world.surface.R.T[1,2]</cref>
+//           <cref>world.surface.R.T[1,3]</cref>
+//           <cref>world.surface.R.T[2,1]</cref>
+//           <cref>world.surface.R.T[2,2]</cref>
+//           <cref>world.surface.R.T[2,3]</cref>
+//           <cref>world.surface.R.T[3,1]</cref>
+//           <cref>world.surface.R.T[3,2]</cref>
+//           <cref>world.surface.R.T[3,3]</cref>
+//       </T>
+//       <r>
+//           <exp>0.0</exp>
+//           <exp>0.0</exp>
+//           <exp>0.0</exp>
+//       </r>
+//       <nu><exp>2</exp></nu>
+//       <nv><exp>2</exp></nv>
+//       <wireframe><bconst>false</bconst></wireframe>
+//       <multiColored><bconst>false</bconst></multiColored>
+//       <color>
+//           <exp>215.0</exp>
+//           <exp>215.0</exp>
+//           <exp>215.0</exp>
+//       </color>
+//       <specCoeff><exp>0.0</exp></specCoeff>
+//       <transparency><exp>0.0</exp></transparency>
+//   </surface>
+//   <surface>
 //       <ident>surface</ident>
 //       <T>
 //           <exp>1.0</exp>
@@ -858,36 +888,6 @@ getErrorString();
 //       </color>
 //       <specCoeff><exp>0.0</exp></specCoeff>
 //   </shape>
-//   <surface>
-//       <ident>world.surface</ident>
-//       <T>
-//           <exp>1.0</exp>
-//           <exp>6.123233995736766e-17</exp>
-//           <exp>-6.123233995736766e-17</exp>
-//           <exp>6.123233995736766e-17</exp>
-//           <exp>-1.0</exp>
-//           <exp>0.0</exp>
-//           <exp>-6.123233995736766e-17</exp>
-//           <exp>-3.749399456654644e-33</exp>
-//           <exp>-1.0</exp>
-//       </T>
-//       <r>
-//           <exp>0.0</exp>
-//           <exp>0.0</exp>
-//           <exp>0.0</exp>
-//       </r>
-//       <nu><exp>2</exp></nu>
-//       <nv><exp>2</exp></nv>
-//       <wireframe><bconst>false</bconst></wireframe>
-//       <multiColored><bconst>false</bconst></multiColored>
-//       <color>
-//           <exp>215.0</exp>
-//           <exp>215.0</exp>
-//           <exp>215.0</exp>
-//       </color>
-//       <specCoeff><exp>0.0</exp></specCoeff>
-//       <transparency><exp>0.0</exp></transparency>
-//   </surface>
 // </visualization>"
 // ""
 // endResult