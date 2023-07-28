467e9b8fbdb3009ab668cab32a631f5b453c2490
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2023-07-28 16:32:45 +0200

    [WIP] remove TLM from build

--- omsimulator.cmake
+++ omsimulator.cmake
@@ -16,13 +16,13 @@ ExternalProject_Add(OMSimulator_external
                                                 config-3rdParty
                                                 BUILD_TYPE=${CMAKE_BUILD_TYPE}
                                                 CERES=OFF
-                                                OMTLM=OFF  # TODO AHeu: Re-activate when OMTLMSimulator can be compiled without OMDev
+                                                OMTLM=OFF
                                                 host_short=${CMAKE_LIBRARY_ARCHITECTURE}
                                                 CMAKE="${CMAKE_COMMAND}"
                       COMMAND ${CMAKE_MAKE_PROGRAM} -C ${CMAKE_CURRENT_SOURCE_DIR}/OMSimulator
                                                 -j${NUM_PROCESSPRS}
                                                 config-OMSimulator
-                                                OMTLM=OFF  # TODO AHeu: Re-activate when OMTLMSimulator can be compiled without OMDev
+                                                OMTLM=OFF
                                                 BUILD_TYPE=${CMAKE_BUILD_TYPE}
                                                 OMSYSIDENT=OFF
                                                 OMBUILDDIR=${CMAKE_CURRENT_BINARY_DIR}/OMSimulator
@@ -33,7 +33,7 @@ ExternalProject_Add(OMSimulator_external
     BUILD_COMMAND COMMAND ${CMAKE_MAKE_PROGRAM} -C ${CMAKE_CURRENT_SOURCE_DIR}/OMSimulator
                                                 -j${NUM_PROCESSPRS}
                                                 OMSimulator
-                                                OMTLM=OFF  # TODO AHeu: Re-activate when OMTLMSimulator can be compiled without OMDev
+                                                OMTLM=OFF
                                                 BUILD_TYPE=${CMAKE_BUILD_TYPE}
                                                 OMBUILDDIR=${CMAKE_CURRENT_BINARY_DIR}/OMSimulator
                                                 host_short=${CMAKE_LIBRARY_ARCHITECTURE}
