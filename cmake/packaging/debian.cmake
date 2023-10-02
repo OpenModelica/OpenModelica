da018ab11cbf7fc8e4e402f30221bbef49572f60
Mahder Gebremedhin <mahge@users.noreply.github.com>
2023-10-02 10:51:35 +0200

    Initial support for CMake based NSIS and RPM packaging. (#11295)
      - These is a very basic support. Most of the information is similar
        between these packages and is already available. For example, these
        use the same components, contact info, licence ... as the existing
        debian and macOS productbuild package support.
    
      - NSIS packaging should be improved a bit more to create the shortcuts
        and other Windows related information.
    
      - The packages install and seem to work but need a lot of testing to make
        sure they are usable.
    
      - The only big platform that does not have a packaging support is Arch
        Linux. Unfortunately, cmake does not have support for pacman packages
        yet.

--- cmake/packaging/debian.cmake
+++ cmake/packaging/debian.cmake
@@ -1,10 +1,13 @@
-# Options and settings that are specific to debian packages.
+# Options and settings that are specific to Debian packages.
+# https://cmake.org/cmake/help/latest/cpack_gen/deb.html
+# usage: cpack -G DEB
 
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
 
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "OpenModelica Build System <${CPACK_PACKAGE_CONTACT}>")
 
-# Enable component based packaging (openmodelica-compiler, openmodelica-gui, ...)
+# Enable component based packaging (omc, omedit, omsimulator, fmu, simrtcpp ...)
+# See the file common.cmake for a list of the components.
 set(CPACK_DEB_COMPONENT_INSTALL ON)
 
 # use dpkg-shlibdeps to generate better package dependency list.
---
+++ cmake/packaging/nsis.cmake
@@ -0,0 +1,30 @@
+# Options and settings that are specific to Windows NSIS packages.
+# https://cmake.org/cmake/help/latest/cpack_gen/nsis.html
+# usage: cpack -G NSIS64
+
+set(CPACK_PACKAGING_INSTALL_PREFIX ".")
+
+# This assumes the images are in the root OpenModelica directory for now.
+set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}\\OpenModelica.ico")
+set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP "${CMAKE_SOURCE_DIR}\\openmodelica.bmp")
+set(CPACK_NSIS_MUI_UNWELCOMEFINISHPAGE_BITMAP "${CMAKE_SOURCE_DIR}\\openmodelica.bmp")
+
+SET (CPACK_NSIS_HELP_LINK      "https://openmodelica.org/doc/OpenModelicaUsersGuide/latest/")
+SET (CPACK_NSIS_URL_INFO_ABOUT "http://openmodelica.org")
+SET (CPACK_NSIS_CONTACT        "openmodelica@ida.liu.se")
+
+# These need a bit more testing and work. The shortcuts do not work yet.
+set(CPACK_CREATE_DESKTOP_LINKS OMEdit)
+set(CPACK_CREATE_DESKTOP_LINKS OMNotebook)
+set(CPACK_CREATE_DESKTOP_LINKS OMShell)
+
+# Ask to modify path during installation
+set(CPACK_NSIS_MODIFY_PATH ON)
+
+# Ask to uninstall previous installation if the same version is installed.
+set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
+
+# Ask to start OMEdit after installation. right now this says 'Start OpenModelica'.
+# The text at the second command below seems to be ignored for some reason.
+set(CPACK_NSIS_MUI_FINISHPAGE_RUN "OMEdit.exe")
+set(CPACK_NSIS_MUI_FINISHPAGE_RUN_TEXT "Start OpenModelica Connection Editor (OMEdit)")
