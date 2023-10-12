34d9b6e699e72239c16d504b6012390399bf0cfe
Adeel Asghar <adeel.asghar@liu.se>
2023-10-12 14:34:48 +0200

    Preserve the state of the painter before drawing (#11370)
    Fixes #11366 and fixes #11368

--- OMEdit/OMEditLIB/Element/Element.cpp
+++ OMEdit/OMEditLIB/Element/Element.cpp
@@ -2248,11 +2248,15 @@ ModelInstance::Component* Element::getModelComponentByName(ModelInstance::Model 
 void Element::reDrawConnector(QPainter *painter)
 {
   if (mpDefaultElementRectangle && mpDefaultElementRectangle->isVisible()) {
+    painter->save();
     mpDefaultElementRectangle->drawAnnotation(painter, true);
+    painter->restore();
   }
 
   if (mpDefaultElementText && mpDefaultElementText->isVisible()) {
+    painter->save();
     mpDefaultElementText->drawAnnotation(painter, true);
+    painter->restore();
   }
 
   foreach (Element *pInheritedElement, mInheritedElementsList) {
@@ -2260,7 +2264,9 @@ void Element::reDrawConnector(QPainter *painter)
   }
 
   foreach (ShapeAnnotation *pShapeAnnotation, mShapesList) {
+    painter->save();
     pShapeAnnotation->drawAnnotation(painter, true);
+    painter->restore();
   }
 
   foreach (Element *pElement, mElementsList) {
