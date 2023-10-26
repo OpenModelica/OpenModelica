f2337ab5f525451fa488abf578eff5648de418c6
Mahder Gebremedhin <mahge@users.noreply.github.com>
2023-10-26 19:47:27 +0200

    Reorganize the CMake based packaging support. (#11465)
      - We now provide options based on what package generator is selected when
        CPack is invoked. Previously we set this options at CMake configure time
        which meant we could choose some options properly because different
        generators wanted them set differently. Since we do not (in general)
        know what generator will be used at CMake configure time, that approach
        was not going to work out.
    
        This new way, of telling CPack to use some setting file at runtime and
        specifying our options in that file, is what should have been done from
        the get go. Now we know.
    
      - Update OMSimulator/3rdParty to remove addition of unwanted installtion
        component.

--- cmake/packaging/rpm.cmake
+++
@@ -1,9 +0,0 @@
-# Options and settings that are specific to RPM packages.
-# https://cmake.org/cmake/help/latest/cpack_gen/rpm.html
-# usage: cpack -G RPM
-
-# Enable component based packaging (omc, omedit, omsimulator, fmu, simrtcpp ...)
-# See the file common.cmake for a list of the components.
-set(CPACK_RPM_COMPONENT_INSTALL ON)
-
-set(CPACK_RPM_PACKAGE_LICENSE "${PROJECT_SOURCE_DIR}/OSMC-License.txt")
