cdea2ff72a23258548518f5735b7dc5367b137cf
AustraleanPerson <147278612+AustraleanPerson@users.noreply.github.com>
2023-12-08 13:38:54 +0300

    Merge recent changes.

--- OMEdit/OMEditLIB/Annotations/TextAnnotation.cpp
+++ OMEdit/OMEditLIB/Annotations/TextAnnotation.cpp
@@ -435,10 +435,10 @@ void TextAnnotation::drawAnnotation(QPainter *painter)
     painter->setFont(font);
   }
   /* Try to get the elided text if calculated font size <= Helper::minimumTextFontSize
-   * OR if font size is absolute.
+   * OR if font size is absolute and text is not multiline.
    */
   QString textToDraw = textString;
-  if (mappedBoundingRect.width() > 1 && ((mFontSize <= 0 && painter->font().pointSizeF() <= Helper::minimumTextFontSize) || mFontSize > 0)) {
+  if (mappedBoundingRect.width() > 1 && ((mFontSize <= 0 && painter->font().pointSizeF() <= Helper::minimumTextFontSize) || (mFontSize > 0 && !Utilities::isMultiline(textString)))) {
     QFontMetrics fontMetrics(painter->font());
     textToDraw = fontMetrics.elidedText(textString, Qt::ElideRight, mappedBoundingRect.width());
     // if we get "..." i.e., QChar(0x2026) as textToDraw then don't draw anything
