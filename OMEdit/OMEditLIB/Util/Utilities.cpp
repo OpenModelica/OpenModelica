4abaee9acdc610c7168c0d1a71882a30bc343529
Adeel Asghar <adeel.asghar@liu.se>
2024-03-04 16:47:43 +0100

    Allow entering values in exponential form for parameters (#12064)

--- OMEdit/OMEditLIB/Util/Utilities.cpp
+++ OMEdit/OMEditLIB/Util/Utilities.cpp
@@ -649,7 +649,7 @@ bool Utilities::isValueLiteralConstant(QString value)
    * Issue #11840. Allow setting array of values.
    * The following regular expression allows decimal values and array of decimal values. The values can be negative.
    */
-  QRegExp rx("\\{?\\s*-?\\d+(\\.\\d+)?(?:\\s*,\\s*-?\\d+(\\.\\d+)?)*\\s*\\}?");
+  QRegExp rx("\\{?\\s*-?\\d+(\\.\\d+)?([eE][-+]?\\d+)?(?:\\s*,\\s*-?\\d+(\\.\\d+)?([eE][-+]?\\d+)?)*\\s*\\}?");
   return rx.exactMatch(value);
 }
 
