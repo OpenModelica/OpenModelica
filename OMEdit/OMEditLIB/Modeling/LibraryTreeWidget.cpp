91f85c499c8b9d4aed4f110e8ffbaab94e34a222
Adeel Asghar <adeel.asghar@liu.se>
2024-06-05 14:35:32 +0200

    Utilize the horizontal space to display heading (#12506)
    #12466

--- OMEdit/OMEditLIB/Modeling/LibraryTreeWidget.cpp
+++ OMEdit/OMEditLIB/Modeling/LibraryTreeWidget.cpp
@@ -3527,10 +3527,17 @@ void LibraryTreeView::openInformationDialog()
     pInformationDialog->setAttribute(Qt::WA_DeleteOnClose);
     pInformationDialog->setWindowTitle(QString("%1 - %2 - %3").arg(Helper::applicationName, pLibraryTreeItem->getNameStructure(), Helper::information));
     pInformationDialog->setMinimumWidth(300);
+    // Allow to take 90% of screen width
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 11, 0))
+    int screenWidth = QApplication::primaryScreen()->availableGeometry().width() * 0.9;
+#else // QT_VERSION_CHECK
+    int screenWidth = QApplication::desktop()->availableGeometry().width() * 0.9;
+#endif // QT_VERSION_CHECK
+    pInformationDialog->setMaximumWidth(screenWidth);
     Label *pHeadingLabel = Utilities::getHeadingLabel(pLibraryTreeItem->getNameStructure());
     pHeadingLabel->setElideMode(Qt::ElideMiddle);
     QVBoxLayout *pLayout = new QVBoxLayout;
-    pLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+    pLayout->setAlignment(Qt::AlignTop);
     pLayout->addWidget(pHeadingLabel);
     pLayout->addWidget(new Label(tr("Version : %1").arg(pLibraryTreeItem->getVersion())));
     pLayout->addWidget(new Label(tr("Version Date : %1").arg(pLibraryTreeItem->getVersionDate())));
