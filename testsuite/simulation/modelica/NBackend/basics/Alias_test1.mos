OpenModelica/OpenModelica

commit 504611b14fc3716ffed17582a151fa219f94d80d

[NB] Update Alias module (#12526)
* [NB]Update for Alias module

* Add testfiles

* Fixing annotation of tearingSelect attribute

* Optimized tests and deleting unnecessary function

* Fix empty case in checkNominalThreshold

* Add input overwrite in set functions and remove function setFixed2

* Remove not needed comments and create constant string

* Swap fixed values and update collector after setting new attribute values

* Split and minimize test cases

* Adjust error cases

* Adjust makefile

* fix merging errors

* [NB] overwrite start value when replacing with binding

* Adjust testcases and remove unnecessary testcases

---------

Co-authored-by: kabdelhak <karim.abdelhak@hsbi.de>

testsuite/simulation/modelica/NBackend/basics/Alias_test1.mos
@@ -1,0 +1,56 @@
+// name: Alias_test1
+// keywords: NewBackend, Alias
+// status: correct
+// function: Generates a notification if TearingSelect values differ and throws an error if multiple variables are fixed with different start values.
+
+loadString("
+  model Test1
+    Real y;                               // not simple
+    Real x(start=-5, fixed=true);
+    Real z(start=-10, fixed=true);        // regular simple
+  equation
+    x = -2*z;
+    y = time + 3.0;
+    z = sin(time);
+  end Test1;
+"); getErrorString();
+
+setCommandLineOptions("--newBackend -d=dumprepl");
+simulate(Test1); getErrorString();
+
+// Result:
+// true
+// ""
+// true
+// ==========================
+// [dumprepl] Alias Sets:
+// ==========================
+//
+// Alias Set 1:
+// **************
+// 	Constant/Parameter Binding: [SCAL] (1) y = 3.0 + time ($RES_SIM_1)
+// 	###<No Set Equations>
+//
+// Alias Set 2:
+// **************
+// 	<No Constant/Parameter Binding>
+// 	### Set Equations:
+// 	[SCAL] (1) z = $FUN_1 ($RES_SIM_0)
+// 	[SCAL] (1) x = -2.0 * z ($RES_SIM_2)
+//
+// record SimulationResult
+//     resultFile = "",
+//     simulationOptions = "startTime = 0.0, stopTime = 1.0, numberOfIntervals = 500, tolerance = 1e-6, method = 'dassl', fileNamePrefix = 'Test1', options = '', outputFormat = 'mat', variableFilter = '.*', cflags = '', simflags = ''",
+//     messages = "Failed to build model: Test1"
+// end SimulationResult;
+// "Error: Internal error NBAlias.setStartFixed failed because multiple variables are fixed with different start values!
+// 	<No Constant/Parameter Binding>
+// 	### Set Equations:
+// 	[SCAL] (1) z = $FUN_1 ($RES_SIM_0)
+// 	[SCAL] (1) x = -2.0 * z ($RES_SIM_2)
+//
+// 	Fixed start map after replacements:
+// 	(x, -5.0)
+// 	(z, 20.0)
+// "
+// endResult

OMCompiler/Compiler/NBackEnd/Classes/NBEquation.mo
@@ -85,6 +85,7 @@ public
   constant String SIMULATION_STR  = "SIM";
   constant String START_STR       = "SRT";
   constant String PRE_STR         = "PRE";
+  constant String TMP_STR         = "TMP";
 
   type EquationPointer = Pointer<Equation> "mainly used for mapping purposes";
 

OMCompiler/Compiler/NBackEnd/Classes/NBVariable.mo
@@ -45,6 +45,7 @@ public
 
   //NF Imports
   import Attributes = NFAttributes;
+  import BackendExtension = NFBackendExtension;
   import NFBackendExtension.{BackendInfo, StateSelect, VariableAttributes, VariableKind};
   import NFBinding.Binding;
   import Ceval = NFCeval;
@@ -569,6 +570,91 @@ public
       then var;
     end match;
   end setVariableAttributes;
+
+  function setMin
+    input output Variable var;
+    input Option<Expression> min_val;
+    input Boolean overwrite = false;
+  algorithm
+    var := match var
+      local
+        BackendExtension.BackendInfo backendinfo;
+        BackendExtension.VariableAttributes variableAttributes;
+      case NFVariable.VARIABLE(backendinfo = backendinfo as BackendExtension.BACKEND_INFO(attributes = variableAttributes)) algorithm
+
+        backendinfo.attributes := BackendExtension.VariableAttributes.setMin(variableAttributes, min_val, overwrite);
+        var.backendinfo := backendinfo;
+      then var;
+    end match;
+  end setMin;
+
+  function setMax
+    input output Variable var;
+    input Option<Expression> max_val;
+    input Boolean overwrite = false;
+  algorithm
+    var := match var
+      local
+        BackendExtension.BackendInfo backendinfo;
+        BackendExtension.VariableAttributes variableAttributes;
+      case NFVariable.VARIABLE(backendinfo = backendinfo as BackendExtension.BACKEND_INFO(attributes = variableAttributes)) algorithm
+
+        backendinfo.attributes := BackendExtension.VariableAttributes.setMax(variableAttributes, max_val, overwrite);
+        var.backendinfo := backendinfo;
+      then var;
+    end match;
+  end setMax;
+
+  function setStartAttribute
+    input output Variable var;
+    input Expression start_val;
+    input Boolean overwrite = false;
+  algorithm
+    var := match var
+      local
+        BackendExtension.BackendInfo backendinfo;
+        BackendExtension.VariableAttributes variableAttributes;
+      case NFVariable.VARIABLE(backendinfo = backendinfo as BackendExtension.BACKEND_INFO(attributes = variableAttributes)) algorithm
+
+        backendinfo.attributes := BackendExtension.VariableAttributes.setStartAttribute(variableAttributes, start_val, overwrite);
+        var.backendinfo := backendinfo;
+      then var;
+    end match;
+  end setStartAttribute;
+
+  function setStateSelect
+    input output Variable var;
+    input BackendExtension.StateSelect stateSelect_val;
+    input Boolean overwrite = false;
+  algorithm
+    var := match var
+      local
+        BackendExtension.BackendInfo backendinfo;
+        BackendExtension.VariableAttributes variableAttributes;
+      case NFVariable.VARIABLE(backendinfo = backendinfo as BackendExtension.BACKEND_INFO(attributes = variableAttributes)) algorithm
+
+        backendinfo.attributes := BackendExtension.VariableAttributes.setStateSelect(variableAttributes, stateSelect_val, overwrite);
+        var.backendinfo := backendinfo;
+      then var;
+    end match;
+  end setStateSelect;
+
+  function setTearingSelect
+    input output Variable var;
+    input BackendExtension.TearingSelect tearingSelect_val;
+    input Boolean overwrite = false;
+  algorithm
+    var := match var
+      local
+        BackendExtension.BackendInfo backendinfo;
+        BackendExtension.VariableAttributes variableAttributes;
+      case NFVariable.VARIABLE(backendinfo = backendinfo as BackendExtension.BACKEND_INFO(attributes = variableAttributes)) algorithm
+
+        backendinfo.attributes := BackendExtension.VariableAttributes.setTearingSelect(variableAttributes, tearingSelect_val, overwrite);
+        var.backendinfo := backendinfo;
+      then var;
+    end match;
+  end setTearingSelect;
 
   function setVarKind
     "use with caution: some variable kinds have extra information that needs to be correct"
@@ -1166,6 +1252,7 @@ public
   function setFixed
     input output Pointer<Variable> var_ptr;
     input Boolean b = true;
+    input Boolean overwrite = false;
   protected
     Variable var;
   algorithm
@@ -1175,7 +1262,7 @@ public
         BackendInfo binfo;
 
       case Variable.VARIABLE(backendinfo = binfo as BackendInfo.BACKEND_INFO()) algorithm
-        binfo.attributes := VariableAttributes.setFixed(binfo.attributes, var.ty, b);
+        binfo.attributes := VariableAttributes.setFixed(binfo.attributes, var.ty, b, overwrite);
         var.backendinfo := binfo;
       then var;
 
@@ -1201,7 +1288,7 @@ public
 
       case Variable.VARIABLE(backendinfo = binfo as BackendInfo.BACKEND_INFO()) algorithm
         start := Binding.getExp(var.binding);
-        binfo.attributes := VariableAttributes.setStartAttribute(binfo.attributes, start);
+        binfo.attributes := VariableAttributes.setStartAttribute(binfo.attributes, start, true);
         var.backendinfo := binfo;
       then var;
 

OMCompiler/Compiler/NFFrontEnd/NFBackendExtension.mo
@@ -48,6 +48,7 @@ protected
   import Attributes = NFAttributes;
   import NFBinding.Binding;
   import Call = NFCall;
+  import Ceval = NFCeval;
   import ComplexType = NFComplexType;
   import NFComponent.Component;
   import ComponentRef = NFComponentRef;
@@ -579,25 +580,26 @@ public
     function setStartAttribute
       input output VariableAttributes attributes;
       input Expression start;
+      input Boolean overwrite = false;
     algorithm
       attributes := match attributes
-        case VAR_ATTR_REAL() algorithm
+        case VAR_ATTR_REAL() guard(overwrite or isNone(attributes.start)) algorithm
           attributes.start := SOME(start);
         then attributes;
 
-        case VAR_ATTR_INT() algorithm
+        case VAR_ATTR_INT() guard(overwrite or isNone(attributes.start)) algorithm
           attributes.start := SOME(start);
         then attributes;
 
-        case VAR_ATTR_BOOL() algorithm
+        case VAR_ATTR_BOOL() guard(overwrite or isNone(attributes.start)) algorithm
           attributes.start := SOME(start);
         then attributes;
 
-        case VAR_ATTR_STRING() algorithm
+        case VAR_ATTR_STRING() guard(overwrite or isNone(attributes.start)) algorithm
           attributes.start := SOME(start);
         then attributes;
 
-        case VAR_ATTR_ENUMERATION() algorithm
+        case VAR_ATTR_ENUMERATION() guard(overwrite or isNone(attributes.start)) algorithm
           attributes.start := SOME(start);
         then attributes;
 
@@ -628,6 +630,82 @@ public
         else StateSelect.DEFAULT;
       end match;
     end getStateSelect;
+
+    function setMin
+      input output VariableAttributes attributes;
+      input Option<Expression> min_val;
+      input Boolean overwrite = false;
+    algorithm
+      attributes := match attributes
+
+        case VAR_ATTR_REAL() guard(overwrite or isNone(attributes.min)) algorithm
+          attributes.min := min_val;
+        then attributes;
+
+        case VAR_ATTR_INT() guard(overwrite or isNone(attributes.min)) algorithm
+          attributes.min := min_val;
+        then attributes;
+
+        case VAR_ATTR_ENUMERATION() guard(overwrite or isNone(attributes.min)) algorithm
+          attributes.min := min_val;
+        then attributes;
+
+        else attributes;
+      end match;
+    end setMin;
+
+    function setMax
+      input output VariableAttributes attributes;
+      input Option<Expression> max_val;
+      input Boolean overwrite = false;
+    algorithm
+      attributes := match attributes
+
+        case VAR_ATTR_REAL() guard(overwrite or isNone(attributes.max))algorithm
+          attributes.max := max_val;
+        then attributes;
+
+        case VAR_ATTR_INT() guard(overwrite or isNone(attributes.max)) algorithm
+          attributes.max := max_val;
+        then attributes;
+
+        case VAR_ATTR_ENUMERATION() guard(overwrite or isNone(attributes.max)) algorithm
+          attributes.max := max_val;
+        then attributes;
+
+        else attributes;
+      end match;
+    end setMax;
+
+    function setStateSelect
+      input output VariableAttributes attributes;
+      input StateSelect stateSelect_val;
+      input Boolean overwrite = false;
+    algorithm
+      attributes := match attributes
+
+        case VAR_ATTR_REAL() guard(overwrite or isNone(attributes.stateSelect)) algorithm
+          attributes.stateSelect := SOME(stateSelect_val);
+        then attributes;
+
+        else attributes;
+      end match;
+    end setStateSelect;
+
+    function setTearingSelect
+      input output VariableAttributes attributes;
+      input TearingSelect tearingSelect_val;
+      input Boolean overwrite = false;
+    algorithm
+      attributes := match attributes
+
+        case VAR_ATTR_REAL() guard(overwrite or isNone(attributes.tearingSelect)) algorithm
+          attributes.tearingSelect := SOME(tearingSelect_val);
+        then attributes;
+
+        else attributes;
+      end match;
+    end setTearingSelect;
 
     function scalarizeReal
       input ExpressionIterator        quantity_iter "quantity";
@@ -893,7 +971,7 @@ public
       end match;
     end elemType;
 
-  protected
+  //protected
     function attributesToString
       input list<tuple<String, Option<Expression>>> tpl_list;
       input Option<StateSelect> stateSelect;
@@ -907,8 +985,8 @@ public
         buffer := attributeToString(tpl, buffer);
       end for;
 
-      buffer := stateSelectString(stateSelect, buffer);
-      buffer := tearingSelectString(tearingSelect, buffer);
+      buffer := stateSelectStringBuffer(stateSelect, buffer);
+      buffer := tearingSelectStringBuffer(tearingSelect, buffer);
 
       buffer := listReverse(buffer);
 
@@ -938,33 +1016,56 @@ public
     end attributeToString;
 
     function stateSelectString
-      input Option<StateSelect> optStateSelect;
-      input output list<String> buffer;
+      input StateSelect stateSelect;
+      output String str;
     algorithm
-      buffer := match optStateSelect
-        case SOME(StateSelect.NEVER)    then "StateSelect = never" :: buffer;
-        case SOME(StateSelect.AVOID)    then "StateSelect = avoid" :: buffer;
-        case SOME(StateSelect.DEFAULT)  then "StateSelect = default" :: buffer;
-        case SOME(StateSelect.PREFER)   then "StateSelect = prefer" :: buffer;
-        case SOME(StateSelect.ALWAYS)   then "StateSelect = always" :: buffer;
-        else buffer;
+      str := match stateSelect
+        case StateSelect.NEVER    then "StateSelect = never";
+        case StateSelect.AVOID    then "StateSelect = avoid";
+        case StateSelect.DEFAULT  then "StateSelect = default";
+        case StateSelect.PREFER   then "StateSelect = prefer";
+        case StateSelect.ALWAYS   then "StateSelect = always";
       end match;
     end stateSelectString;
 
     function tearingSelectString
-      input Option<TearingSelect> optTearingSelect;
-      input output list<String> buffer;
+      input TearingSelect tearingSelect;
+      output String str;
     algorithm
-      buffer := match optTearingSelect
-        case SOME(TearingSelect.NEVER)    then "TearingSelect = never" :: buffer;
-        case SOME(TearingSelect.AVOID)    then "TearingSelect = avoid" :: buffer;
-        case SOME(TearingSelect.DEFAULT)  then "TearingSelect = default" :: buffer;
-        case SOME(TearingSelect.PREFER)   then "TearingSelect = prefer" :: buffer;
-        case SOME(TearingSelect.ALWAYS)   then "TearingSelect = always" :: buffer;
-        else buffer;
+      str := match tearingSelect
+        case TearingSelect.NEVER    then "TearingSelect = never";
+        case TearingSelect.AVOID    then "TearingSelect = avoid";
+        case TearingSelect.DEFAULT  then "TearingSelect = default";
+        case TearingSelect.PREFER   then "TearingSelect = prefer";
+        case TearingSelect.ALWAYS   then "TearingSelect = always";
       end match;
     end tearingSelectString;
 
+    function stateSelectStringBuffer
+      input Option<StateSelect> optStateSelect;
+      input output list<String> buffer;
+    protected
+      StateSelect stateSelect;
+    algorithm
+      if isSome(optStateSelect) then
+        SOME(stateSelect) := optStateSelect;
+        buffer := stateSelectString(stateSelect) :: buffer;
+      end if;
+    end stateSelectStringBuffer;
+
+    function tearingSelectStringBuffer
+      input Option<TearingSelect> optTearingSelect;
+      input output list<String> buffer;
+    protected
+      TearingSelect tearingSelect;
+    algorithm
+      if isSome(optTearingSelect) then
+        SOME(tearingSelect) := optTearingSelect;
+        buffer := tearingSelectString(tearingSelect) :: buffer;
+      end if;
+    end tearingSelectStringBuffer;
+
+  protected
     function createReal
       input list<tuple<String, Binding>> attrs;
       input Boolean isFinal;

testsuite/simulation/modelica/NBackend/basics/Alias.mos
@@ -5,7 +5,7 @@
 loadString("
   model Alias
     Real y;             // not simple
-    Real x, z;          // regular simple
+    Real x,z;           // regular simple
     Real a, b, c;       // constant simple
   equation
     a = b;
@@ -58,7 +58,7 @@ simulate(Alias); getErrorString();
 //
 // record SimulationResult
 //     resultFile = "Alias_res.mat",
-//     simulationOptions = "startTime = 0.0, stopTime = 1.0, numberOfIntervals = 500, tolerance = 1e-06, method = 'dassl', fileNamePrefix = 'Alias', options = '', outputFormat = 'mat', variableFilter = '.*', cflags = '', simflags = ''",
+//     simulationOptions = "startTime = 0.0, stopTime = 1.0, numberOfIntervals = 500, tolerance = 1e-6, method = 'dassl', fileNamePrefix = 'Alias', options = '', outputFormat = 'mat', variableFilter = '.*', cflags = '', simflags = ''",
 //     messages = "LOG_SUCCESS       | info    | The initialization finished successfully without homotopy method.
 // LOG_SUCCESS       | info    | The simulation finished successfully.
 // "

testsuite/simulation/modelica/NBackend/basics/Alias_test2.mos
@@ -1,0 +1,56 @@
+// name: Alias_test2
+// keywords: NewBackend, Alias
+// status: correct
+// function: Throws an error if there are non literal nominal values.
+
+loadString("
+  model Test2
+    parameter Real p(fixed=false);
+    Real y;                   // not simple
+    Real x(nominal=2);
+    Real z(nominal=p);        // regular simple
+  equation
+    x = -2*z;
+    y = time + 3.0;
+    z = sin(time);
+  end Test2;
+"); getErrorString();
+setCommandLineOptions("--newBackend -d=dumprepl");
+simulate(Test2); getErrorString();
+
+// Result:
+// true
+// ""
+// true
+// ==========================
+// [dumprepl] Alias Sets:
+// ==========================
+//
+// Alias Set 1:
+// **************
+// 	Constant/Parameter Binding: [SCAL] (1) y = 3.0 + time ($RES_SIM_1)
+// 	###<No Set Equations>
+//
+// Alias Set 2:
+// **************
+// 	<No Constant/Parameter Binding>
+// 	### Set Equations:
+// 	[SCAL] (1) z = $FUN_1 ($RES_SIM_0)
+// 	[SCAL] (1) x = -2.0 * z ($RES_SIM_2)
+//
+// record SimulationResult
+//     resultFile = "",
+//     simulationOptions = "startTime = 0.0, stopTime = 1.0, numberOfIntervals = 500, tolerance = 1e-6, method = 'dassl', fileNamePrefix = 'Test2', options = '', outputFormat = 'mat', variableFilter = '.*', cflags = '', simflags = ''",
+//     messages = "Failed to build model: Test2"
+// end SimulationResult;
+// "Warning: NBAlias.checkNominalThreshold failed because non literal nominal values are not allowed.
+// 	<No Constant/Parameter Binding>
+// 	### Set Equations:
+// 	[SCAL] (1) z = $FUN_1 ($RES_SIM_0)
+// 	[SCAL] (1) x = -2.0 * z ($RES_SIM_2)
+//
+// 	Nominal map after replacements:
+// 	(x, 2.0)
+// 	(z, p)
+// "
+// endResult

testsuite/simulation/modelica/NBackend/basics/Alias_test3.mos
@@ -1,0 +1,55 @@
+// name: Alias_test3
+// keywords: NewBackend, Alias
+// status: correct
+// function: Throws an error if multiple variables have StateSelect = always.
+
+loadString("
+  model Test3
+    Real y;                                        // not simple
+    Real x(stateSelect=StateSelect.always);
+    Real z(stateSelect=StateSelect.always);        // regular simple
+  equation
+    x = -2*z;
+    y = time + 3.0;
+    z = sin(time);
+  end Test3;
+"); getErrorString();
+setCommandLineOptions("--newBackend -d=dumprepl");
+simulate(Test3); getErrorString();
+
+// Result:
+// true
+// ""
+// true
+// ==========================
+// [dumprepl] Alias Sets:
+// ==========================
+//
+// Alias Set 1:
+// **************
+// 	Constant/Parameter Binding: [SCAL] (1) y = 3.0 + time ($RES_SIM_1)
+// 	###<No Set Equations>
+//
+// Alias Set 2:
+// **************
+// 	<No Constant/Parameter Binding>
+// 	### Set Equations:
+// 	[SCAL] (1) z = $FUN_1 ($RES_SIM_0)
+// 	[SCAL] (1) x = -2.0 * z ($RES_SIM_2)
+//
+// record SimulationResult
+//     resultFile = "",
+//     simulationOptions = "startTime = 0.0, stopTime = 1.0, numberOfIntervals = 500, tolerance = 1e-6, method = 'dassl', fileNamePrefix = 'Test3', options = '', outputFormat = 'mat', variableFilter = '.*', cflags = '', simflags = ''",
+//     messages = "Failed to build model: Test3"
+// end SimulationResult;
+// "Error: Internal error NBAlias.stateSelectAlways failed because multiple variables have StateSelect = always!
+// 	<No Constant/Parameter Binding>
+// 	### Set Equations:
+// 	[SCAL] (1) z = $FUN_1 ($RES_SIM_0)
+// 	[SCAL] (1) x = -2.0 * z ($RES_SIM_2)
+//
+// 	StateSelect map after replacements:
+// 	(x, StateSelect = always)
+// 	(z, StateSelect = always)
+// "
+// endResult

commit 4f51b36953450c98a19d927aa8d7c5963f00def8

[NB] Fixing issues in Alias module introduced in #12526 (#12697)

OMCompiler/Compiler/NBackEnd/Modules/2_Pre/NBAlias.mo
@@ -861,7 +861,6 @@ protected
     list<Expression> start_lst = UnorderedMap.valueList(start_map);
     list<Expression> fixed_start_lst;
     Integer count_fixed = 0;
-    Boolean fixed_val;
     ComponentRef cref;
     Expression sval, fval;
   algorithm
@@ -874,8 +873,7 @@ protected
       end if;
     end for;
     if count_fixed == 0 then
-      fixed_val := false;
-      if listLength(List.unique(start_lst)) > 1 then // if length = 1, then all values are the same
+      if not List.allEqual(start_lst, Expression.isEqual) then
         if Flags.isSet(Flags.DUMP_REPL) then
           Error.addCompilerWarning(getInstanceName() + ": No variables are fixed and they have different start values.\n"
                                   + AliasSet.toString(set) + "\n\tStart map after replacements:\n\t" + UnorderedMap.toString(start_map, ComponentRef.toString, Expression.toString,"\n\t"));
@@ -883,11 +881,9 @@ protected
           Error.addCompilerWarning(getInstanceName() + ": No variables are fixed and they have different start values. Use -d=dumprepl for more information.\n");
         end if;
       end if;
-    elseif count_fixed == 1 then
-      fixed_val := true;
     elseif count_fixed > 1 then
       fixed_start_lst := UnorderedMap.valueList(fixed_start_map);
-      if listLength(List.unique(fixed_start_lst)) > 1 then // if length = 1, then all values are the same
+      if not List.allEqual(fixed_start_lst, Expression.isEqual) then
         if Flags.isSet(Flags.DUMP_REPL) then
           Error.addMessage(Error.INTERNAL_ERROR,{getInstanceName() + " failed because multiple variables are fixed with different start values!\n" + AliasSet.toString(set)
                            + "\n\tFixed start map after replacements:\n\t" + UnorderedMap.toString(fixed_start_map, ComponentRef.toString, Expression.toString,"\n\t")});
@@ -896,7 +892,7 @@ protected
           Error.addMessage(Error.INTERNAL_ERROR,{getInstanceName() + " failed because multiple variables are fixed with different start values! Use -d=dumprepl for more information.\n"});
           fail();
         end if;
-      elseif listLength(List.unique(fixed_start_lst)) == 1 then
+      elseif List.allEqual(fixed_start_lst, Expression.isEqual) then
         if Flags.isSet(Flags.DUMP_REPL) then
           Error.addCompilerWarning(getInstanceName() + ": Multiple variables are fixed and have identical start values.\n"
                                   + AliasSet.toString(set) + "\n\tFixed start map after replacements:\n\t" + UnorderedMap.toString(fixed_start_map, ComponentRef.toString, Expression.toString,"\n\t"));
@@ -979,13 +975,23 @@ protected
     input AliasSet set;
   protected
     list<TearingSelect> lst_values = UnorderedMap.valueList(map);
+    TearingSelect val, first_val;
+    Boolean equal = true;
   algorithm
-    if listLength(List.unique(lst_values)) > 1 then // if length = 1, then all values are the same
-      if Flags.isSet(Flags.DUMP_REPL) then
-        Error.addCompilerNotification("There are different TearingSelect values.\n" + AliasSet.toString(set) + "\n\tTearingSelect map after replacements:\n\t"
-                                      + UnorderedMap.toString(map, ComponentRef.toString, BackendExtension.VariableAttributes.tearingSelectString,"\n\t"));
-      else
-        Error.addCompilerNotification("There are different TearingSelect values. Use -d=dumprepl for more information.\n");
+    if listLength(lst_values) > 0 then
+      first_val := List.first(lst_values);
+      for val in lst_values loop
+        if first_val <> val then
+          equal := false;
+        end if;
+      end for;
+      if equal == false then
+        if Flags.isSet(Flags.DUMP_REPL) then
+          Error.addCompilerNotification("There are different TearingSelect values.\n" + AliasSet.toString(set) + "\n\tTearingSelect map after replacements:\n\t"
+                                        + UnorderedMap.toString(map, ComponentRef.toString, BackendExtension.VariableAttributes.tearingSelectString,"\n\t"));
+        else
+          Error.addCompilerNotification("There are different TearingSelect values. Use -d=dumprepl for more information.\n");
+        end if;
       end if;
     end if;
   end diffTearingSelect;

OMCompiler/Compiler/Util/List.mo
@@ -284,6 +284,35 @@ algorithm
   end match;
 end isEqualOnTrue;
 
+public function allEqual<T>
+  "Takes a list and an equality function, and returns whether all elements in the list are equal."
+  input list<T> inList;
+  input CompFunc inCompFunc;
+  output Boolean outAllEqual = true;
+
+  partial function CompFunc
+    "Returns true if e1 = e2, otherwise false."
+    input T e1;
+    input T e2;
+    output Boolean res;
+  end CompFunc;
+
+protected
+  T e1;
+  list<T> rest;
+algorithm
+  if not listEmpty(inList) then
+    e1 :: rest := inList;
+    if not listEmpty(rest) then
+      for e in rest loop
+        if not inCompFunc(e1,e) then
+          outAllEqual := false;
+        end if;
+      end for;
+    end if;
+  end if;
+end allEqual;
+
 public function compare<T1, T2>
   "Returns -1 if list1 is shorter than list2 or 1 if list1 is longer than list2.
    If both lists are of equal length it applies the given compare function to

testsuite/simulation/modelica/NBackend/basics/Alias_test1.mos
@@ -5,9 +5,9 @@
 
 loadString("
   model Test1
-    Real y;                               // not simple
-    Real x(start=-5, fixed=true);
-    Real z(start=-10, fixed=true);        // regular simple
+    Real y;                                                                                            // not simple
+    Real x(start=-5, fixed=true) annotation(__OpenModelica_tearingSelect=TearingSelect.avoid);
+    Real z(start=-10, fixed=true) annotation(__OpenModelica_tearingSelect=TearingSelect.never);        // regular simple
   equation
     x = -2*z;
     y = time + 3.0;
@@ -43,7 +43,16 @@ simulate(Test1); getErrorString();
 //     simulationOptions = "startTime = 0.0, stopTime = 1.0, numberOfIntervals = 500, tolerance = 1e-6, method = 'dassl', fileNamePrefix = 'Test1', options = '', outputFormat = 'mat', variableFilter = '.*', cflags = '', simflags = ''",
 //     messages = "Failed to build model: Test1"
 // end SimulationResult;
-// "Error: Internal error NBAlias.setStartFixed failed because multiple variables are fixed with different start values!
+// "Notification: There are different TearingSelect values.
+// 	<No Constant/Parameter Binding>
+// 	### Set Equations:
+// 	[SCAL] (1) z = $FUN_1 ($RES_SIM_0)
+// 	[SCAL] (1) x = -2.0 * z ($RES_SIM_2)
+//
+// 	TearingSelect map after replacements:
+// 	(x, TearingSelect = avoid)
+// 	(z, TearingSelect = never)
+// Error: Internal error NBAlias.setStartFixed failed because multiple variables are fixed with different start values!
 // 	<No Constant/Parameter Binding>
 // 	### Set Equations:
 // 	[SCAL] (1) z = $FUN_1 ($RES_SIM_0)