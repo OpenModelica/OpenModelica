14de16a9b042aa008f9e60cad2477a55e4c76307
Adeel Asghar <adeel.asghar@liu.se>
2024-11-21 14:49:52 +0100

    Improved check for value type

--- OMEdit/OMEditLIB/Element/ElementProperties.h
+++ OMEdit/OMEditLIB/Element/ElementProperties.h
@@ -104,7 +104,11 @@ public:
   FinalEachToolButton *getFixedFinalEachMenu() const {return mpFixedFinalEachMenuButton;}
   void setValueType(ValueType valueType) {mValueType = valueType;}
   void setValueWidget(QString value, bool defaultValue, QString fromUnit, bool valueModified = false, bool adjustSize = true, bool unitComboBoxChanged = false);
-  ValueType getValueType() {return mValueType;}
+  bool isEnumeration() const {return mValueType == Enumeration;}
+  bool isReplaceableComponent() const {return mValueType == ReplaceableComponent;}
+  bool isReplaceableClass() const {return mValueType == ReplaceableClass;}
+  bool isRecord() const {return mValueType == Record;}
+  bool isChoices() const {return mValueType == Choices;}
   QWidget* getValueWidget();
   bool isValueModified();
   QString getValue();
