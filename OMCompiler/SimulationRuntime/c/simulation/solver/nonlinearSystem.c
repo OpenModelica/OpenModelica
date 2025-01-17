987c9a842ffdf236399ca2896d27b5f67908bc20
phannebohm <phannebohm@hsbi.de>
2025-01-17 12:23:15 +0100

    [C] Run newton diagnostics only on initial systems
    If there are lambda0 systems only run those.

--- OMCompiler/SimulationRuntime/c/simulation/solver/nonlinearSystem.c
+++ OMCompiler/SimulationRuntime/c/simulation/solver/nonlinearSystem.c
@@ -1226,8 +1226,11 @@ int solve_nonlinear_system(DATA *data, threadData_t *threadData, int sysNumber)
 #if !defined(OMC_MINIMAL_RUNTIME)
   /* Improve start values with newton diagnostics method */
   if(omc_useStream[OMC_LOG_NLS_NEWTON_DIAGNOSTICS] && data->simulationInfo->initial) {
-    infoStreamPrint(OMC_LOG_NLS_NEWTON_DIAGNOSTICS, 0, "Running newton diagnostics for system %ld", nonlinsys->equationIndex);
-    newtonDiagnostics(data, threadData, sysNumber);
+    EQUATION_INFO eqInfo = modelInfoGetEquation(&data->modelData->modelDataXml, nonlinsys->equationIndex);
+    if (eqInfo.section == EQUATION_SECTION_INIT_LAMBDA0 || (eqInfo.section == EQUATION_SECTION_INITIAL && data->callback->functionInitialEquations_lambda0 == NULL)) {
+      infoStreamPrint(OMC_LOG_NLS_NEWTON_DIAGNOSTICS, 0, "Running newton diagnostics for system %ld", nonlinsys->equationIndex);
+      newtonDiagnostics(data, threadData, sysNumber);
+    }
   }
 #endif
 
