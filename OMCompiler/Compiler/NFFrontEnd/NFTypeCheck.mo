OpenModelica/OpenModelica

commit 1d05ece59d62cc8a483a4a00f5673e180502fecc

[NB] fix array binding creation (#13807)
* [NB] fix array binding creation

 - retype after iterator replacement
 - add testcase
 - fixes #13788

* [NF] add clocked typing

* [testsuite] update tests for new binding update

OMCompiler/Compiler/NFFrontEnd/NFTyping.mo
@@ -1447,6 +1447,8 @@ algorithm
 
     case Expression.MULTARY() then typeExp(SimplifyExp.splitMultary(exp), context, info, retype);
 
+    case Expression.CLKCONST() then (exp, Expression.typeOf(exp), Expression.variability(exp), Expression.purity(exp));
+
     else
       algorithm
         Error.assertion(false, getInstanceName() + " got unknown expression: " + Expression.toString(exp), sourceInfo());

OMCompiler/Compiler/NBackEnd/Classes/NBEquation.mo
@@ -59,6 +59,7 @@ public
   import Statement = NFStatement;
   import Subscript = NFSubscript;
   import Type = NFType;
+  import Typing = NFTyping;
   import Variable = NFVariable;
 
   // Old Backend imports
@@ -574,6 +575,7 @@ public
     algorithm
       (exp, iter) := extractFromCall(exp, EMPTY(), replacements);
       exp := Expression.map(exp, function Replacements.applySimpleExp(replacements = replacements));
+      exp := Typing.typeExp(exp, NFInstContext.RHS, sourceInfo(), true);
     end extract;
 
     function extractFromCall

testsuite/simulation/modelica/NBackend/array_handling/for_exp.mos
@@ -31,9 +31,9 @@ simulate(binding_array); getErrorString();
 // Created Parameter Binding Equations (1):
 // ******************************************
 // 	[FOR-] (5) ($RES_BND_0)
-// [----] for $i1 in 1:5 loop
-// [----]   [SCAL] (1) Cu[$i1] = 1e7 * (1.0 + 0.337 * ((-1) + $i1)) ($RES_BND_1)
-// [----] end for;
+// 	[----] for $i1 in 1:5 loop
+// 	[----]   [SCAL] (1) Cu[$i1] = (($i1 - 1) * 1.348 / 4.0 + 1.0) * 1e7 ($RES_BND_1)
+// 	[----] end for;
 //
 // ##############################
 //

testsuite/simulation/modelica/NBackend/array_handling/testVectorizedPowerSystem.mos
@@ -146,12 +146,16 @@ val(fixedVoltageSource1.p[1], 1.0);
 // ### Equation:
 // 	[SCAL] (1) system.thetaRef = 314.1592653589793 * time ($RES_SIM_7)
 //
-// BLOCK 2: Single Strong Component (status = Solve.UNPROCESSED)
+// ================================
+//   (4) Continuous ODE Partition
+// ================================
+//
+// BLOCK 1: Single Strong Component (status = Solve.UNPROCESSED)
 // ---------------------------------------------------------------
 // ### Variable:
 // 	Real system.thetaRel = system.thetaRef - system.thetaRef
 // ### Equation:
-// 	[SCAL] (1) system.thetaRel = system.thetaRef - system.thetaRef ($RES_BND_21)
+// 	[SCAL] (1) system.thetaRel = 0.0 ($RES_BND_21)
 //
 // #################################################
 //
@@ -281,12 +285,16 @@ val(fixedVoltageSource1.p[1], 1.0);
 // ### Equation:
 // 	[SCAL] (1) system.thetaRef = 314.1592653589793 * time ($RES_SIM_7)
 //
-// BLOCK 2: Single Strong Component (status = Solve.UNPROCESSED)
+// ================================
+//   (4) Continuous ODE Partition
+// ================================
+//
+// BLOCK 1: Single Strong Component (status = Solve.UNPROCESSED)
 // ---------------------------------------------------------------
 // ### Variable:
 // 	Real system.thetaRel = system.thetaRef - system.thetaRef
 // ### Equation:
-// 	[SCAL] (1) system.thetaRel = system.thetaRef - system.thetaRef ($RES_BND_21)
+// 	[SCAL] (1) system.thetaRel = 0.0 ($RES_BND_21)
 //
 // #########################################
 //
@@ -375,16 +383,16 @@ val(fixedVoltageSource1.p[1], 1.0);
 // BLOCK 10: Single Strong Component (status = Solve.UNPROCESSED)
 // ----------------------------------------------------------------
 // ### Variable:
-// 	Real system.thetaRef = system.thetaRef
+// 	Real system.thetaRel = system.thetaRef - system.thetaRef
 // ### Equation:
-// 	[SCAL] (1) system.thetaRef = 314.1592653589793 * time ($RES_SIM_7)
+// 	[SCAL] (1) system.thetaRel = 0.0 ($RES_BND_21)
 //
 // BLOCK 11: Single Strong Component (status = Solve.UNPROCESSED)
 // ----------------------------------------------------------------
 // ### Variable:
-// 	Real system.thetaRel = system.thetaRef - system.thetaRef
+// 	Real system.thetaRef = system.thetaRef
 // ### Equation:
-// 	[SCAL] (1) system.thetaRel = system.thetaRef - system.thetaRef ($RES_BND_21)
+// 	[SCAL] (1) system.thetaRef = 314.1592653589793 * time ($RES_SIM_7)
 //
 // BLOCK 12: Single Strong Component (status = Solve.UNPROCESSED)
 // ----------------------------------------------------------------
@@ -550,7 +558,7 @@ val(fixedVoltageSource1.p[1], 1.0);
 //   (3) Continuous ODE Partition
 // ================================
 //
-// --- Alias of INI[1 | 10] ---
+// --- Alias of INI[1 | 11] ---
 // BLOCK 1: Single Strong Component (status = Solve.EXPLICIT)
 // ------------------------------------------------------------
 // ### Variable:
@@ -558,13 +566,17 @@ val(fixedVoltageSource1.p[1], 1.0);
 // ### Equation:
 // 	[SCAL] (1) system.thetaRef = 314.1592653589793 * time ($RES_SIM_7)
 //
-// --- Alias of INI[1 | 11] ---
-// BLOCK 2: Single Strong Component (status = Solve.EXPLICIT)
+// ================================
+//   (4) Continuous ODE Partition
+// ================================
+//
+// --- Alias of INI[1 | 10] ---
+// BLOCK 1: Single Strong Component (status = Solve.EXPLICIT)
 // ------------------------------------------------------------
 // ### Variable:
 // 	Real system.thetaRel = system.thetaRef - system.thetaRef
 // ### Equation:
-// 	[SCAL] (1) system.thetaRel = system.thetaRef - system.thetaRef ($RES_BND_21)
+// 	[SCAL] (1) system.thetaRel = 0.0 ($RES_BND_21)
 //
 // #####################################
 //
@@ -653,16 +665,16 @@ val(fixedVoltageSource1.p[1], 1.0);
 // BLOCK 10: Single Strong Component (status = Solve.EXPLICIT)
 // -------------------------------------------------------------
 // ### Variable:
-// 	Real system.thetaRef = system.thetaRef
+// 	Real system.thetaRel = system.thetaRef - system.thetaRef
 // ### Equation:
-// 	[SCAL] (1) system.thetaRef = 314.1592653589793 * time ($RES_SIM_7)
+// 	[SCAL] (1) system.thetaRel = 0.0 ($RES_BND_21)
 //
 // BLOCK 11: Single Strong Component (status = Solve.EXPLICIT)
 // -------------------------------------------------------------
 // ### Variable:
-// 	Real system.thetaRel = system.thetaRef - system.thetaRef
+// 	Real system.thetaRef = system.thetaRef
 // ### Equation:
-// 	[SCAL] (1) system.thetaRel = system.thetaRef - system.thetaRef ($RES_BND_21)
+// 	[SCAL] (1) system.thetaRef = 314.1592653589793 * time ($RES_SIM_7)
 //
 // BLOCK 12: Single Strong Component (status = Solve.EXPLICIT)
 // -------------------------------------------------------------

testsuite/simulation/modelica/NBackend/simplification/multary_simplification.mos
@@ -26,9 +26,13 @@ simulate(multary_simplification); getErrorString();
 // [BEFORE] 2.0 * (time - (3.0 + 4.0))
 // [AFTER ] 2.0 * ((-7.0) + time)
 //
+// ### dumpSimplify | NBEquation.Equation.generateBindingEquation ###
+// [BEFORE] 2.0 * ((-7.0) + time)
+// [AFTER ] 2.0 * (time - 7.0)
+//
 // record SimulationResult
 //     resultFile = "multary_simplification_res.mat",
-//     simulationOptions = "startTime = 0.0, stopTime = 1.0, numberOfIntervals = 500, tolerance = 1e-06, method = 'dassl', fileNamePrefix = 'multary_simplification', options = '', outputFormat = 'mat', variableFilter = '.*', cflags = '', simflags = ''",
+//     simulationOptions = "startTime = 0.0, stopTime = 1.0, numberOfIntervals = 500, tolerance = 1e-6, method = 'dassl', fileNamePrefix = 'multary_simplification', options = '', outputFormat = 'mat', variableFilter = '.*', cflags = '', simflags = ''",
 //     messages = "LOG_SUCCESS       | info    | The initialization finished successfully without homotopy method.
 // LOG_SUCCESS       | info    | The simulation finished successfully.
 // "

testsuite/simulation/modelica/NBackend/tickets/13788.mos
@@ -1,0 +1,30 @@
+// name: 13788
+// keywords: NewBackend, binding, array
+// status: correct
+
+loadString("
+model MWE_13788
+  parameter Real[2] y = {2*k for k in 0:1};
+end MWE_13788;
+"); getErrorString();
+
+setCommandLineOptions("--newBackend");
+
+simulate(MWE_13788); getErrorString();
+
+diffSimulationResults("MWE_13788_res.mat", "ReferenceFiles/MWE_13788_res.mat", "MWE_13788_diff.csv", 1e-8);
+
+// Result:
+// true
+// ""
+// true
+// record SimulationResult
+//     resultFile = "MWE_13788_res.mat",
+//     simulationOptions = "startTime = 0.0, stopTime = 1.0, numberOfIntervals = 500, tolerance = 1e-6, method = 'dassl', fileNamePrefix = 'MWE_13788', options = '', outputFormat = 'mat', variableFilter = '.*', cflags = '', simflags = ''",
+//     messages = "LOG_SUCCESS       | info    | The initialization finished successfully without homotopy method.
+// LOG_SUCCESS       | info    | The simulation finished successfully.
+// "
+// end SimulationResult;
+// ""
+// (true, {})
+// endResult

testsuite/simulation/modelica/NBackend/tickets/Makefile
@@ -3,6 +3,7 @@ TEST = ../../../../rtest -v
 TESTFILES = \
 13328.mos \
 13144.mos \
+13788.mos \
 
 # test that currently fail. Move up when fixed.
 # Run make failingtest

commit 7b04c8a0e203b7dc91a7ac07fcac257d34602fbd

[NF] allow non elementwise div for arrays
 - needed for retyping
 - attempt to fix more regressions from #13807

OMCompiler/Compiler/NFFrontEnd/NFTypeCheck.mo
@@ -167,22 +167,23 @@ function checkBinaryOperation
   input Variability var2;
   input InstContext.Type context;
   input SourceInfo info;
+  input Boolean retype "when retyping accept non elementwise operators for elementwise binaries";
   output Expression binaryExp;
   output Type resultType;
 algorithm
   if Type.isConditionalArray(type1) or Type.isConditionalArray(type2) then
-    (binaryExp, resultType) := checkConditionalBinaryOperator(exp1, type1, var1, operator, exp2, type2, var2, context, info);
+    (binaryExp, resultType) := checkConditionalBinaryOperator(exp1, type1, var1, operator, exp2, type2, var2, context, info, retype);
   elseif Type.isComplex(Type.arrayElementType(type1)) or
      Type.isComplex(Type.arrayElementType(type2)) then
     (binaryExp, resultType) := checkOverloadedBinaryOperator(exp1, type1, var1, operator, exp2, type2, var2, context, info);
   elseif Type.isBoxed(type1) and Type.isBoxed(type2) then
-    (binaryExp, resultType) := checkBinaryOperationBoxed(exp1, type1, var1, operator, exp2, type2, var2, context, info);
+    (binaryExp, resultType) := checkBinaryOperationBoxed(exp1, type1, var1, operator, exp2, type2, var2, context, info, retype);
   else
     (binaryExp, resultType) := match operator.op
       case Op.ADD then checkBinaryOperationAdd(exp1, type1, exp2, type2, info);
       case Op.SUB then checkBinaryOperationSub(exp1, type1, exp2, type2, info);
       case Op.MUL then checkBinaryOperationMul(exp1, type1, exp2, type2, info);
-      case Op.DIV then checkBinaryOperationDiv(exp1, type1, exp2, type2, info, isElementWise = false);
+      case Op.DIV then checkBinaryOperationDiv(exp1, type1, exp2, type2, info, isElementWise = retype);
       case Op.POW then checkBinaryOperationPow(exp1, type1, exp2, type2, info);
       case Op.ADD_EW then checkBinaryOperationEW(exp1, type1, exp2, type2, Op.ADD, info);
       case Op.SUB_EW then checkBinaryOperationEW(exp1, type1, exp2, type2, Op.SUB, info);
@@ -201,8 +202,8 @@ algorithm
       case Op.MUL_MATRIX_VECTOR then checkBinaryOperationMul(exp1, type1, exp2, type2, info);
       case Op.SCALAR_PRODUCT    then checkBinaryOperationMul(exp1, type1, exp2, type2, info);
       case Op.MATRIX_PRODUCT    then checkBinaryOperationMul(exp1, type1, exp2, type2, info);
-      case Op.DIV_SCALAR_ARRAY  then checkBinaryOperationDiv(exp1, type1, exp2, type2, info, isElementWise = false);
-      case Op.DIV_ARRAY_SCALAR  then checkBinaryOperationDiv(exp1, type1, exp2, type2, info, isElementWise = false);
+      case Op.DIV_SCALAR_ARRAY  then checkBinaryOperationDiv(exp1, type1, exp2, type2, info, isElementWise = retype);
+      case Op.DIV_ARRAY_SCALAR  then checkBinaryOperationDiv(exp1, type1, exp2, type2, info, isElementWise = retype);
       case Op.POW_SCALAR_ARRAY  then checkBinaryOperationPowEW(exp1, type1, exp2, type2, info);
       case Op.POW_ARRAY_SCALAR  then checkBinaryOperationPowEW(exp1, type1, exp2, type2, info);
       case Op.POW_MATRIX        then checkBinaryOperationPow(exp1, type1, exp2, type2, info);
@@ -345,6 +346,7 @@ public function checkBinaryOperationBoxed
   input Variability var2;
   input InstContext.Type context;
   input SourceInfo info;
+  input Boolean retype;
   output Expression outExp;
   output Type outType;
 protected
@@ -353,7 +355,7 @@ protected
 algorithm
   (e1, ty1) := matchTypes(type1, Type.unbox(type1), exp1);
   (e2, ty2) := matchTypes(type2, Type.unbox(type2), exp2);
-  (outExp, outType) := checkBinaryOperation(e1, ty1, var1, op, e2, ty2, var2, context, info);
+  (outExp, outType) := checkBinaryOperation(e1, ty1, var1, op, e2, ty2, var2, context, info, retype);
 end checkBinaryOperationBoxed;
 
 protected
@@ -367,6 +369,7 @@ function checkConditionalBinaryOperator
   input Variability var2;
   input InstContext.Type context;
   input SourceInfo info;
+  input Boolean retype;
   output Expression outExp;
   output Type outType;
 protected
@@ -384,14 +387,14 @@ algorithm
 
   ErrorExt.setCheckpoint(getInstanceName());
   try
-    (e1, ty1) := checkBinaryOperation(exp1, tty1, var1, op, exp2, tty2, var2, context, info);
+    (e1, ty1) := checkBinaryOperation(exp1, tty1, var1, op, exp2, tty2, var2, context, info, retype);
     valid1 := true;
   else
     valid1 := false;
   end try;
 
   try
-    (e2, ty2) := checkBinaryOperation(exp1, fty1, var1, op, exp2, fty2, var2, context, info);
+    (e2, ty2) := checkBinaryOperation(exp1, fty1, var1, op, exp2, fty2, var2, context, info, retype);
     valid2 := true;
   else
     valid2 := false;

OMCompiler/Compiler/NFFrontEnd/NFTyping.mo
@@ -1362,7 +1362,7 @@ algorithm
         next_context := InstContext.set(context, NFInstContext.SUBEXPRESSION);
         (e1, ty1, var1, pur1) := typeExp(exp.exp1, next_context, info);
         (e2, ty2, var2, pur2) := typeExp(exp.exp2, next_context, info);
-        (exp, ty) := TypeCheck.checkBinaryOperation(e1, ty1, var1, exp.operator, e2, ty2, var2, context, info);
+        (exp, ty) := TypeCheck.checkBinaryOperation(e1, ty1, var1, exp.operator, e2, ty2, var2, context, info, retype);
       then
         (exp, ty, Prefixes.variabilityMax(var1, var2), Prefixes.purityMin(pur1, pur2));
 