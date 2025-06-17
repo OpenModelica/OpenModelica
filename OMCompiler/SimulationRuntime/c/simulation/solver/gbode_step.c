8b13e9a14a6dd639cd3ae465a1874831a39f3d9d
phannebohm <phannebohm@hsbi.de>
2025-06-17 17:47:48 +0200

    Fix interpolation error in ringbuffer

--- OMCompiler/SimulationRuntime/c/simulation/solver/gbode_step.c
+++ OMCompiler/SimulationRuntime/c/simulation/solver/gbode_step.c
@@ -373,7 +373,6 @@ int expl_diag_impl_RK_MR(DATA* data, threadData_t* threadData, SOLVER_INFO* solv
                    gbfData->time,      gbfData->yOld,
                    gbData->nSlowStates, gbData->slowStatesIdx, nStates, gbData->tableau, gbData->x, gbData->k);
 
-
   if (OMC_ACTIVE_STREAM(OMC_LOG_GBODE_NLS)) {
     infoStreamPrint(OMC_LOG_GBODE_NLS, 1, "NLS - used values for extrapolation:");
     printVector_gbf(OMC_LOG_GBODE_NLS, "xL", gbfData->yv + nStates, nStates, gbfData->tv[1], gbData->nFastStates, gbData->fastStatesIdx);
