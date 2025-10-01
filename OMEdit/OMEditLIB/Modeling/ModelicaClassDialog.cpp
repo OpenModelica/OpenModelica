6d57117885ab9765c7e8e459f76c990432d14d39
Adeel Asghar <adeel.asghar@liu.se>
2025-10-01 16:38:13 +0200

    Do not show file extensions filter when saving a file (#14426)

--- OMEdit/OMEditLIB/Modeling/ModelicaClassDialog.cpp
+++ OMEdit/OMEditLIB/Modeling/ModelicaClassDialog.cpp
@@ -1333,7 +1333,7 @@ void SaveTotalFileDialog::saveTotalModel()
   QString fileName;
   QString name = QString("%1Total").arg(mpLibraryTreeItem->getName());
   fileName = StringHandler::getSaveFileName(this, tr("%1 - Save %2 %3 as Total File").arg(Helper::applicationName, mpLibraryTreeItem->mClassInformation.restriction,
-                                                                                          mpLibraryTreeItem->getName()), NULL, Helper::omFileTypes, NULL, "mo", &name);
+                                                                                          mpLibraryTreeItem->getName()), NULL, "", NULL, "mo", &name);
   if (fileName.isEmpty()) { // if user press ESC
     reject();
   } else {
