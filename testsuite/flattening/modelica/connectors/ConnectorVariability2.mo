OpenModelica/OpenModelica

commit f15e413ede154c9f792cc4924e00e4527d294ea2

Check variability of connectors (#14569)

OMCompiler/Compiler/NFFrontEnd/NFAttributes.mo
@@ -479,6 +479,12 @@ public
   algorithm
     if ConnectorType.isConnectorType(cty) then
       if Restriction.isConnector(restriction) then
+        if attributes.variability < Variability.DISCRETE and not InstContext.inRelaxed(context) then
+          Error.addSourceMessage(Error.INVALID_CONNECTOR_VARIABILITY,
+            {Prefixes.variabilityString(attributes.variability), InstNode.name(component)}, InstNode.info(component));
+          fail();
+        end if;
+
         if Restriction.isExpandableConnector(restriction) then
           cty := ConnectorType.setPresent(cty);
         else

testsuite/flattening/modelica/connectors/Makefile
@@ -51,6 +51,7 @@ ConnectorIllegal2.mo \
 ConnectorInheritance1.mo \
 ConnectorSimple.mo \
 ConnectorSubtypeComp.mo \
+ConnectorVariability1.mo \
 ConnectTheSameConnector.mo \
 ConnectTwoSources.mo \
 ConnectTypes.mo \

testsuite/flattening/modelica/scodeinst/ConstantConnector1.mo
@@ -1,6 +1,6 @@
 // name: ConstantConnector1
 // keywords:
-// status: correct
+// status: incorrect
 //
 
 model ConstantConnector1
@@ -12,10 +12,11 @@ equation
 end ConstantConnector1;
 
 // Result:
-// class ConstantConnector1
-//   parameter Real c1 = 0.0;
-//   parameter Real c2 = 0.0;
-// equation
-//   assert(abs(c1 - c2) <= 0.0, "Connected constants/parameters must be equal");
-// end ConstantConnector1;
+// Error processing file: ConstantConnector1.mo
+// [flattening/modelica/scodeinst/ConstantConnector1.mo:9:3-9:19:writable] Error: Invalid variability parameter on connector 'c1'.
+//
+// # Error encountered! Exiting...
+// # Please check the error message and the flags.
+//
+// Execution failed!
 // endResult

testsuite/flattening/modelica/scodeinst/ConstantConnector2.mo
@@ -1,6 +1,6 @@
 // name: ConstantConnector2
 // keywords:
-// status: correct
+// status: incorrect
 //
 
 model ConstantConnector2
@@ -12,10 +12,11 @@ equation
 end ConstantConnector2;
 
 // Result:
-// class ConstantConnector2
-//   constant Real c1 = 1.0;
-//   constant Real c2 = 2.0;
-// equation
-//   assert(false, "Connected constants/parameters must be equal");
-// end ConstantConnector2;
+// Error processing file: ConstantConnector2.mo
+// [flattening/modelica/scodeinst/ConstantConnector2.mo:9:3-9:19:writable] Error: Invalid variability constant on connector 'c1'.
+//
+// # Error encountered! Exiting...
+// # Please check the error message and the flags.
+//
+// Execution failed!
 // endResult

.CI/compliance.failures
@@ -48,7 +48,6 @@ ModelicaCompliance.Connections.Restrictions.ConnectTwoInsideOutput
 ModelicaCompliance.Connections.Restrictions.ConnectTwoOutsideInput
 ModelicaCompliance.Connections.Restrictions.ConnectTwoSignalSources
 ModelicaCompliance.Connections.Restrictions.ConnectTwoSignalSourcesIndirect
-ModelicaCompliance.Connections.Restrictions.ConnectorParameter
 ModelicaCompliance.Connections.Restrictions.SizeOverconstrainedInvalid
 ModelicaCompliance.Connections.Restrictions.SizeNestedInvalid
 ModelicaCompliance.Connections.Restrictions.SizeRecordInvalid

OMCompiler/Compiler/Util/Error.mo
@@ -903,6 +903,8 @@ public constant ErrorTypes.Message CONFLICTING_INHERITED_ANNOTATIONS = ErrorType
   Gettext.gettext("Conflicting '%s' annotations inherited by class '%s':\n  %s from 'extends %s'\n  %s from 'extends %s'"));
 public constant ErrorTypes.Message ASSIGN_ITERATOR_ERROR = ErrorTypes.MESSAGE(415, ErrorTypes.TRANSLATION(), ErrorTypes.ERROR(),
   Gettext.gettext("Assignment to iterator '%s'."));
+public constant ErrorTypes.Message INVALID_CONNECTOR_VARIABILITY = ErrorTypes.MESSAGE(416, ErrorTypes.TRANSLATION(), ErrorTypes.ERROR(),
+  Gettext.gettext("Invalid variability %s on connector '%s'."));
 
 public constant ErrorTypes.Message INITIALIZATION_NOT_FULLY_SPECIFIED = ErrorTypes.MESSAGE(496, ErrorTypes.TRANSLATION(), ErrorTypes.WARNING(),
   Gettext.gettext("The initial conditions are not fully specified. %s."));

testsuite/flattening/modelica/connectors/Bug3649.mos
@@ -1,4 +1,4 @@
-// name:     CGraphBug
+// name:     Bug3649
 // keywords: <insert keywords here>
 // status:   correct
 // cflags: -d=-newInst

testsuite/flattening/modelica/connectors/ConnectorVariability1.mo
@@ -1,0 +1,23 @@
+// name: ConnectorVariability1
+// keywords: connector
+// status: incorrect
+//
+//
+
+connector C
+  input Real x = 1.0;
+end C;
+
+model ConnectorVariability1
+  parameter C c;
+end ConnectorVariability1;
+
+// Result:
+// Error processing file: ConnectorVariability1.mo
+// [flattening/modelica/connectors/ConnectorVariability1.mo:12:3-12:16:writable] Error: Invalid variability parameter on connector 'c'.
+//
+// # Error encountered! Exiting...
+// # Please check the error message and the flags.
+//
+// Execution failed!
+// endResult

commit dbf36621214380555692865f17e533b4b08aab01

Fix regressions caused by #14569
- Allow constant/parameter connectors of builtin type.

OMCompiler/Compiler/NFFrontEnd/NFAttributes.mo
@@ -479,7 +479,8 @@ public
   algorithm
     if ConnectorType.isConnectorType(cty) then
       if Restriction.isConnector(restriction) then
-        if attributes.variability < Variability.DISCRETE and not InstContext.inRelaxed(context) then
+        if attributes.variability < Variability.DISCRETE and not InstContext.inRelaxed(context) and
+           not Class.isBuiltin(InstNode.getClass(component)) then
           Error.addSourceMessage(Error.INVALID_CONNECTOR_VARIABILITY,
             {Prefixes.variabilityString(attributes.variability), InstNode.name(component)}, InstNode.info(component));
           fail();

testsuite/flattening/modelica/connectors/ConnectorVariability2.mo
@@ -1,0 +1,16 @@
+// name: ConnectorVariability2
+// keywords: connector
+// status: correct
+//
+//
+
+model ConnectorVariability2
+  connector RealInput = input Real;
+  parameter RealInput ri = 0;
+end ConnectorVariability2;
+
+// Result:
+// class ConnectorVariability2
+//   parameter input Real ri = 0.0;
+// end ConnectorVariability2;
+// endResult

testsuite/flattening/modelica/connectors/Makefile
@@ -52,6 +52,7 @@ ConnectorInheritance1.mo \
 ConnectorSimple.mo \
 ConnectorSubtypeComp.mo \
 ConnectorVariability1.mo \
+ConnectorVariability2.mo \
 ConnectTheSameConnector.mo \
 ConnectTwoSources.mo \
 ConnectTypes.mo \

testsuite/flattening/modelica/scodeinst/ConstantConnector1.mo
@@ -1,6 +1,6 @@
 // name: ConstantConnector1
 // keywords:
-// status: incorrect
+// status: correct
 //
 
 model ConstantConnector1
@@ -12,11 +12,10 @@ equation
 end ConstantConnector1;
 
 // Result:
-// Error processing file: ConstantConnector1.mo
-// [flattening/modelica/scodeinst/ConstantConnector1.mo:9:3-9:19:writable] Error: Invalid variability parameter on connector 'c1'.
-//
-// # Error encountered! Exiting...
-// # Please check the error message and the flags.
-//
-// Execution failed!
+// class ConstantConnector1
+//   parameter Real c1 = 0.0;
+//   parameter Real c2 = 0.0;
+// equation
+//   assert(abs(c1 - c2) <= 0.0, "Connected constants/parameters must be equal");
+// end ConstantConnector1;
 // endResult

testsuite/flattening/modelica/scodeinst/ConstantConnector2.mo
@@ -1,6 +1,6 @@
 // name: ConstantConnector2
 // keywords:
-// status: incorrect
+// status: correct
 //
 
 model ConstantConnector2
@@ -12,11 +12,10 @@ equation
 end ConstantConnector2;
 
 // Result:
-// Error processing file: ConstantConnector2.mo
-// [flattening/modelica/scodeinst/ConstantConnector2.mo:9:3-9:19:writable] Error: Invalid variability constant on connector 'c1'.
-//
-// # Error encountered! Exiting...
-// # Please check the error message and the flags.
-//
-// Execution failed!
+// class ConstantConnector2
+//   constant Real c1 = 1.0;
+//   constant Real c2 = 2.0;
+// equation
+//   assert(false, "Connected constants/parameters must be equal");
+// end ConstantConnector2;
 // endResult