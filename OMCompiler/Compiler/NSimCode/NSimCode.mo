OpenModelica/OpenModelica

commit c1ce8a87baccc5204852eaf588630030fcc76c2b

[NB] support for adjoint derivatives (#14589)
First version of symbolic reverse-mode in the NB. Generates equations to
efficiently evaluate the ODE Jacobian row-wise instead of column-wise,
i.e., computes `y = J^T*x` instead of `y = J*x`.

This will be needed for FMI 3.0 and perhaps for symbolic Hessians and 
machine learning tasks.
Also this could give better coloring for certain array-equations.

This is implemented for a few equation types, including algebraic loops.
Most builtin operators are already supported. Some array-expressions
also work. The rest is work in progress.

It is acitvated with `--generateDynamicJacobian=symbolicadjoint`.
The flag `-d=debugAdjoint` produces debug information.

---------

Co-authored-by: phannebohm <phannebohm@hsbi.de>

OMCompiler/Compiler/NSimCode/NSimCode.mo
@@ -311,7 +311,7 @@ public
           UnorderedMap<ComponentRef, SimVar> simcode_map;
           UnorderedMap<ComponentRef, SimStrongComponent.Block> equation_map;
           Option<DaeModeData> daeModeData;
-          SimJacobian jacA, jacB, jacC, jacD, jacF, jacH;
+          SimJacobian jacA, jacB, jacC, jacD, jacF, jacH, jacAdjoint;
           list<SimStrongComponent.Block> inlineEquations; // ToDo: what exactly is this?
           mapExp collect_literals;
         case BackendDAE.MAIN(varData = varData as BVariable.VAR_DATA_SIM(), eqData = eqData as BEquation.EQ_DATA_SIM())
@@ -410,10 +410,11 @@ public
             (linearLoops, nonlinearLoops, jacobians, simCodeIndices) := collectAlgebraicLoops(init, init_0, ode, algebraic, daeModeData, simCodeIndices, simcode_map);
 
             if isSome(daeModeData) then
-              (jacA, simCodeIndices) := SimJacobian.createSimulationJacobian(Util.getOption(bdae.dae), simCodeIndices, simcode_map);
+              (jacA, jacAdjoint, simCodeIndices) := SimJacobian.createSimulationJacobian(Util.getOption(bdae.dae), simCodeIndices, simcode_map);
+              // should jacAdjoint be added aswell? -> for now no
               daeModeData := DaeModeData.addJacobian(daeModeData, jacA);
             else
-              (jacA, simCodeIndices) := SimJacobian.createSimulationJacobian(listAppend(bdae.ode, bdae.ode_event), simCodeIndices, simcode_map);
+              (jacA, jacAdjoint, simCodeIndices) := SimJacobian.createSimulationJacobian(listAppend(bdae.ode, bdae.ode_event), simCodeIndices, simcode_map);
             end if;
 
             (jacB, simCodeIndices) := SimJacobian.empty("B", simCodeIndices);
@@ -421,8 +422,8 @@ public
             (jacD, simCodeIndices) := SimJacobian.empty("D", simCodeIndices);
             (jacF, simCodeIndices) := SimJacobian.empty("F", simCodeIndices);
             (jacH, simCodeIndices) := SimJacobian.empty("H", simCodeIndices);
-            //jacobians := jacA :: jacB :: jacC :: jacD :: jacF :: jacobians;
-            jacobians := listReverse(jacH :: jacF :: jacD :: jacC :: jacB :: jacA :: jacobians);
+            //jacobians := jacA :: jacB :: jacC :: jacD :: jacF :: jacH :: jacAdjoint :: jacobians;
+            jacobians := listReverse(jacH :: jacF :: jacD :: jacC :: jacB :: jacAdjoint :: jacA :: jacobians);
 
             for jac in jacobians loop
               if Util.isSome(jac.jac_map) then
@@ -431,7 +432,7 @@ public
             end for;
 
             // jacobian blocks only from simulation jacobians
-            jac_blocks := SimJacobian.getJacobiansBlocks({jacA, jacB, jacC, jacD, jacF, jacH});
+            jac_blocks := SimJacobian.getJacobiansBlocks({jacA, jacB, jacC, jacD, jacF, jacH, jacAdjoint});
             (jac_blocks, simCodeIndices) := SimStrongComponent.Block.fixIndices(jac_blocks, {}, simCodeIndices);
 
             // generate the generic loop calls and replace literal expressions

OMCompiler/Compiler/SimCode/SimCodeMain.mo
@@ -1693,12 +1693,12 @@ algorithm
       if Util.isSome(inBackendDAE.shared.dataReconciliationData) then
         BackendDAE.DATA_RECON(_, _, _, _, jacH) := Util.getOption(inBackendDAE.shared.dataReconciliationData);
         if isSome(jacH) then
-          matrixnames := {"B", "C", "D"};
+          matrixnames := {"ADJ", "B", "C", "D"};
         else
-          matrixnames := {"B", "C", "D", "H"};
+          matrixnames := {"ADJ", "B", "C", "D", "H"};
         end if;
       else
-        matrixnames := {"B", "C", "D", "F", "H"};
+        matrixnames := {"ADJ", "B", "C", "D", "F", "H"};
       end if;
       (daeModeSP, uniqueEqIndex, tempVars) := SimCodeUtil.createSymbolicSimulationJacobian(
         inJacobian      = BackendDAE.GENERIC_JACOBIAN(daeModeJac, daeModeSparsity, daeModeColoring, nonlinearPattern),
@@ -1720,12 +1720,12 @@ algorithm
       if Util.isSome(inBackendDAE.shared.dataReconciliationData) then
         BackendDAE.DATA_RECON(_, _, _, _, jacH) := Util.getOption(inBackendDAE.shared.dataReconciliationData);
         if isSome(jacH) then
-          matrixnames := {"A", "B", "C", "D"};
+          matrixnames := {"A", "ADJ", "B", "C", "D"};
         else
-          matrixnames := {"A", "B", "C", "D", "H"};
+          matrixnames := {"A", "ADJ", "B", "C", "D", "H"};
         end if;
       else
-        matrixnames := {"A", "B", "C", "D", "F", "H"};
+        matrixnames := {"A", "ADJ", "B", "C", "D", "F", "H"};
       end if;
       (symJacs, uniqueEqIndex) := SimCodeUtil.createSymbolicJacobianssSimCode({}, crefToSimVarHT, uniqueEqIndex, matrixnames, {});
     end if;

commit 231d0966971eae63c5c75bf062ccb83bf4aa45a3

Work towards fixing regressions introduced in #14589 (#14961)
* remove hopefully wrong case in match

* Sort ADJ at the end of list

---------

Co-authored-by: phannebohm <phannebohm@hsbi.de>

OMCompiler/Compiler/NSimCode/NSimCode.mo
@@ -423,7 +423,7 @@ public
             (jacF, simCodeIndices) := SimJacobian.empty("F", simCodeIndices);
             (jacH, simCodeIndices) := SimJacobian.empty("H", simCodeIndices);
             //jacobians := jacA :: jacB :: jacC :: jacD :: jacF :: jacH :: jacAdjoint :: jacobians;
-            jacobians := listReverse(jacH :: jacF :: jacD :: jacC :: jacB :: jacAdjoint :: jacA :: jacobians);
+            jacobians := listReverse( jacAdjoint :: jacH :: jacF :: jacD :: jacC :: jacB :: jacA :: jacobians);
 
             for jac in jacobians loop
               if Util.isSome(jac.jac_map) then

OMCompiler/Compiler/SimCode/SimCodeMain.mo
@@ -1693,12 +1693,12 @@ algorithm
       if Util.isSome(inBackendDAE.shared.dataReconciliationData) then
         BackendDAE.DATA_RECON(_, _, _, _, jacH) := Util.getOption(inBackendDAE.shared.dataReconciliationData);
         if isSome(jacH) then
-          matrixnames := {"ADJ", "B", "C", "D"};
+          matrixnames := {"B", "C", "D", "ADJ"};
         else
-          matrixnames := {"ADJ", "B", "C", "D", "H"};
+          matrixnames := {"B", "C", "D", "H", "ADJ"};
         end if;
       else
-        matrixnames := {"ADJ", "B", "C", "D", "F", "H"};
+        matrixnames := {"B", "C", "D", "F", "H", "ADJ"};
       end if;
       (daeModeSP, uniqueEqIndex, tempVars) := SimCodeUtil.createSymbolicSimulationJacobian(
         inJacobian      = BackendDAE.GENERIC_JACOBIAN(daeModeJac, daeModeSparsity, daeModeColoring, nonlinearPattern),
@@ -1720,12 +1720,12 @@ algorithm
       if Util.isSome(inBackendDAE.shared.dataReconciliationData) then
         BackendDAE.DATA_RECON(_, _, _, _, jacH) := Util.getOption(inBackendDAE.shared.dataReconciliationData);
         if isSome(jacH) then
-          matrixnames := {"A", "ADJ", "B", "C", "D"};
+          matrixnames := {"A", "B", "C", "D", "ADJ"};
         else
-          matrixnames := {"A", "ADJ", "B", "C", "D", "H"};
+          matrixnames := {"A", "B", "C", "D", "H", "ADJ"};
         end if;
       else
-        matrixnames := {"A", "ADJ", "B", "C", "D", "F", "H"};
+        matrixnames := {"A", "B", "C", "D", "F", "H", "ADJ"};
       end if;
       (symJacs, uniqueEqIndex) := SimCodeUtil.createSymbolicJacobianssSimCode({}, crefToSimVarHT, uniqueEqIndex, matrixnames, {});
     end if;

OMCompiler/Compiler/SimCode/SimCodeUtil.mo
@@ -5035,12 +5035,12 @@ algorithm
         if Util.isSome(shared.dataReconciliationData) then
           BackendDAE.DATA_RECON(_, _, _, _, jacH) := Util.getOption(shared.dataReconciliationData);
           if isSome(jacH) then // check for matrix H is present which means state estimation algorithm is choosed and jacobian F and H are generated earlier
-            matrixnames := {"A", "ADJ", "B", "C", "D"};
+            matrixnames := {"A", "B", "C", "D", "ADJ"};
           else
-            matrixnames := {"A", "ADJ", "B", "C", "D", "H"};
+            matrixnames := {"A", "B", "C", "D", "H", "ADJ"};
           end if;
         else
-           matrixnames := {"A", "ADJ", "B", "C", "D", "F", "H"};
+           matrixnames := {"A", "B", "C", "D", "F", "H", "ADJ"};
         end if;
         (res, ouniqueEqIndex) := createSymbolicJacobianssSimCode(inSymjacs, crefSimVarHT, iuniqueEqIndex, matrixnames, {});
         // _ := FlagsUtil.set(Flags.EXEC_STAT, b);
@@ -5088,7 +5088,7 @@ algorithm
       Integer uniqueEqIndex, nRows;
 
       list<String> restnames;
-      String name, dummyVar, jacName;
+      String name, dummyVar;
 
       SimCodeVar.SimVars simvars;
       list<SimCode.SimEqSystem> allEquations = {}, constantEqns = {};
@@ -5132,17 +5132,6 @@ algorithm
         linearModelMatrices = tmpJac::inJacobianMatrices;
         (linearModelMatrices, uniqueEqIndex) = createSymbolicJacobianssSimCode(rest, inSimVarHT, iuniqueEqIndex, restnames, linearModelMatrices);
      then
-        (linearModelMatrices, uniqueEqIndex);
-
-    // create empty jacobian if names do not match
-    case ((SOME((_, jacName, _, _, _, _)), _, _, _) :: _, _, _, name::restnames)
-      guard jacName <> name
-      equation
-        tmpJac = SimCode.emptyJacobian;
-        tmpJac.matrixName = name;
-        linearModelMatrices = tmpJac::inJacobianMatrices;
-        (linearModelMatrices, uniqueEqIndex) = createSymbolicJacobianssSimCode(inSymJacobians, inSimVarHT, iuniqueEqIndex, restnames, linearModelMatrices);
-      then
         (linearModelMatrices, uniqueEqIndex);
 
     // if only sparsity pattern is generated

testsuite/simulation/modelica/NBackend/basics/implicitEquation.mos
@@ -52,10 +52,6 @@ simulate(implicitEquation); getErrorString();
 //   [EMPTY] SimCode Jacobian A(idx = 0, partition = 0)
 // ======================================================
 //
-// ========================================================
-//   [EMPTY] SimCode Jacobian ADJ(idx = 1, partition = 0)
-// ========================================================
-//
 // ======================================================
 //   [EMPTY] SimCode Jacobian B(idx = 2, partition = 0)
 // ======================================================
@@ -75,6 +71,10 @@ simulate(implicitEquation); getErrorString();
 // ======================================================
 //   [EMPTY] SimCode Jacobian H(idx = 6, partition = 0)
 // ======================================================
+//
+// ========================================================
+//   [EMPTY] SimCode Jacobian ADJ(idx = 1, partition = 0)
+// ========================================================
 //
 //
 //
@@ -162,8 +162,6 @@ simulate(implicitEquation); getErrorString();
 // ========================================
 // 	Jacobian idx: 0
 //
-// 	Jacobian idx: 1
-//
 // 	Jacobian idx: 2
 //
 // 	Jacobian idx: 3
@@ -173,6 +171,8 @@ simulate(implicitEquation); getErrorString();
 // 	Jacobian idx: 5
 //
 // 	Jacobian idx: 6
+//
+// 	Jacobian idx: 1
 //
 //
 // modelInfo:

testsuite/simulation/modelica/NBackend/differentation/adjoint_jacobian1.mos
@@ -87,6 +87,26 @@ buildModel(scalarAdjointJacobian1); getErrorString();
 //   [EMPTY] SimCode Jacobian A(idx = 0, partition = 0)
 // ======================================================
 //
+// ======================================================
+//   [EMPTY] SimCode Jacobian B(idx = 2, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian C(idx = 3, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian D(idx = 4, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian F(idx = 5, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian H(idx = 6, partition = 0)
+// ======================================================
+//
 // ================================================
 //   SimCode Jacobian ADJ(idx = 1, partition = 1)
 // ================================================
@@ -127,26 +147,6 @@ buildModel(scalarAdjointJacobian1); getErrorString();
 // ************************
 //   {1}
 //   {0}
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian B(idx = 2, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian C(idx = 3, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian D(idx = 4, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian F(idx = 5, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian H(idx = 6, partition = 0)
-// ======================================================
 //
 //
 //
@@ -243,16 +243,6 @@ buildModel(scalarAdjointJacobian1); getErrorString();
 // ========================================
 // 	Jacobian idx: 0
 //
-// 	Jacobian idx: 1
-// 	14: $pDER_ODE_JAC_ADJ.i_C=$SEED_ODE_JAC_ADJ.$DER.V * 1.0 / C [Real]
-// 	13: $pDER_ODE_JAC_ADJ.i_R=-$pDER_ODE_JAC_ADJ.i_C [Real]
-// 	12: $pDER_ODE_JAC_ADJ.V=$pDER_ODE_JAC_ADJ.i_R * 1.0 / R - $SEED_ODE_JAC_ADJ.$DER.i_L * 1.0 / L [Real]
-// 	11: $pDER_ODE_JAC_ADJ.i_L=$pDER_ODE_JAC_ADJ.i_C [Real]
-//
-// columnVars(2)
-// ----------------------
-// index:0: $pDER_ODE_JAC_ADJ.i_L (no alias)  initial: 	no arrCref index:(1) []
-// index:1: $pDER_ODE_JAC_ADJ.V (no alias)  initial: 	no arrCref index:(2) []
 // 	Jacobian idx: 2
 //
 // 	Jacobian idx: 3
@@ -263,6 +253,16 @@ buildModel(scalarAdjointJacobian1); getErrorString();
 //
 // 	Jacobian idx: 6
 //
+// 	Jacobian idx: 1
+// 	14: $pDER_ODE_JAC_ADJ.i_C=$SEED_ODE_JAC_ADJ.$DER.V * 1.0 / C [Real]
+// 	13: $pDER_ODE_JAC_ADJ.i_R=-$pDER_ODE_JAC_ADJ.i_C [Real]
+// 	12: $pDER_ODE_JAC_ADJ.V=$pDER_ODE_JAC_ADJ.i_R * 1.0 / R - $SEED_ODE_JAC_ADJ.$DER.i_L * 1.0 / L [Real]
+// 	11: $pDER_ODE_JAC_ADJ.i_L=$pDER_ODE_JAC_ADJ.i_C [Real]
+//
+// columnVars(2)
+// ----------------------
+// index:0: $pDER_ODE_JAC_ADJ.i_L (no alias)  initial: 	no arrCref index:(1) []
+// index:1: $pDER_ODE_JAC_ADJ.V (no alias)  initial: 	no arrCref index:(2) []
 //
 // modelInfo:
 // ========================================

testsuite/simulation/modelica/NBackend/differentation/adjoint_jacobian2.mos
@@ -148,6 +148,26 @@ buildModel(AdjointJacobian2); getErrorString();
 //   [EMPTY] SimCode Jacobian A(idx = 0, partition = 0)
 // ======================================================
 //
+// ======================================================
+//   [EMPTY] SimCode Jacobian B(idx = 2, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian C(idx = 3, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian D(idx = 4, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian F(idx = 5, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian H(idx = 6, partition = 0)
+// ======================================================
+//
 // ================================================
 //   SimCode Jacobian ADJ(idx = 1, partition = 1)
 // ================================================
@@ -222,26 +242,6 @@ buildModel(AdjointJacobian2); getErrorString();
 //   {2}
 //   {1}
 //   {0}
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian B(idx = 2, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian C(idx = 3, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian D(idx = 4, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian F(idx = 5, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian H(idx = 6, partition = 0)
-// ======================================================
 //
 //
 //
@@ -366,6 +366,16 @@ buildModel(AdjointJacobian2); getErrorString();
 // ========================================
 // 	Jacobian idx: 0
 //
+// 	Jacobian idx: 2
+//
+// 	Jacobian idx: 3
+//
+// 	Jacobian idx: 4
+//
+// 	Jacobian idx: 5
+//
+// 	Jacobian idx: 6
+//
 // 	Jacobian idx: 1
 // 	36: $pDER_ODE_JAC_ADJ.t=$SEED_ODE_JAC_ADJ.$DER.m [Real]
 // 	35: $pDER_ODE_JAC_ADJ.$FUN_1=$SEED_ODE_JAC_ADJ.$DER.m * $FUN_2 [Real]
@@ -388,16 +398,6 @@ buildModel(AdjointJacobian2); getErrorString();
 // index:5: $pDER_ODE_JAC_ADJ.k[1] (no alias)  initial: 	arrCref:$pDER_ODE_JAC_ADJ.k index:(6) []
 // index:6: $pDER_ODE_JAC_ADJ.k[2] (no alias)  initial: 	no arrCref index:(7) []
 // index:7: $pDER_ODE_JAC_ADJ.k[3] (no alias)  initial: 	no arrCref index:(8) []
-// 	Jacobian idx: 2
-//
-// 	Jacobian idx: 3
-//
-// 	Jacobian idx: 4
-//
-// 	Jacobian idx: 5
-//
-// 	Jacobian idx: 6
-//
 //
 // modelInfo:
 // ========================================

testsuite/simulation/modelica/NBackend/differentation/adjoint_jacobian3.mos
@@ -67,6 +67,26 @@ buildModel(scalarAdjointJacobian3); getErrorString();
 //   [EMPTY] SimCode Jacobian A(idx = 0, partition = 0)
 // ======================================================
 //
+// ======================================================
+//   [EMPTY] SimCode Jacobian B(idx = 2, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian C(idx = 3, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian D(idx = 4, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian F(idx = 5, partition = 0)
+// ======================================================
+//
+// ======================================================
+//   [EMPTY] SimCode Jacobian H(idx = 6, partition = 0)
+// ======================================================
+//
 // ================================================
 //   SimCode Jacobian ADJ(idx = 1, partition = 1)
 // ================================================
@@ -109,26 +129,6 @@ buildModel(scalarAdjointJacobian3); getErrorString();
 // ************************
 //   {1}
 //   {0}
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian B(idx = 2, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian C(idx = 3, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian D(idx = 4, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian F(idx = 5, partition = 0)
-// ======================================================
-//
-// ======================================================
-//   [EMPTY] SimCode Jacobian H(idx = 6, partition = 0)
-// ======================================================
 //
 //
 //
@@ -231,6 +231,16 @@ buildModel(scalarAdjointJacobian3); getErrorString();
 // ========================================
 // 	Jacobian idx: 0
 //
+// 	Jacobian idx: 2
+//
+// 	Jacobian idx: 3
+//
+// 	Jacobian idx: 4
+//
+// 	Jacobian idx: 5
+//
+// 	Jacobian idx: 6
+//
 // 	Jacobian idx: 1
 // 	13:  (NONLINEAR) index:2 jacobian: false
 // crefs: $pDER_ODE_JAC_ADJ.$TMP_1 , $pDER_ODE_JAC_ADJ.$TMP_2
@@ -244,16 +254,6 @@ buildModel(scalarAdjointJacobian3); getErrorString();
 // ----------------------
 // index:0: $pDER_ODE_JAC_ADJ.y (no alias)  initial: 	no arrCref index:(1) []
 // index:1: $pDER_ODE_JAC_ADJ.x (no alias)  initial: 	no arrCref index:(2) []
-// 	Jacobian idx: 2
-//
-// 	Jacobian idx: 3
-//
-// 	Jacobian idx: 4
-//
-// 	Jacobian idx: 5
-//
-// 	Jacobian idx: 6
-//
 //
 // modelInfo:
 // ========================================

testsuite/simulation/modelica/NBackend/differentation/algebraicLoop.mos
@@ -103,10 +103,6 @@ buildModel(algebraicLoop); getErrorString();
 //   {1}
 //   {0}
 //
-// ========================================================
-//   [EMPTY] SimCode Jacobian ADJ(idx = 1, partition = 0)
-// ========================================================
-//
 // ======================================================
 //   [EMPTY] SimCode Jacobian B(idx = 2, partition = 0)
 // ======================================================
@@ -126,6 +122,10 @@ buildModel(algebraicLoop); getErrorString();
 // ======================================================
 //   [EMPTY] SimCode Jacobian H(idx = 6, partition = 0)
 // ======================================================
+//
+// ========================================================
+//   [EMPTY] SimCode Jacobian ADJ(idx = 1, partition = 0)
+// ========================================================
 //
 //
 //
@@ -235,8 +235,6 @@ buildModel(algebraicLoop); getErrorString();
 // ----------------------
 // index:0: $pDER_ODE_JAC.$DER.y (no alias)  initial: 	no arrCref index:(1) []
 // index:1: $pDER_ODE_JAC.$DER.x (no alias)  initial: 	no arrCref index:(2) []
-// 	Jacobian idx: 1
-//
 // 	Jacobian idx: 2
 //
 // 	Jacobian idx: 3
@@ -246,6 +244,8 @@ buildModel(algebraicLoop); getErrorString();
 // 	Jacobian idx: 5
 //
 // 	Jacobian idx: 6
+//
+// 	Jacobian idx: 1
 //
 //
 // modelInfo: