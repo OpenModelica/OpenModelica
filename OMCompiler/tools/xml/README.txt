553bf430c07e39cd08f5b27add3262b9d97aacee
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2026-04-27 17:21:00 +0200

    Update OMCompiler/tools/xml License
    * Added OSMC-PL 1.8 Header
      * AGPL 3 + OSMC-PL 1.8
      * Checked 47 files, skipped 5 (excluded), 0 failures, 46 fixed.

--- OMCompiler/tools/xml/README.txt
+++
@@ -1,5 +0,0 @@
-
-All XML tools are (C) OSMC.
-Read LICENSE file.
-
-Adrian Pop [adrpo@ida.liu.se]
--- OMCompiler/tools/xml/modelicaxml/modelica_tree_parser.hpp
+++ OMCompiler/tools/xml/modelicaxml/modelica_tree_parser.hpp
@@ -1,3 +1,38 @@
+/*
+ * This file is part of OpenModelica.
+ *
+ * Copyright (c) 1998-2026, Open Source Modelica Consortium (OSMC),
+ * c/o Linköpings universitet, Department of Computer and Information Science,
+ * SE-58183 Linköping, Sweden.
+ *
+ * All rights reserved.
+ *
+ * THIS PROGRAM IS PROVIDED UNDER THE TERMS OF AGPL VERSION 3 LICENSE OR
+ * THIS OSMC PUBLIC LICENSE (OSMC-PL) VERSION 1.8.
+ * ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES
+ * RECIPIENT'S ACCEPTANCE OF THE OSMC PUBLIC LICENSE OR THE GNU AGPL
+ * VERSION 3, ACCORDING TO RECIPIENTS CHOICE.
+ *
+ * The OpenModelica software and the OSMC (Open Source Modelica Consortium)
+ * Public License (OSMC-PL) are obtained from OSMC, either from the above
+ * address, from the URLs:
+ * http://www.openmodelica.org or
+ * https://github.com/OpenModelica/ or
+ * http://www.ida.liu.se/projects/OpenModelica,
+ * and in the OpenModelica distribution.
+ *
+ * GNU AGPL version 3 is obtained from:
+ * https://www.gnu.org/licenses/licenses.html#GPL
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE, EXCEPT AS EXPRESSLY SET FORTH
+ * IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE CONDITIONS OF OSMC-PL.
+ *
+ * See the full OSMC Public License conditions for more details.
+ *
+ */
+
 #ifndef INC_modelica_tree_parser_hpp_
 #define INC_modelica_tree_parser_hpp_
 
@@ -33,7 +68,7 @@ Date:       2003-06-10
 Revised on 2003-10-26 17:58:42 (write the definition even if has no childs)
 Comments: we walk on the modelica tree, buil a XML DOM tree and serialize
 ************************************************************************/
-  
+
   #define null 0
 
   extern "C"
@@ -335,7 +370,7 @@ public:
 	{
 		return ANTLR_USE_NAMESPACE(antlr)RefAST(returnAST);
 	}
-	
+
 protected:
 	RefMyAST returnAST;
 	RefMyAST _retTree;
@@ -348,7 +383,7 @@ private:
 		NUM_TOKENS = 157
 	};
 #endif
-	
+
 	static const unsigned long _tokenSet_0_data_[];
 	static const ANTLR_USE_NAMESPACE(antlr)BitSet _tokenSet_0;
 	static const unsigned long _tokenSet_1_data_[];
