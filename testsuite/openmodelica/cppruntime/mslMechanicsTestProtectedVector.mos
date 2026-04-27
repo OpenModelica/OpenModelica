OpenModelica/OpenModelica

commit d4817cddedba5a6b9d45fcfc4a4163b639bc9d35

fix fmiFilter when using --exportClocksInModelDescription (#12895)
* fix fmi_index when using nfScalarize

OMCompiler/Compiler/SimCode/SimCodeUtil.mo
@@ -8578,9 +8578,10 @@ algorithm
     end if;
   elseif Flags.getConfigEnum(Flags.FMI_FILTER) == Flags.FMI_PROTECTED then
     // All protected model variables will be filtered out in addition
-    // to --fmiFilter=internal.
+    // to --fmiFilter=internal (with minor exceptions. e.g.
+    // for state sets.clocked states and previous vars)
     if not (ComponentReference.isInternalCref(var.varName) and (not BackendVariable.isStateVar(var) and not BackendVariable.isClockedStateVar(var))) then
-      if not (BackendVariable.isProtected(var) and (not BackendVariable.isStateVar(var) and not BackendVariable.isClockedStateVar(var))) then
+      if not (BackendVariable.isProtected(var) and (not BackendVariable.isStateVar(var) and not BackendVariable.isClockedStateVar(var) and not ComponentReference.isPreviousCref(var.varName))) then
         exportVar := SOME(var.varName);
       end if;
     end if;
@@ -15274,6 +15275,12 @@ algorithm
         local Integer index;
         case SOME(index)
         then SOME(index + getScalarElementIndex(subs, List.map(sv.numArrayElement, stringInt)) - 1);
+      end match;
+      // fix fmi_index when using nfScalarize
+      sv.fmi_index := match sv.fmi_index
+        local Integer fmiIndex;
+        case SOME(fmiIndex)
+        then SOME(fmiIndex + getScalarElementIndex(subs, List.map(sv.numArrayElement, stringInt)) - 1);
       end match;
     end if;
     sv := match sv.aliasvar

testsuite/openmodelica/fmi/ModelExchange/2.0/Makefile
@@ -25,6 +25,7 @@ fmi_attributes_21.mos \
 fmi_attributes_22.mos \
 fmi_attributes_23.mos \
 fmi_attributes_24.mos \
+fmiFilterTest.mos \
 FMUResourceTest.mos \
 QuotedIdentifierExport.mos \
 testBug2764.mos \

OMCompiler/Compiler/Template/CodegenFMUCommon.tpl
@@ -487,7 +487,7 @@ match simVar
   let description = if comment then 'description="<%Util.escapeModelicaStringToXmlString(comment)%>"'
   let variability_ = if getClockIndex(simVar, simCode) then "discrete" else getVariability2(variability)
   let clockIndex = getClockIndex(simVar, simCode)
-  let previous = match varKind case CLOCKED_STATE(__) then '<%getVariableIndex(cref2simvar(previousName, simCode))%>'
+  let previous = match varKind case CLOCKED_STATE(__) then '<%getVariableFMIIndex(cref2simvar(previousName, simCode))%>'
   let caus = getCausality2(causality)
   let initial = getFmiInitialAttributeStr(simVar)
   <<

testsuite/openmodelica/fmi/ModelExchange/2.0/fmiFilterTest.mos
@@ -1,0 +1,128 @@
+// name: fmiFilterTest.mos
+// keywords: FMI 2.0 export
+// status: correct
+// teardown_command: rm -rf fmiFilterTest.fmu fmiFilterTest.log fmiFilterTest.xml fmiFilterTest_tmp.xml fmiFilterTest_info.json
+
+
+setCommandLineOptions("--exportClocksInModelDescription"); getErrorString();
+
+loadString("
+  model fmiFilterTest
+    input Real u;
+    Integer id(start = 0);
+  protected
+    Real p = 1;
+    Real xc(start = 0);
+    Real xd(start = 0);
+  equation
+    der(xc) = u;
+    when Clock() then
+      xd = previous(xd) + p*u;
+      id = previous(id) + 1;
+    end when;
+  end fmiFilterTest;
+"); getErrorString();
+
+buildModelFMU(fmiFilterTest, fileNamePrefix="fmiFilterTest"); getErrorString();
+
+// unzip to console, quiet, extra quiet
+system("unzip -cqq fmiFilterTest.fmu modelDescription.xml > fmiFilterTest_tmp.xml"); getErrorString();
+
+system("sed -n \"/<ModelVariables>/,/<\\/ModelVariables>/p\" fmiFilterTest_tmp.xml > fmiFilterTest.xml"); getErrorString();
+readFile("fmiFilterTest.xml"); getErrorString();
+
+system("sed -n \"/<ModelStructure>/,/<\\/ModelStructure>/p\" fmiFilterTest_tmp.xml > fmiFilterTest.xml"); getErrorString();
+readFile("fmiFilterTest.xml"); getErrorString();
+
+// Result:
+// true
+// ""
+// true
+// ""
+// "fmiFilterTest.fmu"
+// "Warning: The initial conditions are not fully specified. For more information set -d=initialization. In OMEdit Tools->Options->Simulation->Show additional information from the initialization process, in OMNotebook call setCommandLineOptions(\"-d=initialization\").
+// "
+// 0
+// ""
+// 0
+// ""
+// "  <ModelVariables>
+//   <!-- Index of variable = \"1\" -->
+//   <ScalarVariable
+//     name=\"xc\"
+//     valueReference=\"0\"
+//     initial=\"exact\">
+//     <Real start=\"0.0\"/>
+//   </ScalarVariable>
+//   <!-- Index of variable = \"2\" -->
+//   <ScalarVariable
+//     name=\"der(xc)\"
+//     valueReference=\"1\"
+//     >
+//     <Real derivative=\"1\"/>
+//   </ScalarVariable>
+//   <!-- Index of variable = \"3\" -->
+//   <ScalarVariable
+//     name=\"previous(xd)\"
+//     valueReference=\"2\"
+//     variability=\"discrete\"
+//     clockIndex=\"1\"
+//     >
+//     <Real/>
+//   </ScalarVariable>
+//   <!-- Index of variable = \"4\" -->
+//   <ScalarVariable
+//     name=\"u\"
+//     valueReference=\"4\"
+//     causality=\"input\"
+//     >
+//     <Real start=\"0.0\"/>
+//   </ScalarVariable>
+//   <!-- Index of variable = \"5\" -->
+//   <ScalarVariable
+//     name=\"xd\"
+//     valueReference=\"5\"
+//     variability=\"discrete\"
+//     clockIndex=\"1\"
+//     previous=\"3\"
+//     initial=\"exact\">
+//     <Real start=\"0.0\"/>
+//   </ScalarVariable>
+//   <!-- Index of variable = \"6\" -->
+//   <ScalarVariable
+//     name=\"previous(id)\"
+//     valueReference=\"0\"
+//     variability=\"discrete\"
+//     clockIndex=\"1\"
+//     >
+//     <Integer/>
+//   </ScalarVariable>
+//   <!-- Index of variable = \"7\" -->
+//   <ScalarVariable
+//     name=\"id\"
+//     valueReference=\"1\"
+//     variability=\"discrete\"
+//     clockIndex=\"1\"
+//     previous=\"6\"
+//     initial=\"exact\">
+//     <Integer start=\"0\"/>
+//   </ScalarVariable>
+//   </ModelVariables>
+// "
+// ""
+// 0
+// ""
+// "  <ModelStructure>
+//     <Derivatives>
+//       <Unknown index=\"2\" dependencies=\"4\" dependenciesKind=\"dependent\" />
+//     </Derivatives>
+//     <DiscreteStates>
+//       <Unknown index=\"5\" dependencies=\"3 4\" dependenciesKind=\"dependent dependent\" />
+//     </DiscreteStates>
+//     <InitialUnknowns>
+//       <Unknown index=\"2\" dependencies=\"6\" dependenciesKind=\"dependent\" />
+//     </InitialUnknowns>
+//   </ModelStructure>
+// "
+// ""
+// endResult

commit 91ed340dfb63e92398df08ee659176fec1d6e064

Fix lookup of SimVar without FMI export (#15427)
Without else branch, the function SimCodeUtil.simVarFromHT fails if there is no fmiIndex,
e.g. as it was filtered out.
This lets cref2simvar assume that the variable would not exist.
Bug was introduced with commit d4817cd on Sep 19, 2024.

OMCompiler/Compiler/SimCode/SimCodeUtil.mo
@@ -15409,12 +15409,14 @@ algorithm
         local Integer index;
         case SOME(index)
         then SOME(index + getScalarElementIndex(subs, List.map(sv.numArrayElement, stringInt)) - 1);
+        else sv.variable_index;
       end match;
       // fix fmi_index when using nfScalarize
       sv.fmi_index := match sv.fmi_index
         local Integer fmiIndex;
         case SOME(fmiIndex)
         then SOME(fmiIndex + getScalarElementIndex(subs, List.map(sv.numArrayElement, stringInt)) - 1);
+        else sv.fmi_index;
       end match;
     end if;
     sv := match sv.aliasvar

testsuite/openmodelica/cppruntime/Makefile
@@ -23,6 +23,7 @@ mslElectricalMachinesTest.mos \
 mslElectricalMachinesTestDAE.mos \
 mslElectricalSensorsTest.mos \
 mslMathFFT1Test.mos \
+mslMechanicsTestProtectedVector \
 mslThermalMassesTestNB.mos \
 nameClashTest.mos \
 functionPointerTest.mos \

testsuite/openmodelica/cppruntime/mslMechanicsTestProtectedVector.mos
@@ -1,0 +1,28 @@
+// name: mslMechanicsTestProtectedVector
+// keywords: protected body.Q_start
+// status: correct
+// teardown_command: rm -f *Pendulum*
+
+setCommandLineOptions("--simCodeTarget=Cpp");
+
+loadModel(Modelica, {"3.2.3"}); getErrorString();
+
+simulate(Modelica.Mechanics.MultiBody.Examples.Elementary.Pendulum);
+val(body.w_a[3], 5);
+val(body.z_a[3], 5);
+getErrorString();
+
+// Result:
+// true
+// true
+// true
+// ""
+// record SimulationResult
+//     resultFile = "Modelica.Mechanics.MultiBody.Examples.Elementary.Pendulum_res.mat",
+//     simulationOptions = "startTime = 0.0, stopTime = 5.0, numberOfIntervals = 500, tolerance = 1e-6, method = 'dassl', fileNamePrefix = 'Modelica.Mechanics.MultiBody.Examples.Elementary.Pendulum', options = '', outputFormat = 'mat', variableFilter = '.*', cflags = '', simflags = ''",
+//     messages = ""
+// end SimulationResult;
+// -2.318178134998348
+// 3.045459364536539
+// ""
+// endResult