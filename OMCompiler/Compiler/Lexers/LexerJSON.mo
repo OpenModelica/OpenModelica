OpenModelica/OpenModelica

commit 16c54a3ffdad7f000b8359685e32fc9233347928

Remove Modelica-style arrays from the MM code (#15488)

OMCompiler/Compiler/Lexers/LexerJSON.mo
@@ -20,14 +20,14 @@ constant Boolean debug = false;
 replaceable package LexTable
   constant Integer yy_limit;
   constant Integer yy_finish;
-  constant Integer yy_acclist[:];
-  constant Integer yy_accept[:];
-  constant Integer yy_ec[:];
-  constant Integer yy_meta[:];
-  constant Integer yy_base[:];
-  constant Integer yy_def[:];
-  constant Integer yy_nxt[:];
-  constant Integer yy_chk[:];
+  constant array<Integer> yy_acclist;
+  constant array<Integer> yy_accept;
+  constant array<Integer> yy_ec;
+  constant array<Integer> yy_meta;
+  constant array<Integer> yy_base;
+  constant array<Integer> yy_def;
+  constant array<Integer> yy_nxt;
+  constant array<Integer> yy_chk;
 end LexTable;
 
 function scan "Scan starts the lexical analysis, load the tables and consume the program to output the tokens"
@@ -500,16 +500,16 @@ algorithm
 end checkArray;
 
 function checkArrayModelica
-  input Integer arr[:];
+  input array<Integer> arr;
   input Integer index;
   input SourceInfo info;
 protected
   String filename;
   Integer lineStart;
 algorithm
-  if index<1 or index>size(arr,1) then
+  if index<1 or index>arrayLength(arr) then
     SOURCEINFO(fileName=filename, lineNumberStart=lineStart) := info;
-    print("\n[" + filename + ":" + String(lineStart) + "]: checkArray failed: arrayLength="+String(size(arr,1))+" index=" + String(index) + "\n");
+    print("\n[" + filename + ":" + String(lineStart) + "]: checkArray failed: arrayLength="+String(arrayLength(arr))+" index=" + String(index) + "\n");
     fail();
   end if;
 end checkArrayModelica;
@@ -517,20 +517,20 @@ end checkArrayModelica;
 package LexTable
   constant Integer yy_limit = 46;
   constant Integer yy_finish = 82;
-  constant Integer yy_acclist[:] = array(
+  constant array<Integer> yy_acclist = listArray({
        17,   16,   15,   16,   16,   13,   16,    5,   16,   14,
        16,   11,   16,   12,   16,   16,   16,   16,    9,   16,
        10,   16,   15,    1,    5,    2,    3,    4,    8,    6,
         3,    7
-   );
-  constant Integer yy_accept[:] = array(
+   });
+  constant array<Integer> yy_accept = listArray({
         1,    1,    1,    2,    3,    5,    6,    8,   10,   12,
        14,   16,   17,   18,   19,   21,   23,   24,   24,   25,
        25,   25,   26,   26,   26,   26,   26,   27,   27,   28,
        28,   29,   29,   29,   29,   29,   29,   29,   30,   31,
        31,   31,   32,   33,   33,   33
-   );
-  constant Integer yy_ec[:] = array(
+   });
+  constant array<Integer> yy_ec = listArray({
         1,    1,    1,    1,    1,    1,    1,    1,    2,    2,
         1,    1,    2,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -559,29 +559,29 @@ package LexTable
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1
-   );
-  constant Integer yy_meta[:] = array(
+  });
+  constant array<Integer> yy_meta = listArray({
         1,    1,    2,    1,    1,    1,    1,    2,    3,    1,
         3,    3,    1,    2,    1,    3,    4,    3,    4,    1,
         2,    2,    1,    2,    2,    1,    1
-   );
-  constant Integer yy_base[:] = array(
+   });
+  constant array<Integer> yy_base = listArray({
         0,    0,   81,   82,   78,   25,   82,   22,   82,   82,
        82,   63,   53,   55,   82,   82,   74,   27,   82,   50,
        65,   26,   39,   53,   52,   37,   82,    0,   37,   45,
        43,   27,   27,   24,    0,   47,   19,   82,   82,    0,
        27,   23,   82,    0,   82,   56,   59,   61,   63,   65,
        67
-   );
-  constant Integer yy_def[:] = array(
+  });
+  constant array<Integer> yy_def = listArray({
        45,    1,   45,   45,   45,   46,   45,   45,   45,   45,
        45,   45,   45,   45,   45,   45,   45,   46,   45,   47,
        45,   45,   45,   45,   45,   45,   45,   48,   45,   45,
        45,   45,   45,   45,   49,   45,   45,   45,   45,   50,
        45,   45,   45,   51,    0,   45,   45,   45,   45,   45,
        45
-   );
-  constant Integer yy_nxt[:] = array(
+  });
+  constant array<Integer> yy_nxt = listArray({
         4,    5,    6,    4,    7,    4,    4,    4,    8,    9,
         4,    4,   10,    4,   11,    4,    4,    4,   12,    4,
        13,    4,    4,   14,    4,   15,   16,   19,   21,   27,
@@ -594,8 +594,8 @@ package LexTable
        45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
 
        45,   45,   45,   45,   45,   45,   45,   45,   45
-   );
-  constant Integer yy_chk[:] = array(
+  });
+  constant array<Integer> yy_chk = listArray({
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    6,    8,   18,
@@ -608,7 +608,7 @@ package LexTable
        45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
 
        45,   45,   45,   45,   45,   45,   45,   45,   45
-   );
+   });
 
 end LexTable;
 

OMCompiler/Compiler/Lexers/LexerModelicaDiff.mo
@@ -20,14 +20,14 @@ constant Boolean debug = false;
 replaceable package LexTable
   constant Integer yy_limit;
   constant Integer yy_finish;
-  constant Integer yy_acclist[:];
-  constant Integer yy_accept[:];
-  constant Integer yy_ec[:];
-  constant Integer yy_meta[:];
-  constant Integer yy_base[:];
-  constant Integer yy_def[:];
-  constant Integer yy_nxt[:];
-  constant Integer yy_chk[:];
+  constant array<Integer> yy_acclist;
+  constant array<Integer> yy_accept;
+  constant array<Integer> yy_ec;
+  constant array<Integer> yy_meta;
+  constant array<Integer> yy_base;
+  constant array<Integer> yy_def;
+  constant array<Integer> yy_nxt;
+  constant array<Integer> yy_chk;
 end LexTable;
 
 function scan "Scan starts the lexical analysis, load the tables and consume the program to output the tokens"
@@ -989,16 +989,16 @@ algorithm
 end checkArray;
 
 function checkArrayModelica
-  input Integer arr[:];
+  input array<Integer> arr;
   input Integer index;
   input SourceInfo info;
 protected
   String filename;
   Integer lineStart;
 algorithm
-  if index<1 or index>size(arr,1) then
+  if index<1 or index>arrayLength(arr) then
     SOURCEINFO(fileName=filename, lineNumberStart=lineStart) := info;
-    print("\n[" + filename + ":" + String(lineStart) + "]: checkArray failed: arrayLength="+String(size(arr,1))+" index=" + String(index) + "\n");
+    print("\n[" + filename + ":" + String(lineStart) + "]: checkArray failed: arrayLength="+String(arrayLength(arr))+" index=" + String(index) + "\n");
     fail();
   end if;
 end checkArrayModelica;
@@ -1006,7 +1006,7 @@ end checkArrayModelica;
 package LexTable
   constant Integer yy_limit = 395;
   constant Integer yy_finish = 453;
-  constant Integer yy_acclist[:] = array(
+  constant array<Integer> yy_acclist = listArray({
       115,  114,    1,  114,    2,  114,  114,  101,  114,  114,
        64,  114,   65,  114,   85,  114,   87,  114,   72,  114,
        86,  114,   97,  114,   94,  114,  100,  114,   75,  114,
@@ -1060,8 +1060,8 @@ package LexTable
        98,   98,   95,   98,    8,   98,   98,   16,   98,   98,
        98,   25,   98,   98,   98,   98,   98,   22,   98,   98,
        55,   98,   98,   24,   98,   79,   98,   27,   98
-   );
-  constant Integer yy_accept[:] = array(
+   });
+  constant array<Integer> yy_accept = listArray({
         1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         3,    5,    7,    8,   10,   11,   13,   15,   17,   19,
        21,   23,   25,   27,   29,   31,   33,   35,   37,   39,
@@ -1106,8 +1106,8 @@ package LexTable
       471,  472,  474,  475,  476,  477,  478,  480,  481,  483,
       484,  486,  488,  490,  490
 
-   );
-  constant Integer yy_ec[:] = array(
+   });
+  constant array<Integer> yy_ec = listArray({
         1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    4,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -1136,16 +1136,16 @@ package LexTable
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1
-   );
-  constant Integer yy_meta[:] = array(
+   });
+  constant array<Integer> yy_meta = listArray({
         1,    1,    1,    1,    2,    2,    3,    3,    2,    2,
         2,    2,    2,    2,    2,    2,    4,    2,    2,    2,
         5,    2,    4,    4,    2,    5,    2,    2,    6,    6,
         4,    4,    4,    6,    4,    4,    4,    4,    4,    4,
         6,    4,    4,    4,    6,    4,    4,    4,    6,    4,
         4,    6,    4,    2,    2
-   );
-  constant Integer yy_base[:] = array(
+   });
+  constant array<Integer> yy_base = listArray({
         0,    0,   53,   54,  449,  448,   55,   56,  450,  453,
        64,  453,  446,  453,  422,  453,  453,  453,  453,  453,
       453,   56,   60,   62,   57,  453,   39,  426,  425,    0,
@@ -1190,8 +1190,8 @@ package LexTable
       150,    0,  148,  154,  156,  131,    0,   91,    0,   39,
         0,    0,    0,  453,  201,  207,  213,  218,  221,  225
 
-   );
-  constant Integer yy_def[:] = array(
+   });
+  constant array<Integer> yy_def = listArray({
       394,    1,  395,  395,  396,  396,  397,  397,  394,  394,
       394,  394,  394,  394,  398,  394,  394,  394,  394,  394,
       394,  394,  394,  394,  394,  394,  394,  394,  394,  399,
@@ -1236,8 +1236,8 @@ package LexTable
       399,  399,  399,  399,  399,  399,  399,  399,  399,  399,
       399,  399,  399,    0,  394,  394,  394,  394,  394,  394
 
-   );
-  constant Integer yy_nxt[:] = array(
+   });
+  constant array<Integer> yy_nxt = listArray({
        10,   11,   12,   13,   11,   10,   14,   15,   16,   17,
        18,   19,   20,   21,   22,   23,   24,   25,   26,   27,
        28,   29,   30,   30,   31,   10,   32,   33,   34,   35,
@@ -1294,8 +1294,8 @@ package LexTable
       394,  394,  394,  394,  394,  394,  394,  394,  394,  394,
 
       394,  394,  394,  394,  394,  394,  394,  394
-   );
-  constant Integer yy_chk[:] = array(
+   });
+  constant array<Integer> yy_chk = listArray({
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -1352,7 +1352,7 @@ package LexTable
       394,  394,  394,  394,  394,  394,  394,  394,  394,  394,
 
       394,  394,  394,  394,  394,  394,  394,  394
-   );
+   });
 
 end LexTable;
 

OMCompiler/Compiler/Util/Config.mo
@@ -448,9 +448,9 @@ protected function languageStandardInt
   input LanguageStandard inStandard;
   output Integer outValue;
 protected
-  constant Integer lookup[LanguageStandard] = array(10, 20, 30, 31, 32, 33, 34, 35, 36, 1000, 9999);
+  constant array<Integer> lookup = listArray({10, 20, 30, 31, 32, 33, 34, 35, 36, 1000, 9999});
 algorithm
-  outValue := lookup[inStandard];
+  outValue := arrayGet(lookup, Integer(inStandard));
 end languageStandardInt;
 
 protected function intLanguageStandard
@@ -476,9 +476,9 @@ public function languageStandardString
   input LanguageStandard inStandard;
   output String outString;
 protected
-  constant String lookup[LanguageStandard] = array("1.x","2.x","3.0","3.1","3.2","3.3","3.4","3.5","3.6","3.6","experimental" /*Change this to latest version if you add more versions!*/);
+  constant array<String> lookup = listArray({"1.x","2.x","3.0","3.1","3.2","3.3","3.4","3.5","3.6","3.6","experimental" /*Change this to latest version if you add more versions!*/});
 algorithm
-  outString := lookup[inStandard];
+  outString := arrayGet(lookup, Integer(inStandard));
 end languageStandardString;
 
 public function setLanguageStandardFromMSL

OMCompiler/Compiler/Util/StringUtil.mo
@@ -403,13 +403,13 @@ function convertCharNonAsciiToHex "Converts a single character string to a hex r
   input output String s;
 protected
   Integer i;
-  constant String hex[:] = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F");
+  constant array<String> hex = listArray({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"});
 algorithm
   i := stringCharInt(s);
   if i < 128 then
     return;
   end if;
-  s := "0x" + hex[intDiv(i, 16)+1] + hex[intMod(i, 16)+1];
+  s := "0x" + arrayGet(hex, intDiv(i, 16)+1) + arrayGet(hex, intMod(i, 16)+1);
 end convertCharNonAsciiToHex;
 
 function stripBOM

OMCompiler/Compiler/FrontEnd/InstStateMachineUtil.mo
@@ -79,7 +79,7 @@ end FlatSMGroup;
 public uniontype AdjacencyTable
   record ADJACENCY_TABLE
     HashTable.HashTable cref2index "Map cref to corresponding index in adjacency matrix";
-    Boolean adjacency[:,:] "Adjacency matrix showing which modes are connected by transitions";
+    array<array<Boolean>> adjacency "Adjacency matrix showing which modes are connected by transitions";
   end ADJACENCY_TABLE;
 end AdjacencyTable;
 
@@ -695,7 +695,7 @@ transitive closure associated with that initial state."
   output list<FlatSMGroup> flatSMGroup;
 protected
   HashTable.HashTable cref2index;
-  Boolean adjacency[nStates,nStates];
+  array<array<Boolean>> adjacency;
   list<tuple<DAE.ComponentRef, Integer>> entries;
   array<DAE.ComponentRef> i2cref;
   DAE.ComponentRef cref;
@@ -719,8 +719,8 @@ algorithm
     i := BaseHashTable.get(cref, cref2index);
     members := {};
     for j in 1:n loop
-      if adjacency[i,j] then
-        members := i2cref[j]::members;
+      if arrayGet(arrayGet(adjacency,i),j) then
+        members := arrayGet(i2cref,j)::members;
       end if;
     end for;
 
@@ -798,7 +798,7 @@ http://de.wikipedia.org/wiki/Warshall-Algorithmus
   output AdjacencyTable  transClosure;
 protected
   HashTable.HashTable cref2index;
-  Boolean adjacency[nStates,nStates];
+  array<array<Boolean>> adjacency;
   Integer n,k,i,j;
   Boolean c;
 algorithm
@@ -810,10 +810,10 @@ algorithm
   // Warshall's algorithm for computing the transitive closure
   for k in 1:n loop
     for i in 1:n loop
-      if adjacency[i,k] then
+      if arrayGet(arrayGet(adjacency,i),k) then
         for j in 1:n loop
-          if adjacency[k,j] then
-            adjacency[i,j] := true;
+          if arrayGet(arrayGet(adjacency,k),j) then
+            arrayUpdate(arrayGet(adjacency,i), j, true);
           end if;
         end for;
       end if;
@@ -831,7 +831,7 @@ Create adjacency table showing which modes are connected by transitions."
   output AdjacencyTable iTable;
 protected
   HashTable.HashTable cref2index "Map cref to corresponding index in adjacency matrix";
-  Boolean adjacency[nStates,nStates] "Adjacency matrix showing which states are connected by transitions";
+  array<array<Boolean>> adjacency "Adjacency matrix showing which states are connected by transitions";
   array<Boolean> iRow;
   Integer n,m,i,j,k;
   DAE.ComponentRef cref;
@@ -842,7 +842,7 @@ algorithm
   n := arrayLength(crefs1);
   cref2index := HashTable.emptyHashTableSized(n);
   assert(n == nStates, "Value of nStates needs to be equal to number of modes within mode table argument.");
-  adjacency := fill(false,n,n);
+  adjacency := listArray(list(arrayCreate(n, false) for i in 1:n));
 
   for i in 1:n loop
     cref2index := BaseHashTable.addNoUpdCheck((crefs1[i], i), cref2index);
@@ -855,7 +855,7 @@ algorithm
     for j in 1:m loop
       cref := crefs2[j];
       k := BaseHashTable.get(cref, cref2index);
-      adjacency[i,k] := true;
+      arrayUpdate(arrayGet(adjacency,i), k, true);
     end for;
   end for;
 
@@ -869,7 +869,7 @@ Print adjacency table."
   input Integer nStates "Number of states";
 protected
   HashTable.HashTable cref2index;
-  Boolean adjacency[nStates,nStates];
+  array<array<Boolean>> adjacency;
   list<tuple<DAE.ComponentRef, Integer>> entries;
   tuple<DAE.ComponentRef, Integer> entry;
   DAE.ComponentRef cref;
@@ -903,7 +903,7 @@ algorithm
   for i in 1:n loop
     str := Util.stringPadRight(intString(i), padn, pads);
     for j in 1:n loop
-      b := adjacency[i,j];
+      b := arrayGet(arrayGet(adjacency,i),j);
       str := str + Util.stringPadLeft(boolString(b)+",", padn, pads);
     end for;
     print(str + "\n");

commit 48a5f446a0570a1d21ade4e33f586e7f1f4facc4

Use listArrayLiteral for constants (#15503)
Fixes a regression introduced by #15488

OMCompiler/Compiler/Lexers/LexerJSON.mo
@@ -517,20 +517,20 @@ end checkArrayModelica;
 package LexTable
   constant Integer yy_limit = 46;
   constant Integer yy_finish = 82;
-  constant array<Integer> yy_acclist = listArray({
+  constant array<Integer> yy_acclist = MetaModelica.Dangerous.listArrayLiteral({
        17,   16,   15,   16,   16,   13,   16,    5,   16,   14,
        16,   11,   16,   12,   16,   16,   16,   16,    9,   16,
        10,   16,   15,    1,    5,    2,    3,    4,    8,    6,
         3,    7
    });
-  constant array<Integer> yy_accept = listArray({
+  constant array<Integer> yy_accept = MetaModelica.Dangerous.listArrayLiteral({
         1,    1,    1,    2,    3,    5,    6,    8,   10,   12,
        14,   16,   17,   18,   19,   21,   23,   24,   24,   25,
        25,   25,   26,   26,   26,   26,   26,   27,   27,   28,
        28,   29,   29,   29,   29,   29,   29,   29,   30,   31,
        31,   31,   32,   33,   33,   33
    });
-  constant array<Integer> yy_ec = listArray({
+  constant array<Integer> yy_ec = MetaModelica.Dangerous.listArrayLiteral({
         1,    1,    1,    1,    1,    1,    1,    1,    2,    2,
         1,    1,    2,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -560,12 +560,12 @@ package LexTable
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1
   });
-  constant array<Integer> yy_meta = listArray({
+  constant array<Integer> yy_meta = MetaModelica.Dangerous.listArrayLiteral({
         1,    1,    2,    1,    1,    1,    1,    2,    3,    1,
         3,    3,    1,    2,    1,    3,    4,    3,    4,    1,
         2,    2,    1,    2,    2,    1,    1
    });
-  constant array<Integer> yy_base = listArray({
+  constant array<Integer> yy_base = MetaModelica.Dangerous.listArrayLiteral({
         0,    0,   81,   82,   78,   25,   82,   22,   82,   82,
        82,   63,   53,   55,   82,   82,   74,   27,   82,   50,
        65,   26,   39,   53,   52,   37,   82,    0,   37,   45,
@@ -573,7 +573,7 @@ package LexTable
        27,   23,   82,    0,   82,   56,   59,   61,   63,   65,
        67
   });
-  constant array<Integer> yy_def = listArray({
+  constant array<Integer> yy_def = MetaModelica.Dangerous.listArrayLiteral({
        45,    1,   45,   45,   45,   46,   45,   45,   45,   45,
        45,   45,   45,   45,   45,   45,   45,   46,   45,   47,
        45,   45,   45,   45,   45,   45,   45,   48,   45,   45,
@@ -581,7 +581,7 @@ package LexTable
        45,   45,   45,   51,    0,   45,   45,   45,   45,   45,
        45
   });
-  constant array<Integer> yy_nxt = listArray({
+  constant array<Integer> yy_nxt = MetaModelica.Dangerous.listArrayLiteral({
         4,    5,    6,    4,    7,    4,    4,    4,    8,    9,
         4,    4,   10,    4,   11,    4,    4,    4,   12,    4,
        13,    4,    4,   14,    4,   15,   16,   19,   21,   27,
@@ -595,7 +595,7 @@ package LexTable
 
        45,   45,   45,   45,   45,   45,   45,   45,   45
   });
-  constant array<Integer> yy_chk = listArray({
+  constant array<Integer> yy_chk = MetaModelica.Dangerous.listArrayLiteral({
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    6,    8,   18,

OMCompiler/Compiler/Lexers/LexerModelicaDiff.mo
@@ -1006,7 +1006,7 @@ end checkArrayModelica;
 package LexTable
   constant Integer yy_limit = 395;
   constant Integer yy_finish = 453;
-  constant array<Integer> yy_acclist = listArray({
+  constant array<Integer> yy_acclist = MetaModelica.Dangerous.listArrayLiteral({
       115,  114,    1,  114,    2,  114,  114,  101,  114,  114,
        64,  114,   65,  114,   85,  114,   87,  114,   72,  114,
        86,  114,   97,  114,   94,  114,  100,  114,   75,  114,
@@ -1061,7 +1061,7 @@ package LexTable
        98,   25,   98,   98,   98,   98,   98,   22,   98,   98,
        55,   98,   98,   24,   98,   79,   98,   27,   98
    });
-  constant array<Integer> yy_accept = listArray({
+  constant array<Integer> yy_accept = MetaModelica.Dangerous.listArrayLiteral({
         1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         3,    5,    7,    8,   10,   11,   13,   15,   17,   19,
        21,   23,   25,   27,   29,   31,   33,   35,   37,   39,
@@ -1107,7 +1107,7 @@ package LexTable
       484,  486,  488,  490,  490
 
    });
-  constant array<Integer> yy_ec = listArray({
+  constant array<Integer> yy_ec = MetaModelica.Dangerous.listArrayLiteral({
         1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    4,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -1137,7 +1137,7 @@ package LexTable
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1
    });
-  constant array<Integer> yy_meta = listArray({
+  constant array<Integer> yy_meta = MetaModelica.Dangerous.listArrayLiteral({
         1,    1,    1,    1,    2,    2,    3,    3,    2,    2,
         2,    2,    2,    2,    2,    2,    4,    2,    2,    2,
         5,    2,    4,    4,    2,    5,    2,    2,    6,    6,
@@ -1145,7 +1145,7 @@ package LexTable
         6,    4,    4,    4,    6,    4,    4,    4,    6,    4,
         4,    6,    4,    2,    2
    });
-  constant array<Integer> yy_base = listArray({
+  constant array<Integer> yy_base = MetaModelica.Dangerous.listArrayLiteral({
         0,    0,   53,   54,  449,  448,   55,   56,  450,  453,
        64,  453,  446,  453,  422,  453,  453,  453,  453,  453,
       453,   56,   60,   62,   57,  453,   39,  426,  425,    0,
@@ -1191,7 +1191,7 @@ package LexTable
         0,    0,    0,  453,  201,  207,  213,  218,  221,  225
 
    });
-  constant array<Integer> yy_def = listArray({
+  constant array<Integer> yy_def = MetaModelica.Dangerous.listArrayLiteral({
       394,    1,  395,  395,  396,  396,  397,  397,  394,  394,
       394,  394,  394,  394,  398,  394,  394,  394,  394,  394,
       394,  394,  394,  394,  394,  394,  394,  394,  394,  399,
@@ -1237,7 +1237,7 @@ package LexTable
       399,  399,  399,    0,  394,  394,  394,  394,  394,  394
 
    });
-  constant array<Integer> yy_nxt = listArray({
+  constant array<Integer> yy_nxt = MetaModelica.Dangerous.listArrayLiteral({
        10,   11,   12,   13,   11,   10,   14,   15,   16,   17,
        18,   19,   20,   21,   22,   23,   24,   25,   26,   27,
        28,   29,   30,   30,   31,   10,   32,   33,   34,   35,
@@ -1295,7 +1295,7 @@ package LexTable
 
       394,  394,  394,  394,  394,  394,  394,  394
    });
-  constant array<Integer> yy_chk = listArray({
+  constant array<Integer> yy_chk = MetaModelica.Dangerous.listArrayLiteral({
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,

OMCompiler/Compiler/Util/Config.mo
@@ -448,7 +448,7 @@ protected function languageStandardInt
   input LanguageStandard inStandard;
   output Integer outValue;
 protected
-  constant array<Integer> lookup = listArray({10, 20, 30, 31, 32, 33, 34, 35, 36, 1000, 9999});
+  constant array<Integer> lookup = MetaModelica.Dangerous.listArrayLiteral({10, 20, 30, 31, 32, 33, 34, 35, 36, 1000, 9999});
 algorithm
   outValue := arrayGet(lookup, Integer(inStandard));
 end languageStandardInt;
@@ -476,7 +476,7 @@ public function languageStandardString
   input LanguageStandard inStandard;
   output String outString;
 protected
-  constant array<String> lookup = listArray({"1.x","2.x","3.0","3.1","3.2","3.3","3.4","3.5","3.6","3.6","experimental" /*Change this to latest version if you add more versions!*/});
+  constant array<String> lookup = MetaModelica.Dangerous.listArrayLiteral({"1.x","2.x","3.0","3.1","3.2","3.3","3.4","3.5","3.6","3.6","experimental" /*Change this to latest version if you add more versions!*/});
 algorithm
   outString := arrayGet(lookup, Integer(inStandard));
 end languageStandardString;

OMCompiler/Compiler/Util/StringUtil.mo
@@ -403,7 +403,7 @@ function convertCharNonAsciiToHex "Converts a single character string to a hex r
   input output String s;
 protected
   Integer i;
-  constant array<String> hex = listArray({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"});
+  constant array<String> hex = MetaModelica.Dangerous.listArrayLiteral({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"});
 algorithm
   i := stringCharInt(s);
   if i < 128 then