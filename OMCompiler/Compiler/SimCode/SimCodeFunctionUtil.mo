OpenModelica/OpenModelica

commit accdd9f7a56081b7a3d9b7879cfa0fb8f3aa1cfa

Ported the refactor-fix-notifications script to Python3 (#15627)

OMCompiler/Compiler/SimCode/SimCodeFunctionUtil.mo
@@ -2268,7 +2268,7 @@ algorithm
         Error.addCompilerNotification("X11 library is not needed under Windows. It is not linked from the external library resource directory.\n");
       then  ({},{});
 
-    case Absyn.STRING(str as "omcruntime")
+    case Absyn.STRING("omcruntime")
       algorithm
         if "Windows_NT" == Autoconf.os then
           // omcruntime on windows needs linking with mico2313 and wsock and then some :)

commit 95a5dd7773b2053f84f9df9c516b93a4381e5f26

Fix case in SimCodeFunctionUtil broken by #15627

OMCompiler/Compiler/SimCode/SimCodeFunctionUtil.mo
@@ -2268,7 +2268,7 @@ algorithm
         Error.addCompilerNotification("X11 library is not needed under Windows. It is not linked from the external library resource directory.\n");
       then  ({},{});
 
-    case Absyn.STRING("omcruntime")
+    case Absyn.STRING(str as "omcruntime")
       algorithm
         if "Windows_NT" == Autoconf.os then
           // omcruntime on windows needs linking with mico2313 and wsock and then some :)