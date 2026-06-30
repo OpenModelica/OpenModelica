66959208a248b2f6d85de09a2a7dcc64ee911086
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2026-06-30 12:43:53 +0200

    Fix common.groovy

--- .CI/common.groovy
+++ .CI/common.groovy
@@ -470,12 +470,11 @@ def getVersion() {
   }
 }
 
-void compliance() {
+void compliance(stash) {
   if (isWindows()) {
     // do nothing for now
   } else {
   standardSetup()
-  unstash 'omc-resolute-clang'
   makeLibsAndCache()
   sh 'HOME=$PWD/libraries/ build/bin/omc -g=MetaModelica build/share/doc/omc/testmodels/ComplianceSuite.mos'
   sh "mv ${env.COMPLIANCEPREFIX}.html ${env.COMPLIANCEPREFIX}-current.html"
