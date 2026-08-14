88e4765a68c24b19f4ccfd4dd882dc5cf9b6df1e
Adeel Asghar <adeel.asghar@liu.se>
2026-08-14 15:01:12 +0200

    Fix middle click closing two tabs in tabbed view (#16291)
    #16264
    Since Qt 6.11, QTabBar::mouseReleaseEvent() emits tabCloseRequested() on a middle mouse button release.
    The QMdiAreaTabBar already closes the tab under the cursor on the middle mouse button press,
    so a single middle click closed the clicked tab plus the one that shifted into the released position after the relayout.
    Install an event filter that swallows the middle mouse button release so only the clicked tab is closed.
    Added a TabbedView test to check the middle click behavior.

---
+++ OMEdit/Testsuite/TabbedView/TabbedViewTest.h
@@ -0,0 +1,62 @@
+/*
+ * This file is part of OpenModelica.
+ *
+ * Copyright (c) 1998-2026, Open Source Modelica Consortium (OSMC),
+ * c/o Linköpings universitet, Department of Computer and Information Science,
+ * SE-58183 Linköping, Sweden.
+ *
+ * All rights reserved.
+ *
+ * THIS PROGRAM IS PROVIDED UNDER THE TERMS OF AGPL VERSION 3 LICENSE OR
+ * THIS OSMC PUBLIC LICENSE (OSMC-PL) VERSION 1.8.
+ * ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES
+ * RECIPIENT'S ACCEPTANCE OF THE OSMC PUBLIC LICENSE OR THE GNU AGPL
+ * VERSION 3, ACCORDING TO RECIPIENTS CHOICE.
+ *
+ * The OpenModelica software and the OSMC (Open Source Modelica Consortium)
+ * Public License (OSMC-PL) are obtained from OSMC, either from the above
+ * address, from the URLs:
+ * http://www.openmodelica.org or
+ * https://github.com/OpenModelica/ or
+ * http://www.ida.liu.se/projects/OpenModelica,
+ * and in the OpenModelica distribution.
+ *
+ * GNU AGPL version 3 is obtained from:
+ * https://www.gnu.org/licenses/licenses.html#GPL
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE, EXCEPT AS EXPRESSLY SET FORTH
+ * IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE CONDITIONS OF OSMC-PL.
+ *
+ * See the full OSMC Public License conditions for more details.
+ *
+ */
+
+/*
+ * @author Adeel Asghar <adeel.asghar@liu.se>
+ */
+
+#ifndef TABBEDVIEWTEST_H
+#define TABBEDVIEWTEST_H
+
+#include <QObject>
+
+/*!
+ * \brief The TabbedViewTest class
+ * Tests the tabbed view of the ModelWidgetContainer.
+ */
+class TabbedViewTest: public QObject
+{
+  Q_OBJECT
+private slots:
+  void initTestCase();
+  /*!
+   * \brief middleClickClosesOnlyClickedTab
+   * Tests that a middle click on a tab closes only the clicked tab (issue #16264).
+   */
+  void middleClickClosesOnlyClickedTab();
+  void cleanupTestCase();
+};
+
+#endif // TABBEDVIEWTEST_H
