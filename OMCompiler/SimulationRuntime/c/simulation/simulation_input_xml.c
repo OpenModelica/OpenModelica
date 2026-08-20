81fa8ec3defc38e8a1e554e9460b32186296c424
Julien Schueller <schueller@phimeca.com>
2026-08-20 23:03:08 +0200

    free mi

--- OMCompiler/SimulationRuntime/c/simulation/simulation_input_xml.c
+++ OMCompiler/SimulationRuntime/c/simulation/simulation_input_xml.c
@@ -926,6 +926,7 @@ omc_ModelInput* parse_input_xml(const char *filename, const char* initXMLData, t
             XML_ErrorString(XML_GetErrorCode(parser)),
             XML_GetCurrentLineNumber(parser));
         XML_ParserFree(parser);
+        freeModelInput(mi);
         throwStreamPrint(threadData, "see last warning");
       }
     } while(!done);
@@ -939,6 +940,7 @@ omc_ModelInput* parse_input_xml(const char *filename, const char* initXMLData, t
               XML_ErrorString(XML_GetErrorCode(parser)),
               XML_GetCurrentLineNumber(parser));
       XML_ParserFree(parser);
+      freeModelInput(mi);
       throwStreamPrint(threadData, "see last warning");
     }
   }
